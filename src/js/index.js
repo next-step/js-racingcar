53ff36796376b9e86a8b777b0334f26dceb211c6
김형욱 <59253551+khw970421@users.noreply.github.com>
2021-04-16 13:45:16 +0900

    setInterval 및 애니메이션 결과 구현 (끝날때 돌아가는것도 삭제) 및 코드수정

--- src/js/index.js
+++ src/js/index.js
@@ -3,58 +3,55 @@ const $All = (selector) => document.querySelectorAll(selector)
 
 const [carButtonDom,tryButtonDom, restartButtonDom] = $All(".btn-cyan")
 const [carNameDom, tryNumberDom] = $All('.w-100');
-
 const progressTitle = $('.mt-4');
+
 let carNameArray,tryNumber,isGo,countArray={};
 let carMovingDom;
-
 
 const splitCarName = (val)=>val.split(',');
 
 const carButtonEvent = carButtonDom.addEventListener('click',()=>{
   carNameArray = splitCarName(carNameDom.value);
-  console.log(carNameArray)
 })
 
 const tryButtonEvent = tryButtonDom.addEventListener('click',()=>{
   tryNumber = tryNumberDom.value;
-  console.log(tryNumber)
   startRacing(tryNumber,carNameArray)
 })
 
-const startRacing = (tryNumber,array)=>{
+const startRacing = (tryNumber,carName)=>{
     let count=0;
-    carNameArray.map(val => {
-      progressTitle.innerHTML += setting(val);
-    })
+    carName.map(val => progressTitle.innerHTML += setting(val));
     carMovingDom = $All('.car-player');
-
-  let timer = setInterval(()=> {
-      isGo = array.map((val) => Math.floor(Math.random() * 10) >= 4 ? true : false);
-      makeSetting(carNameArray, isGo);
-      count++;
-      if (count === Number(tryNumber)) {
-        clearInterval(timer);
-        document.querySelectorAll('.relative').forEach(x=>x.remove())
-      }
-    },1000);
-
+    timerCheck(count,carName);
 }
 
-const makeSetting = (carNameArray,isGo)=>{
-  carNameArray.map((val,idx) => {
+const timerCheck = (count,carName)=>{
+  let timer = setInterval(()=> {
+    isGo = carName.map(() => Math.floor(Math.random() * 10) >= 4 ? true : false);
+    goCarMove(carNameArray, isGo);
+    count++;
+    if (count === Number(tryNumber)) {
+      clearInterval(timer);
+      $All('.relative').forEach(x=>x.remove())
+    }
+  },1000);
+}
 
+const goCarMove = (carNameArray,isGo)=>{
+  carNameArray.map((val,idx) => {
     if (countArray[val] !== undefined)
       countArray[val] = isGo[idx] ? ++countArray[val] : countArray[val];
     else
       countArray[val] = isGo[idx] ? 1 : 0;
-
-    if(isGo[idx]){
-      carMovingDom[idx].insertAdjacentHTML('afterend',moving());
-    }
+    addMovingDom(isGo,idx)
   })
 }
 
+const addMovingDom = (isGo,idx)=>{
+  if(isGo[idx])
+    carMovingDom[idx].insertAdjacentHTML('afterend',moving());
+}
 
 const setting = (carName)=>` <div class="mr-2">
             <div class="car-player">${carName}</div>
