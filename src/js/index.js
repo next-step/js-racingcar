b533f5b0b5b36896e2c3a90d006ecbada78fff64
김형욱 <59253551+khw970421@users.noreply.github.com>
2021-04-16 13:07:26 +0900

    setInterval 및 애니메이션 결과 구현

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,11 @@ const $All = (selector) => document.querySelectorAll(selector)
 
 const [carButtonDom,tryButtonDom, restartButtonDom] = $All(".btn-cyan")
 const [carNameDom, tryNumberDom] = $All('.w-100');
+
+const progressTitle = $('.mt-4');
 let carNameArray,tryNumber,isGo,countArray={};
+let carMovingDom;
+
 
 const splitCarName = (val)=>val.split(',');
 
@@ -19,18 +23,45 @@ const tryButtonEvent = tryButtonDom.addEventListener('click',()=>{
 })
 
 const startRacing = (tryNumber,array)=>{
-  for(let i=0;i<tryNumber;i++) {
-    isGo = array.map((val) => Math.floor(Math.random() * 10) >= 4 ? true : false);
-    makeSetting(carNameArray, isGo)
-  }
+    let count=0;
+    carNameArray.map(val => {
+      progressTitle.innerHTML += setting(val);
+    })
+    carMovingDom = $All('.car-player');
+
+  let timer = setInterval(()=> {
+      isGo = array.map((val) => Math.floor(Math.random() * 10) >= 4 ? true : false);
+      makeSetting(carNameArray, isGo);
+      count++;
+      if (count === Number(tryNumber)) {
+        clearInterval(timer);
+      }
+    },1000);
+
 }
 
 const makeSetting = (carNameArray,isGo)=>{
   carNameArray.map((val,idx) => {
 
-    if(countArray[val] !== undefined)
-      countArray[val] = isGo[idx] ?  ++countArray[val] : countArray[val];
+    if (countArray[val] !== undefined)
+      countArray[val] = isGo[idx] ? ++countArray[val] : countArray[val];
     else
-      countArray[val] = 0;
-    console.log(countArray,isGo[idx])})
+      countArray[val] = isGo[idx] ? 1 : 0;
+
+    if(isGo[idx]){
+      carMovingDom[idx].insertAdjacentHTML('afterend',moving());
+    }
+  })
 }
+
+
+const setting = (carName)=>` <div class="mr-2">
+            <div class="car-player">${carName}</div>
+            <div class="d-flex justify-center mt-3">
+              <div class="relative spinner-container">
+                <span class="material spinner"></span>
+              </div>
+            </div>
+          </div>`
+
+const moving = ()=>`<div class="forward-icon mt-2">⬇️️</div>`
