65d97f1d756ae964c53357e1701f225814ea0ed3
lsw1164 <qkqkfo@gmail.com>
2021-04-18 20:14:30 +0900

    Bind button event

--- src/components/GameForm.ts
+++ src/components/GameForm.ts
@@ -5,6 +5,21 @@ export default class GameForm extends Component {
     super($target, props);
   }
 
+  onClickCarBtn() {}
+
+  onClickTryBtn() {}
+
+  componentInit() {
+    this.$target.addEventListener("click", (e) => {
+      const $eventTarget = e.target as HTMLElement;
+      if ($eventTarget.matches(".btn-car")) {
+        console.log($eventTarget);
+      } else if ($eventTarget.matches(".btn-try")) {
+        console.log($eventTarget);
+      }
+    });
+  }
+
   getInnerHTML() {
     return `
         <fieldset>
@@ -14,15 +29,15 @@ export default class GameForm extends Component {
             예시) EAST, WEST, SOUTH, NORTH
           </p>
           <div class="d-flex">
-            <input type="text" class="w-100 mr-2" placeholder="자동차 이름" />
-            <button type="button" class="btn btn-cyan">확인</button>
+            <input type="text" class="w-100 mr-2 input-car" placeholder="자동차 이름" />
+            <button type="button" class="btn btn-cyan btn-car">확인</button>
           </div>
         </fieldset>
         <fieldset>
           <p>시도할 횟수를 입력해주세요.</p>
           <div class="d-flex">
-            <input type="number" class="w-100 mr-2" placeholder="시도 횟수" />
-            <button type="button" class="btn btn-cyan">확인</button>
+            <input type="number" class="w-100 mr-2 input-try" placeholder="시도 횟수" />
+            <button type="button" class="btn btn-cyan btn-try">확인</button>
           </div>
         </fieldset>
     `;
