d44715a118f57ca1f26b0c2038005935550294f8
YimJiYoung <chiy323@gmail.com>
2021-04-18 20:44:52 +0900

    test: alert 부분에 대한 테스트 코드 수정
    * 기존에 alert가 안떠도 통과되던 부분 수정
    
    참고 : https://stackoverflow.com/questions/51795306/how-can-we-test-the-alert-and-text-inside-is-displaying-using-cypress-io-js-auto

--- cypress/integration/js-racingcar.spec.js
+++ cypress/integration/js-racingcar.spec.js
@@ -5,11 +5,14 @@ describe('js-racing.spec.js', () => {
   });
 
   describe('자동차 이름 입력', () => {
-    it('5자 이상의 이름 입력 시 경고창이 뜬다.', () => {
-      cy.inputCarName('abcde');
-      cy.on('window:alert', (message) => {
-        expect(message).to.contains('유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다');
-      });
+    it('6자 이상의 이름 입력 시 경고창이 뜬다.', () => {
+      let alerted;
+      cy.on('window:alert', (message) => { alerted = message; });
+
+      cy.inputCarName('abcdef')
+        .then(() => {
+          expect(alerted).to.contains('유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다');
+        });
     });
 
     it('쉼표(,)를 기준으로 구분된 이름은 각 자동차의 이름으로 표시된다.', () => {
@@ -27,22 +30,24 @@ describe('js-racing.spec.js', () => {
 
   describe('이동 횟수 입력', () => {
     it('0 이하의 수 입력 시 경고창이 뜬다.', () => {
-      cy.inputCarName('a,b,c,d');
-      cy.inputRaceTimes(0);
+      let alerted;
+      cy.on('window:alert', (message) => { alerted = message; });
 
-      cy.on('window:alert', (message) => {
-        expect(message).to.contains('입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다.');
-      });
+      cy.inputCarName('a,b,c,d');
+      cy.inputRaceTimes(0)
+        .then(() => expect(alerted).to.contains('입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다.'));
     });
 
     it('이동 횟수s 만큼의 시간이 지난 후 자동차는 최대 이동 횟수만큼 이동한다.', () => {
-      cy.inputCarName('a,b,c,d');
+      const names = ['a', 'b', 'c', 'd'];
+      cy.inputCarName(names.join(','));
       cy.inputRaceTimes(4);
 
       cy.tick(4000);
 
-      ['a', 'b', 'c', 'd'].forEach((name) => {
+      names.forEach((name) => {
         cy.contains('.car-player', name).siblings('.race-field')
+        // TODO: find 시 forwaord-icon이 없는 경우 test fail하지 않도록 수정
           .find('.forward-icon')
           .its('length')
           .should('lte', 4);
@@ -50,13 +55,21 @@ describe('js-racing.spec.js', () => {
     });
   });
 
-  //   describe('우승자 표시', () => {
-  //     it('자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다 (우승자 한 명 이상)', () => {
+  describe('우승자 표시', () => {
+    it('자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다 (우승자 한 명 이상)', () => {
+      // TODO: 우승자가 맞는지 확인하는 테스트 작성
+    });
 
-  //     });
+    it('2초 후에 축하의 alert 메세지를 띄운다.', () => {
+      let alerted;
+      cy.on('window:alert', (message) => { alerted = message; });
 
-  //     it('2초 후에 축하의 alert 메세지를 띄운다.', () => {
+      const names = ['a', 'b', 'c', 'd'];
+      cy.inputCarName(names.join(','));
+      cy.inputRaceTimes(4);
 
-//     });
-//   });
+      cy.tick(6000)
+        .then(() => expect(alerted).to.contains('축하합니다!'));
+    });
+  });
 });
