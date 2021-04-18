14929a85d85322d4a9e889ac5c4914b240087c88
sgdg <seungdeng17@gmail.com>
2021-04-18 22:34:38 +0900

    test: 테스트 코드 추가

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -5,7 +5,49 @@ describe("racingcar", () => {
         cy.visit("/");
     });
 
-    it("", () => {
+    const inputCarNames = (names) => {
+        cy.get('#input-car-name').type(names);
+        cy.get('#submit-car-name').click();
+    };
 
+    const inputGoalCount = (count) => {
+        cy.get('#input-race-times').type(count);
+        cy.get('#submit-race-times').click();
+    }
+
+    it("자동차 이름 입력", () => {
+        const names = "A, BB, CCC, DDDD, EEEEE";
+        inputCarNames(names);
+    });
+
+    it("자동차 이름 입력 오류", () => {
+        const names = "A, BB, CCC, DDDD, EEEEE, FFFFFF";
+        inputCarNames(names);
+        cy.on('window:alert', txt => expect(txt).to.contains(MESSAGES.INVALID_CAR_NAME));
+    });
+
+
+    it("시도 횟수 입력", () => {
+        const names = "A, BB, CCC, DDDD, EEEEE";
+        const count = "3";
+        inputCarNames(names);
+        inputGoalCount(count);
+    });
+
+    it("시도 횟수 입력 오류", () => {
+        const names = "A, BB, CCC, DDDD, EEEEE";
+        const count = "0";
+        inputCarNames(names);
+        inputGoalCount(count);
+        cy.on('window:alert', txt => expect(txt).to.contains(MESSAGES.INVALID_GOAL_COUNT));
+    });
+
+    it("게임 오버", () => {
+        const names = "A, BB, CCC, DDDD, EEEEE";
+        const count = "3";
+        inputCarNames(names);
+        inputGoalCount(count);
+        cy.wait(+count * 1000);
+        cy.on('window:alert', txt => expect(txt).to.contains(MESSAGES.CONGRATS));
     });
 });
