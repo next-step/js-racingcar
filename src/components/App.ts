37285bace835d1138b6d1b808ffa43995fbc4d83
lsw1164 <qkqkfo@gmail.com>
2021-04-18 16:22:37 +0900

    Init Components

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,11 +1,36 @@
 import Component from "../core/Component";
+import GameForm from "./GameForm";
+import GamePlay from "./GamePlay";
+import GameResult from "./GameResult";
 
 export default class App extends Component {
+  private gameFormComp?: GameForm;
+  private gamePlayComp?: GamePlay;
+  private gameResultComp?: GameResult;
+
   constructor($target: Element | null) {
     super($target);
   }
 
+  componentDidMount() {
+    this.gameFormComp = new GameForm(document.querySelector(".game-form"));
+    this.gamePlayComp = new GamePlay(document.querySelector(".game-play"));
+    this.gameResultComp = new GameResult(
+      document.querySelector(".game-result")
+    );
+  }
+
   getInnerHTML() {
-    return "Hello world";
+    return `
+      <section class="d-flex justify-center mt-5">
+        <form class="game-form"></form>
+      </section>
+      <section class="d-flex justify-center mt-5">
+        <div class="mt-4 d-flex game-play"></div>
+      </section>
+      <section class="d-flex justify-center mt-5">
+        <div class="game-result"></div>
+      </section>
+    `;
   }
 }
---
+++ src/components/GameResult.ts
@@ -0,0 +1,7 @@
+import Component from "../core/Component";
+
+export default class GameResult extends Component {
+  constructor($target: Element | null, props?: Object) {
+    super($target, props);
+  }
+}
