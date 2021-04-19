60c5f2f393e3795ec21b5d7522eb2d9ebee43504
hwookim <gusdn9754@gmail.com>
2021-04-20 01:53:14 +0900

    test: 연속적인 테스트 and로 묶어서 관리

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -8,13 +8,15 @@ describe("racing car game", () => {
   context("자동차 이름 입력 시", () => {
     it(",를 기준으로 하단에 이름이 반영된다.", () => {
       const carNames = "A, B, C, D";
+
       cy.get(".car-name-input").type(carNames);
       cy.get(".car-name-submit").click();
 
-      cy.get(".car-list").should("contain.text", "A");
-      cy.get(".car-list").should("contain.text", "B");
-      cy.get(".car-list").should("contain.text", "C");
-      cy.get(".car-list").should("contain.text", "D");
+      cy.get(".car-list")
+        .should("contain.text", "A")
+        .and("contain.text", "B")
+        .and("contain.text", "C")
+        .and("contain.text", "D");
     });
 
     it("하나 이상의 이름이 1자 미만이면 경고창이 뜬다.", () => {
@@ -26,9 +28,10 @@ describe("racing car game", () => {
       cy.on("window:alert", (error) =>
         expect(error).to.contains(ERROR_MESSAGE.NAME_LENGTH)
       );
-      cy.get(".car-list").should("not.contain.text", "A");
-      cy.get(".car-list").should("not.contain.text", "C");
-      cy.get(".car-list").should("not.contain.text", "D");
+      cy.get(".car-list")
+        .should("not.contain.text", "A")
+        .and("not.contain.text", "C")
+        .and("not.contain.text", "D");
     });
 
     it("하나 이상의 이름이 5자를 초과하면 경고창이 뜬다.", () => {
@@ -40,10 +43,11 @@ describe("racing car game", () => {
       cy.on("window:alert", (error) =>
         expect(error).to.contains(ERROR_MESSAGE.NAME_LENGTH)
       );
-      cy.get(".car-list").should("not.contain.text", "ABCDEF");
-      cy.get(".car-list").should("not.contain.text", "B");
-      cy.get(".car-list").should("not.contain.text", "C");
-      cy.get(".car-list").should("not.contain.text", "D");
+      cy.get(".car-list")
+        .should("not.contain.text", "ABCDEF")
+        .and("not.contain.text", "B")
+        .and("not.contain.text", "C")
+        .and("not.contain.text", "D");
     });
   });
 });
