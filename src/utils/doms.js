2f182fa80aa4f964ab3c998ec1a11349b0fa8bf2
Yeewon <yes3427@kookmin.ac.kr>
2021-04-20 16:35:49 +0900

    feat: dom 분리

---
+++ src/utils/doms.js
@@ -0,0 +1,6 @@
+export const $inputCarName = document.getElementById('input-car-name');
+export const $inputNumber = document.getElementById('input-number');
+export const $track = document.getElementById('main');
+export const $number = document.getElementById('try-number');
+export const $start = document.getElementById('start');
+export const $result = document.getElementById('result');
