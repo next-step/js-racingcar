fd2bfa8e899a7fb440d9fc6428ba0ba4ec9367d4
Yeewon <yes3427@kookmin.ac.kr>
2021-04-21 15:51:27 +0900

    feat: 코드 수정

--- src/js/racing.js
+++ src/js/racing.js
@@ -8,45 +8,36 @@ export const initRacing = () => {
 };
 
 const onRacing = () => {
-    if (CAR_NAME_LIST.length == 0) {
-        alert('자동차 이름을 입력해주세요.');
-        return;
-    }
-    if (TRY_NUMBER == 0) {
-        alert('시도 횟수를 입력해주세요.');
-        return;
-    }
+    if (CAR_NAME_LIST.length == 0) return alert('자동차 이름을 입력해주세요.');
+
+    if (TRY_NUMBER == 0) return alert('시도 횟수를 입력해주세요.');
+
     if ($start.classList.contains('checked')) {
         $track.innerHTML = '';
         document.querySelector('.winner').innerHTML = '';
-        CAR_NAME_LIST.map((carName) => {
-            $track.insertAdjacentHTML('beforeend', carTemplate(carName));
-        });
+        CAR_NAME_LIST.map((carName) => $track.insertAdjacentHTML('beforeend', carTemplate(carName)));
         $start.classList.remove('checked');
     }
     const distance = new Array(CAR_NAME_LIST.length).fill(0);
+
     let cnt = 0;
-
     const race = setInterval(() => {
-        let i = 0;
-        CAR_NAME_LIST.map((carName) => {
+        CAR_NAME_LIST.forEach((carName, index) => {
             const randNum = Math.floor(Math.random() * 9);
-
             if (randNum >= 4) {
-                distance[i] += 1;
+                distance[index] += 1;
                 document.getElementById(carName).insertAdjacentHTML('beforeend', moveTemplate(randNum));
             }
-            i += 1;
         });
         cnt += 1;
-        if (cnt == TRY_NUMBER) {
-            clearInterval(race);
-        }
+
+        if (cnt == TRY_NUMBER) clearInterval(race);
     }, 1000);
 
     setTimeout(() => {
         const winnerList = [];
         const maxDist = Math.max.apply(null, distance);
+
         distance.forEach((dist, i) => {
             if (dist == maxDist) winnerList.push(CAR_NAME_LIST[i]);
         });
@@ -55,8 +46,6 @@ const onRacing = () => {
         $start.innerText = '경주 다시 시작하기';
         $start.className += ' checked';
 
-        setTimeout(() => {
-            alert(winnerList.join(',') + '님 우승을 축하합니다.');
-        }, 1000 * 2);
+        setTimeout(() => alert(winnerList.join(',') + '님 우승을 축하합니다.'), 1000 * 2);
     }, 1000 * TRY_NUMBER);
 };
--- src/js/register/registerNumber.js
+++ src/js/register/registerNumber.js
@@ -5,12 +5,9 @@ export let TRY_NUMBER = 0;
 
 export const onRegisterNumber = () => {
     TRY_NUMBER = $inputNumber.value;
-    if (TRY_NUMBER < 1) {
-        alert('시도 횟수는 1 이상만 가능합니다.');
-        return;
-    }
-    TRY_NUMBER = $inputNumber.value;
+    if (TRY_NUMBER < 1) return alert('시도 횟수는 1 이상만 가능합니다.');
+
+    $inputNumber.value = '';
     $number.innerText = '';
     $number.insertAdjacentHTML('afterbegin', tryNumberTemplate(TRY_NUMBER));
-    $inputNumber.value = '';
 };
