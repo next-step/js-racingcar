a99b4db04f0045d3ae61494bb1b1a33f056954e5
YimJiYoung <chiy323@gmail.com>
2021-04-24 11:28:39 +0900

    refactor: Input 이벤트 핸들러 함수 분리, disabled 속성, input 초기화 설정

--- src/js/Input.js
+++ src/js/Input.js
@@ -6,45 +6,57 @@ export class Input {
     this.raceTimes = 0;
     this.onSubmit = onSubmit;
 
+    this.carNameInput = $('#input-car-name');
+    this.carNameSubmitBtn = $('#submit-car-name');
+
+    this.raceTimesInput = $('#input-race-times');
+    this.raceTimesSubmitBtn = $('#submit-race-times');
+
+    this.raceTimesSection = $('#section-race-times');
+
     this.initEventListener();
   }
 
   initEventListener() {
-    const carNameInput = $('#input-car-name');
-    const carNameSubmitBtn = $('#submit-car-name');
+    this.carNameSubmitBtn.addEventListener('click', this.handleCarNameSubmit.bind(this));
+    this.raceTimesSubmitBtn.addEventListener('click', this.handleRaceTimesSubmit.bind(this));
+  }
 
-    const raceTimesSection = $('#section-race-times');
-    const raceTimesInput = $('#input-race-times');
-    const raceTimesSubmitBtn = $('#submit-race-times');
+  handleCarNameSubmit() {
+    this.cars = this.carNameInput.value.split(',').map((name) => name.trim());
+    const invalidNames = this.cars.filter((name) => !Input.isValidName(name));
 
-    carNameSubmitBtn.addEventListener('click', () => {
-      this.cars = carNameInput.value.split(',');
-      const invalidNames = this.cars.filter((name) => !Input.isValidName(name));
+    if (invalidNames.length > 0) {
+      return alert('유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다');
+    }
 
-      if (invalidNames.length > 0) {
-        return alert('유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다');
-      }
+    this.raceTimesSection.classList.remove('hidden');
+    this.carNameSubmitBtn.disabled = true;
+  }
 
-      raceTimesSection.classList.remove('hidden');
-    });
+  handleRaceTimesSubmit() {
+    const raceTimes = Number(this.raceTimesInput.value);
 
-    raceTimesSubmitBtn.addEventListener('click', () => {
-      const raceTimes = Number(raceTimesInput.value);
+    if (!Input.isValidTimes(raceTimes)) {
+      return alert('입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다.');
+    }
 
-      if (!Input.isValidTimes(raceTimes)) {
-        return alert('입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다.');
-      }
-
-      this.raceTimes = raceTimes;
-      this.onSubmit?.({ cars: this.cars, raceTimes: this.raceTimes });
-    });
+    this.raceTimes = raceTimes;
+    this.onSubmit?.({ cars: this.cars, raceTimes: this.raceTimes });
+    this.raceTimesSubmitBtn.disabled = true;
   }
 
   reset() {
     this.cars = [];
     this.raceTimes = 0;
-    const raceTimesSection = $('#section-race-times');
-    raceTimesSection.classList.add('hidden');
+
+    this.carNameSubmitBtn.disabled = false;
+    this.raceTimesSubmitBtn.disabled = false;
+
+    this.carNameInput.value = '';
+    this.raceTimesInput.value = '';
+
+    this.raceTimesSection.classList.add('hidden');
   }
 
   static isValidName(name) {
