2da10cef57247972ffd115e7af4ec9dc46ec55fd
inhyuck <dlsgur1447@gmail.com>
2021-06-06 00:32:12 +0900

    test: 자동차 이름부여 테스트

---
+++ cypress/integration/racingcar/namingCar.spec.js
@@ -0,0 +1,28 @@
+describe('자동차에 이름을 부여할 수 있다. 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8080');
+    });
+
+    it('자동차에 이름을 부여할 수 있다.', () => {
+        //given
+        const names = 'EAST, WEST, SOUTH, NORTH';
+        const splitNames = names.split(', ');
+
+        //when
+        cy.getBySel('names-input')
+          .type(names);
+        cy.getBySel('names-input-button')
+          .click();
+
+        //then
+        cy.getBySel('cars')
+          .each(($el, index) => {
+              cy.wrap($el)
+                .contains(splitNames[index]);
+          });
+    });
+
+    it('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
+
+    });
+});
