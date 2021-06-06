250f8746384ba98298f81c4e0d88e1495fc4db99
inhyuck <dlsgur1447@gmail.com>
2021-06-06 11:44:53 +0900

    test: 자동차 이름부여 테스트
    data-test selector 추가

--- cypress/integration/racingcar/namingCar.spec.js
+++ cypress/integration/racingcar/namingCar.spec.js
@@ -13,16 +13,20 @@ describe('자동차에 이름을 부여할 수 있다. 전진하는 자동차를
           .type(names);
         cy.getBySel('names-input-button')
           .click();
+        cy.getBySel('count-input')
+          .type(3);
+        cy.getBySel('count-input-button')
+          .click();
 
         //then
-        cy.getBySel('cars')
+        cy.getBySel('car')
           .each(($el, index) => {
               cy.wrap($el)
                 .contains(splitNames[index]);
           });
     });
 
-    it('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
-
-    });
+    // it('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
+    //
+    // });
 });
