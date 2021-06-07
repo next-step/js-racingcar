faa8bfca76c64b1493572018bd0091d05b4b69a0
sanam <simian114@gmail.com>
2021-06-07 23:04:41 +0900

    refactor: describe - context - it 의 순서로 테스트 진행

--- cypress/integration/input.spec.js
+++ cypress/integration/input.spec.js
@@ -1,56 +1,58 @@
-const testNameSection = () => {
-  cy.typeToTarget('@nameInput', 'haha, good');
-  cy.clickTarget('@nameBtn');
-  cy.get('@nameInput').should('be.disabled');
-  cy.get('@nameBtn').should('be.disabled');
+// NOTE: alias를 만들어두면 이후에 @를 접두사로해서 접근할 수 있게 된다.
+const setAlias = () => {
+  cy.getBySel('car-names-input').as('nameInput');
+  cy.getBySel('car-names-btn').as('nameBtn');
+  cy.getBySel('try-number-input').as('tryNumInput');
+  cy.getBySel('try-number-btn').as('tryNumBtn');
 };
 
-describe('Racing Test!', () => {
+describe('레이싱 시작 전 레이싱카 이름과 시도 횟수에 대한 테스트', () => {
   beforeEach(() => {
-    // NOTE: baseUrl을 설정해놓음
     cy.visit('/');
-    // NOTE: alias를 만들어두면 이후에 @를 접두사로해서 접근할 수 있게 된다.
-    cy.getBySel('car-names-input').as('nameInput');
-    cy.getBySel('car-names-btn').as('nameBtn');
-    cy.getBySel('try-number-input').as('tryNumInput');
-    cy.getBySel('try-number-btn').as('tryNumBtn');
+    setAlias();
   });
 
-  it('인풋, 버튼 disabled 테스트', () => {
-    // NOTE: 이렇게 함수로 빼는게 좋을까? 아니면 그냥 아래처럼 이 칸 안에 나열하는게 좋을까
-    // NOTE: names 섹션 담당
-    testNameSection();
+  context('names 인풋, 버튼 테스트', () => {
+    it('올바른 이름을 넣으면 name 관련 인풋과 버튼은 disabled가 되어야합니다.', () => {
+      cy.typeToTarget('@nameInput', 'haha, good').clickTarget('@nameBtn');
+      cy.get('@nameInput').should('be.disabled');
+      cy.get('@nameBtn').should('be.disabled');
+    });
 
-    // NOTE: 아래는 try num 섹션
-    cy.typeToTarget('@tryNumInput', 3).clickTarget('@tryNumBtn');
-    cy.get('@tryNumInput').should('be.disabled');
-    cy.get('@tryNumBtn').should('be.disabled');
+    it('자동차 이름 5글자 이상일 시 alert 발생', () => {
+      const veryLongName = 'sssdsdsczsdasdasdasdasd';
+      const stub = cy.stub();
+      const msg = Cypress.env('ALERT')['CAR_NAME'];
+      cy.on('window:alert', stub);
+      cy.typeToTarget('@nameInput', veryLongName)
+        .clickTarget('@nameBtn')
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(msg);
+        });
+    });
   });
 
-  it('자동차 이름 5글자 이상일 시 alert 발생', () => {
-    const veryLongName = 'sssdsdsczsdasdasdasdasd';
-    // NOTE: stub사용은 공식문서에서 아래처럼 사용하고 있다.
-    // TODO: cy.on 과  stub 동작에 대한 이해를 확실히 하도록하자.
-    const stub = cy.stub();
-    const msg = Cypress.env('ALERT')['CAR_NAME'];
-    cy.on('window:alert', stub);
-    cy.typeToTarget('@nameInput', veryLongName)
-      .clickTarget('@nameBtn')
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith(msg);
-      });
-  });
+  context('시도횟수 인풋, 버튼 테스트', () => {
+    beforeEach(() => {
+      cy.typeToTarget('@nameInput', 'haha, good').clickTarget('@nameBtn');
+    });
 
-  it('try num 값이 0 이하일 대 alert 발생', () => {
-    const tryNum = 0;
-    const stub = cy.stub();
-    const msg = Cypress.env('ALERT')['ROUNDS'];
-    cy.on('window:alert', stub);
-    cy.typeToTarget('@nameInput', 'abcd').clickTarget('@nameBtn');
-    cy.typeToTarget('@tryNumInput', tryNum)
-      .clickTarget('@tryNumBtn')
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith(msg);
-      });
+    it('올바른 시도횟수를 넣으면 관련 인풋과 버튼은 disabled과 되어야합니다.', () => {
+      cy.typeToTarget('@tryNumInput', 3).clickTarget('@tryNumBtn');
+      cy.get('@tryNumInput').should('be.disabled');
+      cy.get('@tryNumBtn').should('be.disabled');
+    });
+
+    it('시도 횟수 값이 0 이하일 때 alert 발생', () => {
+      const tryNum = 0;
+      const stub = cy.stub();
+      const msg = Cypress.env('ALERT')['ROUNDS'];
+      cy.on('window:alert', stub);
+      cy.typeToTarget('@tryNumInput', tryNum)
+        .clickTarget('@tryNumBtn')
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(msg);
+        });
+    });
   });
 });
--- cypress/integration/racing.spec.js
+++ cypress/integration/racing.spec.js
@@ -1,50 +1,95 @@
+import Racing from '../../src/js/components/Racing';
 const NAMES = ['EAST', 'WEST', 'SOUTH', 'NORTH'];
 const ROUND = 3;
 
-const startGame = () => {
+const startRacing = () => {
   cy.getBySel('car-names-input').as('nameInput');
   cy.getBySel('car-names-btn').as('nameBtn');
   cy.getBySel('try-number-input').as('tryNumInput');
   cy.getBySel('try-number-btn').as('tryNumBtn');
-  // TODO: fixture로 car의 이름을 가져오도록 만들자
   cy.typeToTarget('@nameInput', NAMES.join(',')).clickTarget('@nameBtn');
   cy.typeToTarget('@tryNumInput', ROUND).clickTarget('@tryNumBtn');
-}
+};
 
-describe('Racing TEST!!', () => {
-  beforeEach(function () {
-    // NOTE: baseUrl을 설정해놓음
+describe('레이싱 진행 테스트', () => {
+  beforeEach(() => {
     cy.visit('/');
-    startGame();
   });
 
-  it('자동차에 이름을 부여할 수 있다.', () => {
-    NAMES.forEach((carName, i) => {
-      cy.get('.racing-track').find(`[data-car="${i}"]`).contains(carName);
+  context('Racing 컴포넌트를 인위적으로 만들고 테스트합니다.', () => {
+    beforeEach(() => {
+      cy.visit('/');
+      cy.document().then((doc) => {
+        const racing = new Racing(doc.querySelector('#app'));
+        const gameProcess = racing.getGameProcess();
+        gameProcess.setTrack(doc.querySelector('#racing-track'));
+        const resultPane = racing.getResultPane();
+        resultPane.setPane(doc.querySelector('.result-pane'));
+        gameProcess.ready(NAMES);
+        const racingCars = gameProcess.getRacingCars();
+        cy.wrap(racing).as('racing');
+        cy.wrap(gameProcess).as('gameProcess');
+        cy.wrap(racingCars).as('racingCars');
+        cy.wrap(racingCars.cars[0]).as(NAMES[0]);
+        cy.wrap(racingCars.cars[1]).as(NAMES[1]);
+        cy.wrap(racingCars.cars[2]).as(NAMES[2]);
+        cy.wrap(racingCars.cars[3]).as(NAMES[3]);
+      });
+    });
+
+    it('랜덤값이 4 이상이면 전진한다.', () => {
+      cy.get('@EAST').invoke('goRound', 4);
+      cy.get('.racing-track').contains('EAST').parent().contains('⬇️️');
+    });
+
+    it('랜덤값이 3 이하면 정지한다.', () => {
+      cy.get('@WEST').invoke('goRound', 3);
+      cy.get('.racing-track').contains('WEST').parent().find('.spinner');
+    });
+
+    it('게임이 끝나면 우승자가 보여야한다. 만약 우승자가 여러명일 경우 , 를 이용해서 구분한다.', () => {
+      cy.get('@racing')
+        .invoke('getResultPane')
+        .then((resultPane) => {
+          cy.get('@gameProcess').invoke('start', ROUND, resultPane.showWinner);
+          cy.wait(1000 * ROUND + 2000);
+          cy.get('@racingCars')
+            .invoke('getWinner')
+            .then((winners) => {
+              cy.get('.result-pane').contains(
+                `🏆 최종 우승자: ${winners.join(', ')}🏆`
+              );
+            });
+        });
     });
   });
-  
-  it('정상적으로 게임의 턴이 다 동작된 후에는 결과를 보여주고 2초 후에 축하의 alert 메세지를 띄운다.', () => {
-      cy.window().then((window) => cy.stub(window, "alert").as("alert"));
-      cy.get("@alert").should("not.be.called");
+
+  context('게임이 시작되었습니다.', () => {
+    beforeEach(() => {
+      startRacing();
+    });
+
+    it('자동차에 이름을 부여할 수 있다.', () => {
+      NAMES.forEach((carName, i) => {
+        cy.get('.racing-track').find(`[data-car="${i}"]`).contains(carName);
+      });
+    });
+
+    it('정상적으로 게임의 턴이 다 동작된 후에는 결과를 보여주고 2초 후에 축하의 alert 메세지를 띄운다.', () => {
+      cy.window().then((window) => cy.stub(window, 'alert').as('alert'));
+      cy.get('@alert').should('not.be.called');
       cy.wait(1000 * ROUND + 2000);
-      cy.get("@alert").should("be.calledWith", '축하합니다');
+      cy.get('@alert').should('be.calledWith', '축하합니다');
     });
 
-  it('다시시작하기 버튼을 누르면 게임이 다시 시작된다.', () => {
-    // NOTE: beforeEach로 게임은 이미 시작됨.
-    cy.wait(1000 * ROUND + 2000)
-    cy.get('.result-pane__btn')
-      .click();
-    startGame();
-    cy.get('.result-pane')
-      .should('have.not.class', 'hidden')
+    it('다시시작하기 버튼을 누르면 게임이 다시 시작된다.', () => {
+      // NOTE: 이미 시작된 게임이 끝나기를 기다림
+      cy.wait(1000 * ROUND + 2000);
+      cy.clickTarget('.result-pane__btn');
+      startRacing();
+      cy.get('.result-pane').should('have.not.class', 'hidden');
+    });
+    // TODO: 구현 못함
+    it('자동차 경주 게임의 턴이 진행 될 때마다 1초의 텀(progressive)를 두고 진행한다.', () => {});
   });
-  
-    // TODO: 심화 선택 요구사항
-    // it('자동차 경주 게임의 턴이 진행 될 때마다 1초의 텀(progressive)를 두고 진행한다.', () => {
-    // });
-  
 });
-
-// TODO:
