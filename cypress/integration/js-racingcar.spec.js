9b8b16afd1b6ce1f3f7c0f00c248b9b8104c8f93
dorrdorr9311 <dorrdorr9311@gmail.com>
2021-07-15 14:10:11 +0900

    정상 이름 입력시 시도횟수 입력창 출력코드 작성

--- cypress/integration/js-racingcar.spec.js
+++ cypress/integration/js-racingcar.spec.js
@@ -1,11 +1,11 @@
 describe('js-racingcar', () => {
   beforeEach(() => {
     //페이지 접속
-    cy.visit('/');
+    cy.visit('http://192.168.200.186:5500/index.html');
   })
 
   describe('자동차에 이름을 부여한다.', () => {
-    it('쉼표로 구분된 5자 이하의 이름들을 입력하면 시도횟수 입력창이 나타난다.',() => {
+    it('쉼표로 구분된 5자 이하의 이름들을 입력하면 시도횟수 입력창이 나타난다.', () => {
       const carNames = ['one', 'two', 'three', 'four'];
       cy.get('#input-cars-name').type(carNames.join(','));
 
@@ -13,8 +13,9 @@ describe('js-racingcar', () => {
 
       cy.get('#trial-count-container').should('be.visible');
     })
-  })
 
+  
+  })
 })
 
 
---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const $ = (selector) => document.querySelector(selector);
+
+const $submitCarNameBtn = $('#submit-cars-name');
+const $inputCarName = $('#input-cars-name');
+const $carNameContainer = $('#car-name-container');
+
+
+$submitCarNameBtn.addEventListener('click', () => {
+  const names = $inputCarName.value.split(',');
+  let isValid = true;
+
+  names.forEach(name => {
+    if(name.length < 1 || name.length > 5) {
+      isValid = false
+      return;
+    }
+  })
+  const $trialCountContainer = `<fieldset id="trial-count-container">
+  <p>시도할 횟수를 입력해주세요.</p>
+  <div class="d-flex">
+    <input type="number" class="w-100 mr-2" placeholder="시도 횟수" />
+    <button type="button" class="btn btn-cyan">확인</button>
+  </div>
+</fieldset>`
+
+  if (isValid) {
+    $carNameContainer.insertAdjacentHTML('afterend', $trialCountContainer);
+  }
+})
+
+
+
+
