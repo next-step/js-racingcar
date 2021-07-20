369c8e906b7475a133e628299adb275349cf7984
soso01 <6125024@gmail.com>
2021-07-20 10:09:36 +0900

    기능 구현

---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+export const ERROR_TEXT = {
+  CARS_NAME_LENGTH:
+    "유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.",
+  ZERO_TURNS_INPUT:
+    "입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다.",
+};
+
+export const CLASS_TYPE = {
+  DISPLAY_NONE: "d-none",
+};
---
+++ src/js/racing.js
@@ -0,0 +1,88 @@
+import { ERROR_TEXT, CLASS_TYPE } from "./constants.js";
+import { validator, createEl, helper } from "./utils.js";
+
+const racing = (() => {
+  let cars = [];
+  let turns = 0;
+  let stadium;
+
+  return {
+    clear: (inputs, sections) => {
+      cars = [];
+      turns = 0;
+      stadium.remove();
+      inputs.forEach((el) => {
+        el.value = "";
+        el.disabled = false;
+      });
+      sections.forEach((el) => el.classList.add(CLASS_TYPE.DISPLAY_NONE));
+    },
+
+    setCars: (str, next) => {
+      cars = str.split(",").map((name) => ({ name: name.trim(), value: 0 }));
+      if (validator.carsName(cars)) {
+        next.classList.remove(CLASS_TYPE.DISPLAY_NONE);
+
+        return true;
+      } else {
+        alert(ERROR_TEXT.CARS_NAME_LENGTH);
+
+        return false;
+      }
+    },
+
+    setTurns: async (value) => {
+      if (validator.turns(value)) {
+        turns = value;
+
+        return true;
+      } else {
+        alert(ERROR_TEXT.ZERO_TURNS_INPUT);
+
+        return false;
+      }
+    },
+
+    play: (stadiumSection) =>
+      new Promise((resolve) => {
+        stadiumSection.classList.remove(CLASS_TYPE.DISPLAY_NONE);
+        stadium = stadiumSection.appendChild(createEl.stadium());
+
+        cars.forEach((car) => {
+          const carEl = createEl.car(car.name);
+          car.el = carEl;
+          stadium.appendChild(carEl);
+        });
+
+        const interval = setInterval(() => {
+          cars.forEach((car) => {
+            const lastEl = car.el.lastChild;
+            if (helper.isForward()) {
+              car.el.insertBefore(createEl.forwardIcon(), lastEl);
+            }
+          });
+
+          if (!--turns) {
+            clearInterval(interval);
+            resolve();
+          }
+        }, 1000);
+      }),
+
+    printResult: (resultSection, resultText) => {
+      // 스피너 삭제
+      cars.forEach((car) => {
+        car.el.lastChild.remove();
+      });
+
+      resultSection.classList.remove(CLASS_TYPE.DISPLAY_NONE);
+
+      const result = resultSection.querySelector("h2");
+      result.textContent = `🏆 최종 우승자: ${helper
+        .getWinner(cars)
+        .join(", ")} 🏆`;
+    },
+  };
+})();
+
+export default racing;
