9d9b2bea9ae54ba09d6df874fa84626ea7469462
sinholee <dltlsgh5@gmail.com>
2021-07-20 16:47:30 +0900

    delete: mvc_study 정리

--- src/study_mvc/classicMVC/Model.js
+++
@@ -1,5 +0,0 @@
-export default class Model {
-	constructor() {
-		this.heading = 'Hello';
-	}
-}
--- src/study_mvc/classicMVC/View.js
+++
@@ -1,8 +0,0 @@
-export default class View {
-	constructor(controller) {
-		this.controller = controller;
-		this.heading = document.querySelector('#heading');
-		this.heading.innerHTML = controller.getModelHeading();
-		this.heading.addEventListener('click', controller);
-	}
-}
