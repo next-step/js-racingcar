33018b5e45c20aa156827ab291a3c5ef271a0e8a
g1moon <gmoon.cho@gmail.com>
2021-12-08 16:26:34 +0900

    chore

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
+import CarNamesForm from './components/CarNamesForm.js';
+import TryCountsForm from './components/TryCountsForm.js';
+
 window.onerror = function (msg) {
   alert(msg);
   return true;
 };
-
-import CarNamesForm from './components/CarNamesForm.js';
-import TryCountsForm from './components/TryCountsForm.js';
 
 new CarNamesForm().render();
 new TryCountsForm().render();
