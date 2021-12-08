ab27dbd0753bc9d06dfcaba7b00c45352a1ffb30
g1moon <gmoon.cho@gmail.com>
2021-12-08 16:16:00 +0900

    formatter

--- src/js/utils/service.js
+++ src/js/utils/service.js
@@ -10,6 +10,5 @@ export const isForward = (num) => {
   return num >= 4 ? 1 : 0;
 };
 
-export const createProcessArray = (tryCounts) => {
-  return createRandomArray(tryCounts, 0, 9).map((number) => isForward(number));
-};
+export const createProcessArray = (tryCounts) =>
+  createRandomArray(tryCounts, 0, 9).map((number) => isForward(number));
