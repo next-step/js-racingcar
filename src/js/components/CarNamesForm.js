aa09c1e32a959e299408ba5834c0d35b966a2426
g1moon <gmoon.cho@gmail.com>
2021-12-10 01:34:06 +0900

    refactor: render하는 부분에서 this.$element를 초기화는 방법으로 변경해 jsx처럼 동작하게끔 수정

--- src/js/components/CarNamesForm.js
+++ src/js/components/CarNamesForm.js
@@ -4,41 +4,12 @@ import el from '../utils/dom.js';
 import { $ } from '../utils/utils.js';
 
 export default class CarNamesForm extends Component {
-  static #template = `
-        <form> 
-          <fieldset>
-            <h1 class="text-center">🏎️ 자동차 경주 게임</h1>
-            <p>
-              5자 이하의 자동차 이름을 콤마로 구분하여 입력해주세요. <br />
-              예시) EAST, WEST, SOUTH, NORTH
-            </p>
-            <div class="d-flex">
-              <input
-                type="text"
-                class="w-100 mr-2 car-name-input"
-                placeholder="자동차 이름"
-              />
-              <button type="button" class="btn btn-cyan car-name-submit-btn">
-                확인
-              </button>
-            </div>
-          </fieldset>
-        </form>
-`;
-
-  $carNamesForm;
-  $carNamesInput;
-
   constructor() {
     super({ store });
-    this.$carNamesForm = el(CarNamesForm.#template);
-    this.$carNamesInput = this.$carNamesForm.querySelector('.car-name-input');
-
-    this.bindEvent();
   }
 
   bindEvent() {
-    this.$carNamesForm.addEventListener('submit', (event) => this.onSubmitCarNames(event));
+    this.$element.addEventListener('submit', (event) => this.onSubmitCarNames(event));
   }
 
   onSubmitCarNames(event) {
@@ -46,10 +17,35 @@ export default class CarNamesForm extends Component {
     store.dispatch('setCarNames', {
       carNames: this.$carNamesInput.value.split(','),
     });
-    this.show();
   }
 
   render() {
-    $('#app section').insertAdjacentElement('afterbegin', this.$carNamesForm);
+    this.$element = el(`
+        <form class="car-names-form">
+            <fieldset>
+                <h1 class="text-center">🏎️ 자동차 경주 게임</h1>
+                <p>
+                    5자 이하의 자동차 이름을 콤마로 구분하여 입력해주세요. <br/>
+                    예시) EAST, WEST, SOUTH, NORTH
+                </p>
+                <div class="d-flex">
+                    <input
+                            type="text"
+                            class="w-100 mr-2 car-name-input"
+                            placeholder="자동차 이름"
+                    />
+                    <button type="submit" class="btn btn-cyan car-name-submit-btn">
+                        확인
+                    </button>
+                </div>
+            </fieldset>
+        </form>
+    `);
+
+    this.$carNamesInput = this.$element.querySelector('.car-name-input');
+
+    this.bindEvent();
+
+    $('.form-section').appendChild(this.$element);
   }
 }
---
+++ src/js/components/WinnerBoard.js
@@ -0,0 +1,25 @@
+import Component from '../lib/component.js';
+import store from '../store/index.js';
+import el from '../utils/dom.js';
+import { $ } from '../utils/utils.js';
+
+export default class WinnerBoard extends Component {
+  constructor() {
+    super({ store });
+  }
+
+  render() {
+    this.$element = el(`
+        <section class="d-flex justify-center mt-5 winner-board" style="display: none">
+            <div>
+                <h2>🏆 최종 우승자: ${store.state.winners[0]} 🏆</h2>
+                <div class="d-flex justify-center">
+                    <button type="button" class="btn btn-cyan">다시 시작하기</button>
+                </div>
+            </div>
+        </section>
+    `);
+
+    $('.winner-board').replaceWith(this.$element);
+  }
+}
