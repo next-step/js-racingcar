b3589c97f5ce9ca37c2fa6d71930347120b91ba5
g1moon <gmoon.cho@gmail.com>
2021-12-10 01:40:16 +0900

    refactor: Component에서 stateChange를 subscribe 할 경우 매 변경마다 자식 컴포넌트 개수만큼 작업을 하므로 app에서 한번만 감지되게 수정

--- src/js/lib/component.js
+++ src/js/lib/component.js
@@ -1,15 +1,5 @@
 import Store from '../store/store.js';
 
 export default class Component {
-  constructor(props = {}) {
-    this.render = this.render || function () {};
-
-    if (props.store instanceof Store) {
-      props.store.events.subscribe('stateChange', () => this.render());
-    }
-
-    if (props.hasOwnProperty('element')) {
-      this.element = props.element;
-    }
-  }
+  constructor(props = {}) {}
 }
