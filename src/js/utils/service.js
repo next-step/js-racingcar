ac9ecb699a614313f50d2df0454a2099bfdb3767
g1moon <gmoon.cho@gmail.com>
2021-12-11 22:28:20 +0900

    refactor: 전진인지 멈춤인지를 관리하는 배열에 0, 1로 관리되던 것을 True, False 불리언 값으로 관리하도록 수정

--- src/js/utils/service.js
+++ src/js/utils/service.js
@@ -6,6 +6,7 @@ export const validateCarName = (carName) => {
   if (carName.length > 5) throw Error(ERROR_MESSAGES.MAXIMUM_CAR_NAMES_LENGTH);
 };
 
-export const isForward = (num) => (num >= 4 ? 1 : 0);
+export const isForward = (num) => num >= 4;
 
-export const createProcessArray = (tryCounts) => createRandomArray(tryCounts, 0, 9).map((number) => isForward(number));
+export const createProcessArray = (tryCounts) =>
+  createRandomArray(tryCounts, 0, 9).map((number) => isForward(number));
