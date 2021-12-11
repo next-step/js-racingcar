6c3016de71023fa07c5bb53cdf289cd4b2ab1121
Tanney <dhxodms89@gmail.com>
2021-12-11 12:11:10 +0900

    feat: 마크업 구조 변경 및 js 셀렉터 추가

---
+++ src/js/constants/selectors.js
@@ -0,0 +1,11 @@
+const CLASS_NAME = {
+  CAR_NAMES_FORM: "js-car-names-form",
+  RACING_COURSE: "js-racing-course",
+  CAR_PLAYER: "js-car-player",
+};
+
+const INPUT_NAME = {
+  CAR_NAMES: "car-names",
+};
+
+export { CLASS_NAME, INPUT_NAME };
