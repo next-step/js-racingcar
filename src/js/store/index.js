5044b6821c45abbf44b1c52a95cc44cc7fd47391
g1moon <gmoon.cho@gmail.com>
2021-12-14 17:40:41 +0900

    refactor: initailState를 따로 관리 할 수 있도록 수정

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,10 @@
 import actions from './actions.js';
 import mutations from './mutations.js';
-import state from './state.js';
+import initialState from './initialState.js';
 import Store from './store.js';
 
 export default new Store({
   actions,
   mutations,
-  state,
+  state: { ...initialState },
 });
