48d3b53cd2534dc28e05ba315709930d563e189b
Heeji <limhizy15@ajou.ac.kr>
2021-12-14 20:09:43 +0900

    [step1] chores: cypress 환경구축

---
+++ cypress/integration/racingcar.spec.js
@@ -0,0 +1,11 @@
+beforeEach(() => {
+  cy.visit("http://localhost:5500/");
+});
+
+describe("racing-car", () => {
+  context("확인", () => {
+    it("뭐뭐할 때 어떻게 되야함", () => {
+      cy.get();
+    });
+  });
+});
