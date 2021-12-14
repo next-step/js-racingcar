f5544e7dea25fc9c8f59d2237fcfa10018dc81ee
g1moon <gmoon.cho@gmail.com>
2021-12-14 23:22:20 +0900

    feat: 상수 추가

--- src/js/constants.js
+++ src/js/constants.js
@@ -11,15 +11,28 @@ export const ERROR_MESSAGES = {
   NO_CAR_NAMES: '자동차 이름을 입력하세요.',
   NO_TRY_COUNTS: '시도 횟수를 입력해주세요.',
   MAXIMUM_CAR_NAMES_LENGTH: '자동차의 이름은 5글자를 넘을 수 없습니다.',
-};
-
-export const EVENTS = {
-  STATE_CHANGE: 'stateChange',
+  MINIMUM_TRY_COUNTS: '시도 횟수는 0을 초과해야 합니다.',
 };
 
 export const ACTIONS = {
   SET_CAR_NAMES: 'setCarNames',
-  SET_PROCESS_MATRIX: 'setProcessMatrix',
+  SET_PROGRESS_MATRIX: 'setProgressMatrix',
   SET_WINNERS: 'setWinners',
   SET_TRY_COUNTS: 'setTryCounts',
+  INIT_STATE: 'initState',
 };
+
+export const TEST_DOM = {
+  CAR_NAMES_FORM: 'carNamesForm',
+  CAR_NAMES_INPUT: 'carNamesInput',
+  CAR_NAMES_SUBMIT_BUTTON: 'carNamesSubmitBtn',
+  TRY_COUNTS_FORM: 'tryCountsForm',
+  TRY_COUNTS_INPUT: 'tryCountsInput',
+  TRY_COUNTS_SUBMIT_BUTTON: 'tryCountsSubmitBtn',
+  CAR_PROGRESS_CONTAINER: 'carProgressContainer',
+  WINNER_BOARD: 'winnerBoard',
+  CAR_PROGRESS: 'carProgress',
+};
+
+export const ONE_SECOND = 1000;
+export const DONE_MESSAGE = '축하합니다.';
