57ba2eef6766b3cf03cb2a724bdbc4e1df0cc6ce
Heeji <limhizy15@ajou.ac.kr>
2021-12-14 21:28:51 +0900

    [step1] fix: 이름 입력 전 시도횟수 입력 시 예외 처리

--- src/components/TrialCountForm.js
+++ src/components/TrialCountForm.js
@@ -11,19 +11,33 @@ export default class TrialCountForm extends Component {
   }
 
   setEvent() {
-    const { getTrialCount } = this.$props;
     this.addEvent("click", ".trialCount-submitBtn", (e) => {
       e.preventDefault();
-      getTrialCount(document.querySelector(".trialCount-input").value);
-      document.getElementById("fieldset-carName").disabled = true;
-      document.getElementById("fieldset-trialCount").disabled = true;
+      this.submitTrialCount();
     });
     this.addEvent("keyup", ".trialCount-container", (e) => {
       if (e.key != "Enter") return;
       e.preventDefault();
-      getTrialCount(document.querySelector(".trialCount-input").value);
-      document.getElementById("fieldset-carName").disabled = true;
-      document.getElementById("fieldset-trialCount").disabled = true;
+      this.submitTrialCount();
     });
+  }
+
+  submitTrialCount() {
+    if (!this.checkCarName()) {
+      alert("자동차 이름을 입력하고 시도횟수를 입력해주세요.");
+      return;
+    }
+    const { getTrialCount } = this.$props;
+    getTrialCount(document.querySelector(".trialCount-input").value);
+    document.getElementById("fieldset-carName").disabled = true;
+    document.getElementById("fieldset-trialCount").disabled = true;
+  }
+
+  checkCarName() {
+    const $fieldSetCarName = document.querySelector("#fieldset-carName");
+
+    if (!$fieldSetCarName.hasAttribute("disabled")) return false;
+
+    return true;
   }
 }
