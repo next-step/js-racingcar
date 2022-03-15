8b0dc2c3a0ef41fd3a3feeb15fd331f204213e15
che-ri <rnjs1135@gmail.com>
2022-03-16 08:11:02 +0900

    [클린코드 1기 권지영] 자동차 경주 미션 Step1 (#41)
    * chore: 폴더구조잡기
    
    * feat: step1 구현

---
+++ src/js/constants/selector.js
@@ -0,0 +1,7 @@
+export const RACE_CAR_LIST_INPUT = "#race-car-list-input";
+export const RACE_CAR_LIST_BUTTON = "#race-car-list-button";
+export const RACE_CNT_CONTAINER = "#race-cnt-container";
+export const RACE_CNT_INPUT = "#race-cnt-input";
+export const RACE_CNT_BUTTON = "#race-cnt-button";
+export const RACE_GROUND = "#race-ground";
+export const RESULT_CONTINAER = "#result-container";
