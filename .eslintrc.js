67e9e640f7f4eff0a0ea088f423791c9d412e9ef
정현수 <jung660317@naver.com>
2022-03-29 21:38:19 +0900

    chore: eslint & prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['off'],
+    'class-methods-use-this': 'off',
+    'no-alert': 'off',
+    'no-undef': 'off',
+    'no-new': 'off',
+  },
+};
