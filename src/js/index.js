10e61aed5c50f5476ca3936c53f495b18d7a02cc
hdpyo <hudongpyo.dev@gmail.com>
2022-03-30 23:44:28 +0900

    refactor: 테스트 코드 리팩토링
      - 앱을 처음 실행하면 자동차 이름을 입력할 창이 보여야 한다.
      - 자동차 이름이 1자 미만, 5자 초과라면 경고창을 호출한다.

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,28 @@
 const carNamesSubmit = document.querySelector('#car-names-submit');
 
-carNamesSubmit.addEventListener('click', e => {
-  const carNamesInput = document.querySelector('#car-names-input');
-  if (carNamesInput.value === '') {
-    alert('자동차 이름을 입력해주세요.');
-  }
-
-  if (carNamesInput.value.length > 5) {
-    alert('자동차 이름은 5글자까지만 가능합니다.');
-  }
-});
-
 const racingTryCount = document.querySelector('#racing-try-count');
 const racingTrack = document.querySelector('#racing-track');
 const racingResult = document.querySelector('#racing-result');
 racingTryCount.style.display = 'none';
 racingTrack.style.display = 'none';
 racingResult.style.display = 'none';
+
+function submitCarNames() {
+  const carNames = document.querySelector('#car-names-input').value.split(',');
+
+  // 자동차 이름 입력값 유효성 검사
+  if (carNames.every(isValidCarName)) {
+    alert('자동차 이름은 최소 1글자에서 최대 5글자까지 입력해주세요.');
+    return;
+  }
+
+  const a = '';
+  // 실행 내용
+}
+
+// todo : 자동차 이름 유효성 검증
+function isValidCarName(carName) {
+  return carName.length < 1 || carName.length > 5;
+}
+
+carNamesSubmit.addEventListener('click', submitCarNames);
