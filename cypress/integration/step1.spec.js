664876edaa6b116fe05620ea1fe8f195574dbb40
padawanR0k <padawanR0k@gmail.com>
2022-03-31 00:40:52 +0900

    up: 자동차이름 입력 기능 해피패스만 구현

--- cypress/integration/step1.spec.js
+++ cypress/integration/step1.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('step1', () => {
-    beforeEach(() => {
+    before(() => {
         cy.visit('../../index.html')
     })
 
@@ -12,7 +12,9 @@ describe('step1', () => {
     context('자동차이름을 쉼표를 기준으로 구분하여 입력하고 확인버튼을 누르면', () => {
         before(() => {
             cy.get('[placeholder="자동차 이름"]').type('자동차1,자동차2,자동차3');
-            cy.get('[placeholder="자동차 이름"] + button:contains("확인")').click();
+            cy.get('[placeholder="자동차 이름"] + button:contains("확인")').click().then(() => {
+
+            });
         })
 
         it('시도횟수 입력 입력폼이 노출된다', () => {
@@ -25,9 +27,9 @@ describe('step1', () => {
                 cy.get('[placeholder="시도 횟수"] + button:contains("확인")').click();
             })
             it('쉼표를 기준으로 나뉘어 자동차 이름이 지정된다.', () => {
-                cy.get('.car-player').get(0).should('have.text', '자동차1');
-                cy.get('.car-player').get(1).should('have.text', '자동차2');
-                cy.get('.car-player').get(2).should('have.text', '자동차3');
+                cy.get('.car-player').eq(0).should('have.text', '자동차1');
+                cy.get('.car-player').eq(1).should('have.text', '자동차2');
+                cy.get('.car-player').eq(2).should('have.text', '자동차3');
             })
             // it('경주가 진행되고 결과가 나온다.', () => {
             //
---
+++ src/app.js
@@ -0,0 +1,59 @@
+
+function initApp() {
+    const $app = document.querySelector('#app');
+    const $carsNameInput = $app.querySelector('[placeholder="자동차 이름"]');
+    const $carsNameSubmit = $app.querySelector('[placeholder="자동차 이름"] + button');
+    const $tryCntFieldSet = $app.querySelector('.try-count');
+    const $tryCntInput = $app.querySelector('[placeholder="시도 횟수"]');
+    const $tryCntSubmit = $app.querySelector('[placeholder="시도 횟수"] + button');
+    const $raceContainer = $app.querySelector('.race');
+
+    function getCarsNames() {
+        const carsNames = $carsNameInput.value.split(',');
+        if (carsNames.length === 0  || carsNames.some(name => name.trim() === '')) {
+            throw new Error('자동차 이름을 입력해주세요.')
+        }
+        return carsNames;
+    }
+
+    function getTryCount() {
+        const count = parseInt($tryCntInput.value);
+        if (!count) {
+            throw new Error('0이 아닌 숫자를 입력해주세요.')
+        }
+        return count;
+    }
+
+    function drawCars(names) {
+        return `<div class="mt-4 d-flex">
+            ${names.map(name => `<div class="mr-2">
+                <div class="car-player">${name}</div>
+            </div>`).join('')}
+        </div>`
+    }
+
+
+    $carsNameSubmit.addEventListener('click', () => {
+        const names = getCarsNames();
+
+        if (names instanceof Error) {
+            alert(names.message);
+            return ;
+        }
+        $tryCntFieldSet.classList.remove('hidden');
+    })
+
+    $tryCntSubmit.addEventListener('click', () => {
+        const count = getTryCount();
+
+        if (count instanceof Error) {
+            alert(count.message);
+            return ;
+        }
+        $raceContainer.classList.remove('hidden');
+        const names = getCarsNames();
+        $raceContainer.innerHTML = drawCars(names);
+    })
+}
+
+initApp();
