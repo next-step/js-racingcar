3ec9c67bcac7aa99919091e82595dce0ce271931
circlegiven <circlegiven@gmail.com>
2022-03-31 23:09:12 +0900

    [Test] 초기화면 테스트 케이스 명칭 수정 및 자동차 입력화면 테스트 WIP

--- cypress/integration/racing.spec.js
+++ cypress/integration/racing.spec.js
@@ -24,8 +24,11 @@ describe('자동차 경주 게임', () => {
   });
 
   describe('초기 화면 테스트', () => {
-    it('초기에 자동차 이름을 입력할 수 있는 form이 나온다.', () => {
+    it('자동차 이름 입력 input과 경기횟수 input을 다룰 수 있는 form이 존재한다.', () => {
       cy.get('form').should('be.visible');
+    });
+    it('초기에 자동차 이름을 입력할 수 있는 input이 보인다.', () => {
+      cy.carNameInput().should('be.visible');
     });
     it('초기에 횟수를 입력할 수 있는 input은 보이지 않는다.', () => {
       cy.tryNumberInput().should('not.exist');
@@ -35,6 +38,12 @@ describe('자동차 경주 게임', () => {
     });
     it('초기에 경기의 우승 결과는 보이지 않는다.', () => {
       cy.racingResult().should('not.exist');
+    });
+  });
+
+  describe('자동차 입력 화면 테스트', () => {
+    it('초기에 자동차 이름을 입력할 수 있는 input은 비어있는 상태이다.', () => {
+      cy.isInitialCarName();
     });
   });
 });
