32ce322d3acf47cbd058cdfa2afed2901f54ab41
circlegiven <circlegiven@gmail.com>
2022-03-31 23:04:41 +0900

    [Test] 초기화면 랜더링 테스트 완료

--- cypress/integration/racing.spec.js
+++ cypress/integration/racing.spec.js
@@ -6,6 +6,10 @@ Cypress.Commands.add('tryNumberInput', () =>
   cy.get('div.d-flex > input[type=number]')
 );
 
+Cypress.Commands.add('racingScetion', () => cy.get('#racing'));
+
+Cypress.Commands.add('racingResult', () => cy.get('#result'));
+
 Cypress.Commands.add('isInitialCarName', () =>
   cy.carNameInput().should('have.value', '')
 );
@@ -20,14 +24,17 @@ describe('자동차 경주 게임', () => {
   });
 
   describe('초기 화면 테스트', () => {
-    it('초기에 자동차 이름과 시도횟수를 입력할 수 있는 form이 나온다.', () => {
+    it('초기에 자동차 이름을 입력할 수 있는 form이 나온다.', () => {
       cy.get('form').should('be.visible');
     });
-    it('초기에 자동차 이름을 입력할 수 있는 input은 비어있는 상태이다.', () => {
-      cy.isInitialCarName();
+    it('초기에 횟수를 입력할 수 있는 input은 보이지 않는다.', () => {
+      cy.tryNumberInput().should('not.exist');
     });
-    it('초기에 횟수를 입력할 수 있는 input은 비어있는 상태이다.', () => {
-      cy.isInitialTryNumber();
+    it('초기에 경기가 진행되는 현황은 보이지 않는다.', () => {
+      cy.racingScetion().should('not.exist');
+    });
+    it('초기에 경기의 우승 결과는 보이지 않는다.', () => {
+      cy.racingResult().should('not.exist');
     });
   });
 });
