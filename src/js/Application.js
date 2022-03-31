a6d45af7fe8e9dcb7d90d776c9d7ae8fff8b5800
Tene <tene@nextunicorn.kr>
2022-04-01 02:26:30 +0900

    ✒️Feat: 응용 계층이 렌더링을 담당하도록 개발

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,5 @@
 import { Car, RacingGame } from './domain/index.js';
+import { Setup, Game, Result } from './infrastructure/index.js';
 
 export default class Application {
   #car;
@@ -7,5 +8,14 @@ export default class Application {
   constructor() {
     this.#car = new Car();
     this.#racingGame = new RacingGame();
+  }
+
+  render() {
+    const $app = document.createElement('div');
+    $app.setAttribute('id', 'app');
+    $app.replaceChildren(Setup(), Game(), Result());
+
+    document.body.replaceChildren();
+    document.body.insertAdjacentElement('afterbegin', $app);
   }
 }
