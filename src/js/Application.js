6b89baf749a3cb6e151ad3a3be4bbe43a80ece5c
Tene <tene@nextunicorn.kr>
2022-04-02 17:30:40 +0900

    ✒️Feat: Application 기능 고도화 - diff 알고리즘 추가 - publish/subsribe 패턴 적용

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,19 +1,47 @@
+import { diff } from './@helper/index.js';
 import { Car, RacingGame } from './domain/index.js';
 import { Setup, Game, Result } from './infrastructure/index.js';
 
+const publisher = () => {
+  const observers = {};
+  const subscribe = callback => {
+    Object.keys(callback).forEach(_key => {
+      observers[_key] = callback[_key];
+    });
+  };
+
+  const notify = () => {
+    Object.keys(observers).forEach(_key => {
+      observers[_key]();
+    });
+  };
+
+  return {
+    subscribe,
+    notify,
+  };
+};
 export default class Application {
   #racingGame;
+  #publisher = publisher();
 
   constructor() {
     this.#racingGame = new RacingGame();
+    this.#publisher.subscribe({ render: this.render.bind(this) });
+    this.#publisher.notify();
   }
 
   render() {
-    const $app = document.createElement('div');
-    $app.setAttribute('id', 'app');
-    $app.replaceChildren(Setup(Car, this.#racingGame), Game(), Result());
+    const $root = document.body;
+    const $old = $root.firstChild;
+    const $new = document.createElement('div');
+    $new.setAttribute('id', 'app');
+    $new.replaceChildren(
+      Setup({ Car, RacingGame: this.#racingGame }, this.#publisher),
+      Game({ RacingGame: this.#racingGame }, this.#publisher),
+      Result({ RacingGame: this.#racingGame }, this.#publisher),
+    );
 
-    document.body.replaceChildren();
-    document.body.insertAdjacentElement('afterbegin', $app);
+    diff($root, $old, $new);
   }
 }
