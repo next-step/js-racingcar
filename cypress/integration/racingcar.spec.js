c3721d0699e5616aa10ed5481c35cb7caac69d9b
yukyung <s2018w39@e-mirim.hs.kr>
2022-04-02 17:19:02 +0900

    test: 자동차의 이름이 5글자를 초과하면 경고창을 띄워준다.

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -17,4 +17,18 @@ describe('레이싱카 앱 테스트', () => {
 				);
 			});
 	});
+
+	it('자동차의 이름이 5글자를 초과하면 경고창을 띄워준다.', () => {
+		const alertStub = cy.stub();
+		cy.on('window:alert', alertStub);
+
+		cy.get('#car-names-input').type('가나다라마바');
+		cy.get('#car-names-submit')
+			.click()
+			.then(() => {
+				expect(alertStub.getCall(0)).to.be.calledWith(
+					'자동차의 이름은 최대 5글자까지 입력 가능합니다.'
+				);
+			});
+	});
 });
--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,11 @@ $button.addEventListener('click', () => {
 	if (!$input.value) {
 		// TODO: 에러 메세지를 상수화한다.
 		alert('자동차의 이름을 입력해주세요.');
+		return;
+	}
+
+	// TODO: 5라는 값을 상수화한다.
+	if ($input.value.length > 5) {
+		alert('자동차의 이름은 최대 5글자까지 입력 가능합니다.');
 	}
 });
