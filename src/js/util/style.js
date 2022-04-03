7a445746f4a956bb4721affdffca0dff44fda4d1
yukyung <s2018w39@e-mirim.hs.kr>
2022-04-04 00:59:28 +0900

    feat: style util 파일 추가

---
+++ src/js/util/style.js
@@ -0,0 +1,14 @@
+export const style = {
+	block(el) {
+		el.style.display = 'block';
+	},
+	flex(el) {
+		el.style.display = 'flex';
+	},
+	none(el) {
+		el.style.display = 'none';
+	},
+	disabled(el) {
+		el.disabled = true;
+	},
+};
