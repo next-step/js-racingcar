5f15f3e5a5bb514b5507b55268f8c29e016d7251
yukyung <s2018w39@e-mirim.hs.kr>
2022-04-04 01:02:47 +0900

    feat: util 파일 추가

---
+++ src/js/util/util.js
@@ -0,0 +1,13 @@
+import { MAX_CAR_NAME_LENGTH, RANDOM_NUM } from './constatns.js';
+
+export function isCheckCarNameLength(carName) {
+	return carName
+		.split(', ')
+		.every((item) => item.length <= MAX_CAR_NAME_LENGTH);
+}
+
+export function isRace() {
+	const randomNum =
+		Math.floor(Math.random() * RANDOM_NUM.MAX_NUM) + RANDOM_NUM.MIN_NUM;
+	return randomNum >= RANDOM_NUM.FORWARD_NUM;
+}
