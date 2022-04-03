8d2689782f7e9af512a755c0843b984cf442b74a
wanho1108 <wanho1108@gmail.com>
2022-04-03 20:52:56 +0900

    feat: 자동차 이름을 쉼표를 기준으로 5자 초과로 입력하면 경고창 출력 작업

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const $carNameSubmit = document.querySelector('#car-name-submit');
 $carNameSubmit.addEventListener('click', () => {
   const $carNameInput = document.querySelector('#car-name-input');
   const value = $carNameInput.value.trim();
+  const carNames = value.split(',');
   const regexp = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣,]+$/g; // TODO: 변수 이름 변경
 
   if (!value) {
@@ -13,4 +14,12 @@ $carNameSubmit.addEventListener('click', () => {
   if (!regexp.test(value)) {
     alert('자동차 이름은 영문, 한글, 쉼표만 입력할 수 있습니다');
   }
+
+  // eslint-disable-next-line consistent-return
+  carNames.forEach((carName) => {
+    if (carName.length > 5) {
+      alert('자동차 이름은 쉼표를 기준으로 5자 이하만 가능합니다.');
+      return false;
+    }
+  });
 });
