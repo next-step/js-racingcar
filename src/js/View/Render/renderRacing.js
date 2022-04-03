c0fc8684281bc6cc6d847ee02225b08a29b933a2
Blackraven93 <reblackraven@gmail.com>
2022-04-04 02:17:05 +0900

    Refactor: renderRacing 리팩토링

--- src/js/View/Render/renderRacing.js
+++ src/js/View/Render/renderRacing.js
@@ -3,39 +3,26 @@ import { selectorAll } from "../../util/consts.js";
 import { Component, render } from "../index.js";
 
 const renderArrow = (index) => {
-  render(selectorAll('.racing-track')[index], Component.create(Go()));
+  render(
+    selectorAll('.racing-track')[index],
+    Component.create(Go())
+  );
 }
 
 const renderDelay = (index, i) => {
-  (function (index) {
-    setTimeout(renderArrow(index), 1000 * (i + 1));
-  })(index)
-}
-
-const canMoveOn = (condition, callback) => {
-  if (condition) {
-    callback()
-  }
+  setTimeout(() => {
+    renderArrow(index)
+  }, 1000 * (i + 1));
 }
 
 const setUpRacing = (racing, i) => {
   racing.start().forEach((condition, index) => {
-    canMoveOn(condition, renderDelay(index, i))
+    if (condition) renderDelay(index, i)
   }
 )}
 
 const renderRacingGame = (racing, value) => {
   Array.from({ length: value }).forEach((e, i) => setUpRacing(racing, i));
 }
-
-
-
-
-
-
-
-
-
-
 
 export default renderRacingGame;
