948b8f37469655e2a14d0ada017215334d1b74e3
yukyung <s2018w39@e-mirim.hs.kr>
2022-04-04 00:55:24 +0900

    feat: 상수 파일 추가

---
+++ src/js/util/constatns.js
@@ -0,0 +1,13 @@
+export const MAX_CAR_NAME_LENGTH = 5;
+
+export const RANDOM_NUM = {
+	MIN_NUM: 0,
+	MAX_NUM: 9,
+	FORWARD_NUM: 4,
+};
+
+export const ERR_MSG = {
+	EMPTY_CAR_NAME: '자동차의 이름을 입력해주세요.',
+	OVER_CAR_NAME_LENGTH: `자동차의 이름은 최대 ${MAX_CAR_NAME_LENGTH}글자까지 입력 가능합니다.`,
+	EMPTY_TRY_NUM: '시도할 횟수를 입력해주세요.',
+};
