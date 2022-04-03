fae44b5b30a98d9d55705e34c75ebd1b227554cf
wanho1108 <wanho1108@gmail.com>
2022-04-03 20:58:08 +0900

    test: 자동차 이름을 입력하면 시도 횟수 입력창과 버튼이 노출된다 테스트 작성

--- cypress/integration/reacingcar.spec.js
+++ cypress/integration/reacingcar.spec.js
@@ -46,4 +46,11 @@ describe('자동차 경주 게임', () => {
         expect(alertStub.getCall(0)).to.be.calledWith('자동차 이름은 쉼표를 기준으로 5자 이하만 가능합니다.');
       });
   });
+
+  it('자동차 이름을 입력하면 시도 횟수 입력창과 버튼이 노출된다.', () => {
+    cy.get('#car-name-input').type('EAST,WEST,SOUTH,NORTH');
+    cy.get('#car-name-submit').click();
+    cy.get('#race-lap-input').should('be.visible');
+    cy.get('#race-lap-submit').should('be.visible');
+  });
 });
