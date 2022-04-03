88c7447ad46785ee24756c45a3d81722e8a44f50
wanho1108 <wanho1108@gmail.com>
2022-04-03 19:38:24 +0900

    test: 자동차 이름을 입력하는 입력창과 확인 버튼이 존재한다 테스트 작성

--- cypress/integration/reacingcar.spec.js
+++ cypress/integration/reacingcar.spec.js
@@ -2,4 +2,9 @@ describe('자동차 경주 게임', () => {
   beforeEach(() => {
     cy.visit('../../index.html');
   });
+
+  it('자동차 이름을 입력하는 입력창과 확인 버튼이 존재한다.', () => {
+    cy.get('#car-name-input').should('be.visible');
+    cy.get('#car-name-submit').should('be.visible');
+  });
 });
