e4199b28b6ef8f54e96f83b62da9a66ec73470d9
Blackraven93 <reblackraven@gmail.com>
2022-04-04 04:32:43 +0900

    Fix: 스피너 제거 오류 수정

--- src/js/View/Render/removeSpinner.js
+++ src/js/View/Render/removeSpinner.js
@@ -2,7 +2,7 @@ import { selectorAll } from "../../util/consts.js"
 
 
 const removeSpinner = (value) => {
-  setTimeout(
+  setTimeout(() => 
     selectorAll('.track-container')
       .forEach((tag) => tag.removeChild(tag.querySelector('.spinner-component')))
   , 1000 * value)
