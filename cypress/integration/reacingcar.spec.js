6727b30823bfd314d8ad0f54dbec69f03579c292
wanho1108 <wanho1108@gmail.com>
2022-04-03 20:00:40 +0900

    test: 자동차 이름으로 빈값을 입력하면 경고창이 출력된다 테스트 작성

--- cypress/integration/reacingcar.spec.js
+++ cypress/integration/reacingcar.spec.js
@@ -7,4 +7,17 @@ describe('자동차 경주 게임', () => {
     cy.get('#car-name-input').should('be.visible');
     cy.get('#car-name-submit').should('be.visible');
   });
+
+  it('자동차 이름으로 빈값을 입력하면 경고창이 출력된다.', () => {
+    const alertStub = cy.stub();
+
+    cy.on('window:alert', alertStub);
+
+    cy.get('#car-name-input').clear();
+    cy.get('#car-name-submit')
+      .click()
+      .then(() => {
+        expect(alertStub.getCall(0)).to.be.calledWith('자동차 이름을 입력해주세요.');
+      });
+  });
 });
