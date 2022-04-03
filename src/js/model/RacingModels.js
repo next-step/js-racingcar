3bcac8733a92a3d6848142df8fc0529a60530570
hdpyo <hudongpyo.dev@gmail.com>
2022-04-03 18:26:30 +0900

    refactor: RacingModel.js 리팩토링
    - 불필요한 주석, Getter, Setter 를 제거했다.

--- src/js/model/RacingModels.js
+++ src/js/model/RacingModels.js
@@ -1,28 +1,6 @@
 export default class RacingModel {
-  /* 
-    관리해야 할 데이터
-    - [] 자동차 이름
-    - [] 시도 횟수
-  */
-
   constructor() {
     this.carNames = [];
     this.tryCount = 0;
-  }
-
-  getCarNames() {
-    return this.carNames;
-  }
-
-  setCarNames(carName) {
-    this.carNames.push(carName);
-  }
-
-  getTryCount() {
-    return this.tryCount;
-  }
-
-  setTryCount(count) {
-    this.tryCount = count;
   }
 }
