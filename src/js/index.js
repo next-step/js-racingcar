eeb5348ecd660276b245c2840b0caf1caedf8fb9
wanho1108 <wanho1108@gmail.com>
2022-04-03 20:27:26 +0900

    feat: 자동차 이름이 영문, 한글, 쉼표가 아니면 경고창 출력 작업

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,14 @@ const $carNameSubmit = document.querySelector('#car-name-submit');
 $carNameSubmit.addEventListener('click', () => {
   const $carNameInput = document.querySelector('#car-name-input');
   const value = $carNameInput.value.trim();
+  const regexp = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣,]+$/g; // TODO: 변수 이름 변경
 
   if (!value) {
     alert('자동차 이름을 입력해주세요.');
+    return;
+  }
+
+  if (!regexp.test(value)) {
+    alert('자동차 이름은 영문, 한글, 쉼표만 입력할 수 있습니다');
   }
 });
