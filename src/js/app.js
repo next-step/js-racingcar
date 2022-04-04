3255b5326e94109ed861bdd999dd559d57ce3c72
dahye1013 <alrean@naver.com>
2022-04-04 11:20:40 +0900

    refactor: 불필요한 isGameRunning 상태값 제거

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,8 @@ import UserRacingInput from './components/UserRacingInput.js';
 import GameProcess from './components/GameProcess.js';
 
 const App = () => {
-  const state = {
-    isGameRunning: false,
-  };
-
   const startGame = ({ carNames, playTimes }) => {
-    state.isGameRunning = true;
-    state.isGameRunning && GameProcess({ carNames, playTimes });
+    GameProcess({ carNames, playTimes });
   };
 
   UserRacingInput({ startGame });
