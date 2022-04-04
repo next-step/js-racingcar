1a2a690c38883c44078d5c029416aa29dd6b0c30
taeyoungs <xoxodudwkd@gmail.com>
2022-04-04 23:31:09 +0900

    add: 프로젝트와 테스트 코드에서 사용되는 상수 추가

---
+++ src/js/utils/constants.js
@@ -0,0 +1,30 @@
+export const MESSAGE = {
+  INVALID_CAR_NAME:
+    '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.',
+  MIN_COUNT:
+    '입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상어이야 합니다.',
+  MAX_COUNT:
+    '입력한 레이싱 횟수가 너무 큽니다. 레이싱 횟수는 20이하여야 합니다.',
+  CONGRATS: '🎇🎇🎇🎇축하합니다!🎇🎇🎇🎇',
+};
+
+export const SELECTOR = {
+  CAR_NAME_FILEDSET: 'fieldset:nth-of-type(1)',
+  CAR_NAME_INPUT: 'fieldset:nth-of-type(1) input',
+  CAR_NAME_BUTTON: 'fieldset:nth-of-type(1) button',
+  TRY_COUNT_FILEDSET: 'fieldset:nth-of-type(2)',
+  TRY_COUNT_INPUT: 'fieldset:nth-of-type(2) input',
+  TRY_COUNT_BUTTON: 'fieldset:nth-of-type(2) button',
+};
+
+export const CLASS_NAME = {
+  RACING_BOARD: 'racing-board',
+  USER_FORM: 'user-form',
+  WINNER: 'winner',
+  RESET_BUTTON: 'reset-button',
+};
+
+export const NUM = {
+  MIN_VALUE: 0,
+  MAX_VALUE: 9,
+};
