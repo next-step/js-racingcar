4de931dda1c19ee2a1871e7873ff2473280e2b97
짱유경 <cozy60dev@gmail.com>
2022-04-05 17:43:59 +0900

    refactor: beforeEach를 before로 수정해 반복되는 작업을 수정

--- cypress/integration/racingcar.spec.js
+++ cypress/integration/racingcar.spec.js
@@ -1,7 +1,7 @@
 import { ERR_MSG } from '../../src/js/util/constatns';
 
 describe('레이싱카 앱 테스트', () => {
-	beforeEach(() => {
+	before(() => {
 		cy.visit('../index.html');
 	});
 
@@ -34,7 +34,7 @@ describe('레이싱카 앱 테스트', () => {
 		// it('자동차의 이름이 중복되면 경고창을 띄워준다.', () => {});
 
 		it('자동차의 이름 입력을 완료하면 필드가 비활성화 된다.', () => {
-			cy.get('#car-names-input').type('EAST, WEST, SOUTH, NORTH');
+			cy.get('#car-names-input').clear().type('EAST, WEST, SOUTH, NORTH');
 			cy.get('#car-names-submit').click();
 			cy.get('#car-names-input').should('be.disabled');
 			cy.get('#car-names-submit').should('be.disabled');
@@ -42,11 +42,7 @@ describe('레이싱카 앱 테스트', () => {
 	});
 
 	context('시도할 횟수 입력', () => {
-		const CAR_NAMES = 'EAST, WEST, SOUTH, NORTH';
-
 		it('전진 횟수를 입력하는 필드가 나타난다.', () => {
-			cy.get('#car-names-input').type(CAR_NAMES);
-			cy.get('#car-names-submit').click();
 			cy.get('#car-try-block').should('be.visible');
 		});
 
@@ -54,8 +50,6 @@ describe('레이싱카 앱 테스트', () => {
 			const alertStub = cy.stub();
 			cy.on('window:alert', alertStub);
 
-			cy.get('#car-names-input').type(CAR_NAMES);
-			cy.get('#car-names-submit').click();
 			cy.get('#car-try-input').clear();
 			cy.get('#car-try-block').should('be.visible');
 			cy.get('#car-try-submit')
@@ -66,8 +60,6 @@ describe('레이싱카 앱 테스트', () => {
 		});
 
 		it('전진 횟수 입력을 완료하면 필드가 비활성화 된다.', () => {
-			cy.get('#car-names-input').type(CAR_NAMES);
-			cy.get('#car-names-submit').click();
 			cy.get('#car-try-input').type('3');
 			cy.get('#car-try-submit').click();
 			cy.get('#car-try-input').should('be.disabled');
@@ -76,13 +68,7 @@ describe('레이싱카 앱 테스트', () => {
 	});
 
 	context('자동차 경주 시작', () => {
-		const CAR_NAMES = 'EAST, WEST, SOUTH, NORTH';
-
 		it('자동차 경주 블록이 나타난다.', () => {
-			cy.get('#car-names-input').type(CAR_NAMES);
-			cy.get('#car-names-submit').click();
-			cy.get('#car-try-input').type('3');
-			cy.get('#car-try-submit').click();
 			cy.get('#car-racing-block').should('be.visible');
 		});
 
