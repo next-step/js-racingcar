9cc192448cd2708e44c22cd4638c28056da52a96
정현수 <jung660317@naver.com>
2022-04-06 19:52:55 +0900

    chore: isAllCarNamesHaveUnderFiveLetter trim 적용

--- src/js/validators/carName.js
+++ src/js/validators/carName.js
@@ -5,7 +5,7 @@ const carNameValidator = {
     if (!carNames) throw new Error(ERROR_MESSAGE.CAR_NAMES_REQUIRED);
   },
   isAllCarNamesHaveUnderFiveLetter: carNames => {
-    const enteredCarNames = carNames.split(GAME.CAR_NAME_SPLITTER);
+    const enteredCarNames = carNames.split(GAME.CAR_NAME_SPLITTER).map(name => name.trim());
     const carNamesUnderFiveLetters = enteredCarNames.filter(
       carName => carName.length <= GAME.CAR_NAME_MAX_LIMIT_LENGTH,
     );
