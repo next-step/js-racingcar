3bff1f109986e269f1e62edd7b0897706c8a4604
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-06 09:26:24 +0900

    💬  dom selector 상수화

---
+++ src/const/selector.js
@@ -0,0 +1,8 @@
+export default {
+  CAR_NAME_FORM: 'form[name="carName"]',
+  PLAY_TIMES_FORM: 'form[name="playTimes"]',
+  CAR_LIST: '.js-car-list',
+  PLAY_RESULT: '.js-play-result',
+  FIELDSET: 'fieldset',
+  INPUT: 'input',
+};
