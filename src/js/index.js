2168c26ae46ff7008751363ac37e220bc900680d
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-06 10:40:55 +0900

    🚧  경주게임 앱 추가

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+import { $ } from '../utils/dom.js';
+import { createStore } from './state/index.js';
+import SELECTOR from '../const/selector.js';
+
+import CarNameForm from './views/CarNameForm.js';
+import Cars from './views/Cars.js';
+import PlayResult from './views/PlayResult.js';
+import PlayTimesForm from './views/PlayTimesForm.js';
+
+const App = () => {
+  const store = createStore({
+    carNames: [],
+    times: 0,
+    init: true,
+    randomNumbers: [],
+  });
+
+  CarNameForm($(SELECTOR.CAR_NAME_FORM), store);
+  PlayTimesForm($(SELECTOR.PLAY_TIMES_FORM), store);
+  Cars($(SELECTOR.CAR_LIST), store);
+  PlayResult($(SELECTOR.PLAY_RESULT), store);
+};
+
+App();
