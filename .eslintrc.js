1e72ead512a6ff6f5afdff2c59182ed2d971da41
hdpyo <hudongpyo.dev@gmail.com>
2022-04-06 16:48:16 +0900

    style: .eslintrc.js 포맷팅 변경
    - prettier 가 적용된 포맷으로 업데이트한다.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,10 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-    'prettier',
-  ],
+  extends: ['airbnb-base', 'prettier'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: {
-  },
+  rules: {},
 };
