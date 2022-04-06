8d45b4ba981ad0da2797f79f24c3ccf3c580c697
wanho1108 <($910418>
2022-04-06 16:43:04 +0900

    refactor: SELECTORS 변경사항 적용

--- cypress/integration/reacingcar.spec.js
+++ cypress/integration/reacingcar.spec.js
@@ -7,7 +7,7 @@ describe('자동차 경주 게임', () => {
 
   it('자동차 이름을 입력하는 입력창과 확인 버튼이 존재한다.', () => {
     cy.get(SELECTORS.CAR_NAME_INPUT).should('be.visible');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT).should('be.visible');
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON).should('be.visible');
   });
 
   it('자동차 이름으로 빈값을 입력하면 경고창이 출력된다.', () => {
@@ -16,7 +16,7 @@ describe('자동차 경주 게임', () => {
     cy.on('window:alert', alertStub);
 
     cy.get(SELECTORS.CAR_NAME_INPUT).clear();
-    cy.get(SELECTORS.CAR_NAME_SUBMIT)
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON)
       .click()
       .then(() => {
         expect(alertStub.getCall(0)).to.be.calledWith(MESSAGES.CAR_NAME_EMPTY);
@@ -29,7 +29,7 @@ describe('자동차 경주 게임', () => {
     cy.on('window:alert', alertStub);
 
     cy.get(SELECTORS.CAR_NAME_INPUT).type('EAST/WEST/SOUTH/NORTH');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT)
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON)
       .click()
       .then(() => {
         expect(alertStub.getCall(0)).to.be.calledWith(MESSAGES.CAR_NAME_NOT_MATCH_REGEXP);
@@ -42,7 +42,7 @@ describe('자동차 경주 게임', () => {
     cy.on('window:alert', alertStub);
 
     cy.get(SELECTORS.CAR_NAME_INPUT).type('EAST,WEST,SOUTH,JAVASCRIPT');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT)
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON)
       .click()
       .then(() => {
         expect(alertStub.getCall(0)).to.be.calledWith(MESSAGES.CAR_NAME_LENGTH_OVER);
@@ -51,16 +51,16 @@ describe('자동차 경주 게임', () => {
 
   it('자동차 이름을 입력하면 자동차 이름 입력창과 버튼이 비활성화 된다.', () => {
     cy.get(SELECTORS.CAR_NAME_INPUT).type('EAST,WEST,SOUTH,NORTH');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT).click();
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON).click();
     cy.get(SELECTORS.CAR_NAME_INPUT).should('be.disabled');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT).should('be.disabled');
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON).should('be.disabled');
   });
 
   it('자동차 이름을 입력하면 시도 횟수 입력창과 버튼이 노출된다.', () => {
     cy.get(SELECTORS.CAR_NAME_INPUT).type('EAST,WEST,SOUTH,NORTH');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT).click();
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON).click();
     cy.get(SELECTORS.RACE_LAP_INPUT).should('be.visible');
-    cy.get(SELECTORS.RACE_LAP_SUBMIT).should('be.visible');
+    cy.get(SELECTORS.RACE_LAP_SUBMIT_BUTTON).should('be.visible');
   });
 
   it('시도할 횟수를 빈값으로 입력하면 경고창이 출력된다.', () => {
@@ -69,21 +69,21 @@ describe('자동차 경주 게임', () => {
     cy.on('window:alert', alertStub);
 
     cy.get(SELECTORS.CAR_NAME_INPUT).type('EAST,WEST,SOUTH,NORTH');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT).click();
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON).click();
     cy.get(SELECTORS.RACE_LAP_INPUT).clear();
-    cy.get(SELECTORS.RACE_LAP_SUBMIT)
+    cy.get(SELECTORS.RACE_LAP_SUBMIT_BUTTON)
       .click()
       .then(() => {
         expect(alertStub.getCall(0)).to.be.calledWith(MESSAGES.RACE_LAP_EMPTY);
       });
   });
 
-  it('시도할 횟수를 입력하면 시도 횟수 입력창과 버튼이 비활성화 된다.', () => {
+  it('시도할 횟수를 제출하면 시도 횟수 입력창과 버튼이 비활성화 된다.', () => {
     cy.get(SELECTORS.CAR_NAME_INPUT).type('EAST,WEST,SOUTH,NORTH');
-    cy.get(SELECTORS.CAR_NAME_SUBMIT).click();
+    cy.get(SELECTORS.CAR_NAME_SUBMIT_BUTTON).click();
     cy.get(SELECTORS.RACE_LAP_INPUT).type('10');
-    cy.get(SELECTORS.RACE_LAP_SUBMIT).click();
+    cy.get(SELECTORS.RACE_LAP_SUBMIT_BUTTON).click();
     cy.get(SELECTORS.RACE_LAP_INPUT).should('be.disabled');
-    cy.get(SELECTORS.RACE_LAP_SUBMIT).should('be.disabled');
+    cy.get(SELECTORS.RACE_LAP_SUBMIT_BUTTON).should('be.disabled');
   });
 });
