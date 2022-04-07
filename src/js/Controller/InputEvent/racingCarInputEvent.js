669b0e5cf59f22d5cca0af1f68ad9f13011e9b80
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-04-07 20:52:30 +0900

    Style: 불필요한 부분들 정리

--- src/js/Controller/InputEvent/racingCarInputEvent.js
+++ src/js/Controller/InputEvent/racingCarInputEvent.js
@@ -1,45 +1,43 @@
-import Racing from "../../Model/Racing.js";
-import renderCountForm from "../../View/Render/renderCountForm.js";
-import convertDisabledAttr from "./commons/convertDisabledAttr.js";
+import Racing from '../../Model/Racing.js';
+import renderCountForm from '../../View/Render/renderCountForm.js';
+import convertDisabledAttr from './commons/convertDisabledAttr.js';
 
-import { VALIDATE } from "../../util/consts.js";
-import { racingCountEvent } from "./index.js";
+import { VALIDATE } from '../../util/consts.js';
+import { racingCountEvent } from './index.js';
 
+export const racingCarInputEvent = (function () {
+  const formHandler = (target) => {
+    const { value } = target.input;
 
-export const racingCarInputEvent = (function(){
-  // form handler가 하는일이 너무 많다 리팩토링 요망 (event를 여기서 설정하고 싶지 않은데.. 방법이 없을까..?)
+    if (Racing.validate.carNameLength(value))
+      return alert(VALIDATE.ALERT_WRONG_RACING_CAR_NAME);
 
-  const formHandler = (target) => {    
-    const { value } = target.input
-    
-    if (Racing.validate.carNameLength(value)) return alert(VALIDATE.ALERT_WRONG_RACING_CAR_NAME)
+    convertDisabledAttr(target);
 
-    convertDisabledAttr(target)
+    renderCountForm();
 
-    renderCountForm()
-
-    racingCountEvent(value)
-  }
+    racingCountEvent(value);
+  };
 
   return {
     racingCarNameClickEvent(event) {
       if (event.target) {
         const target = {
           input: event.target.previousElementSibling,
-          button: event.target
-        }
-        formHandler(target)
+          button: event.target,
+        };
+        formHandler(target);
       }
     },
-  
+
     racingCarNameKeyboardEvent(event) {
       if (event.target && event.key === 'Enter') {
         const target = {
           input: event.target,
-          button: event.target.nextElementSibling
-        }
-        formHandler(target)
+          button: event.target.nextElementSibling,
+        };
+        formHandler(target);
       }
     },
-  }
-})()
+  };
+})();
--- src/js/Controller/InputEvent/racingCountInputEvent.js
+++ src/js/Controller/InputEvent/racingCountInputEvent.js
@@ -1,45 +1,45 @@
-import Racing from "../../Model/Racing.js";
-import convertDisabledAttr from "./commons/convertDisabledAttr.js";
-import renderRacingArena from "../../View/Render/renderRacingArena.js";
-import renderRacingGame from "../../View/Render/renderRacing.js";
-import removeSpinner from "../../View/Render/removeSpinner.js";
+import Racing from '../../Model/Racing.js';
+import convertDisabledAttr from './commons/convertDisabledAttr.js';
+import renderRacingArena from '../../View/Render/renderRacingArena.js';
+import renderRacingGame from '../../View/Render/renderRacing.js';
+import removeSpinner from '../../View/Render/removeSpinner.js';
 
-import { VALIDATE } from "../../util/consts.js";
+import { VALIDATE } from '../../util/consts.js';
 
-export const racingCountInputEvent = (function(){
+export const racingCountInputEvent = (function () {
   const formHandler = (target, cars) => {
-    const { value } = target.input
-    
-    if (Racing.validate.racingCount(value)) return alert(VALIDATE.ALERT_LESS_RACING_COUNT)
+    const { value } = target.input;
 
-    convertDisabledAttr(target)
+    if (Racing.validate.racingCount(value))
+      return alert(VALIDATE.ALERT_LESS_RACING_COUNT);
+    convertDisabledAttr(target);
 
-    const racing = new Racing(cars.split(','))
-    renderRacingArena(cars)
-    renderRacingGame(racing, value)
-    
-    removeSpinner(value)
-  }
+    const racing = new Racing(cars.split(','));
+    renderRacingArena(cars);
+    renderRacingGame(racing, value);
+
+    removeSpinner(value);
+  };
 
   return {
     racingCountClickEvent(event, cars) {
       if (event.target) {
         const target = {
           input: event.target.previousElementSibling,
-          button: event.target
-        }
-        formHandler(target, cars)
+          button: event.target,
+        };
+        formHandler(target, cars);
       }
     },
-  
+
     racingCountKeyboardEvent(event, cars) {
       if (event.target && event.key === 'Enter') {
         const target = {
           input: event.target,
-          button: event.target.nextElementSibling
-        }
-        formHandler(target, cars)
+          button: event.target.nextElementSibling,
+        };
+        formHandler(target, cars);
       }
     },
-  }
-})()
+  };
+})();
