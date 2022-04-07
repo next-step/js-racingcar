3f453148e7fe97a3059de58dc792e7b7466ed0fa
tolluset <dlqud19@gmail.com>
2022-04-07 23:04:06 +0900

    chore: add alert message

--- src/racingcar/constatns/messages.js
+++ src/racingcar/constatns/messages.js
@@ -1,4 +1,5 @@
-const NOT_ALLOWED_NAME_LENGTH =
-  '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.';
+import { MIN_NAME_LENGTH, MAX_NAME_LENGTH } from '.';
+
+const NOT_ALLOWED_NAME_LENGTH = `유효하지 않은 이름 길이입니다. 자동차의 이름은 ${MIN_NAME_LENGTH}자이상, ${MAX_NAME_LENGTH}자 이하만 가능합니다.`;
 
 export { NOT_ALLOWED_NAME_LENGTH };
