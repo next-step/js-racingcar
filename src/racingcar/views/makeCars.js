77b70acff00b44324a6e4f518ddb97bce1e589de
tolluset <dlqud19@gmail.com>
2022-04-07 23:42:09 +0900

    feat: add get user try count input

--- src/racingcar/views/makeCars.js
+++ src/racingcar/views/makeCars.js
@@ -1,10 +1,14 @@
 import { $ } from '../../utils/dom';
-import { MIN_NAME_LENGTH, MAX_NAME_LENGTH } from '../constatns';
-import { NOT_ALLOWED_NAME_LENGTH } from '../constatns/messages';
+import { MIN_NAME_LENGTH, MAX_NAME_LENGTH, MIN_TRY_COUNT } from '../constatns';
+import { NOT_ALLOWED_NAME_LENGTH, NOT_ALLOWED_TRY_COUNT } from '../constatns/messages';
 
 const $makeCars = $('#racingcar-make-cars');
 const $tryCountSection = $('#racingcar-try-count-section');
 const $namingInput = $('#racingcar-naming-input');
+
+const $tryCountInput = $('#racingcar-try-count-input');
+
+const $playCars = $('#racingcar-play-cars');
 
 const makeCarsEventListener = () => {
   $makeCars.addEventListener('click', handleMakeCarsClick);
@@ -15,11 +19,19 @@ const handleMakeCarsClick = (e) => {
   if (e.target.id === 'racingcar-naming-button') {
     validatingName();
   }
+
+  if (e.target.id === 'racingcar-try-count-button') {
+    validateTryCount();
+  }
 };
 
 const handleMakeCarsKeyup = (e) => {
   if (e.target.id === 'racingcar-naming-input' && e.key === 'Enter') {
     validatingName();
+  }
+
+  if (e.target.id === 'racingcar-try-count-input' && e.key === 'Enter') {
+    validateTryCount();
   }
 };
 
@@ -36,6 +48,14 @@ const validateNameLength = (names) => {
   return names.every(
     (name) => MIN_NAME_LENGTH <= name.length && name.length <= MAX_NAME_LENGTH,
   );
+};
+
+const validateTryCount = () => {
+  const counts = $tryCountInput.value;
+
+  counts >= MIN_TRY_COUNT
+    ? $playCars.classList.remove('d-none')
+    : alert(NOT_ALLOWED_TRY_COUNT);
 };
 
 export { makeCarsEventListener };
