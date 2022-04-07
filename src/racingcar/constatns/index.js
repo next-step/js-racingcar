a5aa97a80023671d3ac53c575408f628a3cacbe0
tolluset <dlqud19@gmail.com>
2022-04-07 19:47:04 +0900

    feat: add constants

---
+++ src/racingcar/constatns/index.js
@@ -0,0 +1,4 @@
+const MIN_NAME_LENGTH = 1;
+const MAX_NAME_LENGTH = 5;
+
+export { MIN_NAME_LENGTH, MAX_NAME_LENGTH };
---
+++ src/racingcar/constatns/messages.js
@@ -0,0 +1,4 @@
+const NOT_ALLOWED_NAME_LENGTH =
+  '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.';
+
+export { NOT_ALLOWED_NAME_LENGTH };
