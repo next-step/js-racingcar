98a112eef8f4444405de985240aeed024883199b
Dami Kim <dami.k.dev@gmail.com>
2022-04-08 00:54:32 +0900

    feat: constant 정의

---
+++ src/js/constant/index.js
@@ -0,0 +1,11 @@
+export const CAR_NAME_MAX_LENGTH = 5;
+
+export const ERROR_MESSAGE = {
+  NAME_LENGTH: `자동차 이름은 ${CAR_NAME_MAX_LENGTH}자 이하만 가능합니다`,
+  NAME_EMPTY: "자동차 이름을 입력해주세요.",
+};
+
+export const SELECTOR = {
+  NAME_INPUT: "#input-name",
+  NAME_SUBMIT_BUTTON: "#name-submit-button",
+};
