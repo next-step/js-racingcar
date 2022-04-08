83ff49e5374d86676512ec247342cf53f595e546
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-08 22:45:34 +0900

    getValidCarName 함수 이동

--- src/js/views/CarNameForm.js
+++ src/js/views/CarNameForm.js
@@ -4,12 +4,31 @@ import { $, disabled } from '../../utils/dom.js';
 import { last, splitStringWithComma } from '../../utils/index.js';
 import { carNamePattern } from '../../verification/regex.js';
 
+const getCarNames = (carName) => splitStringWithComma(carName).filter(Boolean);
+
 const validateCarName = (carName) => {
   if (carName?.length > MAX_LENGTH_FOR_CAR_NAME)
     throw new Error(MESSAGE.PLZ_CHECK_MAX_LENGTH_FOR_CAR_NAME);
 };
 
-const getCarNames = (carName) => splitStringWithComma(carName).filter(Boolean);
+const getValidCarName = (target) => {
+  const carName = target.value.replace(carNamePattern, '');
+  const carNames = getCarNames(carName);
+  const lastName = last(carNames)?.trim();
+
+  try {
+    validateCarName(lastName);
+    return splitStringWithComma(carName).join(', ');
+  } catch (error) {
+    alert(error.message);
+
+    carNames.pop();
+    return carNames
+      .concat(lastName.substring(0, MAX_LENGTH_FOR_CAR_NAME))
+      .join(', ')
+      .trim();
+  }
+};
 
 const CarNameForm = ($el, store) => {
   const $fieldset = $(SELECTOR.FIELDSET, $el);
@@ -24,25 +43,6 @@ const CarNameForm = ($el, store) => {
     event.preventDefault();
     disabled($fieldset, true);
     mutateCarNames();
-  };
-
-  const getValidCarName = (target) => {
-    const carName = target.value.replace(carNamePattern, '');
-    const carNames = getCarNames(carName);
-    const lastName = last(carNames)?.trim();
-
-    try {
-      validateCarName(lastName);
-      return splitStringWithComma(carName).join(', ');
-    } catch (error) {
-      alert(error.message);
-
-      carNames.pop();
-      return carNames
-        .concat(lastName.substring(0, MAX_LENGTH_FOR_CAR_NAME))
-        .join(', ')
-        .trim();
-    }
   };
 
   const onKeyUpCarName = ({ target }) => {
