77a55f9897e7aa1833a2aeea89265ca970daaa61
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-08 22:49:38 +0900

    createStore 코드 정리

--- src/js/state/index.js
+++ src/js/state/index.js
@@ -3,20 +3,11 @@ export const createStore = (initialState) => {
     ...initialState,
   };
 
-  const getState = (key) => (key ? state[key] : state);
-
   const actionMap = Object.fromEntries(
     Object.keys(initialState).map((key) => [key, []])
   );
 
-  const subscribe = ({ key, actions }) => {
-    if (actions.length === 0) throw new Error('actions 를 정의하세요.');
-    if (!actionMap[key])
-      throw new Error('initState 에 정의된 키를 확인하세요.');
-
-    const registeredActions = actionMap[key];
-    actionMap[key] = [...registeredActions, ...actions];
-  };
+  const getState = (key) => (key ? state[key] : state);
 
   const setState = (newState) => {
     Object.entries(newState).forEach(([key, value]) => {
@@ -26,6 +17,15 @@ export const createStore = (initialState) => {
       };
       actionMap[key].forEach((action) => action(getState(key)));
     });
+  };
+
+  const subscribe = ({ key, actions }) => {
+    if (actions.length === 0) throw new Error('actions 를 정의하세요.');
+    if (!actionMap[key])
+      throw new Error('initState 에 정의된 키를 확인하세요.');
+
+    const registeredActions = actionMap[key];
+    actionMap[key] = [...registeredActions, ...actions];
   };
 
   return {
