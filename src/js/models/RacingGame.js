0b68796a2f3fdb1f4a8a416dc42e1f3b8921b973
Dami Kim <dami.k.dev@gmail.com>
2022-04-09 02:04:18 +0900

    feat: 전진 여부를 판단하여 Car 객체 생성하는 로직 구현

--- src/js/models/RacingGame.js
+++ src/js/models/RacingGame.js
@@ -1,9 +1,22 @@
-import RacingInputView from "../views/RacingInputView.js";
-import { splitCarName } from "../utils/textUtils.js";
-import { CAR_NAME_MAX_LENGTH, ERROR_MESSAGE } from "../constant/index.js";
+import Car from "../views/Car.js";
+import { MIN_NUMBER_FOR_MOVING } from "../constant/index.js";
 
 const RacingGame = {
-  start: function ({ carName }) {},
+  judgedCarsInfo: null,
+  start(carsInfo) {
+    this.judgedCarsInfo = this.judgeMovement(carsInfo);
+    const car = new Car(carsInfo);
+
+    car.render();
+  },
+  judgeMovement(carsInfo) {
+    return carsInfo.map(({ name, movements }) => {
+      const judgedMovements = movements.filter(
+        movement => movement >= MIN_NUMBER_FOR_MOVING
+      ).length;
+      return Object.freeze({ name, movement: judgedMovements });
+    });
+  },
 };
 
 export default RacingGame;
