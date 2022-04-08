3f0f7a0e2f143cfb460404f57dca5f5bb6b30760
Dami Kim <dami.k.dev@gmail.com>
2022-04-09 02:02:28 +0900

    feat: constant 추가 - MIN_NUMBER_FOR_MOVING -  CAR_CONTAINER

--- src/js/constant/index.js
+++ src/js/constant/index.js
@@ -1,4 +1,5 @@
 export const CAR_NAME_MAX_LENGTH = 5;
+export const MIN_NUMBER_FOR_MOVING = 4;
 
 export const ERROR_MESSAGE = {
   NAME_LENGTH: `자동차 이름은 ${CAR_NAME_MAX_LENGTH}자 이하만 가능합니다`,
@@ -10,4 +11,5 @@ export const SELECTOR = {
   TRY_INPUT: "#input-try",
   NAME_SUBMIT_BUTTON: "#name-submit-button",
   TRY_SUBMIT_BUTTON: "#try-submit-button",
+  CAR_CONTAINER: "#car-container",
 };
