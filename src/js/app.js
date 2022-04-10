27a221a9471def29874520e025333e9be9560cd9
dahye1013 <alrean@naver.com>
2022-04-10 16:34:33 +0900

    refactor: components 생성 app.js에서 일관처리

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,24 @@
+// components
 import UserRacingInput from './components/UserRacingInput.js';
+import GameProcess from './components/GameProcess.js';
+// models
+import GameProcessModel from './model/GameProcessModel.js';
+
 import State from './model/State.js';
 
-const App = () => {
+const App = async () => {
   const state = State.getInstance();
+
+  const startGame = () => {
+    GameProcess({
+      gameProcessState: new GameProcessModel(state.gameProcessState),
+    });
+  };
 
   UserRacingInput({
     setGames: state.setGames,
     updateGameConfiguration: state.updateGameConfiguration,
+    startGame,
   });
 };
 
