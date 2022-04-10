305d2a597227e34a773edf963a63dee66095f893
hamm9u <hamm.9u@gmail.com>
2022-04-10 15:35:54 +0900

    feat: 추가되는 HTML 엘리먼트 템플릿 상수

---
+++ src/js/constants/template.js
@@ -0,0 +1,8 @@
+export const htmlTemplate = {
+  attemptCountInput: `<p>시도할 횟수를 입력해주세요.</p>
+  <div class="d-flex">
+    <input type="number" autofocus id="attempt-count-input" class="w-100 mr-2" placeholder="시도 횟수" />
+    <button type="button" id="attempt-count-submit" class="btn btn-cyan">확인</button>
+  </div>`,
+  racingProgressView: `<div class="mt-4 d-flex racing-container"> </div>`,
+};
