c57e88892315e642553e572dc0969b591fb7ef83
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-04-10 17:11:42 +0900

    feat: constants 파일

---
+++ src/js/constants.js
@@ -0,0 +1,14 @@
+export const CAR_NAME_LENGTH_MAXIMUM = 5;
+
+export const RACING_MINIMUM_NUMBER = 1;
+
+export const FORWRAD = 4;
+export const RANDOM_MAX = 9;
+export const RANDOM_MIN = 0;
+
+export const INVALID_CAR_NAME = `자동차 이름을 입력해주세요.`;
+export const INVALID_CAR_MAXIMUM_NAME = `유효하지 않은 이름 길이입니다. 자동차의 이름은 ${CAR_NAME_LENGTH_MAXIMUM}자 이하만 가능합니다.`;
+
+export const INVALID_RACING_NUMBER = `레이싱 횟수를 입력해주세요.`;
+export const INVALID_RACING_NUMBER_TYPE = `레이싱 횟수는 숫자를 입력해야 합니다.`;
+export const INVALID_RACING_MINIMUM_NUMBER = `입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 ${RACING_MINIMUM_NUMBER}이상이어야 합니다.`;
