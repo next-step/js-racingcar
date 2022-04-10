ddc688ce1213a76398fa05897b1bc37af0341b06
Dami Kim <dami.k.dev@gmail.com>
2022-04-11 00:41:47 +0900

    style: class name 수정 (car-name)

--- src/js/views/RacingInitialView.js
+++ src/js/views/RacingInitialView.js
@@ -3,7 +3,7 @@ import { SELECTOR } from "../constant/index.js";
 const RacingInitialView = {
   makeInitTemplate(carName) {
     return `<div class="mr-2" id="car-${carName}">
-    <div class="car-player">${carName}</div>
+    <div class="car-name">${carName}</div>
     <div class="d-flex justify-center mt-3">
     <div class="relative spinner-container">
       <span class="material spinner"></span>
