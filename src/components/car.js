b149909d18972b404426a89067d06c22ab1bde9b
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2022-04-11 01:18:23 +0900

    refactor: Car 객체 생성자 함수 -> 클래스로 변경
    + 정적 메서드 구현을 위해 ES6 클래스의 static 메서드 사용

--- src/components/car.js
+++ src/components/car.js
@@ -1,20 +1,15 @@
 import { RANDOM_NUMBER, CAR_GO_ABLE_NUMBER } from '../constants/common.js';
 
-export default function Car({ $target, $spinner, $forward, initState }) {
-  this.$car = null;
-  this.$carPlayer = null;
-  this.state = initState;
+export default class Car {
+  constructor({ $target, $spinner, $forward, initState }) {
+    this.$target = $target;
+    this.$spinner = $spinner;
+    this.$forward = $forward;
+    this.state = initState;
+    this.init();
+  }
 
-  this.go = () => {
-    this.$carPlayer.after($forward.cloneNode(true));
-    this.state.goCount++;
-  };
-
-  this.finish = () => {
-    this.$car.removeChild(this.$car.lastChild);
-  };
-
-  this.initCar = () => {
+  init() {
     this.$car = document.createElement('div');
     this.$car.className = 'car mr-2';
     this.$car.dataset.cy = 'car';
@@ -22,13 +17,24 @@ export default function Car({ $target, $spinner, $forward, initState }) {
     this.$carPlayer.className = 'car-player';
     this.$carPlayer.textContent = this.state.carName;
     this.$car.appendChild(this.$carPlayer);
-    this.$car.appendChild($spinner.cloneNode(true));
-    $target.appendChild(this.$car);
-  };
+    this.$car.appendChild(this.$spinner.cloneNode(true));
+    this.$target.appendChild(this.$car);
+  }
 
-  this.initCar();
+  go() {
+    this.$carPlayer.after(this.$forward.cloneNode(true));
+    this.state.goCount++;
+  }
+
+  finish() {
+    this.$car.removeChild(this.$car.lastChild);
+  }
+
+  static getRandomNumber() {
+    return Math.floor(Math.random() * RANDOM_NUMBER.MAX) + RANDOM_NUMBER.MIN;
+  }
+
+  static checkAbleGo(randomNumber) {
+    return randomNumber >= CAR_GO_ABLE_NUMBER;
+  }
 }
-
-Car.getRandomNumber = () => Math.floor(Math.random() * RANDOM_NUMBER.MAX) + RANDOM_NUMBER.MIN;
-
-Car.checkAbleGo = (randomNumber) => randomNumber >= CAR_GO_ABLE_NUMBER;
