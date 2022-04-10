6aca76b938c134549578f966919bda900fdaf4b8
Dami Kim <dami.k.dev@gmail.com>
2022-04-11 00:10:33 +0900

    feat: hideSpinner 추가

--- src/js/views/CarView.js
+++ src/js/views/CarView.js
@@ -1,17 +1,18 @@
-import { SELECTOR, ERROR_MESSAGE } from "../constant/index.js";
 class CarView {
   $car;
+  name;
   constructor({ name }) {
     this.$car = document.querySelector(`#car-${name}`);
+    this.name = name;
   }
   renderArrow() {
-    //TODO: 그 자리에 있던 로딩을 없애고 넣는다.
-    this.renderSpinner();
     this.$car.lastElementChild.remove();
 
     /*html*/
     const $template = `<div class="forward-icon mt-2">⬇️️</div>`;
     this.$car.insertAdjacentHTML("beforeend", $template);
+
+    this.renderSpinner();
   }
 
   renderSpinner() {
@@ -21,7 +22,13 @@ class CarView {
       <span class="material spinner"></span>
     </div>
   </div>`;
+
     this.$car.insertAdjacentHTML("beforeend", $spinner);
+  }
+
+  hideSpinner() {
+    this.$car = document.querySelector(`#car-${this.name}`);
+    this.$car.lastElementChild.remove();
   }
 }
 
