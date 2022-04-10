27c2e09556e3706e3bda88fd53c967eebfbd483f
Dami Kim <dami.k.dev@gmail.com>
2022-04-11 02:00:00 +0900

    style: 불필요한 코드 삭제

--- src/js/views/CarView.js
+++ src/js/views/CarView.js
@@ -7,8 +7,7 @@ class CarView {
   }
 
   renderArrow() {
-    this.$car.lastElementChild.remove();
-
+    this.hideSpinner();
     /*html*/
     const $template = `<div class="forward-icon mt-2">⬇️️</div>`;
     this.$car.insertAdjacentHTML("beforeend", $template);
@@ -28,7 +27,6 @@ class CarView {
   }
 
   hideSpinner() {
-    this.$car = document.querySelector(`#car-${this.name}`);
     this.$car.lastElementChild.remove();
   }
 }
