e141ea902b90e3965989c4c84015b9b37daa47e1
Dami Kim <dami.k.dev@gmail.com>
2022-04-10 13:28:07 +0900

    feat: updateInterval 구현

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -7,3 +7,15 @@ export const generateNumberInRange = ({ min, max }) => {
   const number = Math.floor(Math.random() * (max + 1)) + min;
   return number;
 };
+
+export const updateInterval = ({ fn, interval, times }) => {
+  const timer = count => {
+    setTimeout(() => {
+      if (count >= times) return;
+
+      fn();
+      timer(count + 1);
+    }, interval);
+  };
+  timer(0);
+};
