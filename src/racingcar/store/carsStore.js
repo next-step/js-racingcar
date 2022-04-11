1ee4031389e0fd7cfcd8d6221c758d800fb0f7f0
tolluset <dlqud19@gmail.com>
2022-04-11 10:25:34 +0900

    refactor: modify order of methods in cars store

--- src/racingcar/store/carsStore.js
+++ src/racingcar/store/carsStore.js
@@ -2,24 +2,27 @@ import Car from '../models/Car';
 import { randomNumber } from '../../utils/randoms';
 
 const carsStore = {
-  car_names: [],
   cars: [],
+  car_names: [],
   tryCounts: 0,
   winners: [],
-  GET_CAR_NAMES() {
-    return this.car_names;
-  },
-  SET_CAR_NAMES(_car_names) {
-    return (this.car_names = _car_names);
-  },
   GET_CARS() {
     return this.cars;
   },
   SET_CARS(_car_names) {
     this.cars = _car_names.map((name) => new Car(name));
   },
-  MOVE_CARS() {
-    return this.GET_CARS().forEach((car) => car.move(randomNumber()));
+  GET_CAR_NAMES() {
+    return this.car_names;
+  },
+  SET_CAR_NAMES(_car_names) {
+    return (this.car_names = _car_names);
+  },
+  GET_TRY_COUNTS() {
+    return +this.tryCounts;
+  },
+  SET_TRY_COUNTS(_tryCounts) {
+    this.tryCounts = _tryCounts;
   },
   GET_WINNERS() {
     return this.winners;
@@ -29,11 +32,8 @@ const carsStore = {
       .GET_CARS()
       .filter((car) => car.moved === this.GET_TRY_COUNTS());
   },
-  GET_TRY_COUNTS() {
-    return +this.tryCounts;
-  },
-  SET_TRY_COUNTS(_tryCounts) {
-    this.tryCounts = _tryCounts;
+  MOVE_CARS() {
+    return this.GET_CARS().forEach((car) => car.move(randomNumber()));
   },
 };
 
