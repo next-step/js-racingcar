aa2291435ccfbfe25cfd4916ec69e23c3842a9bc
padawanR0k <padawanR0k@gmail.com>
2022-04-11 21:57:59 +0900

    ref: 명시적인 element 변수 네이밍

--- src/core/templates.mjs
+++ src/core/templates.mjs
@@ -16,20 +16,20 @@ export const forwardIcon = `<div class="forward-icon mt-2">⬇️️</div>`;
  * @param {{onResetButtonClick: Function}} options
  */
 export function TemplateRaceResult(winners, { onResetButtonClick }) {
-    const $el = document.createDocumentFragment();
-    const $div = document.createElement('div');
+    const fragment = document.createDocumentFragment();
+    const $raceResultContainer = document.createElement('div');
 
-    $div.innerHTML = `<div>
+    $raceResultContainer.innerHTML = `<div>
       <h2>🏆 최종 우승자: <span class="winners">${winners.join(', ')}</span> 🏆</h2>
       <div class="d-flex justify-center">
         <button type="button" class="btn btn-cyan">다시 시작하기</button>
       </div>
     </div>`;
-    $div.addEventListener('click', (e) => {
+    $raceResultContainer.addEventListener('click', (e) => {
         if (e.target.matches('button')) {
             onResetButtonClick();
         }
     });
-    $el.appendChild($div);
-    return $el;
+    fragment.appendChild($raceResultContainer);
+    return fragment;
 }
