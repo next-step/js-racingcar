c290b5e5e941a1a6d5767369a4c97ba40da95c73
padawanR0k <padawanR0k@gmail.com>
2022-04-11 22:10:15 +0900

    ref: 명령 결정 요소 상수화

--- src/core/racing.mjs
+++ src/core/racing.mjs
@@ -1,11 +1,11 @@
-import {MSG_ERROR_INVALID_NUMBER, MSG_ERROR_NO_NAMES} from "./constants.mjs";
+import {BOOSTER_GAGE, MSG_ERROR_INVALID_NUMBER, MSG_ERROR_NO_NAMES} from "./constants.mjs";
 import {isEmpty, validationTryCount} from "./validation.mjs";
 
 export const getRandomNumber = (min, max) =>
   Math.floor(Math.random() * (max + 1 - min) + min);
 
 export const [COMMAND_GO, COMMAND_STOP] = ["go", "stop"];
-export const getCommand = (number) => (number >= 4 ? COMMAND_GO : COMMAND_STOP);
+export const getCommand = (number) => (number >= BOOSTER_GAGE ? COMMAND_GO : COMMAND_STOP);
 
 export function getCarsNames(str) {
     if (isEmpty(str)) {
