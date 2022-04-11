5929807747e480de2da9baad3dac5ff06a5a9a83
tolluset <dlqud19@gmail.com>
2022-04-11 10:24:30 +0900

    refactor: use method in object literal

--- src/racingcar/store/carsStore.js
+++ src/racingcar/store/carsStore.js
@@ -6,18 +6,35 @@ const carsStore = {
   cars: [],
   tryCounts: 0,
   winners: [],
-  GET_CAR_NAMES: () => carsStore.car_names,
-  SET_CAR_NAMES: (_car_names) => (carsStore.car_names = _car_names),
-  GET_CARS: () => carsStore.cars,
-  SET_CARS: (_car_names) => (carsStore.cars = _car_names.map((name) => new Car(name))),
-  MOVE_CARS: () => carsStore.GET_CARS().forEach((car) => car.move(randomNumber())),
-  GET_WINNERS: () => carsStore.winners,
-  FIND_AND_SET_WINNERS: () =>
-    (carsStore.winners = carsStore
+  GET_CAR_NAMES() {
+    return this.car_names;
+  },
+  SET_CAR_NAMES(_car_names) {
+    return (this.car_names = _car_names);
+  },
+  GET_CARS() {
+    return this.cars;
+  },
+  SET_CARS(_car_names) {
+    this.cars = _car_names.map((name) => new Car(name));
+  },
+  MOVE_CARS() {
+    return this.GET_CARS().forEach((car) => car.move(randomNumber()));
+  },
+  GET_WINNERS() {
+    return this.winners;
+  },
+  FIND_AND_SET_WINNERS() {
+    this.winners = carsStore
       .GET_CARS()
-      .filter((car) => car.moved === carsStore.GET_TRY_COUNTS())),
-  GET_TRY_COUNTS: () => +carsStore.tryCounts,
-  SET_TRY_COUNTS: (_tryCounts) => (carsStore.tryCounts = _tryCounts),
+      .filter((car) => car.moved === this.GET_TRY_COUNTS());
+  },
+  GET_TRY_COUNTS() {
+    return +this.tryCounts;
+  },
+  SET_TRY_COUNTS(_tryCounts) {
+    this.tryCounts = _tryCounts;
+  },
 };
 
 export default carsStore;
