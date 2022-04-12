6ad1e1836bffe4d68b4d1014e4ab3d8eb096b160
짱유경 <cozy60dev@gmail.com>
2022-04-12 10:22:09 +0900

    refactor: RANDOM_NUM 변수를 사용할 때 구조 분해 할당을 사용하도록 수정

--- src/js/util/util.js
+++ src/js/util/util.js
@@ -17,7 +17,8 @@ export function isCheckCarNameLength(carNames) {
 }
 
 export function isMoveCar() {
-	const randomNum =
-		Math.floor(Math.random() * RANDOM_NUM.MAX_NUM) + RANDOM_NUM.MIN_NUM;
-	return randomNum >= RANDOM_NUM.FORWARD_NUM;
+	const { FORWARD_NUM, MIN_NUM, MAX_NUM } = RANDOM_NUM;
+	const randomNum = Math.floor(Math.random() * MAX_NUM) + MIN_NUM;
+
+	return randomNum >= FORWARD_NUM;
 }
