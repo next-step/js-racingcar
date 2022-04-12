b52154e361e230a9bd156e806d9a1a60f222b9f4
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2022-04-12 18:58:25 +0900

    refacor: gameProcess 컴포넌트를 수정했다.
    + Template 방식에서 Spinner, Forward Element 를 동적으로 생성하는 메서드로 변경
    + 자동차 경주 시작인 prcessRun 메서드를 Promise 로 변경하고 레이스 결과를 resolve 하도록 변경

--- src/components/gameProcess.js
+++ src/components/gameProcess.js
@@ -2,13 +2,11 @@ import { $ } from '../utils/dom.js';
 import { MIN_CARS_NUMBER, MIN_RACE_TIMES } from '../constants/validation.js';
 import { WAIT_TIME } from '../constants/common.js';
 import Car from './car.js';
-import Template from '../template.js';
 
 export default function GameProcess({ initState, handleRaceResult }) {
   this.$gameProcess = $('.game-process-container');
   this.$carsContainer = $('.cars-container');
   this.$cars = null;
-  this.template = new Template();
 
   this.state = initState;
   this.setState = (newState) => {
@@ -16,7 +14,7 @@ export default function GameProcess({ initState, handleRaceResult }) {
     this.render();
   };
 
-  this.render = () => {
+  this.render = async () => {
     const { cars, raceTimes, raceFinishedFlag } = this.state;
 
     if (raceFinishedFlag) return;
@@ -25,11 +23,32 @@ export default function GameProcess({ initState, handleRaceResult }) {
 
     if (this.isPossibleGameProcess(cars, raceTimes)) {
       this.processReady(cars);
-      this.processRun();
+      await this.processRun().then((newState) => handleRaceResult(newState));
     }
   };
 
   this.isPossibleGameProcess = (cars, raceTimes) => cars.length >= MIN_CARS_NUMBER && raceTimes >= MIN_RACE_TIMES;
+
+  this.getSpinnerElement = () => {
+    const $spinnerElement = document.createElement('div');
+    $spinnerElement.className = 'd-flex justify-center mt-3';
+    $spinnerElement.dataset.cy = 'spinner-icon';
+    $spinnerElement.innerHTML = String.raw`
+      <div class="relative spinner-container">
+        <span class="material spinner"></span>
+      </div>
+    `;
+
+    return $spinnerElement;
+  };
+
+  this.getForwardElement = () => {
+    const $forwardElement = document.createElement('div');
+    $forwardElement.className = 'forward-icon mt-2';
+    $forwardElement.textContent = '⬇️';
+
+    return $forwardElement;
+  };
 
   this.processReady = (cars) => {
     this.$carsContainer.innerHTML = '';
@@ -37,30 +56,31 @@ export default function GameProcess({ initState, handleRaceResult }) {
       (car) =>
         new Car({
           $target: this.$carsContainer,
-          $spinner: this.template.$spinnerElement,
-          $forward: this.template.$forwardElement,
+          $spinner: this.getSpinnerElement(),
+          $forward: this.getForwardElement(),
           initState: { id: car.id, carName: car.carName, goCount: car.goCount },
         })
     );
   };
 
-  this.processRun = () => {
-    let currentRaceTimes = MIN_RACE_TIMES;
-    const intervalId = setInterval(() => {
-      this.$cars.forEach(($car) => {
-        if (Car.checkAbleGo(Car.getRandomNumber())) $car.go();
-      });
-
-      if (currentRaceTimes++ >= this.state.raceTimes) {
+  this.processRun = () =>
+    new Promise((resolve) => {
+      let currentRaceTimes = MIN_RACE_TIMES;
+      const intervalId = setInterval(() => {
         this.$cars.forEach(($car) => {
-          $car.finish();
+          if (Car.checkAbleGo(Car.getRandomNumber())) $car.go();
         });
-        clearInterval(intervalId);
-        handleRaceResult({
-          cars: this.$cars.map(($car) => $car.state),
-          raceFinishedFlag: !this.state.raceFinishedFlag,
-        });
-      }
-    }, WAIT_TIME);
-  };
+
+        if (currentRaceTimes++ >= this.state.raceTimes) {
+          this.$cars.forEach(($car) => {
+            $car.finish();
+          });
+          resolve({
+            cars: this.$cars.map(($car) => $car.state),
+            raceFinishedFlag: !this.state.raceFinishedFlag,
+          });
+          clearInterval(intervalId);
+        }
+      }, WAIT_TIME);
+    });
 }
