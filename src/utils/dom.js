db90217cdfebcb6ae4a085705258c20bce269a58
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-13 07:29:08 +0900

    ♻️  setTimeout requestAnimationFrame 으로 변경

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,14 +1,9 @@
 export const $ = (selector, container = document) =>
   container.querySelector(selector);
 
-export const hide = ($el, hide) => $el.classList.toggle('hide', !hide);
+export const hide = ($el, hide) => $el.classList.toggle('hide', hide);
 
-/**
- *
- * Q: window.setTimeout 을 하지 않으면 focus 가 발생하지 않아서 이렇게 처리하였는데
- * 혹시 다른 방법이 있을까요??
- */
-export const focus = ($el) => window.setTimeout(() => $el.focus(), 0);
+export const focus = ($el) => requestAnimationFrame(() => $el.focus());
 
 export const disabled = ($el, use) =>
   use ? $el.setAttribute('disabled', true) : $el.removeAttribute('disabled');
