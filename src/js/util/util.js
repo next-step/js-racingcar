a823637c3e4d0e1e614be574fff94647023fab46
짱유경 <cozy60dev@gmail.com>
2022-04-12 10:18:25 +0900

    feat: 인자를 받는 함수의 경우 인자의 타입을 체크하는 기능 추가

--- src/js/util/util.js
+++ src/js/util/util.js
@@ -1,10 +1,18 @@
 import { MAX_CAR_NAME_LENGTH, RANDOM_NUM } from './constants.js';
 
 export function toNameArray(str) {
+	if (typeof str !== 'string') {
+		throw new Error('인자의 타입이 문자열이 아닙니다.');
+	}
+
 	return str.split(',').map((item) => item.trim());
 }
 
 export function isCheckCarNameLength(carNames) {
+	if (!Array.isArray(carNames)) {
+		throw new Error('인자의 타입이 배열이 아닙니다.');
+	}
+
 	return carNames.every((item) => item.length <= MAX_CAR_NAME_LENGTH);
 }
 
