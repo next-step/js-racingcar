39307d8f26535de86d77db4be778f2ada0e80dbd
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-13 07:31:12 +0900

    🔥  불필요한 코드 삭제

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,7 @@ export const last = (arr) => arr.at(-1);
 export const range = (limit) => Array.from({ length: limit }, (_, i) => i + 1);
 
 export const splitStringWithComma = (value) =>
-  value?.split(',').map((v) => v?.trim());
+  value?.split(',').map((v) => v.trim());
 
 export const generateRandomNumberUnder = (max = 10) =>
   Math.floor(Math.random() * max);
