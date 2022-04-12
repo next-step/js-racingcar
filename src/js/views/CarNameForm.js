ce723b947faf499f174a80a6bcc70ffd1df1ffe2
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-13 01:44:11 +0900

    🔥  onKeyUp 이벤트 삭제

--- src/js/views/CarNameForm.js
+++ src/js/views/CarNameForm.js
@@ -1,67 +1,43 @@
 import { MESSAGE, MAX_LENGTH_FOR_CAR_NAME } from '../../const/index.js';
 import SELECTOR from '../../const/selector.js';
 import { $, disabled } from '../../utils/dom.js';
-import { last, splitStringWithComma } from '../../utils/index.js';
-import { carNamePattern } from '../../verification/regex.js';
+import { splitStringWithComma } from '../../utils/index.js';
 
 const getCarNames = (carName) => splitStringWithComma(carName).filter(Boolean);
 
-const checkCarName = (carName) => {
-  if (!carName?.length) throw new Error(MESSAGE.PLZ_INSERT_CAR_NAME);
-};
+const validateCarName = (carNames) => {
+  if (!carNames.length === 0) throw new Error(MESSAGE.PLZ_INSERT_CAR_NAME);
 
-const validateCarName = (carName) => {
-  if (carName?.length > MAX_LENGTH_FOR_CAR_NAME)
+  if (carNames.some((carName) => carName.length > MAX_LENGTH_FOR_CAR_NAME))
     throw new Error(MESSAGE.PLZ_CHECK_MAX_LENGTH_FOR_CAR_NAME);
-};
 
-const getValidCarName = (target) => {
-  const carName = target.value.replace(carNamePattern, '');
-  const carNames = getCarNames(carName);
-  const lastName = last(carNames)?.trim();
-
-  try {
-    validateCarName(lastName);
-    return splitStringWithComma(carName).join(', ');
-  } catch (error) {
-    alert(error.message);
-
-    carNames.pop();
-    return carNames
-      .concat(lastName.substring(0, MAX_LENGTH_FOR_CAR_NAME))
-      .join(', ')
-      .trim();
-  }
+  return true;
 };
 
 const CarNameForm = ($el, store) => {
   const $fieldset = $(SELECTOR.FIELDSET, $el);
   const $carName = $(SELECTOR.INPUT, $el);
 
-  const mutateCarNames = () =>
+  const mutateCarNames = (carNames) =>
     store.setState({
-      carNames: getCarNames($carName.value),
+      carNames,
     });
 
   const onSubmitCarName = (event) => {
     event.preventDefault();
+    const carNames = getCarNames($carName.value.trim());
     try {
-      checkCarName($carName.value.trim());
-      mutateCarNames();
+      validateCarName(carNames);
+      mutateCarNames(carNames);
       disabled($fieldset, true);
     } catch (error) {
       alert(error.message);
-      $carName.focus();
+      focus($carName);
     }
-  };
-
-  const onKeyUpCarName = ({ target }) => {
-    target.value = getValidCarName(target);
   };
 
   const init = () => {
     $el.addEventListener('submit', onSubmitCarName);
-    $el.addEventListener('keyup', onKeyUpCarName);
 
     store.subscribe({
       key: 'init',
