c9f43da00282b1a9661f3aa21ac0cd23fbefae52
hamm9u <hamm.9u@gmail.com>
2022-04-17 01:56:35 +0900

    modify: 누락된 코드 추가

--- src/js/controller/racingProgressController.js
+++ src/js/controller/racingProgressController.js
@@ -6,6 +6,7 @@ export const progressRacing = (racingCarList) => {
   let count = 1;
   const intervalId = setInterval(() => {
     const isFinishedRacing = count++ === Number(racingCarList.attemptCount) - 1;
+    startRacingGame(racingCarList);
     if (isFinishedRacing) clearInterval(intervalId);
   }, 1000);
 };
