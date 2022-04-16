3f33bcb78761bd1190c5637df1cff3e44af88e88
hamm9u <hamm.9u@gmail.com>
2022-04-17 01:50:27 +0900

    modify: 기능 명시를 위한 함수 생성

--- src/js/view/racingInputView.js
+++ src/js/view/racingInputView.js
@@ -1,8 +1,12 @@
-import { htmlTemplate } from '../constants/template.js';
+import TEMPLATE from '../constants/template.js';
 import { $ } from '../utils/selector.js';
 
 export const renderAttemptCountInput = () => {
-  $('#racing-attempt-fieldset').innerHTML = htmlTemplate.attemptCountInput;
+  $('#racing-attempt-fieldset').innerHTML = TEMPLATE.ATTEMPT_COUNT_INPUT;
+  disableInputOfCarName();
+};
+
+const disableInputOfCarName = () => {
   $('#car-name-input').disabled = true;
   $('#car-name-submit').disabled = true;
 };
