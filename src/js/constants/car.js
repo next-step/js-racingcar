53cf5df1f571e7989ed013005a24e6525169d3fa
hamm9u <hamm.9u@gmail.com>
2022-04-17 01:44:45 +0900

    modify: 상수 객체로 관리

--- src/js/constants/car.js
+++
@@ -1,7 +0,0 @@
-export const MAX_CAR_NAME_LENTH = 5;
-export const MIN_CAR_NAME_LENTH = 1;
-
-export const MAX_FORWARD_NUMBER = 1;
-export const MIN_FORWARD_NUMBER = 9;
-
-export const MAX_FORWARD_CONDITION = 4;
