488437a62cc7e5a356b531d264182d1a6a4dc8f1
circlegiven <circlegiven@gmail.com>
2022-04-18 09:43:35 +0900

    [Fix] RacingCycle 클래스 static 제거 후 singleton으로 변경

--- src/js/RacingCycle.js
+++ src/js/RacingCycle.js
@@ -1,32 +1,34 @@
 const MIN_CYCLE = 1;
 const MAX_CYCLE = 100;
 
-class RacingCycle {
-  static #isEmptyCycle(cycle) {
+class IRacingCycle {
+  #isEmptyCycle(cycle) {
     return cycle === undefined || cycle === null || !cycle;
   }
 
-  static #isDisableRangeCycle(cycle) {
+  #isDisableRangeCycle(cycle) {
     return !(cycle >= MIN_CYCLE && cycle <= MAX_CYCLE);
   }
 
-  static #validateEmptyCycle(cycle) {
-    if (RacingCycle.#isEmptyCycle(cycle)) {
+  #validateEmptyCycle(cycle) {
+    if (this.#isEmptyCycle(cycle)) {
       throw new Error('횟수를 입력하세요.');
     }
   }
 
-  static #validateEnableRangeCycle(cycle) {
-    if (RacingCycle.#isDisableRangeCycle(cycle)) {
+  #validateEnableRangeCycle(cycle) {
+    if (this.#isDisableRangeCycle(cycle)) {
       throw new Error(
         `횟수를 ${MIN_CYCLE} ~ ${MAX_CYCLE} 사이 값을 입력하세요.`
       );
     }
   }
 
-  static validate(cycle) {
-    RacingCycle.#validateEmptyCycle(cycle);
-    RacingCycle.#validateEnableRangeCycle(cycle);
+  validate(cycle) {
+    this.#validateEmptyCycle(cycle);
+    this.#validateEnableRangeCycle(cycle);
   }
 }
+const RacingCycle = new IRacingCycle();
+Object.freeze(RacingCycle);
 export default RacingCycle;
