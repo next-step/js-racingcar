4c51dad9091f367f61defec1061de4a74b48c7c2
circlegiven <circlegiven@gmail.com>
2022-04-18 09:42:38 +0900

    [Fix] AbstractView 클래스 static 제거

--- src/js/view/AbstractView.js
+++ src/js/view/AbstractView.js
@@ -1,11 +1,11 @@
 export default class AbstractView {
-  static eventBindings() {
+  eventBindings() {
     throw new Error(
       'AbstractView는 추상 클래스입니다. 별도의 구현이 필요합니다.'
     );
   }
 
-  static initialize() {
+  initialize() {
     throw new Error(
       'AbstractView는 추상 클래스입니다. 별도의 구현이 필요합니다.'
     );
