9145416903bf44e23682218c9a676516751f32ca
jlee0505 <hj960322@gmail.com>
2022-07-22 01:57:40 +0900

    test: 랜더링 테스트

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+// html divided into 4 sections
+export const htmlNames = {
+  CAR_NAME_FILEDSET: "#car-name",
+  TRIAL_NUM_FILEDSET: "#trial-num",
+  GAME_SECTION: "#game",
+  RESULT_SECTION: "#result",
+};
