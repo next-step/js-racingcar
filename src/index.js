6858b9a158facb2bfa40c0546c39ab960559b1f9
jlee0505 <hj960322@gmail.com>
2022-07-24 00:12:23 +0900

    feat: 자동차 이름 입력시 이름의 길이가 유효하지 않은지 검사하는 기능을 추가

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import { SELECTORS } from "./constants.js";
+import { SELECTORS, ERROR_MESSAGES } from "./constants.js";
 
+const carNameForm = document.querySelector(SELECTORS.CAR_NAME_FORM);
 const trialNum = document.querySelector(SELECTORS.TRIAL_NUM_FIELDSET);
-const button = document.querySelector("button");
-const handleClick = (e) => {
-  trialNum.classList.remove("hidden");
+const gameSection = document.querySelector(SELECTORS.GAME_SECTION);
+
+const validateNameLength = (name) => {
+  if (name.length > 5) {
+    throw ERROR_MESSAGES.WORD_LENGTH_ERROR;
+  }
 };
-button.addEventListener("click", handleClick);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e);
+  console.log(e.target[1].value);
+
+  // 각 자동차 이름이 5글자가 넘으면 경고 메세지를 표시한다.
+  if (e.target[1].value) {
+    e.target[1].value.split(",").map((name) => {
+      try {
+        validateNameLength(name);
+        trialNum.classList.remove("hidden");
+        gameSection.classList.remove("hidden");
+      } catch (error) {
+        alert(error);
+      }
+    });
+  }
+};
+
+carNameForm.addEventListener("submit", handleSubmit);
