d887b4e7956610c83a7318b92c65ceaa2573e183
jlee0505 <hj960322@gmail.com>
2022-07-23 12:51:38 +0900

    test: 테스트 케이스 추가 2-2(자동차 이름을 submit 하면 시도 횟수 입력창이 생성된다.)

--- cypress/e2e/racing.cy.js
+++ cypress/e2e/racing.cy.js
@@ -1,13 +1,15 @@
-import { htmlNames } from "/src/constants.js";
+import { SELECTORS, ERROR_MESSAGES } from "/src/constants.js";
 
 const shouldShowAlert = (err, typeVal) => {
   cy.on("window:alert", (alertMessage) => {
     expect(alertMessage).to.eq(err);
   });
-  if (typeVal) cy.get(htmlNames.CAR_NAME_INPUT).type(typeVal);
-  cy.get(htmlNames.CAR_NAME_FORM).submit();
+  submitForm(SELECTORS.CAR_NAME_INPUT, typeVal);
 };
-
+const submitForm = (selector, typeVal) => {
+  if (typeVal) cy.get(selector).type(typeVal);
+  cy.get(SELECTORS.CAR_NAME_FORM).submit();
+};
 describe("test racingcar", () => {
   before(() => {
     cy.visit("http://127.0.0.1:5501/index.html");
@@ -15,25 +17,21 @@ describe("test racingcar", () => {
 
   context("1.첫 시작은 자동차 입력 모델만 있다", () => {
     it("처음 시작했을 때 자동차 이름 입력 외에 다른 창은 뜨지 않는다", () => {
-      cy.get(htmlNames.CAR_NAME_FILEDSET).should("not.have.class", "hidden");
+      cy.get(SELECTORS.CAR_NAME_FILEDSET).should("not.have.class", "hidden");
     });
   });
 
   context("2. 인풋을 통해 자동차에 이름을 부여할 수 있다.", () => {
     it("input의 길이가 0 이하이거나 6자 이상일 때, alert 이 뜬다.", () => {
       // input 에 "" 입력 (if)
-      shouldShowAlert(
-        "유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.",
-        "123456"
-      );
-      shouldShowAlert(
-        "유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.",
-        ""
-      );
+      shouldShowAlert(ERROR_MESSAGES.WORD_LENGTH_ERROR, "123456");
+      shouldShowAlert(ERROR_MESSAGES.WORD_LENGTH_ERROR, "");
     });
-    it("전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.", () => {
-      cy.get(htmlNames.CAR_NAME_FORM).submit();
+    it("자동차 이름을 submit 하면 시도 횟수 입력창이 생성된다.", () => {
+      submitForm(SELECTORS.CAR_NAME_INPUT, "1, 2, 3");
+      cy.get(SELECTORS.TRIAL_NUM_FILEDSET).should("not.have.class", "hidden");
     });
+    it("전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.", () => {});
     it("자동차 이름은 쉼표(,)를 기준으로 구분하여서 자동차 이름을 출력한다.", () => {});
   });
   // context("사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다."), () => {}
