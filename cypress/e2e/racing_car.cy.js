01bbcd377f7034a6d7c3222694a14136a2c74dbe
sujin lee <jmakeaz@gmail.com>
2022-07-23 20:18:56 +0900

    Add: 자동차 이름 글자수 확인

--- cypress/e2e/racing_car.cy.js
+++ cypress/e2e/racing_car.cy.js
@@ -5,6 +5,25 @@ beforeEach(() => {
 describe('자동차 경주 게임 초기화', () => {
   it('자동차 이름을 입력할 수 있는 필드만 보인다.', () => {
     cy.get('#form-car-name').should('be.visible');
-    cy.get('#form-try-count, #car-racing, #result-winner').should('not.be.visible');
-  })
-})
+    cy.get('#form-car-name input').should('not.have.value');
+    cy.get('#form-try-count, #car-racing, #result-winner').should(
+      'not.be.visible'
+    );
+  });
+});
+
+describe('자동차 이름 입력', () => {
+  it('자동차 이름이 5자를 초과하면 에러메시지를 확인한다.', () => {
+    const stub = cy.stub();
+    cy.on('window:alert', stub);
+
+    cy.get('#form-car-name input').type('sujin2');
+    cy.get('#form-car-name button')
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.'
+        );
+      });
+  });
+});
---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import { isValidNumberOfCharacters } from './validation/index.js';
+
+const $formCarName = document.querySelector('#form-car-name');
+
+const handleSubmitCarName = (event) => {
+  event.preventDefault();
+
+  const carNameValue = new FormData(event.target).get('car-name');
+  const carNames = carNameValue.split(',').map((text) => text.trim());
+
+  if (!isValidNumberOfCharacters(carNames)) {
+    alert(
+      '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.'
+    );
+  }
+};
+
+$formCarName.addEventListener('submit', handleSubmitCarName);
