b8885d202bc0f242195b58be35332e47fce036e9
ag502 <ag502@naver.com>
2022-07-24 15:27:44 +0900

    add: move count input 입력시 model 로직 추가

--- src/js/model/racingInfoModel.js
+++ src/js/model/racingInfoModel.js
@@ -1,10 +1,21 @@
 class RacingInfoModel {
   constructor() {
     this.entires = [];
+    this.moveCount = 0;
   }
 
   setEntries(entries) {
-    this.entires = [...entries];
+    this.entires = [];
+    entries.forEach((entry) => {
+      if (entry.length === 0) {
+        throw new Error("자동차의 이름이 유효하지 않습니다");
+      }
+      this.entires.push(entries);
+    });
+  }
+
+  setMoveCount(count) {
+    this.moveCount = count;
   }
 }
 
