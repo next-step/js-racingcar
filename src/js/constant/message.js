d9dc2b829ea9580649cf8a6f4b3bff524c38a6da
junzero741 <junzero741@gmail.com>
2022-07-25 08:17:19 +0900

    [FEAT] : 자동차 이름 부여를 위한 상수 선택자, 메시지 작성

---
+++ src/js/constant/message.js
@@ -0,0 +1,3 @@
+export const errorMessage = {
+   INVALID_CAR_NAME : "유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다."
+}
---
+++ src/js/constant/selector.js
@@ -0,0 +1,11 @@
+export const inputSelctor = {
+    INPUT_CAR_NAME : '#input-car-name'
+}
+
+export const buttonSelector = {
+    SUBMIT_CAR_NAME : '#submit-car-name'
+}
+
+export const spanSelector = {
+    CAR_NAME : '.car-player'
+}
