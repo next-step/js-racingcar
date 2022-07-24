de14479e8ca75a8cbe74e8fbc9778c73d0baa741
ag502 <ag502@naver.com>
2022-07-24 17:33:27 +0900

    add: 랜덤 숫자 범위 및 이동 조건 상수 추가

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -1,4 +1,7 @@
 export const MAX_NUM_OF_ENTRIES = 5;
+export const RANDOM_NUM_MIN_RANGE = 0;
+export const RANDOM_NUM_MAX_RANGE = 10;
+export const STANDARD_MOVE_VALUE = 3;
 
 export const notifyTypes = {
   ENTRY_CONFIRM: "ENTRY_CONFIRM",
