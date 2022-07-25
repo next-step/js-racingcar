3472d77df0846f5740109552d15ed5b394748a35
Leebonggu <leebonggu12@hanmail.net>
2022-07-26 00:37:15 +0900

    feat: 레이싱 계산 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import { makeElementDisabled } from './lib/index.js';
+import { Racing, RacingInformation } from './store.js';
 import { minTrialValidator, participantValidator } from './lib/index.js';
 
 const $racingParticipantForm = document.querySelector('#racing-participant-form');
@@ -8,34 +9,55 @@ const $racingTrialForm = document.querySelector('#racing-trial-form');
 const $racingTrialInput = document.querySelector('#racing-trial-input');
 const $racingTrailSubmitButton = document.querySelector('#racing-trial-submit-button');
 
+const startRacing = (racingInformation) => {
+	const { participants, trial } = racingInformation;
+
+	const participantWithCar = participants.map((participant) => new Racing(participant));
+
+	Array.from({ length: trial }).forEach(() => {
+		participantWithCar.forEach((participant) => {
+			participant.racing();
+		});
+	});
+
+	return participantWithCar;
+};
+
 const submitParticipants = (e) => {
 	e.preventDefault();
 	const participantsInputValue = e.target.elements.participant.value;
 	const participants = participantsInputValue.split(',');
 
-	const [{ isValid, msg }] = participants.map((participant) => participantValidator(participant));
-
-	if (!isValid) {
-		window.alert(msg);
-		return;
+	for (let i = 0; i < participants.length; i += 1) {
+		const { isValid, msg } = participantValidator(participants[i]);
+		if (!isValid) {
+			window.alert(msg);
+			return;
+		}
 	}
 
 	makeElementDisabled($racingParticipantInput, $racingParticipantSubmitButton);
 	$racingTrialForm.classList.remove('d-none');
+
+	RacingInformation.participants = [...participants];
 };
 
 const submitTrial = (e) => {
 	e.preventDefault();
 
-	const trialInputValid = e.target.elements.trial.value;
-	const { isValid, msg } = minTrialValidator(trialInputValid);
+	const trialInputValue = e.target.elements.trial.value;
+	const { isValid, msg } = minTrialValidator(trialInputValue);
 
 	if (!isValid) {
 		window.alert(msg);
 		return;
 	}
 
+	RacingInformation.trial = trialInputValue;
 	makeElementDisabled($racingTrialInput, $racingTrailSubmitButton);
+
+	const result = startRacing(RacingInformation);
+	RacingInformation.result = [...result];
 };
 
 $racingParticipantForm.addEventListener('submit', submitParticipants);
--- src/js/lib/index.js
+++ src/js/lib/index.js
@@ -1,7 +1,5 @@
 import { minTrialValidator, participantValidator } from './validator.js';
 
-export const generateRandomNumber = () => Math.floor(Math.random() * 10);
-
 export const makeElementDisabled = (...elements) => {
 	elements.forEach((element) => (element.disabled = true));
 };
