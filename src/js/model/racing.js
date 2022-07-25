5fa62869ead14ff425ac019ffed957a4ecdad2b5
junzero741 <junzero741@gmail.com>
2022-07-26 04:09:53 +0900

    [FEAT] : 레이스 재귀함수 작성

--- src/js/model/racing.js
+++ src/js/model/racing.js
@@ -1,10 +1,18 @@
 import { racingGameStore } from "./index.js"
 
-export const initRacing = function () {
-  const {getCars, getCarTry} = racingGameStore
-  console.log('init racing')
-  let tryCount = getCarTry();
+export const race = function () {
+  const RACE_END_COUNT = 0;
+  const {getCars, getCarTry, setCarTry} = racingGameStore
+  const tryCount = getCarTry();
   const cars = getCars();
-  
-  console.log(cars, tryCount)
+
+  if(tryCount > RACE_END_COUNT) {
+    cars.forEach((car) => {
+      car.go()
+    })
+    setCarTry(tryCount-1)
+    race();
+  } else {
+    return;
+  }
 }
