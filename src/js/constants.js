b10f3d395b7bb6ae329faea67fd68a85e92291a5
wondonghwi <ehdehd1289@naver.com>
2022-07-29 22:22:57 +0900

    ✨ 사용되는조건, alert문 상수로 작성

---
+++ src/js/constants.js
@@ -0,0 +1,13 @@
+const DELAY = 1000;
+
+const ALERT_MESSAGE = {
+  NOT_VALIDATE_NAME: '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.',
+  NO_VALUE_ENTERED: '입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다.',
+};
+
+const LENGTH = {
+  MIN_LENGTH: 0,
+  MAX_LENGTH: 5,
+};
+
+export { ALERT_MESSAGE, LENGTH, DELAY };
