6738d94540c943d47fe1e0a0f219db1ce18ef788
sujin lee <jmakeaz@gmail.com>
2022-07-30 16:30:31 +0900

    Modify: 상수 Uppercase로 변경

--- src/js/util/index.js
+++ src/js/util/index.js
@@ -2,6 +2,6 @@ export const getRandomIntInclusive = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 export const getIsForward = (num) => {
-  const forwardStandard = 3;
-  return num > forwardStandard;
+  const FORWARD_STANDARD = 3;
+  return num > FORWARD_STANDARD;
 };
