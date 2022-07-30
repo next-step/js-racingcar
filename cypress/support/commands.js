3632e469664d12f6761b571d43fb85417f43c5cc
wondonghwi <ehdehd1289@naver.com>
2022-07-30 17:20:01 +0900

    ✅ 레이싱카 E2E 테스트코드 작성

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,43 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('getCarNameInput', () => {
+  cy.get('input[name="car-name-input"]');
+});
+Cypress.Commands.add('getCarNameFieldset', () => {
+  cy.get('#car-name');
+});
+Cypress.Commands.add('getCarTryNumberSection', () => {
+  cy.get('#car-try-section');
+});
+Cypress.Commands.add('getCarTryNumberFieldset', () => {
+  cy.get('#car-try-number');
+});
+Cypress.Commands.add('getCarTryNumberInput', () => {
+  cy.get('input[name="car-try-number-input"]');
+});
+Cypress.Commands.add('getCarPlayer', () => {
+  cy.get('.car-player');
+});
+Cypress.Commands.add('getGameSeciton', () => {
+  cy.get('#game');
+});
+
+Cypress.Commands.add('setEmptyStirngToInput', (name) => {
+  cy.getCarNameInput()
+    .clear()
+    .then((e) => {
+      if (name !== '') cy.wrap(e).type(name).get('#car-name-form').submit();
+    })
+    .get('#car-name-form')
+    .submit();
+});
+
+Cypress.Commands.add('submitCarNameForm', (name) => {
+  cy.getCarNameInput().type(name, { force: true }).get('#car-name-form').submit();
+});
+
+Cypress.Commands.add('submitCarTryNumberForm', (name) => {
+  cy.getCarTryNumberInput().type(name, { force: true }).get('#car-try-number-form').submit();
+});
+
+Cypress.Commands.add('onOccurAlert', (text) => {
+  cy.on('window:alert', (alert) => expect(alert).to.eq(text));
+});
