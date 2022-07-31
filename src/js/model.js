beb32b48228fc52c5ce5ea8e8be66704c4971e3e
junzero741 <junzero741@gmail.com>
2022-07-31 21:34:01 +0900

    [RESET] : 새로운 개발 시도 리셋

--- src/js/model.js
+++
@@ -1,6 +0,0 @@
-export const getCars = () => {
-	return [
-		{ name: 'abc', position: 3 },
-		{ name: 'dce', position: 4 },
-	]
-}
--- src/js/view.js
+++
@@ -1,8 +0,0 @@
-import carsView from './carsView'
-
-export default (targetElement, state) => {
-	const carList = targetElement.querySelector(ulSelector.CAR_LIST)
-	carList.replaceWith(carsView(carList, state))
-
-	return element
-}
