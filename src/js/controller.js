e753164b61a1e939fe333bd078d5ffbcee5ac58a
junzero741 <junzero741@gmail.com>
2022-07-31 18:56:02 +0900

    [FEAT] : 가상 DOM 을 위한 diff 알고리즘 작성

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,14 +3,18 @@ import carsView from './carsView.js'
 
 import registry from './registry.js'
 
+import { applyDiff } from './diff.js'
+
 registry.add('cars', carsView)
 
 const state = {
 	cars: getCars(),
 }
 
-window.requestAnimationFrame(() => {
-	const main = document.querySelector('#app')
-	const newMain = registry.renderRoot(main, state)
-	main.replaceWith(newMain)
-})
+const render = () => {
+	window.requestAnimationFrame(() => {
+		const main = document.querySelector('#app')
+		const newMain = registry.renderRoot(main, state)
+		applyDiff(document.body, main, newMain)
+	})
+}
