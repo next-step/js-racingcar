13868ebb28458e13c8a5ff6b2c45b3b66394e9be
wondonghwi <ehdehd1289@naver.com>
2022-08-03 02:41:24 +0900

    ✨ hidden 클래스를 추가하는 util함수 작성

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,7 @@
+export const addHiddenClass = (selector) => {
+  selector.classList.add('hidden');
+};
+
 export const removeHiddenClass = (selector) => {
   selector.classList.remove('hidden');
 };
