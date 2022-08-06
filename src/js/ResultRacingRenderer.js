6a50e1cdfb0305174a197681eba7b703b8f8c4d4
mingdy <alswl1500@naver.com>
2022-08-06 18:34:27 +0900

    feat: 최종 우승자 띄워주기

--- src/js/ResultRacingRenderer.js
+++ src/js/ResultRacingRenderer.js
@@ -1,11 +1,25 @@
+import RacingCarInfo from "./state/RacingCarInfo.js";
+
 class ResultRacingRenderer {
   #resultRacingview;
+  #winner = { count: -1, name: [] };
+
   constructor(resultRacingview) {
     this.#resultRacingview = resultRacingview;
   }
 
   initRenderer() {
-    this.#resultRacingview.initView();
+    for (let name in RacingCarInfo.getRaceForwardCount()) {
+      if (RacingCarInfo.getRaceForwardCount()[name] > this.#winner.count) {
+        this.#winner.count = RacingCarInfo.getRaceForwardCount()[name];
+        this.#winner.name = [name];
+      } else if (
+        RacingCarInfo.getRaceForwardCount()[name] == this.#winner.count
+      ) {
+        this.#winner.name.push(name);
+      }
+    }
+    this.#resultRacingview.resultView(this.#winner.name.join(","));
   }
 }
 export default ResultRacingRenderer;
--- src/js/ResultRacingView.js
+++ src/js/ResultRacingView.js
@@ -3,15 +3,14 @@ import View from "./View.js";
 class ResultRacingView extends View {
   setElement(e) {}
 
-  initView() {
+  initView() {}
+
+  resultView(winners) {
     document.querySelector("#app").insertAdjacentHTML(
       "beforeend",
       `<section class="d-flex justify-center mt-5">
         <div>
-          <h2>🏆 최종 우승자: EAST, WEST 🏆</h2>
-          <div class="d-flex justify-center">
-            <button type="button" class="btn btn-cyan">다시 시작하기</button>
-          </div>
+          <h2>🏆 최종 우승자: ${winners} 🏆</h2>
         </div>
       </section>`
     );
