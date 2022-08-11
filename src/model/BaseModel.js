2a83e73b1ea480f6e317a682bc9fef45b6e542f3
정대윤 <sensecodevalue@gmail.com>
2022-08-11 20:48:48 +0900

    refactor: 기본 model class 구현

---
+++ src/model/BaseModel.js
@@ -0,0 +1,31 @@
+export default class BaseModel {
+  #defaultState;
+
+  constructor(state) {
+    this.state = { ...state };
+    this.#defaultState = state;
+    this.listeners = [];
+  }
+
+  suscribe(render) {
+    this.listeners.push(render);
+  }
+
+  notify() {
+    this.listeners.forEach(listener => listener());
+  }
+
+  setState(key, payload) {
+    if (!Object.keys(this.state).find(_key => _key === key)) {
+      throw Error(`state에 해당 ${key}의 값이 없습니다.`);
+    }
+
+    this.state[key] = payload;
+    this.notify();
+  }
+
+  resetState() {
+    this.state = { ...this.#defaultState };
+    this.notify();
+  }
+}
