3372b9a4c6767dd62e0d42d0eedc1cdb8472084a
정대윤 <sensecodevalue@gmail.com>
2022-08-11 21:32:43 +0900

    refactor: [View 분리하기] CarPlayerList view 분리

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 import App from './core/App.js';
 import AttemptForm from './controller/Attempt/Form.js';
-import CarPlayerList from './controller/CarPlayer/List.js';
 import CarPlayerNameForm from './controller/CarPlayer/NameForm.js';
 import Result from './controller/Result.js';
 import Winner from './controller/Winner.js';
 import model from './model/index.js';
+import CarPlayerList from './view/CarPlayer/List.js';
 
 function main() {
   const app = new App(model);
-  app.useController(CarPlayerList);
   app.useController(CarPlayerNameForm);
   app.useController(AttemptForm);
   app.useController(Result);
   app.useController(Winner);
+  app.useView(CarPlayerList);
 
   app.model.notify();
 }
