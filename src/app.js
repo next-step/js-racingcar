c55f6f1a42f45d25bec96bb09969f16af38913b1
정대윤 <sensecodevalue@gmail.com>
2022-08-11 20:50:45 +0900

    refactor: App class 에서 model관련 로직 제거 및 model을 받아 사용하도록 변경

--- src/app.js
+++ src/app.js
@@ -4,23 +4,17 @@ import CarPlayerList from './controller/CarPlayer/List.js';
 import CarPlayerNameForm from './controller/CarPlayer/NameForm.js';
 import Result from './controller/Result.js';
 import Winner from './controller/Winner.js';
-
-const state = {
-  carPlayerNames: [],
-  racingSteps: {},
-  attempt: 0,
-  winners: [],
-};
+import model from './model/index.js';
 
 function main() {
-  const app = new App(state);
+  const app = new App(model);
   app.useController(CarPlayerList);
   app.useController(CarPlayerNameForm);
   app.useController(AttemptForm);
   app.useController(Result);
   app.useController(Winner);
 
-  app.render();
+  app.model.notify();
 }
 
 main();
--- src/core/App.js
+++ src/core/App.js
@@ -1,40 +1,15 @@
 export default class App {
-  #defaultState; // BaseModel
-
-  constructor(state) {
-    this.state = { ...state }; // BaseModel
-    this.#defaultState = state; // BaseModel
-    this.renderList = []; // BaseModel
+  constructor(model) {
+    this.model = model;
   }
 
-  // BaseModel
-  setState(key, payload) {
-    if (!Object.keys(this.state).find(_key => _key === key)) {
-      throw Error(`state에 해당 ${key}의 값이 없습니다.`);
-    }
-
-    this.state[key] = payload;
-    this.render();
-  }
-
-  // BaseModel
-  setRender(render) {
-    this.renderList.push(render);
-  }
-
-  resetState() {
-    this.state = { ...this.#defaultState };
-
-    this.render();
-  }
-
-  // 해당 부분은 존재해야한다. 어느한군데서는 MVC를 관리하는 부분이 있어야하니...
   useController(Controller) {
+    // if ! Controller  instanceof BaseController throw error
     new Controller(this);
   }
 
-  // BaseModel
-  render() {
-    this.renderList.forEach(render => render());
+  useView(View) {
+    // if ! View  instanceof BaseView throw error
+    new View(this);
   }
 }
