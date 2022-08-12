247d034fca5c5bdd0309f85fee826d6a412f38ec
정대윤 <sensecodevalue@gmail.com>
2022-08-13 02:07:17 +0900

    refactor: [추상 메서드] View에서 render를 반드시 구현하도록 변경

--- src/view/BaseView.js
+++ src/view/BaseView.js
@@ -1,8 +1,10 @@
 export default class BaseView {
   constructor(app) {
+    if (this.render === undefined) {
+      throw new TypeError('BaseController render must be overrided');
+    }
+
     this.model = app.model;
     this.model.suscribe(this.render.bind(this));
   }
-
-  render() {} // BaseView
 }
