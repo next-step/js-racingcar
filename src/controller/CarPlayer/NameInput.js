edf385ce64abefebabf0f68bf27053877f6cb8c6
정대윤 <sensecodevalue@gmail.com>
2022-08-13 01:50:41 +0900

    fix: typo MAX를 맨앞으로

--- src/controller/CarPlayer/NameInput.js
+++ src/controller/CarPlayer/NameInput.js
@@ -1,7 +1,7 @@
 import BaseInput from '../BaseInput.js';
 
-export const NAME_MAX_LENGTH = 5;
-export const NAME_LIST_MAX_LENGHT = 5;
+export const MAX_NAME_LENGTH = 5;
+export const MAX_NAME_LIST_LENGHT = 5;
 
 export default class CarNameInput extends BaseInput {
   constructor() {
@@ -13,11 +13,11 @@ export default class CarNameInput extends BaseInput {
     const { value } = event.target;
     const carNames = value.split(',').map(carName => carName.trim());
 
-    if (carNames.some(carName => carName.length > NAME_MAX_LENGTH)) {
+    if (carNames.some(carName => carName.length > MAX_NAME_LENGTH)) {
       this.setCustomValidity('플레이어 이름은 5자 이하로 작성해 주세요.');
     } else if (carNames.some(carName => carName === '')) {
       this.setCustomValidity('플레이어 이름을 입력해주세요.');
-    } else if (carNames.length >= NAME_LIST_MAX_LENGHT) {
+    } else if (carNames.length >= MAX_NAME_LIST_LENGHT) {
       this.setCustomValidity('플레이어 이름을 5개이하로 입력해주세요.');
     } else if (carNames.length !== new Set(carNames).size) {
       this.setCustomValidity('중복된 플레이어가 있습니다 다시 입력해 주세요.');
