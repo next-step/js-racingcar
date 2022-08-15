f31d478d16f242a4ed8be2c7b16317c88e9c7ecf
jlee0505 <hj960322@gmail.com>
2022-08-16 07:56:18 +0900

    refactor: 접근 방식 수정

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
-import { SELECTORS } from "./utils/constants/selectors.js";
-import { RacingModel } from "./model/racingModel.js";
+import { RacingGame } from "../src/model/RacingGame.js";
 import { $ } from "./utils/dom.js";
-import { removeHiddenClass } from "./utils/utils.js";
 
-export const racingModel = new RacingModel();
-
-const handleSubmitCarName = (e) => {
-  e.preventDefault();
-
-  const carNames = e.target["car-name__input"].value;
-
-  if (!carNames) {
-    return;
-  }
-  racingModel.setCarNames(carNames);
-  racingModel.displayCars();
-};
-
-const handleSubmitCount = (e) => {
-  const $count = e.target["count__input"].value;
-
-  e.preventDefault();
-
-  if (!$count) {
-    return;
-  }
-
-  racingModel.setPlayTime(Number($count));
-};
-
-$(SELECTORS.CAR_NAME_FORM).addEventListener("submit", handleSubmitCarName);
-$(SELECTORS.COUNT_FORM).addEventListener("submit", handleSubmitCount);
+new RacingGame($("#app"));
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,15 +1,16 @@
-import { templateForward } from "../view/template.js";
+export const addCls = (target, ...clsName) =>
+  target?.classList?.add(...clsName);
 
-export const removeHiddenClass = ($selector) => {
-  $selector.classList.remove("hidden");
+export const removeCls = (target, ...clsName) =>
+  target?.classList?.remove(...clsName);
+
+export const warnMsg = (msg) => alert(msg);
+
+export const createElement = (tagName, ...clsName) => {
+  const element = document.createElement(tagName);
+  element.classList.add(...clsName);
+  return element;
 };
 
-export const displayTemplate = ($selector, template) => {
-  $selector.innerHTML = template;
-};
-
-export const getRandomInt = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+export const getRandomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
