1c6a9fa6f93cafe25845d51fa47c6fbb84c931d6
Hojoong Choi <inegg.apps@gmail.com>
2022-11-25 23:05:17 +0900

    test: 시도 횟수 입력에 대한 테스트 케이스 코드 작성

--- cypress/e2e/dom.cy.js
+++ cypress/e2e/dom.cy.js
@@ -20,10 +20,12 @@ describe('자동차 경주 게임 요구사항을 점검한다', () => {
       const stub = cy.stub();
       cy.on('window:alert', stub);
 
-      cy.get(ELEMENT.INPUT.CAR_NAMES).type('갑을병정무기경신임계');
-      cy.get(ELEMENT.BUTTON.CAR_NAME_CONFIRM)
-        .click()
-        .then(() => expect(stub.getCall(0)).to.be.calledWith(ALERT_MESSAGE.INVALID.CAR_NAMES_LENGTH));
+      ['갑을병정무기경신임계', '동해물과백두산이', '동,,해물과백두산', '동해,물,과,백두,산이,'].forEach((names) => {
+        cy.get(ELEMENT.INPUT.CAR_NAMES).type(names);
+        cy.get(ELEMENT.BUTTON.CAR_NAME_CONFIRM)
+          .click()
+          .then(() => expect(stub.getCall(0)).to.be.calledWith(ALERT_MESSAGE.INVALID.CAR_NAMES_LENGTH));
+      });
     });
 
     it('자동차 이름 입력 후 확인 버튼을 누르면 시도할 횟수를 입력하는 상자가 뜬다', () => {
@@ -40,17 +42,37 @@ describe('자동차 경주 게임 요구사항을 점검한다', () => {
   });
 
   describe('2단계: 시도할 횟수를 입력한다', () => {
-    it('시도 횟수는 1회 이상이어야 한다', () => {
-      expect(false).to.be.true;
+    beforeEach(() => {
+      cy.get(ELEMENT.INPUT.CAR_NAMES).type(STRING_CAR_NAMES).type('{enter}');
     });
-    it('시도 횟수 입력 후 확인 버튼을 누를 수 있다', () => {
-      expect(false).to.be.true;
+
+    it('시도 횟수 입력 버튼이 있다', () => {
+      cy.get(ELEMENT.BUTTON.ATTEMT_TIMES_CONFIRM).should('be.visible');
     });
-    it('시도 횟수 입력 후 확인 버튼을 누르면  누르면 자동차 경주를 시작한다', () => {
-      expect(false).to.be.true;
+
+    it.only('시도 횟수가 1회 미만인 경우 예외를 발생시킨다', () => {
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+
+      ['0', '-1', String(Number.MIN_SAFE_INTEGER)].forEach((input) => {
+        cy.get(ELEMENT.INPUT.ATTEMPT_TIMES).type(input);
+        cy.get(ELEMENT.BUTTON.ATTEMT_TIMES_CONFIRM)
+          .click()
+          .then(() => {
+            expect(stub.getCall(0)).to.be.calledWith(ALERT_MESSAGE.INVALID.ATTEMPT_TIMES);
+          });
+      });
     });
-    it('시도 횟수 입력 후 엔터를 누르면 자동차 경주를 시작한다', () => {
-      expect(false).to.be.true;
+
+    it('시도 횟수 입력 후 확인 버튼을 누르면 자동차 경주 영역이 보인다', () => {
+      cy.get(ELEMENT.INPUT.ATTEMPT_TIMES).type('1');
+      cy.get(ELEMENT.BUTTON.ATTEMT_TIMES_CONFIRM).click();
+      cy.get(ELEMENT.SECTION.CAR_RACING).should('be.visible');
+    });
+
+    it('시도 횟수 입력 후 엔터를 누르면 자동차 경주 영역이 보인다', () => {
+      cy.get(ELEMENT.INPUT.ATTEMPT_TIMES).type('1').type('{enter}');
+      cy.get(ELEMENT.SECTION.CAR_RACING).should('be.visible');
     });
   });
 
