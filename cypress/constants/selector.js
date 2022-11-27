e50177d03bd9905dd12458b49de9ca93edace90a
Seung-wan <tmddhks0104@gmail.com>
2022-11-27 22:19:35 +0900

    Feat: 테스트코드 작성

---
+++ cypress/constants/selector.js
@@ -0,0 +1,10 @@
+export const SELECTOR = {
+  CAR_NAME_INPUT: 'car-name-input',
+  CAR_NAME_FORM: 'car-name-form',
+
+  CAR_ATTEMPTS_COUNT_FORM: 'car-attempts-count-form',
+  CAR_ATTEMPTS_COUNT_INPUT: 'car-attempts-count-input',
+
+  CAR_ROAD: 'car-road',
+  CAR_PLAYER: 'car-player',
+};
