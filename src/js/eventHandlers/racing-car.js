3d62e7ff513947a3967d54803302cf2e0cd1b45d
Seung-wan <tmddhks0104@gmail.com>
2022-11-27 22:21:45 +0900

    Feat: 이벤트 핸들러 작성

---
+++ src/js/eventHandlers/racing-car.js
@@ -0,0 +1,40 @@
+import model from '../model/Model.js';
+import { assertCarAttemptsCount, assertCarName } from '../utils/validator.js';
+import {
+  disableCarAttemptsCountForm,
+  disableCarNameForm,
+  getCarAttemptsCount,
+  getCarName,
+  renderCarRoad,
+  showCarAttemptsCountForm,
+} from '../view/racing-car.js';
+
+export const handleCarNameSubmit = (e) => {
+  e.preventDefault();
+
+  try {
+    model.carName = getCarName();
+
+    assertCarName(model.carName);
+
+    disableCarNameForm();
+    showCarAttemptsCountForm();
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+export const handleCarAttemptsCountSubmit = (e) => {
+  e.preventDefault();
+
+  try {
+    const carAttemptsCount = getCarAttemptsCount();
+    assertCarAttemptsCount(carAttemptsCount);
+
+    disableCarAttemptsCountForm();
+    renderCarRoad(model.carName);
+    model.gameStart();
+  } catch (error) {
+    alert(error.message);
+  }
+};
