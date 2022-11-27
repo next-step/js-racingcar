add97eb79a132d2da98043ab9d751c610d0e6b93
Seung-wan <tmddhks0104@gmail.com>
2022-11-27 22:21:14 +0900

    Feat: 예외처리 작성

---
+++ src/js/utils/validator.js
@@ -0,0 +1,37 @@
+import { ERROR_MESSAGE } from '../constants/errorMessage.js';
+import { RACING_CAR } from '../constants/racing-car.js';
+
+export const isMoveForwardNumber = (num) => {
+  return num >= RACING_CAR.STANDARD_MOVE_FORWARD_NUMBER;
+};
+
+const isLessThanOrEqualToLengthInArray = (value, length) => {
+  if (!Array.isArray(value)) return;
+
+  return value.every((elem) => elem.length <= length);
+};
+
+const hasZeroElementLengthInArray = (value) => {
+  if (!Array.isArray(value)) return;
+
+  return value.some((elem) => elem.length === 0);
+};
+
+const assert = (predicate, message) => {
+  if (!predicate) {
+    throw new Error(message);
+  }
+};
+
+export const assertCarName = (carName) => {
+  assert(
+    isLessThanOrEqualToLengthInArray(carName, RACING_CAR.NAME_LENGTH_LIMIT),
+    ERROR_MESSAGE.INVALID_CAR_NAME_LENGTH,
+  );
+
+  assert(!hasZeroElementLengthInArray(carName), ERROR_MESSAGE.INVALID_CAR_NAME_LENGTH);
+};
+
+export const assertCarAttemptsCount = (count) => {
+  assert(count >= RACING_CAR.MIN_ATTEMPTS_COUNT, ERROR_MESSAGE.INVALID_CAR_ATTEMPTS_COUNT);
+};
