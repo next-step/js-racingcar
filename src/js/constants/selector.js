336cf534c7a9c7df7f1ac645bbcf3bd7393c6027
Seung-wan <tmddhks0104@gmail.com>
2022-11-27 22:21:02 +0900

    Feat: 셀렉터 상수 작성

---
+++ src/js/constants/selector.js
@@ -0,0 +1,13 @@
+export const SELECTOR = {
+  CAR_NAME_FORM: '.js-car-name-form',
+  CAR_NAME_INPUT: '.js-car-name-input',
+  CAR_NAME_BUTTON: '.js-car-name-button',
+
+  CAR_ATTEMPTS_COUNT_FORM: '.js-car-attempts-count-form',
+  CAR_ATTEMPTS_COUNT_INPUT: '.js-car-attempts-count-input',
+  CAR_ATTEMPTS_COUNT_BUTTON: '.js-car-attempts-count-button',
+
+  CAR_ROAD: '.js-car-road',
+  CAR_ROAD_WRAPPER: '.js-car-road-wrapper',
+  CAR_WRAPPER: '.js-car-wrapper',
+};
