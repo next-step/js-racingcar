e57681dc3e30ba9d320477ad9ac7d8da06692b8a
Seung-wan <tmddhks0104@gmail.com>
2022-11-29 02:15:53 +0900

    Refactor: 모듈 네이밍 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import { $ } from './utils/dom.js';
 import { SELECTOR } from './constants/selector.js';
-import { handleCarAttemptsCountSubmit, handleCarNameSubmit } from './eventHandlers/racing-car.js';
+import { handleCarAttemptsCountSubmit, handleCarNameSubmit } from './eventHandlers/racingCar.js';
 
 const bindEvents = () => {
   $(SELECTOR.CAR_NAME_FORM).addEventListener('submit', handleCarNameSubmit);
