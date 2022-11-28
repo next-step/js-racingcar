07a46acbc8d24161688f7e2763ee81b1cd5d3fb6
Seung-wan <tmddhks0104@gmail.com>
2022-11-29 03:41:09 +0900

    Refactor: Custom Commands 작성

--- cypress/e2e/racing-car.cy.js
+++ cypress/e2e/racing-car.cy.js
@@ -1,21 +1,6 @@
 import { ERROR_MESSAGE } from '../../src/js/constants/errorMessage.js';
 import { SELECTOR } from '../constants/selector.js';
 
-const typeCarName = (carName) => {
-  cy.getByDataset(SELECTOR.CAR_NAME_INPUT).type(carName);
-  cy.getByDataset(SELECTOR.CAR_NAME_FORM).submit();
-};
-
-const typeCarAttemptsCount = (count) => {
-  cy.getByDataset(SELECTOR.CAR_ATTEMPTS_COUNT_INPUT).type(count);
-  cy.getByDataset(SELECTOR.CAR_ATTEMPTS_COUNT_FORM).submit();
-};
-
-const startRacingGame = (carName, count) => {
-  typeCarName(carName);
-  typeCarAttemptsCount(count);
-};
-
 beforeEach(() => {
   cy.visit('index.html');
 });
@@ -47,7 +32,7 @@ describe('자동차 경주 게임을 테스트한다.', () => {
     });
 
     it('자동차 이름을 입력할 때 쉼표로 구분하여 입력하고 확인 버튼을 누르면 이동 횟수를 입력하는 form이 나타난다. ', () => {
-      typeCarName('car1, car2, car3, car4, car5');
+      cy.typeCarName('car1, car2, car3, car4, car5');
 
       cy.getByDataset(SELECTOR.CAR_ATTEMPTS_COUNT_FORM).should('not.have.class', 'd-none');
     });
@@ -56,7 +41,7 @@ describe('자동차 경주 게임을 테스트한다.', () => {
   context('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
     it('이동할 횟수를 입력하고 확인 버튼을 누르면 입력한 자동차 이름이 보여진다', () => {
       const carName = 'car1, car2, car3, car4, car5';
-      startRacingGame(carName, 4);
+      cy.startRacingGame(carName, 4);
 
       cy.getByDataset(SELECTOR.CAR_ROAD).should('have.class', 'd-flex');
       cy.getByDataset(SELECTOR.CAR_ROAD).should('not.have.class', 'd-none');
@@ -66,7 +51,7 @@ describe('자동차 경주 게임을 테스트한다.', () => {
   context('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
     it('자동차 이름과 횟수를 입력하면, 입력한 자동차 이름이 순서대로 출력된다.', () => {
       const carName = 'car1, car2, car3, car4, car5';
-      startRacingGame(carName, 4);
+      cy.startRacingGame(carName, 4);
 
       const carNameArray = 'car1, car2, car3, car4, car5'.split(',').map((car) => car.trim());
 
@@ -86,7 +71,7 @@ describe('자동차 경주 게임을 테스트한다.', () => {
     '전진하는 조건은 0에서 9 사이에서 random 값을 구한 후 random 값이 4 이상일 경우 전진하고, 3 이하의 값이면 멈춘다.',
     () => {
       it('random값이 4 이상이면 화살표가 화면에 출력되고 3 이하의 값이면 아무것도 출력되지 않는다.', () => {
-        startRacingGame('car1, car2, car3, car4', 5);
+        cy.startRacingGame('car1, car2, car3, car4', 5);
       });
     },
   );
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,18 @@
+import { SELECTOR } from '../constants/selector.js';
+
 Cypress.Commands.add('getByDataset', (dataset) => cy.get(`[data-cy="${dataset}"]`));
+
+Cypress.Commands.add('typeCarName', (carName) => {
+  cy.getByDataset(SELECTOR.CAR_NAME_INPUT).type(carName);
+  cy.getByDataset(SELECTOR.CAR_NAME_FORM).submit();
+});
+
+Cypress.Commands.add('typeCarAttemptsCount', (count) => {
+  cy.getByDataset(SELECTOR.CAR_ATTEMPTS_COUNT_INPUT).type(count);
+  cy.getByDataset(SELECTOR.CAR_ATTEMPTS_COUNT_FORM).submit();
+});
+
+Cypress.Commands.add('startRacingGame', (carName, count) => {
+  cy.typeCarName(carName);
+  cy.typeCarAttemptsCount(count);
+});
