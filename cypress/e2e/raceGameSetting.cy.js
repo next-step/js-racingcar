966d9d32f73aae566f6541cdcf299e01d078de72
Dongmin Ahn <eamon3481@naver.com>
2022-12-01 01:31:43 +0900

    fix : solve test code error

--- cypress/e2e/raceGameSetting.cy.js
+++ cypress/e2e/raceGameSetting.cy.js
@@ -40,12 +40,13 @@ describe('자동차에 이름을 부여할 수 있다.', () => {
  });
 
  it('자동차 이름을 적고 제출할 확인 버튼이 존재한다.', () => {
-  cy
-   .get(carNameFieldsetSelector)
-   .within(() => {
-    cy.get('button').should('exist').should('be.visible');
-   })
-   .should('have.text', '확인');
+  cy.get(carNameFieldsetSelector).within(() => {
+   cy
+    .get('button')
+    .should('exist')
+    .should('be.visible')
+    .should('have.text', '확인');
+  });
  });
 
  it('자동차 이름은 쉼표(,)를 기준으로 구분하며 이름은 5자 이하만 가능하다.', () => {
@@ -54,7 +55,7 @@ describe('자동차에 이름을 부여할 수 있다.', () => {
    cy.get('button').click();
    cy.on('window:alert', (str) => {
     expect(str).to.equal(
-     ' 유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.'
+     '유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.'
     );
    });
   });
@@ -69,18 +70,16 @@ describe('자동차에 이름을 부여할 수 있다.', () => {
 });
 
 describe('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', () => {
+ beforeEach(() => {
+  cy.visit('http://localhost:3000');
+ });
  it('시도할 횟수를 입력하는 input이 존재한고 자동차 이름이 입력되지 않았다면 보여지지 않아야한다.', () => {
   cy.get('input[name="racing-count"]').should('exist').should('be.not.visible');
  });
 
  it('자동차 이름을 입력하면, 시도한 횟수를 입력하는 input이 보여진다.', () => {
   cy.carNameTypo('kia,benz,audi,tesla,volvo');
-  cy.get('input[name="racing-count"]').should('exist').should('be.not.visible');
- });
-
- it('자동차 이름을 입력하면, 시도한 횟수를 입력하는 input이 보여진다.', () => {
-  cy.carNameTypo('kia,benz,audi,tesla,volvo');
-  cy.get('input[name="racing-count"]').should('exist').should('be.not.visible');
+  cy.get('input[name="racing-count"]').should('exist').should('be.visible');
  });
 
  it('1 미만의 횟수를 입력하고 확인 버튼을 누를시 "입력한 레이싱 횟수가 너무 적습니다. 레이싱 횟수는 1이상이어야 합니다." 라는 alert 을 띄워준다.', () => {
