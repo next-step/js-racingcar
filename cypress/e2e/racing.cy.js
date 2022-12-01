02917cc50fc857e77a7fbdafa5465f10f26ea2ba
Geuni <dlrmsgnl620@naver.com>
2022-12-01 23:48:58 +0900

    Test: 요구사항 Test code 작성

---
+++ cypress/e2e/racing.cy.js
@@ -0,0 +1,30 @@
+const INPUT_CAR_NAMES = "EAST, WEST, SOUTH, NORTH";
+const INVALID_INPUT_CAR_NAMES = "EASTTT, WESTTT, SOUTHH, NORTHH";
+
+describe("자동차 경주 게임", () => {
+  beforeEach(() => {
+    cy.visit("../../index.html");
+  });
+  context("사용자가 자동차 게임에 참여하기 전", () => {
+    it("화면에서 자동차 이름을 부여할 input창과 확인버튼이 존재한다", () => {
+      cy.isVisible(".input-car-name");
+      cy.isVisible(".name-submit-btn");
+      cy.isNotVisible(".number-of-attempts");
+    });
+
+    it("자동차 이름은 쉼표(,)를 기준으로 구분한다", () => {
+      cy.submitCarNames(INPUT_CAR_NAMES);
+      cy.submitNumberOfAttempts(3);
+
+      cy.containCarPlayer("EAST");
+      cy.containCarPlayer("WEST");
+      cy.containCarPlayer("SOUTH");
+      cy.containCarPlayer("NORTH");
+    });
+
+    it("자동차 이름은 5자 이하이다.", () => {
+      cy.submitCarNames(INVALID_INPUT_CAR_NAMES);
+      cy.alertMessage("자동차 이름은 5자 이하로 작성해야합니다.");
+    });
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,30 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("isVisible", (target) => {
+  cy.get(target).should("be.visible");
+});
+
+Cypress.Commands.add("isNotVisible", (target) => {
+  cy.get(target).should("be.not.visible");
+});
+
+Cypress.Commands.add("submitCarNames", (names) => {
+  cy.get(".car-player").type(names);
+  cy.get(".btn-cyan").click();
+});
+
+Cypress.Commands.add("containCarPlayer", (player) => {
+  cy.get(".car-player").contains(player);
+});
+
+Cypress.Commands.add("submitNumberOfAttempts", (attempts) => {
+  cy.get(".number-of-attempts").type(attempts);
+});
+
+Cypress.Commands.add("alertMessage", (message) => {
+  cy.on("window:alert", (text) => {
+    expect(text).to.contain(message);
+  });
+});
