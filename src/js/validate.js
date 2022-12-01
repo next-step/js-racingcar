5337cbcb9e588d311d19c44f91d9478b6cc232cb
nara04040 <nara04040@naver.com>
2022-12-02 01:35:04 +0900

    style : 주석 제거

--- src/js/validate.js
+++ src/js/validate.js
@@ -1,21 +1,3 @@
 export const isCorrectRange = (carName) => {
-  //   console.log(carName);
-
-  //   carName.forEach((element) => console.log(element.length));
-  //   carName.forEach((element) => element.length >= 1 && element.length <= 5);
-  //   carName.forEach((element) => {
-  //     console.log(element.length);
-  //     if (element.length < 1) {
-  //       return false;
-  //     }
-  //     return true;
-  //   });
   return carName.every((element) => element.length >= 1 && element.length <= 5);
-
-  //   carName.forEach((element) => element.length > 1 && element.length < 5);
 };
-
-// export const validNames = (carName) => {
-//   console.log(isCorrectRange(carName));
-//   if (isCorrectRange(carName)) alert('자동차이름을 한글자 이상 다섯글자 이하로 입력하세요.');
-// };
