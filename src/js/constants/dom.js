3c7668dad1b3acef34f58891e06e2632b9249e69
Geuni <dlrmsgnl620@naver.com>
2022-12-03 14:35:08 +0900

    Chore: car render dom add

--- src/js/constants/dom.js
+++ src/js/constants/dom.js
@@ -3,8 +3,10 @@ import {$} from "../utils/selector.js";
 export const DOM = Object.freeze({
   CAR_NAMES_ID_INPUT: $(".input-car-name"),
   CAR_NAMES_SUBMIT_BTN: $(".btn-name-submit"),
+
+  NUMBER_OF_ATTEMPTS_FIELDSET: $(".fieldset-attempts-number"),
   NUMBER_OF_ATTEMPTS_INPUT: $(".input-number-attempts"),
   NUMBER_OF_ATTEMPTS_BTN: $(".btn-attempts-submit"),
 
-  NUMBER_OF_ATTEMPTS_FIELDSET: $(".fieldset-attempts-number"),
+  RACING_CAR_RENDER_SECTION: $("section-car-render"),
 });
