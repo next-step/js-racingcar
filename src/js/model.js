b13d6874990462254ffb2b59da5ae5818034f936
Geuni <dlrmsgnl620@naver.com>
2022-12-03 14:48:14 +0900

    Chore: validation 추가
    - Array 아닐 시 return

--- src/js/model.js
+++ src/js/model.js
@@ -1,8 +1,9 @@
 export const isValidation = (namesArr) => {
   const namesError = namesArr
     .split(",")
-    .filter((name) => name.trim().length >= 6).length;
+    .filter((name) => (name.trim().length >= 6).length);
 
+  if (!namesArr) return false;
   if (namesError > 0) return false;
 
   return true;
