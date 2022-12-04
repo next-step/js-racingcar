d1dfbedef02deaeb0170b1748cd03e94f2095948
Seung-wan <tmddhks0104@gmail.com>
2022-12-05 05:23:25 +0900

    Refactor: 쓰레기코드 제거

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,6 +1,3 @@
 export const generateRandomNumber = (min, max) => {
   return Math.floor(Math.random() * max - min) + min;
 };
-
-export const sortObjectByValue = (obj, option = 'asc') =>
-  Object.entries(obj).sort(([, a], [, b]) => (option === 'asc' ? a - b : b - a));
