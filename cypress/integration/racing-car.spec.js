2ea3100be1003ef78a4a748430fcfac8fcb04601
kjhwert <kjhwert11@naver.com>
2022-12-04 16:12:04 +0900

    refactor: 테스트코드 개선
    1. 함수 분리 및 네이밍 변경

--- cypress/integration/racing-car.spec.js
+++ cypress/integration/racing-car.spec.js
@@ -1,96 +1,125 @@
 import RACING_GAME from "../../src/constants.js";
 
-const registerCarNames = (carNames) => {
-  cy.get("#car-names-input").type(carNames);
-  cy.get("#car-names-button").click();
-};
-
-const registerRacingCount = (count) => {
-  cy.get("#racing-count-input").type(count.toString());
-  cy.get("#racing-count-button").click();
-};
+const $carNamesInput = "#car-names-input";
+const $carNamesButton = "#car-names-button";
+const $racingCountInput = "#racing-count-input";
+const $racingCountButton = "#racing-count-button";
 
 describe("레이싱 게임", () => {
+  let alertStub;
+
+  const whenTargetClicked = (target, callback) => {
+    cy.get(target).click().then(callback);
+  };
+
+  const alertMessageCalledWith = (message) => {
+    expect(alertStub.getCall(0)).to.be.calledWith(message);
+  };
+
   beforeEach(() => {
     cy.visit("../../index.html");
   });
 
-  context("5자 이하의 자동차 이름을 콤마로 구분하여 입력한다.", () => {
-    it("자동차 명은 1자 이상 5자 이하이다.", () => {
-      cy.get("#car-names-input").type("{backspace}");
-      cy.alertMessage({
-        selector: "#car-names-button",
-        message: RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH,
+  context(
+    "자동차명이 올바르게 입력되지 않은 경우 에러메시지가 노출된다.",
+    () => {
+      beforeEach(() => {
+        alertStub = cy.stub();
+        cy.on("window:alert", alertStub);
       });
 
-      cy.get("#car-names-input").type("123456");
-      cy.alertMessage({
-        selector: "#car-names-button",
-        message: RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH,
+      it("자동차명은 공백일 수 없다.", () => {
+        cy.get($carNamesInput).type("{backspace}");
+
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
       });
-    });
-    it("특이 케이스 테스트", () => {
-      cy.get("#car-names-input").type(",12,23");
-      cy.alertMessage({
-        selector: "#car-names-button",
-        message: RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH,
+      it("자동차 명은 1자 이상이다.", () => {
+        cy.get($carNamesInput).type("1, 1, ,");
+
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
       });
 
-      cy.get("#car-names-input").type("12,23,");
-      cy.alertMessage({
-        selector: "#car-names-button",
-        message: RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH,
+      it("자동차 명은 5자 이하이다.", () => {
+        cy.get($carNamesInput).type("123456");
+
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
+
+        cy.get($carNamesInput).type("1234, 123456");
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
       });
 
-      cy.get("#car-names-input").type(",,");
-      cy.alertMessage({
-        selector: "#car-names-button",
-        message: RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH,
+      it("자동차명은 중복되어서는 안된다.", () => {});
+
+      it("특이 케이스 테스트", () => {
+        cy.get($carNamesInput).type(",12,23");
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
+
+        cy.get($carNamesInput).type("12,23,");
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
+
+        cy.get($carNamesInput).type(",,");
+        whenTargetClicked($carNamesButton, () => {
+          alertMessageCalledWith(RACING_GAME.MESSAGES.CAR_NAMES_MISMATCH);
+        });
       });
-    });
-  });
+    }
+  );
 
   context("사용자는 몇 번 이동할지 횟수를 입력한다.", () => {
     beforeEach(() => {
-      cy.get("#car-names-input").type("123");
-      cy.get("#car-names-button").click();
+      alertStub = cy.stub();
+      cy.on("window:alert", alertStub);
+
+      cy.get($carNamesInput).type("123");
+      cy.get($carNamesButton).click();
     });
 
     it("최소 1 이상의 숫자를 입력해야 한다.", () => {
-      cy.get("#racing-count-input").type("{backspace}");
-      cy.alertMessage({
-        selector: "#racing-count-button",
-        message: RACING_GAME.MESSAGES.RACING_COUNT_MISMATCH,
+      cy.get($racingCountInput).type("{backspace}");
+      whenTargetClicked($racingCountButton, () => {
+        alertMessageCalledWith(RACING_GAME.MESSAGES.RACING_COUNT_MISMATCH);
       });
 
-      cy.get("#racing-count-input").type("0");
-      cy.alertMessage({
-        selector: "#racing-count-button",
-        message: RACING_GAME.MESSAGES.RACING_COUNT_MISMATCH,
+      cy.get($racingCountInput).type("0");
+      whenTargetClicked($racingCountButton, () => {
+        alertMessageCalledWith(RACING_GAME.MESSAGES.RACING_COUNT_MISMATCH);
       });
     });
     it("최대 10까지 입력할 수 있다.", () => {
-      cy.get("#racing-count-input").type("11");
-      cy.alertMessage({
-        selector: "#racing-count-button",
-        message: RACING_GAME.MESSAGES.RACING_COUNT_MISMATCH,
+      cy.get($racingCountInput).type("11");
+      whenTargetClicked($racingCountButton, () => {
+        alertMessageCalledWith(RACING_GAME.MESSAGES.RACING_COUNT_MISMATCH);
       });
     });
   });
 
   context("자동차를 등록하면 등록한 자동차명이 노출된다.", () => {
     it("자동차명이 (감자, 고구마)인 경우", () => {
-      const carNames = "감자, 고구마";
-      registerCarNames(carNames);
-      registerRacingCount(5);
+      cy.get($carNamesInput).type("감자, 고구마");
+      cy.get($carNamesButton).click();
+      cy.get($racingCountInput).type("5");
+      cy.get($racingCountButton).click();
 
       cy.get(`#car-name-감자`).should("exist");
       cy.get(`#car-name-고구마`).should("exist");
     });
     it("자동차명에 공백이 있는 경우 제거한다.", () => {
-      const carNames = "    감자    ,  고구마 ";
-      registerCarNames(carNames);
-      registerRacingCount(5);
+      cy.get($carNamesInput).type("    감자    ,  고구마 ");
+      cy.get($carNamesButton).click();
+      cy.get($racingCountInput).type("5");
+      cy.get($racingCountButton).click();
 
       cy.get(`#car-name-감자`).should("exist");
       cy.get(`#car-name-고구마`).should("exist");
