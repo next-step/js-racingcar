4a4ab1b1196fc04c1e82ff5b8673d7abfd20abde
Dongmin Ahn <eamon3481@naver.com>
2022-12-05 13:32:06 +0900

    refactor : divide App class in App.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import './css/index.css';
+import App from './js/App';
 import RaceModel from './js/Model/RaceModel';
 import { $raceFormSection, $raceProcessSection } from './js/View/dom';
 import FormView from './js/View/FormView';
 import ProcessView from './js/View/ProcessView';
 
-class App {
- constructor(Model, views) {
-  const model = new Model();
-  views.forEach(({ view, target }) => {
-   new view(target, model);
-  });
- }
-}
 new App(RaceModel, [
  { view: FormView, target: $raceFormSection },
  { view: ProcessView, target: $raceProcessSection },
