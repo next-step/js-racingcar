436e97123a994c4dda1c03178943e34bb037d049
Woo KyungJun <natcho9010@gmail.com>
2022-12-06 17:21:53 +0900

    feat: add CarNameInputView

---
+++ src/js/Views/CarNameInputView.js
@@ -0,0 +1,32 @@
+import { View } from "../core/View.js";
+
+class CarNameInputView extends View {
+  inputElement;
+  submitButton;
+
+  constructor() {
+    const rootElement = document.getElementById('car-name-input-set');
+    super(rootElement);
+    this.inputElement = rootElement.getElementsByTagName('input')[0];
+    this.submitButton = rootElement.getElementsByTagName('button')[0];
+  }
+
+  onClick = ({ onClickButton }) => {
+    this.submitButton.addEventListener('click', (e) => {
+      onClickButton({ inputElement: this.inputElement }, e);
+    });
+  };
+
+  initial = () => {
+    this.rootElement.removeAttribute('disabled');
+    this.inputElement.value = '';
+  };
+
+  disable = () => {
+    this.rootElement.setAttribute('disabled', true);
+  };
+}
+
+const carNameInputSetView = new CarNameInputView();
+
+export { carNameInputSetView };
