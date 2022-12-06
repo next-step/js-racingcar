b99b27450cf22cf745ddc20d88c078067aba4a92
Woo KyungJun <natcho9010@gmail.com>
2022-12-06 21:15:09 +0900

    test: add countInput test

---
+++ cypress/integration/countInput.spec.js
@@ -0,0 +1,33 @@
+/// <reference types="cypress" />
+
+describe('Car Name Input', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5000');
+
+    const carNameInput = cy.get('#car-name-input-set').find('input');
+    carNameInput.type('abcd');
+
+    const carNameSubmitButton = cy.get('#car-name-input-set').find('button');
+    carNameSubmitButton.click();
+  });
+
+  it('횟수에 아무것도 없이 클릭하면 경고창을 띄운다.', () => {
+    const countInputButton = cy.get('#count-input-set').find('button');
+    countInputButton.click();
+
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('횟수를 입력해주세요!');
+    });
+  });
+
+  it('횟수를 입력하면, 횟수 입력 fieldset이 disable되고 race Track의 hide가 없어진다.', () => {
+    const countInput = cy.get('#count-input-set').find('input');
+    countInput.type('3');
+
+    const countInputButton = cy.get('#count-input-set').find('button');
+    countInputButton.click();
+
+    cy.get('#count-input-set').should('be.disabled');
+    cy.get('#race-track').should('be.visible');
+  });
+});
