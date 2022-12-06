34313ebcdcfd01d1fcd713f733598e5aab8c7d8b
Woo KyungJun <natcho9010@gmail.com>
2022-12-07 00:21:09 +0900

    feat: add global state model

---
+++ src/js/Model/global.js
@@ -0,0 +1,37 @@
+let state = {
+  carNames: [],
+  iterationCount: 0,
+};
+
+function setState(newState) {
+  state = newState;
+}
+
+export function dispatch(action, payload) {
+  switch(action) {
+    case('addCarNames'): {
+      setState({
+        ...state,
+        carNames: payload,
+      });
+      break;
+    }
+    case('iterationCount'): {
+      setState({
+        ...state,
+        iterationCount: payload,
+      });
+      break;
+    }
+    default: {
+      setState({
+        carNames: [],
+        iterationCount: 0,
+      });
+    }
+  }
+}
+
+export function getState() {
+  return state;
+}
