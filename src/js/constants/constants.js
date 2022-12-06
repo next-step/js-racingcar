6b22236f4dae0203971f3d794541490b318b4738
Geuni <dlrmsgnl620@naver.com>
2022-12-07 00:36:23 +0900

    Chore: number constants 추가
    - 시도횟수 최소값, 제로 값

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,10 +1,11 @@
-const CAR_NAME_MAX_LENGTH = 5;
-
-export const ALERT_MESSAGE = {
-  INVALID_INPUT_CAR_NAMES: `유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, ${CAR_NAME_MAX_LENGTH}자 이하만 가능합니다`,
-  INVALID_INPUT_NUMBER_OF_ATTEMPTS: `시도횟수는 0 또는 음수가 불가능합니다. 숫자 1이상 입력해주세요!`,
-};
-
 export const CAR = {
   GO_OR_STOP_STANDARD: 4,
+  NAME_MAX_LENGTH: 5,
+  MIN_NUMBER_OF_ATTEMPTS: 1,
+  NUMBER_OF_ATTEMPTS_ZERO: 0,
+};
+
+export const ALERT_MESSAGE = {
+  INVALID_INPUT_CAR_NAMES: `유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, ${CAR.NAME_MAX_LENGTH}자 이하만 가능합니다`,
+  INVALID_INPUT_NUMBER_OF_ATTEMPTS: `시도횟수는 ${CAR.NUMBER_OF_ATTEMPTS_ZERO} 또는 음수가 불가능합니다. 숫자 ${CAR.MIN_NUMBER_OF_ATTEMPTS}이상 입력해주세요!`,
 };
