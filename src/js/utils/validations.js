ded382245a23b46f421c60e0bf093f42f2178cfc
cameo29 <wodls0209@naver.com>
2022-12-06 17:10:49 +0900

    validations 추가

---
+++ src/js/utils/validations.js
@@ -0,0 +1,30 @@
+import { CAR_NAME, CAR_RACE, ERROR_MESSAGE } from '../constant.js';
+import {
+  duplicatedName,
+  isInvalidMinNumber,
+  isInValidName,
+} from './validator.js';
+
+export const nameValidations = [
+  (value) => {
+    const isInvalid = isInValidName(value, CAR_NAME.MIN, CAR_NAME.MAX);
+    if (isInvalid) {
+      throw new Error(ERROR_MESSAGE.INVALID_CAR_NAME_LENGTH);
+    }
+  },
+  (value) => {
+    const isInvalid = duplicatedName(value);
+    if (isInvalid) {
+      throw new Error(ERROR_MESSAGE.DUPLICATED_CAR_NAME);
+    }
+  },
+];
+
+export const trialTimesValidations = [
+  (value) => {
+    const isInvalid = isInvalidMinNumber(value, CAR_RACE.MIN_TIMES);
+    if (isInvalid) {
+      throw new Error(ERROR_MESSAGE.INVALID_TRIAL_NUMBER);
+    }
+  },
+];
