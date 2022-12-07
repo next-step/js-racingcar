a9b593fee4cd747580a0f07487c2a3609b723c6d
Dongmin Ahn <eamon3481@naver.com>
2022-12-07 21:42:45 +0900

    fix : fix test race Game Setting

--- cypress/e2e/raceGameSetting.cy.js
+++ cypress/e2e/raceGameSetting.cy.js
@@ -54,8 +54,8 @@ describe('자동차에 이름을 부여할 수 있다.', () => {
     cy.get(SELECTOR.FIELDSET.CAR_NAME).within(() => {
       cy.get('input').type('kia,benz,audi,tesla,volvo');
       cy.get('button').click();
-      cy.get('input').should('be.disabled');
     });
+    cy.get(SELECTOR.INPUT.CAR_NAME).should('be.disabled');
   });
 });
 
