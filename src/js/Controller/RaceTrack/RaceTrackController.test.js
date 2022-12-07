3d29e8e776760b1a3662811be9f2bb6a36fb8587
Woo KyungJun <natcho9010@gmail.com>
2022-12-07 17:21:01 +0900

    test: change tests

--- src/js/Controller/RaceTrack/RaceTrackController.test.js
+++ src/js/Controller/RaceTrack/RaceTrackController.test.js
@@ -1,29 +1,32 @@
-import { checkIsCanBeAdvanced, progressRace } from './RaceTrackController.js';
-import { getState, dispatch as dispatchRaceState } from '../../Model/race.js';
-import { dispatch } from '../../Model/global.js';
+/**
+ * @jest-environment jsdom
+ */
+const { checkIsCanBeAdvanced, progressRace } = require('./RaceTrackController.js');
+const { getState, dispatch: dispatchRaceState } = require('../../Model/race.js');
+const { dispatch } = require('../../Model/global.js');
 
 describe('Race Controller Test', () => {
   describe('랜덤숫자를 뽑고 4이상이면 전진한다.', () => {
     it('4 이상의 숫자는 true이다.', () => {
-      expect(checkIsCanBeAdvanced(9)).equal(true);
-      expect(checkIsCanBeAdvanced(8)).equal(true);
-      expect(checkIsCanBeAdvanced(7)).equal(true);
-      expect(checkIsCanBeAdvanced(6)).equal(true);
-      expect(checkIsCanBeAdvanced(5)).equal(true);
-      expect(checkIsCanBeAdvanced(4)).equal(true);
-      expect(checkIsCanBeAdvanced(4.1)).equal(true);
+      expect(checkIsCanBeAdvanced(9)).toBe(true);
+      expect(checkIsCanBeAdvanced(8)).toBe(true);
+      expect(checkIsCanBeAdvanced(7)).toBe(true);
+      expect(checkIsCanBeAdvanced(6)).toBe(true);
+      expect(checkIsCanBeAdvanced(5)).toBe(true);
+      expect(checkIsCanBeAdvanced(4)).toBe(true);
+      expect(checkIsCanBeAdvanced(4.1)).toBe(true);
     });
 
     it('4 이하의 숫자는 false이다.', () => {
-      expect(checkIsCanBeAdvanced(1)).equal(false);
-      expect(checkIsCanBeAdvanced(2)).equal(false);
-      expect(checkIsCanBeAdvanced(3)).equal(false);
-      expect(checkIsCanBeAdvanced(3.999)).equal(false);
+      expect(checkIsCanBeAdvanced(1)).toBe(false);
+      expect(checkIsCanBeAdvanced(2)).toBe(false);
+      expect(checkIsCanBeAdvanced(3)).toBe(false);
+      expect(checkIsCanBeAdvanced(3.999)).toBe(false);
     });
   });
 
   describe('경기 진행 기록', () => {
-    it('경기 진행 상황이 state에 기록되어야한다.', () => {
+    it('경기 진행 상황이 state에 기록되어야한다.', async () => {
       const carDistances = [
         {
           name: 'first',
@@ -36,8 +39,9 @@ describe('Race Controller Test', () => {
       ];
 
       progressRace({ carDistances });
+      await new Promise((r) => setTimeout(r, 3001));
       const { raceCount } = getState();
-      expect(raceCount).equal(1);
+      expect(raceCount).toBe(1);
     });
 
     it('iterateCount를 3번 입력했으면 raceCount가 3으로 찍혀야 한다.', async () => {
@@ -46,7 +50,7 @@ describe('Race Controller Test', () => {
 
       await new Promise((r) => setTimeout(r, 3001));
       const { raceCount } = getState();
-      expect(raceCount).equal(3);
+      expect(raceCount).toBe(3);
     });
   });
 });
