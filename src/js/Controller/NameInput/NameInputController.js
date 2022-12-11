af77409e183a4c9cb070bbc0c44a6635f94a5232
Woo KyungJun <natcho9010@gmail.com>
2022-12-11 21:39:55 +0900

    feat: add constant

--- src/js/Controller/NameInput/NameInputController.js
+++ src/js/Controller/NameInput/NameInputController.js
@@ -5,10 +5,13 @@ import { dispatch } from '../../Model/global.js';
 import { carNameInputSetView } from '../../Views/CarNameInputView.js';
 import { countInputSetView } from '../../Views/CountInputView.js';
 
+const CAR_NAME_SEPERATE_FACTOR = ',';
+const CAR_NAME_LIMIT_LENGTH = 5;
+
 carNameInputSetView.onClick({
   onClickButton: ({ inputElement }) => {
-    const inputValues = inputElement.value?.split(',');
-    const isInputValLengthCorrect = inputValues.some((inputVal) => !!inputVal && inputVal.length <= 5);
+    const inputValues = inputElement.value?.split(CAR_NAME_SEPERATE_FACTOR);
+    const isInputValLengthCorrect = inputValues.some((inputVal) => !!inputVal && inputVal.length <= CAR_NAME_LIMIT_LENGTH);
 
     if (!isInputValLengthCorrect) {
       alert('유효하지 않은 이름 길이입니다. 자동차의 이름은 1자이상, 5자 이하만 가능합니다.');
