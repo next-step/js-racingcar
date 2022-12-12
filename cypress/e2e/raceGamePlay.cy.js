9060dd557265e4e5e41944a2d286c3cf6ff1f086
Dongmin Ahn <eamon3481@naver.com>
2022-12-12 13:07:35 +0900

    refactor : use cypress each

--- cypress/e2e/raceGamePlay.cy.js
+++ cypress/e2e/raceGamePlay.cy.js
@@ -1,6 +1,6 @@
 import { SELECTOR } from '../../src/js/constants';
 
-describe('자동차 경주 게임울 시작한다.', () => {
+describe('자동차 경주 게임을 시작한다.', () => {
   const carNames = ['EAST', 'WEST', 'SOUTH', 'NORTH'];
   const SEC = 1000;
   const COUNT = 5;
@@ -12,7 +12,7 @@ describe('자동차 경주 게임울 시작한다.', () => {
 
   it('자동차 이름과 시도 횟수를 입력하고 확인을 누르면 자동차 경주 게임이 진행된다.', () => {
     cy.get(SELECTOR.ID.RACE_PROCESS).within(() => {
-      carNames.forEach((carName) => {
+      cy.wrap(carNames).each((carName) => {
         cy.contains(carName).should('be.visible');
       });
     });
