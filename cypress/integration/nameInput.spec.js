e54509f922167c540ed057430a0382fd1d8476fc
Woo KyungJun <natcho9010@gmail.com>
2022-12-14 16:39:41 +0900

    Test: enter input test

--- cypress/integration/nameInput.spec.js
+++ cypress/integration/nameInput.spec.js
@@ -7,47 +7,86 @@ describe('Car Name Input', () => {
     cy.visit('http://localhost:5000');
   });
 
-  it('5글자 이상의 이름들이 주어지면, alert을 띄운다.', () => {
+  function testNameInput(input, isClick, resultCallback) {
     const carNameInput = cy.get('#car-name-input-set').find('input');
-    carNameInput.type('abcdef');
+    if (input) { carNameInput.type(input); }
 
-    const carNameSubmitButton = cy.get('#car-name-input-set').find('button');
-    carNameSubmitButton.click();
+    if (isClick) {
+      const carNameSubmitButton = cy.get('#car-name-input-set').find('button');
+      carNameSubmitButton.click();
+    } else {
+      const carNameInput = cy.get('#car-name-input-set').find('input');
+      carNameInput.type('{enter}');
+    }
 
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(alertMessage);
+    resultCallback();
+  }
+
+  it('5글자 이상의 이름들이 주어지면, alert을 띄운다.', () => {
+    testNameInput('abcdef', true, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
+    });
+
+    testNameInput('abcdef', false, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
+    });
+  });
+
+  it('5글자 이상의 이름들을 입력하고 엔터를 하면, alert을 띄운다.', () => {
+    testNameInput('abcdef', false, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
     });
   });
 
   it('input이 비어있으면 alert을 띄운다.', () => {
-    const carNameSubmitButton = cy.get('#car-name-input-set').find('button');
-    carNameSubmitButton.click();
+    testNameInput('', true, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
+    });
+  });
 
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(alertMessage);
+  it('input이 비어있는 상태로 엔터를 누르면 alert을 띄운다.', () => {
+    testNameInput('', false, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
     });
   });
 
   it('comma로 나누어진 문자열에 문자가 없으면 alert을 띄운다.', () => {
-    const carNameInput = cy.get('#car-name-input-set').find('input');
-    carNameInput.type('abcde,,qwert');
+    testNameInput('abcde,,qwert', true, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
+    });
+  });
 
-    const carNameSubmitButton = cy.get('#car-name-input-set').find('button');
-    carNameSubmitButton.click();
-
-    cy.on('window:alert', (str) => {
-      expect(str).to.equal(alertMessage);
+  it('comma로 나누어진 문자열에 문자가 없는데 엔터를 눌렀을 경우, alert을 띄운다.', () => {
+    testNameInput('abcde,,qwert', false, () => {
+      cy.on('window:alert', (str) => {
+        expect(str).to.equal(alertMessage);
+      });
     });
   });
 
   it('5글자 이하의 이름들이 주어지면 자기 fieldset을 disable하고 다음 fieldset을 켠다.', () => {
-    const carNameInput = cy.get('#car-name-input-set').find('input');
-    carNameInput.type('adfg,qwt,zxcvb');
+    testNameInput('adfg,qwt,zxcvb', true, () => {
+      cy.get('#car-name-input-set').should('be.disabled');
+      cy.get('#count-input-set').should('be.visible');
+    });
+  });
 
-    const carNameSubmitButton = cy.get('#car-name-input-set').find('button');
-    carNameSubmitButton.click();
-
-    cy.get('#car-name-input-set').should('be.disabled');
-    cy.get('#count-input-set').should('be.visible');
+  it('5글자 이하의 이름들을 입력하고 엔터를 누르면 자기 fieldset을 disable하고 다음 fieldset을 켠다.', () => {
+    testNameInput('adfg,qwt,zxcvb', false, () => {
+      cy.get('#car-name-input-set').should('be.disabled');
+      cy.get('#count-input-set').should('be.visible');
+    });
   });
 });
