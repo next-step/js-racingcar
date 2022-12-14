aa1184d98ba52ecb624e0a36be575adf3b53e75a
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2022-12-14 11:56:32 +0900

    refector : 타입 체크 메서드의 매직넘버 명시

--- src/js/validate/validate.js
+++ src/js/validate/validate.js
@@ -1,5 +1,8 @@
 import { alertIfError } from '../view/main.js';
 
+const sliceBegin = 8;
+const sliceEnd = -1;
+
 export const catchMessage = message => alertIfError(message);
 
-export const getType = target => Object.prototype.toString.call(target).slice(8, -1);
+export const getType = target => Object.prototype.toString.call(target).slice(sliceBegin, sliceEnd);
