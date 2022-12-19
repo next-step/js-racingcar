998a3f72778e5c492548402e3c0ff48d830a97b1
yousooa <kinsk2839@naver.com>
2022-12-19 16:42:33 +0900

    test: 게임 플레이 섹션이 보이는지 확인하는 테스트 추가

--- cypress/e2e/gamePlay.cy.js
+++ cypress/e2e/gamePlay.cy.js
@@ -1,4 +1,4 @@
-import { INPUT_CONDITION } from '../../src/js/constants/condition.js';
+import { GAME_CONDITION, INPUT_CONDITION } from '../../src/js/constants/condition.js';
 import { SELECTOR } from '../../src/js/constants/selector.js';
 
 describe('게임 플레이 테스트', () => {
@@ -8,19 +8,28 @@ describe('게임 플레이 테스트', () => {
   };
 
   before(() => {
+    cy.clock();
     cy.visit('../../index.html');
     cy.registerNamesByButton(TYPE.CAR_NAMES);
     cy.registerCountByButton(TYPE.TRIAL_COUNT);
   });
 
-  it('이름과 게임 횟수를 입력 완료한 후 자동차 이름을 화면에 표시한다.', () => {
-    const carNamesToArray = TYPE.CAR_NAMES.split(INPUT_CONDITION.SEPARATOR_CAR_NAME).map((name) =>
-      name.trim()
-    );
+  context('이름과 시도 횟수를 입력 완료했을 때', () => {
+    it('자동차 이름을 화면에 표시한다.', () => {
+      const carNamesToArray = TYPE.CAR_NAMES.split(INPUT_CONDITION.SEPARATOR_CAR_NAME).map((name) =>
+        name.trim()
+      );
 
-    cy.get(SELECTOR.CAR_NAME).each(($ele, idx) => {
-      cy.get($ele).should('be.visible');
-      cy.get($ele).should('contain', carNamesToArray[idx]);
+      cy.get(SELECTOR.CAR_NAME).each(($ele, idx) => {
+        cy.get($ele).should('be.visible');
+        cy.get($ele).should('contain', carNamesToArray[idx]);
+      });
+    });
+
+    it('각 자동차의 전진 상황을 화면에 표시한다.', () => {
+      cy.clock();
+      cy.runAllTurns(TYPE.TRIAL_COUNT);
+      cy.get(SELECTOR.PLAY_GAME).should('be.visible');
     });
   });
 });
