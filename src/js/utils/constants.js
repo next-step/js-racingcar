e9dda5028a3222226f748b942297ae5eb5544ff2
cheolhwan <publizm@gmail.com>
2023-01-03 16:20:59 +0900

    fix: 이름 글자수 제한 매직넘버 제거

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,6 +1,6 @@
 export const ERROR_MESSAGES = {
   NOT_ALLOW_EMPTY_VALUE: "빈 값은 입력할 수 없습니다.",
-  NOT_ALLOW_NAME_LENGTH: "자동차 이름은 5자 이상 입력할 수 없습니다.",
+  NOT_ALLOW_NAME_LENGTH: "자동차 이름은 5자를 초과해서 입력할 수 없습니다.",
   NOT_ALLOW_NAME_TYPE: "자동차 이름은 한/영문으로만 입력이 가능합니다.",
   NOT_ALLOW_COUNT: "0보다 큰 수를 입력해주세요.",
 };
@@ -12,6 +12,8 @@ export const ACTION_TYPE = {
   ATTEMPT_COUNT: "ATTEMPT_COUNT",
   RESET: "RESET",
 };
+
+export const MAX_CAR_NAME_LENGTH = 5;
 
 export const MAX_NUMBER = 9;
 export const MINIMUM_CONDITIONS_FOR_MOVEMENT = 4;
