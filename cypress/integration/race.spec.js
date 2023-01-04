d5c1b93adcb6d64d99452307f65631894fb7f69b
Woo KyungJun <natcho9010@gmail.com>
2023-01-04 19:44:50 +0900

    correct: add 초

--- cypress/integration/race.spec.js
+++ cypress/integration/race.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const 초 = 1000;
+
 describe('race test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5000');
@@ -12,7 +14,7 @@ describe('race test', () => {
 
   it('반복횟수를 3으로 입력했으면, 3000밀리초 약간 후에 경기가 끝나서 결과 창이 보여야 합니다.', () => {
     const iterationCount = 3;
-    const waitMs = iterationCount * 1000;
+    const waitMs = iterationCount * 초;
 
     const countInput = cy.get('#count-input-set').find('input');
     countInput.type(String(iterationCount));
