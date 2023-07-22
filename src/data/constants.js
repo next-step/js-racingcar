4f15dd87fb49ec83035028f4609e929ca7016e29
bxxmi <bmi6638@gmail.com>
2023-07-22 21:24:37 +0900

    [feat] 최소한 입력되어야 할 자동차 갯수와 자동차 이름 길이 상수 관리

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,4 +1,7 @@
 export const MAX_RACING_ROUNDS = 5;
+export const MIN_CAR_COUNT = 2;
 
 export const MIN_FORWARD_RANGE = 0;
 export const MAX_FORWARD_RANGE = 9;
+
+export const MAX_CAR_NAME_LENGTH = 5;
