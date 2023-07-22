cc66469f41b315dc67c0d7eb039d2bbeb3428041
Gn0lee <jinho605@gmail.com>
2023-07-22 13:27:06 +0900

    자동차 이름 유효성 검사 기능구현

--- src/class/RacingCarGame.js
+++ src/class/RacingCarGame.js
@@ -3,6 +3,15 @@ import * as readline from "readline";
 const CAR_NAME_INPUT_GUIDE =
   "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n";
 
+const CAR_NAME_SEPARATOR = ",";
+
+const CAR_NAME_LENGTH = 5;
+
+const ERROR_MESSAGES = {
+  INVALID_EMPTY_NAME: "자동차 이름은 빈값일 수 없습니다.",
+  INVALID_NAME_LENGTH: "자동차 이름은 5자를 넘길 수 없습니다.",
+};
+
 export default class RacingCarGame {
   constructor() {
     this.cars = new Map();
@@ -14,11 +23,43 @@ export default class RacingCarGame {
 
   startGame() {
     this.readline.question(CAR_NAME_INPUT_GUIDE, (answer) => {
-      console.log(answer);
+      const splitedAnswer = answer.split(CAR_NAME_SEPARATOR);
+
+      try {
+        this.validateCarName(splitedAnswer);
+      } catch (error) {
+        if (
+          error instanceof Error &&
+          error.message === ERROR_MESSAGES.INVALID_EMPTY_NAME
+        ) {
+          console.log(ERROR_MESSAGES.INVALID_EMPTY_NAME);
+        }
+
+        if (
+          error instanceof Error &&
+          error.message === ERROR_MESSAGES.INVALID_NAME_LENGTH
+        ) {
+          console.log(ERROR_MESSAGES.INVALID_NAME_LENGTH);
+        }
+      } finally {
+        this.endGame();
+      }
     });
   }
 
   endGame() {
     this.readline.close();
+  }
+
+  validateCarName(names) {
+    for (const name of names) {
+      if (name.trim().length < 1) {
+        throw new Error(ERROR_MESSAGES.INVALID_EMPTY_NAME);
+      }
+
+      if (name.length > CAR_NAME_LENGTH) {
+        throw new Error(ERROR_MESSAGES.INVALID_NAME_LENGTH);
+      }
+    }
   }
 }
