05b3abb838715165c850d99f250ea333ef3e80c7
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-23 01:33:42 +0900

    refactor: 하드코딩 값 삭제

--- src/Views/InputView.js
+++ src/Views/InputView.js
@@ -1,12 +1,10 @@
 import { Console } from '../utils';
+import { MESSAGE } from '../constants';
 
 export const InputView = {
   readCarNames(callback) {
-    Console.readLine(
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
-      (userInput) => {
-        callback(userInput);
-      }
-    );
+    Console.readLine(MESSAGE.CONSOLE.READ.CAR_NAME, (userInput) => {
+      callback(userInput);
+    });
   },
 };
---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+const READ = Object.freeze({
+  CAR_NAME: '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).',
+});
+
+const CONSOLE = Object({ READ });
+
+export const MESSAGE = Object.freeze({ CONSOLE });
