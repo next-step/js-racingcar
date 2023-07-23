343f6d6af63e3543a778c4558fb84efb5bf48360
Hoseok Na <hsna7024@gmail.com>
2023-07-24 00:38:47 +0900

    test: 자동차 전진 테스트

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -15,4 +15,23 @@ describe('자동차 class 속성 테스트', () => {
   test('자동차의 주행거리의 초기값은 0이다.', () => {
     expect(car.getDistanceDriven()).toBe(0);
   });
+
+  describe('자동차 전진 테스트', () => {
+    test('자동차가 전진시 주행거리의 값이 1 늘어난다.', () => {
+      const prevDistanceDriven = car.getDistanceDriven();
+
+      car.moveForward();
+
+      expect(car.getDistanceDriven()).toBe(prevDistanceDriven + 1);
+    });
+
+    test('자동차가 두번 전진시 주행거리의 값이 2 늘어난다.', () => {
+      const prevDistanceDriven = car.getDistanceDriven();
+
+      car.moveForward();
+      car.moveForward();
+
+      expect(car.getDistanceDriven()).toBe(prevDistanceDriven + 2);
+    });
+  });
 });
