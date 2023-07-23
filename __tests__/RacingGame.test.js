ba0fd8ca8245fe9faeb1cf11d32784d5ba84cce9
Hoseok Na <hsna7024@gmail.com>
2023-07-24 01:53:08 +0900

    test: 경주 중인 자동차들 중에 가장 멀리간 거리를 구하는 테스트

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -9,3 +9,19 @@ describe('레이싱 게임 세팅 테스트', () => {
     expect(racingGame.getCars().every((car) => car instanceof Car)).toBe(true);
   });
 });
+
+describe('레이싱 게임 우승 테스트', () => {
+  test('가장 멀리 간 거리를 구할 수 있다.', () => {
+    const carNames = ['자동차1', '자동차2', '자동차3'];
+    const cars = carNames.map((name) => new Car(name));
+    const racingGame = new RacingGame(cars);
+
+    cars.forEach((car, index) => {
+      for (let i = 0; i < index; i++) {
+        car.moveForward();
+      }
+    });
+
+    expect(racingGame.getMaxDistanceDriven()).toBe(2);
+  });
+});
