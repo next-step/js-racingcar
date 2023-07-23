e389a58d78e5801cac65715f4aff677d9a0f199f
Jason <jasondal91@gmail.com>
2023-07-23 17:31:34 +0900

    feat: 자동차 전진 구현

--- src/constants/car.js
+++ src/constants/car.js
@@ -1,1 +1,2 @@
 export const NAME_LENGTH_LIMIT = 5
+export const MOVE_POSSIBLE_NUMBER = 4
--- src/utils/common.js
+++ src/utils/common.js
@@ -2,9 +2,22 @@ export const isStringOverLength = (value, length) => {
     return value.toString().length > length
 }
 
+export const isMoreThanNumber = (value, number) => {
+    if(typeof value !== 'number'){
+        return false;
+    }
+
+    return value >= number
+}
+
 export const getStringFromArray = (array) => {
     if(!Array.isArray(array)){
         return 
     }
     return array.join(', ')
+}
+
+export const getRandomInt = (max) => {
+    const result = Math.floor(Math.random() * max)
+    return result
 }
