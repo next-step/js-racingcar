e65fb9f916d7327000e779e706b34478db77093f
Jason <jasondal91@gmail.com>
2023-07-23 18:05:25 +0900

    feat: 자동차 경주 5회 고정 구현

--- src/domain/carRace.js
+++ src/domain/carRace.js
@@ -1,18 +1,25 @@
-import { getStringFromArray } from '../utils/common'
+import { RACE_LAP_LIMIT } from '../constants';
+import { getStringFromArray } from '../utils/common';
 
 export default class CarRace {
-#participants
-    constructor(participants){
-        this.#participants = participants
+  #participants;
+  constructor(participants) {
+    this.#participants = participants;
+  }
+
+  get participants() {
+    return this.#participants;
+  }
+
+  get participantNames() {
+    const names = this.#participants.map((car) => car.name);
+    const result = getStringFromArray(names);
+    return result;
+  }
+
+  start() {
+    for (let i = 0; i < RACE_LAP_LIMIT; i++) {
+      this.#participants.forEach((car) => car.runOneLap());
     }
-
-get participants(){
-    return this.#participants
-}
-
-get participantNames(){
-    const names = this.#participants.map((car) => car.name)
-    const result = getStringFromArray(names)
-    return result
-}
+  }
 }
