a5eeff5379f8d7c216c2d012490016ce4759ae1c
Jason <jasondal91@gmail.com>
2023-07-23 16:21:14 +0900

    test: 자동차 이름 쉼표 구분 테스트 추가

---
+++ __tests__/CarRace.test.js
@@ -0,0 +1,9 @@
+import Car from '../src/domain/car'
+
+describe('CarRace', () => {
+    it('자동차 이름은 쉼표(,)를 기준으로 구분한다.', () => {
+        const carRace = new CarRace([new Car('광민'), new Car('문광민'), new Car('Jason') ])
+        
+        expect(carRace.participantNames).toBe('광민, 문광민, Jason')
+    })
+})
