2174d4a0ab4b10ffe3639b2a881d63085610a4bb
Hoseok Na <hsna7024@gmail.com>
2023-07-24 00:21:28 +0900

    test: 자동차 주행거리 속성 테스트

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -7,4 +7,11 @@ describe('자동차 class 속성 테스트', () => {
 
     expect(car.getName()).toBe(carName);
   });
+
+  test('자동차의 주행거리의 초기값은 0이다.', () => {
+    const carName = '자동차';
+    const car = new Car(carName);
+
+    expect(car.getDistanceDriven()).toBe(0);
+  });
 });
