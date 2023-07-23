dab27c51ec673856c8414f6006262e306dd4bfca
Hoseok Na <hsna7024@gmail.com>
2023-07-24 01:40:14 +0900

    test: RacingGame에 자동차 이름들이 아닌 생성된 자동차 클래스를 받도록 수정

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -3,7 +3,8 @@ import RacingGame from '../src/RacingGame';
 
 describe('레이싱 게임 세팅 테스트', () => {
   test('경주에 참여할 자동차들이 있다.', () => {
-    const racingGame = new RacingGame(['자동차1', '자동차2', '자동차3']);
+    const carNames = ['자동차1', '자동차2', '자동차3'];
+    const racingGame = new RacingGame(carNames.map((name) => new Car(name)));
 
     expect(racingGame.getCars().every((car) => car instanceof Car)).toBe(true);
   });
