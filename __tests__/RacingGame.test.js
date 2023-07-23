1d3b2da1a7ed86f2b65f6c0446f751547deb14b1
Hoseok Na <hsna7024@gmail.com>
2023-07-24 02:01:13 +0900

    test: RacingGame 클래스 생성 중복 리팩토링

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -1,27 +1,33 @@
 import Car from '../src/Car';
 import RacingGame from '../src/RacingGame';
 
-describe('레이싱 게임 세팅 테스트', () => {
-  test('경주에 참여할 자동차들이 있다.', () => {
-    const carNames = ['자동차1', '자동차2', '자동차3'];
-    const racingGame = new RacingGame(carNames.map((name) => new Car(name)));
+describe('RacingGame 테스트', () => {
+  const carNames = ['자동차1', '자동차2', '자동차3'];
+  let cars = null;
+  let racingGame = null;
 
-    expect(racingGame.getCars().every((car) => car instanceof Car)).toBe(true);
+  beforeEach(() => {
+    cars = carNames.map((name) => new Car(name));
+    racingGame = new RacingGame(cars);
   });
-});
 
-describe('레이싱 게임 우승 테스트', () => {
-  test('가장 멀리 간 거리를 구할 수 있다.', () => {
-    const carNames = ['자동차1', '자동차2', '자동차3'];
-    const cars = carNames.map((name) => new Car(name));
-    const racingGame = new RacingGame(cars);
-
-    cars.forEach((car, index) => {
-      for (let i = 0; i < index; i++) {
-        car.moveForward();
-      }
+  describe('레이싱 게임 세팅 테스트', () => {
+    test('경주에 참여할 자동차들이 있다.', () => {
+      expect(racingGame.getCars().every((car) => car instanceof Car)).toBe(
+        true
+      );
     });
+  });
 
-    expect(racingGame.getMaxDistanceDriven()).toBe(2);
+  describe('레이싱 게임 우승 테스트', () => {
+    test('가장 멀리 간 거리를 구할 수 있다.', () => {
+      cars.forEach((car, index) => {
+        for (let i = 0; i < index; i++) {
+          car.moveForward();
+        }
+      });
+
+      expect(racingGame.getMaxDistanceDriven()).toBe(2);
+    });
   });
 });
