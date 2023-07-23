8f4146ccf395786b31705b0a2d393fb0a9c229b8
Hoseok Na <hsna7024@gmail.com>
2023-07-24 00:25:43 +0900

    test: 자동차 class 생성 중복되는 부분 리팩토링

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,17 +1,18 @@
 import Car from '../src/Car';
 
 describe('자동차 class 속성 테스트', () => {
-  test('Car class는 name이 있다.', () => {
-    const carName = '자동차';
-    const car = new Car(carName);
+  const carName = '자동차';
+  let car = null;
 
+  beforeEach(() => {
+    car = new Car(carName);
+  });
+
+  test('Car class는 name이 있다.', () => {
     expect(car.getName()).toBe(carName);
   });
 
   test('자동차의 주행거리의 초기값은 0이다.', () => {
-    const carName = '자동차';
-    const car = new Car(carName);
-
     expect(car.getDistanceDriven()).toBe(0);
   });
 });
