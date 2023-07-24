3d16f9b0e9f38c93d6f6c90c9d86bb6a4538cb1c
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-24 09:27:51 +0900

    refactor: randomCarMovement 로직을 racingGame으로 이동한다

--- src/Models/Car.js
+++ src/Models/Car.js
@@ -8,16 +8,7 @@ export class Car {
     this.#name = name;
   }
 
-  randomCarMovement() {
-    const randomInt = getRandomIntInRange(
-      UTIL.RANDOM_INT_MIN,
-      UTIL.RANDOM_INT_MAX
-    );
-
-    if (randomInt >= RACING_GAME.MOVEMENT_THRESHOLD) this.advance();
-  }
-
   advance() {
-    this.score += RACING_GAME.SCORE_UNIT;
+    this.#score += RACING_GAME.SCORE_UNIT;
   }
 }
--- src/Models/RacingGame.js
+++ src/Models/RacingGame.js
@@ -1,4 +1,6 @@
 import { Car } from './';
+import { RACING_GAME } from '../constants';
+import { getRandomIntInRange } from '../utils';
 
 export class RacingGame {
   #cars;
@@ -15,7 +17,16 @@ export class RacingGame {
 
   #raceWithTotalRounds(totalRounds) {
     for (let round = 0; round < totalRounds; round++) {
-      this.#cars.forEach((car) => car.randomCarMovement());
+      this.#cars.forEach((car) => this.#randomCarMovement(car));
     }
+  }
+
+  #randomCarMovement(car) {
+    const randomInt = getRandomIntInRange(
+      UTIL.RANDOM_INT_MIN,
+      UTIL.RANDOM_INT_MAX
+    );
+
+    if (randomInt >= RACING_GAME.MOVEMENT_THRESHOLD) car.advance();
   }
 }
