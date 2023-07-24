a9fa009d56507399fcd298d15169fec09fc5fb3c
Hoseok Na <hsna7024@gmail.com>
2023-07-24 12:12:27 +0900

    test: 자동차 전진 조건 테스트 위치 수정

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -16,6 +16,24 @@ describe('자동차 class 속성 테스트', () => {
     expect(car.getDistanceDriven()).toBe(0);
   });
 
+  describe('자동차 전진 조건 테스트', () => {
+    test('랜덤으로 생성한 숫자가 4이상이면 자동차가 전진할 수 있는 상태이다', () => {
+      jest.spyOn(global.Math, 'random').mockReturnValue(0.4);
+
+      expect(car.canMoveForward()).toBe(true);
+
+      jest.spyOn(global.Math, 'random').mockRestore();
+    });
+
+    test('랜덤으로 생성한 숫자가 4보다 작으면 자동차가 전진할 수 없는 상태이다.', () => {
+      jest.spyOn(global.Math, 'random').mockReturnValue(0.3);
+
+      expect(car.canMoveForward()).toBe(false);
+
+      jest.spyOn(global.Math, 'random').mockRestore();
+    });
+  });
+
   describe('자동차 전진 테스트', () => {
     test('자동차가 전진시 주행거리의 값이 1 늘어난다.', () => {
       const prevDistanceDriven = car.getDistanceDriven();
@@ -32,24 +50,6 @@ describe('자동차 class 속성 테스트', () => {
       car.moveForward();
 
       expect(car.getDistanceDriven()).toBe(prevDistanceDriven + 2);
-    });
-
-    describe('자동차 전진 조건 테스트', () => {
-      test('랜덤으로 생성한 숫자가 4이상이면 자동차가 전진할 수 있는 상태이다', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.4);
-
-        expect(car.canMoveForward()).toBe(true);
-
-        jest.spyOn(global.Math, 'random').mockRestore();
-      });
-
-      test('랜덤으로 생성한 숫자가 4보다 작으면 자동차가 전진할 수 없는 상태이다.', () => {
-        jest.spyOn(global.Math, 'random').mockReturnValue(0.3);
-
-        expect(car.canMoveForward()).toBe(false);
-
-        jest.spyOn(global.Math, 'random').mockRestore();
-      });
     });
   });
 });
