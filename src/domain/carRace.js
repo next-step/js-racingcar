b006a3f447603df00e07d56afc42e99522e31943
Jason <jasondal91@gmail.com>
2023-07-24 17:33:11 +0900

    feat: winner 구하는 로직 추가

--- src/domain/carRace.js
+++ src/domain/carRace.js
@@ -3,6 +3,9 @@ import { getStringFromArray } from '../utils/common';
 
 export default class CarRace {
   #participants;
+  #winners;
+  #isRaceStarted = false;
+
   constructor(participants) {
     this.#participants = participants;
   }
@@ -17,9 +20,26 @@ export default class CarRace {
     return result;
   }
 
+  get winners() {
+    if (!this.#participants || !this.#isRaceStarted) {
+      return;
+    }
+
+    const sortedParticipants = [...this.#participants].sort(
+      (car1, car2) => car2.distance - car1.distance
+    );
+
+    this.#winners = sortedParticipants.filter(
+      (car) => sortedParticipants[0].distance === car.distance
+    );
+
+    return this.#winners;
+  }
+
   start() {
     for (let i = 0; i < RACE_LAP_LIMIT; i++) {
       this.#participants.forEach((car) => car.runOneLap());
     }
+    this.#isRaceStarted = true;
   }
 }
