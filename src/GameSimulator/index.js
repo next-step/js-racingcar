f4e9b5a607694c73c6780696851ef5ef57457e88
Hoseok Na <hsna7024@gmail.com>
2023-07-24 23:27:13 +0900

    feat: GameSimulator 자동차 입력 받기 구현

---
+++ src/GameSimulator/index.js
@@ -0,0 +1,19 @@
+import Car from '../Car/index.js';
+import RacingGame from '../RacingGame/index.js';
+import { getUserInputByQuestion } from '../utils/getUserInputByQuestion.js';
+import { splitStringByComma } from '../utils/splitStringByComma.js';
+
+class GameSimulator {
+  #racingGame;
+
+  async startGame() {
+    const inputString = await getUserInputByQuestion(
+      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
+    );
+    const carNames = splitStringByComma(inputString);
+
+    this.#racingGame = new RacingGame(carNames.map((name) => new Car(name)));
+  }
+}
+
+export default GameSimulator;
