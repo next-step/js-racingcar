706f053311aa3569c4cec11dc8342a3204b0d62c
Hoseok Na <hsna7024@gmail.com>
2023-07-25 20:48:10 +0900

    refactor: game 세팅 부분 함수로 분리

--- src/GameSimulator/index.js
+++ src/GameSimulator/index.js
@@ -6,13 +6,17 @@ import { splitStringByComma } from '../utils/splitStringByComma.js';
 class GameSimulator {
   #racingGame;
 
-  async startGame() {
+  async setRacingGame() {
     const inputString = await getUserInputByQuestion(
       '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
     );
     const carNames = splitStringByComma(inputString);
 
     this.#racingGame = new RacingGame(carNames.map((name) => new Car(name)));
+  }
+
+  async startGame() {
+    await this.setRacingGame();
   }
 }
 
