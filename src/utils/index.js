28141f16063ee0d818519d1c952919652333c6db
손진영 <sm099@naver.com>
2023-07-25 22:27:13 +0900

    refactor: InputView 관련 함수들을 InputView 하나의 모듈에서 관리하도록 변경

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,3 @@
-export * from './view.js';
 export * from './validate.js';
 export * from './racingTrack.js';
 export * from './racingWinners.js';
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,10 +1,44 @@
-import { getUserInputByQuestion, processUserInput } from '../utils/view.js';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { INPUT_MESSAGE } from '../constants/index.js';
+import Validator from '../Validator.js';
+import { convertStringToArray } from '../utils/commons.js';
+import OutputView from './OutputView.js';
 
-const InputView = {
-  async input(message) {
-    const userInput = await getUserInputByQuestion(message);
-    return processUserInput(userInput, message);
-  },
-};
+const InputView = (function InputView() {
+  const createUserInputByQuestion = async (message) => {
+    const readMachine = readline.createInterface({ input, output });
+    const userInput = await readMachine.question(message);
+    readMachine.close();
+    return userInput;
+  };
+
+  const getUserInput = async (message) => {
+    try {
+      const userInput = await createUserInputByQuestion(message);
+      Validator.check(userInput, message);
+      return userInput;
+    } catch (error) {
+      OutputView.print(error.message);
+      return getUserInput(message);
+    }
+  };
+
+  const processUserInput = (userInput, message) => {
+    switch (message) {
+      case INPUT_MESSAGE.RACING_CAR:
+        return convertStringToArray(userInput, ',');
+      default:
+        return userInput;
+    }
+  };
+
+  return {
+    async input(message) {
+      const userInput = await getUserInput(message);
+      return processUserInput(userInput, message);
+    },
+  };
+})();
 
 export default InputView;
