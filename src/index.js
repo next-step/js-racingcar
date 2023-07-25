8561b63d29cfb5b06a7f5d20c134e0a4a78e6153
Jason <jasondal91@gmail.com>
2023-07-25 17:14:55 +0900

    refactor: carRaceView 변수 삭제

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import Car from './domain/car';
 import CarRace from './domain/carRace';
 import CarRaceView from './view';
 
-const carRaceView = new CarRaceView();
 const carRace = new CarRace([
   new Car('pobi'),
   new Car('crong'),
   new Car('honux'),
 ]);
 
-carRace.start(carRaceView);
+carRace.start(new CarRaceView());
