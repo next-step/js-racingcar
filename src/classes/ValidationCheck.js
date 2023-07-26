f23a4c698dc0b4031244a157d2e28a1c2a96bf8a
Finn <bmi6638@gmail.com>
2023-07-26 21:59:19 +0900

    [fix] 에러 메세지 내 상수 적용

--- src/classes/ValidationCheck.js
+++ src/classes/ValidationCheck.js
@@ -3,7 +3,7 @@ import { MAX_CAR_NAME_LENGTH, MIN_CAR_COUNT } from "../data/constants";
 export default class ValidationCheck {
   validateEmptyName(carList) {
     carList.forEach((car) => {
-      if (car === "") throw new Error("빈 값은 입력할 수 없습니다.");
+      if (!car || car.includes(" ")) throw new Error("빈 값은 입력할 수 없습니다.");
     });
   }
 
@@ -17,12 +17,15 @@ export default class ValidationCheck {
 
   validateNameLength(carList) {
     carList.forEach((car) => {
-      if (car.length > MAX_CAR_NAME_LENGTH) throw new Error("이름의 길이는 5자를 넘길 수 없습니다.");
+      if (car.length > MAX_CAR_NAME_LENGTH) {
+        throw new Error(`이름의 길이는 ${MAX_CAR_NAME_LENGTH}자를 넘길 수 없습니다.`);
+      }
     });
   }
 
   validateCarCount(carList) {
-    if (carList.length < MIN_CAR_COUNT) throw new Error("2개 이상의 자동차를 입력해주세요.");
-    return carList;
+    if (carList.length < MIN_CAR_COUNT) {
+      throw new Error(`${MIN_CAR_COUNT}개 이상의 자동차를 입력해주세요.`);
+    }
   }
 }
