e69df7b6abe6a147f6344290035ad9bfec2b8e5e
gusrb3164 <gusrb3164@gmail.com>
2023-07-26 17:28:00 +0900

    feat: 자동차 전진 조건 및 전진 메서드 추가

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,5 +1,21 @@
 export class Controller {
-  static MAXIMUM_RACING_NUMBER = 5;
+  static MAXIMUM_CAR_NAME_LENGTH = 5;
+  static FORWARD_THRESHOLD = 4;
+  #cars = [];
+
+  race = () => {
+    this.#currentRaceNumber += 1;
+    this.#cars.forEach(car => {
+      if (this.canForward()) {
+        car.move();
+      }
+    });
+  };
+
+  canForward = () => {
+    const randomNumber = Math.floor(Math.random() * 10);
+    return randomNumber >= Controller.FORWARD_THRESHOLD;
+  };
 
   isValidCarNames = carNames => {
     return carNames.every(carName => carName.length <= Controller.MAXIMUM_CAR_NAME_LENGTH);
