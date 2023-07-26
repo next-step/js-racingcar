76f40c6a52d95b4b110edfa43587345c58a1093b
손진영 <sm099@naver.com>
2023-07-26 17:30:41 +0900

    refactor(RacingTrack): 클래스 내 메서드들이 하나의 역할만 하도록 변경

--- src/model/RacingTrack.js
+++ src/model/RacingTrack.js
@@ -1,55 +1,56 @@
 import { EXIT_COUNT, INIT_RACING_COUNT } from '../constants/index.js';
-import RacingCars from './RacingCars.js';
+import { RacingCars, RacingRecorder } from './index.js';
 
 class RacingTrack {
   #racingCount;
 
-  #racingResult;
-
   #racingCars;
+
+  #racingRecorder;
 
   constructor() {
     this.#racingCount = INIT_RACING_COUNT;
-    this.#racingResult = [];
     this.#racingCars = new RacingCars();
-  }
-
-  setRacingCars(racingCars) {
-    this.#racingCars.initMoveStatus(racingCars);
+    this.#racingRecorder = new RacingRecorder();
   }
 
   getRacingResult() {
-    return this.#racingResult;
+    return this.#racingRecorder.getRacingResult();
   }
 
-  #updateMoveStatus(racers) {
-    return this.#racingCars.move(racers);
+  getRacingWinners() {
+    return this.#racingRecorder.getRacingWinners();
   }
 
-  #updateRacingResult(newRacingCarStatus) {
-    const newResult = Object.entries({ ...newRacingCarStatus })
-      .map((racerInfo) => racerInfo.join(' : '))
-      .join('\n');
-    this.#racingResult.push(newResult);
+  updateMoveStatus(racingCars) {
+    this.#racingCars.initMoveStatus(racingCars);
+  }
+
+  #updateMoveStatus(carNames) {
+    return this.#racingCars.move(carNames);
+  }
+
+  #updateRacingResults(newMoveStatus) {
+    this.#racingRecorder.updateResults(newMoveStatus);
+  }
+
+  #updateRacingWinners() {
+    this.#racingRecorder.updateWinners();
   }
 
   #minusRacingCount() {
     this.#racingCount -= 1;
   }
 
-  #requestCarNames() {
-    return this.#racingCars.getCarNames();
-  }
-
   #isExitRace() {
     return this.#racingCount === EXIT_COUNT;
   }
 
-  race() {
-    const carNames = this.#requestCarNames();
+  race(carNames) {
     while (!this.#isExitRace()) {
       const newMoveStatus = this.#updateMoveStatus(carNames);
-      this.#updateRacingResult(newMoveStatus);
+      this.#updateRacingResults(newMoveStatus);
+      this.#updateRacingWinners();
       this.#minusRacingCount();
     }
   }
