dabae82e969932130d9096296e3f079cdb8759c3
손진영 <sm099@naver.com>
2023-07-26 17:31:32 +0900

    refactor(Controller): GameController 내 메서드들이 하나의 역할만 수행하도록 변경

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -1,46 +1,52 @@
 import { INPUT_MESSAGE, OUTPUT_MESSAGE } from '../constants/message.js';
-import { RacingTrack, RacingWinners } from '../model/index.js';
+import { RacingTrack } from '../model/index.js';
 import { InputView, OutputView } from '../view/index.js';
 
 class GameController {
   constructor() {
     this.racingTrack = new RacingTrack();
-    this.racingWinners = new RacingWinners();
   }
 
-  #printRaceResult() {
-    const results = this.racingTrack.getRacingResult();
+  static async #getRacingCarNames() {
+    const racingCarNames = await InputView.input(INPUT_MESSAGE.RACING_CAR);
+    return racingCarNames;
+  }
+
+  static #printRaceResult(results) {
     OutputView.print(OUTPUT_MESSAGE.RESULT);
     results.forEach((result) => {
       OutputView.print(`${result}\n`);
     });
   }
 
-  async #settingRacingCar() {
-    const racingCars = await InputView.input(INPUT_MESSAGE.RACING_CAR);
-    this.racingTrack.setRacingCars(racingCars);
-  }
-
-  #requestRaceStart() {
-    this.racingTrack.race();
-  }
-
-  #requestRacingWinners() {
-    const results = this.racingTrack.getRacingResult();
-    this.racingWinners.setRacingWinners(results);
-    return this.racingWinners.getRacingWinners();
-  }
-
-  #printRacingWinners() {
-    const winners = this.#requestRacingWinners();
+  static #printRacingWinners(winners) {
     OutputView.print(OUTPUT_MESSAGE.WINNERS(winners));
   }
 
+  #getRacingResult() {
+    return this.racingTrack.getRacingResult();
+  }
+
+  #getRacingWinners() {
+    return this.racingTrack.getRacingWinners();
+  }
+
+  #updateRacingCars(racingCarNames) {
+    this.racingTrack.updateMoveStatus(racingCarNames);
+  }
+
+  #raceStart(carNames) {
+    this.racingTrack.race(carNames);
+  }
+
   async run() {
-    await this.#settingRacingCar();
-    this.#requestRaceStart();
-    this.#printRaceResult();
-    this.#printRacingWinners();
+    const racingCarNames = await GameController.#getRacingCarNames();
+    this.#updateRacingCars(racingCarNames);
+    this.#raceStart(racingCarNames);
+    const result = this.#getRacingResult();
+    const racingWinners = this.#getRacingWinners();
+    GameController.#printRaceResult(result);
+    GameController.#printRacingWinners(racingWinners);
   }
 }
 
