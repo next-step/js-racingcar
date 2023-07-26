16fc3797fba1c55f1a94d75f5542cd927afff10d
Finn <bmi6638@gmail.com>
2023-07-26 20:51:22 +0900

    [feat] document 업데이트 및 테스트 코드 작성 (진행 중)

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,16 +1,27 @@
-describe("자동차 경주 게임 테스트입니다.", () => {
-  // case 1. 자동차 이름 설정
-  // case 2. 자동차 전진 시 '-' 출력
-  // case 3. 우승자 추출
-});
+import ValidationCheck from "../src/classes/ValidationCheck";
+import RacingController from "../src/controllers/RacingController";
+import RacingModel from "../src/model/RacingModel";
+import { createRandomValue, readlineInterface } from "../src/utils/util";
 
-describe("자동차 경주 게임 예외 사항에 대한 테스트입니다.", () => {
-  // case 1. 자동차의 이름
-  // case 1-1. 자동차의 이름을 빈 값으로 설정했을 경우 -> default name을 부여합니다.
-  // case 1-2. 자동차의 이름 길이가 5 이상 시 에러를 반환합니다.
-  // case 1-1. 입력한 자동차가 1개 이하일 경우 에러를 반환합니다.
-  // case 2. 자동차 전진 조건
-  // case 2-1. 무작위 값이 4 이하일 경우 전진하지 않습니다.
-  // case 3. 사용자가 잘못된 값을 작성한 경우 (case 1과 연관)
-  // case 3-1. 자동차의 이름 중 쉼표를 제외한 특수 문자 또는 공백이 들어갈 경우 에러를 반환합니다.
+describe("자동차 경주 게임 테스트입니다.", () => {
+  const validation = new ValidationCheck();
+  const racingModel = new RacingModel();
+  const racingController = new RacingController();
+
+  test("자동차 이름은 5자 이하로 작성해야 합니다.", () => {
+    const carNames = ["car1", "car2", "car3", "toolongname"];
+    expect(() => validation.validateNameLength(carNames)).toThrow("이름의 길이는 5자를 넘길 수 없습니다.");
+  });
+
+  test("자동차 이름은 비어있을 수 없습니다.", () => {});
+
+  test("자동차 이름은 중복될 수 없습니다.", () => {});
+
+  test("경기 진행을 위해 자동차의 이름을 최소 2개 이상 작성해야 합니다.", () => {});
+
+  test("사용자가 잘못된 값을 입력한 경우 게임은 바로 종료됩니다.", () => {});
+
+  test("랜덤 값이 4이상이 나와야 전진할 수 있습니다.", () => {});
+
+  test("우승자가 여러명일 때 콤마로 구분해서 출력합니다.", () => {});
 });
