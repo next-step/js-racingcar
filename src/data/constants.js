b2c13ba143d421f961fff61b69162d5a86efbc55
Finn <bmi6638@gmail.com>
2023-07-26 20:45:29 +0900

    [feat] 필요없는 상수 제거

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,7 +1,3 @@
 export const MAX_RACING_ROUNDS = 5;
 export const MIN_CAR_COUNT = 2;
-
-export const MIN_FORWARD_RANGE = 0;
-export const MAX_FORWARD_RANGE = 9;
-
 export const MAX_CAR_NAME_LENGTH = 5;
