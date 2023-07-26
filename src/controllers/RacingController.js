2cba02e0f0a993b551fe067ee4b446a71a9d3a2a
bxxmi <bmi6638@gmail.com>
2023-07-27 01:01:25 +0900

    [fix] 자동차 이동 한계점 상수로 관리

--- src/controllers/RacingController.js
+++ src/controllers/RacingController.js
@@ -3,7 +3,7 @@ import RacingModel from "../model/RacingModel";
 import RacingView from "../view/RacingView";
 
 import { createRandomValue, readlineInterface } from "../utils/util";
-import { MAX_RACING_ROUNDS } from "../data/constants";
+import { MAX_RACING_ROUNDS, MOVE_CAR_THRESHOLD } from "../data/constants";
 
 export default class RacingController {
   racingModel;
@@ -15,9 +15,12 @@ export default class RacingController {
   }
 
   initCarNamesBeforeStartRacingGame() {
-    readlineInterface.question("자동차의 이름을 입력하세요. (각각의 이름은 콤마로 구분합니다.): ", (carNames) => {
-      this.checkValidationCarNames(carNames);
-    });
+    readlineInterface.question(
+      "자동차의 이름을 입력하세요. (각각의 이름은 콤마로 구분합니다.): ",
+      (carNames) => {
+        this.checkValidationCarNames(carNames);
+      }
+    );
   }
 
   checkValidationCarNames(carNames) {
@@ -45,12 +48,12 @@ export default class RacingController {
       carList.forEach((_, carIndex) => {
         const randomValue = createRandomValue();
 
-        if (randomValue >= 4) this.moveCar(carIndex);
+        if (randomValue >= MOVE_CAR_THRESHOLD) this.moveCar(carIndex);
       });
 
       this.racingView.showRacingGameProgress();
 
-      if (i === 4) {
+      if (i === MOVE_CAR_THRESHOLD) {
         this.racingView.showRacingGameWinners();
         this.readlineInterface.close();
       }
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,3 +1,7 @@
 export const MAX_RACING_ROUNDS = 5;
+
 export const MIN_CAR_COUNT = 2;
+
 export const MAX_CAR_NAME_LENGTH = 5;
+
+export const MOVE_CAR_THRESHOLD = 4;
