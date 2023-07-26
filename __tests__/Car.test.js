9b8497feda77977191952adefbc979ceee4cb947
Finn <bmi6638@gmail.com>
2023-07-26 21:59:49 +0900

    [feat] test 케이스 작성

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,23 +1,36 @@
 import ValidationCheck from "../src/classes/ValidationCheck";
 import RacingController from "../src/controllers/RacingController";
 import RacingModel from "../src/model/RacingModel";
-import { createRandomValue, readlineInterface } from "../src/utils/util";
 
 describe("자동차 경주 게임 테스트입니다.", () => {
   const validation = new ValidationCheck();
   const racingModel = new RacingModel();
   const racingController = new RacingController();
 
-  test("자동차 이름은 5자 이하로 작성해야 합니다.", () => {
-    const carNames = ["car1", "car2", "car3", "toolongname"];
-    expect(() => validation.validateNameLength(carNames)).toThrow("이름의 길이는 5자를 넘길 수 없습니다.");
+  test("경기 진행을 위해 자동차의 이름을 최소 2개 이상 작성해야 합니다.", () => {
+    expect(() => validation.validateCarCount(["car"])).toThrow("2개 이상의 자동차를 입력해주세요.");
+    expect(() => validation.validateCarCount([""])).toThrow("2개 이상의 자동차를 입력해주세요.");
   });
 
-  test("자동차 이름은 비어있을 수 없습니다.", () => {});
+  test("자동차 이름은 5자 이하로 작성해야 합니다.", () => {
+    expect(() => validation.validateNameLength(["carcars"])).toThrow("이름의 길이는 5자를 넘길 수 없습니다.");
+  });
 
-  test("자동차 이름은 중복될 수 없습니다.", () => {});
+  test("자동차 이름은 비어있을 수 없습니다.", () => {
+    expect(() => validation.validateEmptyName(["car1", "", "car2"])).toThrow("빈 값은 입력할 수 없습니다.");
+    expect(() => validation.validateEmptyName(["car1", "car 2", "car2"])).toThrow("빈 값은 입력할 수 없습니다.");
+    expect(() => validation.validateEmptyName(["car1", " ", "car2"])).toThrow("빈 값은 입력할 수 없습니다.");
+    expect(() => validation.validateEmptyName(["car1", " car", "car2"])).toThrow("빈 값은 입력할 수 없습니다.");
+  });
 
-  test("경기 진행을 위해 자동차의 이름을 최소 2개 이상 작성해야 합니다.", () => {});
+  test("자동차 이름은 중복될 수 없습니다.", () => {
+    expect(() => validation.validateDuplicateName(["car1", "car1", "car2"])).toThrow(
+      "중복되는 이름은 입력할 수 없습니다."
+    );
+    expect(() => validation.validateDuplicateName(["car1", "안녕", "car2", "안녕"])).toThrow(
+      "중복되는 이름은 입력할 수 없습니다."
+    );
+  });
 
   test("사용자가 잘못된 값을 입력한 경우 게임은 바로 종료됩니다.", () => {});
 
