7a831a34cfb6af47240ba7ab2b238393aaf3a1bc
함인규 <hig1207@midasin.com>
2023-07-27 19:49:50 +0900

    Car 이름 부여에 대한 Validation Check 기능 구현, Test 생성 밑 통과

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -6,6 +6,12 @@ describe("Car", () => {
   beforeEach(() => {
     car = new Car(DEFAULT_NAME);
   });
+  it("자동차의 이름이 한글자 미만일 경우 에러를 발생시킨다.", () => {
+    const name = "";
+    expect(() => {
+      new Car(name);
+    }).toThrow("이름은 공백을 제외한 한글자 이상이어야합니다.");
+  });
   it("자동차의 이름을 알 수 있다.", () => {
     expect(car.getName()).toBe(DEFAULT_NAME);
   });
@@ -13,7 +19,7 @@ describe("Car", () => {
     car.run(4);
     expect(car.getPosition()).toBe(START_POSITION + RUN_UNIT);
   });
-  it("자동차는 ${RUN_THRESHOLD} 미만의 입력값을 받으면 전진할 수 없다.", () => {
+  it(`자동차는 ${RUN_THRESHOLD} 미만의 입력값을 받으면 전진할 수 없다.`, () => {
     car.run(3);
     expect(car.getPosition()).toBe(START_POSITION);
   });
--- src/Car.js
+++ src/Car.js
@@ -1,15 +1,21 @@
 export const START_POSITION = 0;
 export const RUN_THRESHOLD = 4;
 export const RUN_UNIT = 1;
-
+const MIN_NAME_LENGTH = 1;
 export class Car {
   #name;
   #position;
 
   constructor(name) {
+    if (!this.validateCarName(name)) return;
     this.#name = name;
     this.#position = START_POSITION;
   }
+  validateCarName = (name) => {
+    if (name.trim().length < MIN_NAME_LENGTH)
+      throw new Error("이름은 공백을 제외한 한글자 이상이어야합니다.");
+    return true;
+  };
 
   getName() {
     return this.#name;
