f9dc1fb47f72459e943b3313b41310f2f9a4eba6
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-28 03:58:06 +0900

    ✨ feat: 자동차 경주 상수 파일 추가 (에러메시지, 규칙 설정)

---
+++ src/constants/configure.js
@@ -0,0 +1,11 @@
+export const NAME_CONFIGURE = Object.freeze({
+  MIN_LENGTH: 1,
+  MAX_LENGTH: 5,
+  SEPARATOR: ','
+});
+
+export const RACE_CONFIGURE = Object.freeze({
+  MAX_SPEED: 10,
+  MIN_SPEED: 0,
+  MOVE_UNIT: 4
+});
---
+++ src/constants/message.js
@@ -0,0 +1,6 @@
+export const ERROR_MESSAGE = Object.freeze({
+  CAR_NAME_INCORRECT_LENGTH: '자동차 이름은 최대 5글자 까지 입력하실 수 있습니다.',
+  CAR_NAME_NOT_STRING: '자동차 이름은 문자열 타입만 허용됩니다.'
+});
+
+export const ALERT_MESSAGE = Object.freeze({});
