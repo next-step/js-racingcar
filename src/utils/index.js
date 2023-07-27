15922d064f5ecfa8adc38fbebb33cdff9ca7def4
손진영 <sm099@naver.com>
2023-07-27 13:57:21 +0900

    style(utils): 불필요한 racingCars, racingWinners 파일을 utils 폴더에서 제거

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,3 @@
 export * from './validate.js';
-export * from './racingCars.js';
-export * from './racingWinners.js';
 export * from './commons.js';
 export * from './readline.js';
--- src/utils/racingCars.js
+++
@@ -1,7 +0,0 @@
-import NumberMaker from '../NumberMaker.js';
-import { AVALIABLE_RANDOM_NUMBER } from '../constants/index.js';
-
-export const isMove = (carName) => {
-  const randomNumber = NumberMaker.createRandomNumber(carName);
-  return randomNumber >= AVALIABLE_RANDOM_NUMBER;
-};
