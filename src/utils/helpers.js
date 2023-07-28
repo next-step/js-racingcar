c31866b283635c324308eb5bcc6fad4c45b12830
Frank Kim <frankuserconnect@gmail.com>
2023-07-28 18:34:18 +0900

    improve: 함수명 수정 + 기능 하나씩

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,14 +1,10 @@
-import {
-  MAX_CAR_NAME_LENGTH,
-  MIN_CAR_NAME_LENGTH,
-  RACE_FORWARD_RANDOM_NUMBER_LIMIT,
-} from "../constants/rules";
+import { MAX_CAR_NAME_LENGTH, MIN_CAR_NAME_LENGTH } from "../constants/rules";
 
 /**
  * 이름은 1~5자만 가능하다.
  * @param {string} name
  */
-export const isValidateInputLength = name => {
+export const isValidInputLength = name => {
   return (
     name !== undefined &&
     name.length >= MIN_CAR_NAME_LENGTH &&
@@ -17,13 +13,10 @@ export const isValidateInputLength = name => {
 };
 
 /**
- * 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
  * @param {number} min - minimum number
  * @param {number} max
  * @returns number
  */
 export const getRandomNumber = (min, max) => {
-  const randomValue = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  return randomValue >= RACE_FORWARD_RANDOM_NUMBER_LIMIT;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
