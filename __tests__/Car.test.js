d365d335d7578d4b095142bbf4b70a5c4e5ecd45
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-28 23:59:57 +0900

    🧪 test: 자동차 경주 이름 입력 테스트코드 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,7 +1,14 @@
+import {
+  DUMMY_CORRECT_CARS,
+  DUMMY_INCORRECT_LENGTH_CARS_NAMES,
+  DUMMY_NOT_STRING_CAR_NAMES,
+  DUMMY_INPUT_CAR_NAMES,
+  DUMMY_INCORRECT_INPUT_CAR_NAMES
+} from './constants';
 import { NAME_CONFIGURE, ERROR_MESSAGE } from '../src/constants/index';
-import { DUMMY_CORRECT_CARS, DUMMY_INCORRECT_LENGTH_CARS_NAMES, DUMMY_NOT_STRING_CAR_NAMES } from './constants';
-import validate from '../src/utils/validate';
+import { validateCarName } from '../src/utils/index';
 import Car from '../src/Car';
+import CarPrompter from '../src/CarPrompter';
 
 describe('자동차 이름 충족 조건 테스트', () => {
   test.each(DUMMY_CORRECT_CARS)('자동차는 이름($name)을 가질 수 있다.', ({ name }) => {
@@ -12,11 +19,34 @@ describe('자동차 이름 충족 조건 테스트', () => {
   test.each(DUMMY_INCORRECT_LENGTH_CARS_NAMES)(
     `자동차 이름($name)은 최소 ${NAME_CONFIGURE.MIN_LENGTH}글자 이상 최대${NAME_CONFIGURE.MAX_LENGTH} 글자 까지 허용한다.`,
     ({ name }) => {
-      expect(() => validate(name)).toThrow(ERROR_MESSAGE.CAR_NAME_INCORRECT_LENGTH);
+      expect(() => validateCarName(name)).toThrow(ERROR_MESSAGE.CAR_NAME_INCORRECT_LENGTH);
     }
   );
 
-  test.each(DUMMY_NOT_STRING_CAR_NAMES)(`자동차 이름($name)은 문자열 타입만 허용됩니다. `, ({ name }) => {
-    expect(() => validate(name)).toThrow(ERROR_MESSAGE.CAR_NAME_NOT_STRING);
-  });
+  test.each(DUMMY_NOT_STRING_CAR_NAMES)(
+    `자동차 이름($name)은 문자열 타입만 허용한다. `,
+    ({ name }) => {
+      expect(() => validateCarName(name)).toThrow(ERROR_MESSAGE.CAR_NAME_NOT_STRING);
+    }
+  );
+});
+
+describe('자동차 경주 테스트', () => {
+  test.each(DUMMY_INPUT_CAR_NAMES)(
+    '자동차 경주에 참여하는 자동차 이름은 쉼표(,)로 구분하여 입력한다.($input)',
+    ({ input }) => {
+      expect(() => {
+        CarPrompter.validate(input);
+      }).not.toThrow();
+    }
+  );
+
+  test.each(DUMMY_INCORRECT_INPUT_CAR_NAMES)(
+    '경주에 참여하는 자동차는 중복이될 수 없다.',
+    ({ input }) => {
+      expect(() => {
+        CarPrompter.validate(input);
+      }).toThrow(ERROR_MESSAGE.DUPLICATE_CAR);
+    }
+  );
 });
--- __tests__/constants.js
+++ __tests__/constants.js
@@ -1,6 +1,11 @@
 import { ERROR_MESSAGE } from '../src/constants/index';
 
-export const DUMMY_CORRECT_CARS = [{ name: 'mario' }, { name: 'yoshi' }, { name: 'koopa' }, { name: 'peach' }];
+export const DUMMY_CORRECT_CARS = [
+  { name: 'mario' },
+  { name: 'yoshi' },
+  { name: 'koopa' },
+  { name: 'peach' }
+];
 
 export const DUMMY_INCORRECT_LENGTH_CARS_NAMES = [
   { name: '' },
@@ -9,10 +14,23 @@ export const DUMMY_INCORRECT_LENGTH_CARS_NAMES = [
   { name: 'JR.Koopa' },
   { name: 'sexyboy' }
 ];
+
 export const DUMMY_NOT_STRING_CAR_NAMES = [
   { name: {} },
   { name: 1 },
   { name: undefined },
   { name: () => {} },
   { name: true }
+];
+
+export const DUMMY_INPUT_CAR_NAMES = [
+  { input: 'car1,car2,car3,car4' },
+  { input: 'luigi,yoshi' },
+  { input: 'mario' }
+];
+
+export const DUMMY_INCORRECT_INPUT_CAR_NAMES = [
+  { input: 'racer,car,racer' },
+  { input: 'car,car' },
+  { input: 'car4,car3,car2,car2' }
 ];
