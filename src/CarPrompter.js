9d452a55d7e7bd431ed0b9136c065f22bf2c84d0
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-28 23:57:43 +0900

    ✨ feat: 경주할 자동차 이름을 입력받는 클래스 구현

---
+++ src/CarPrompter.js
@@ -0,0 +1,22 @@
+import { stdin as input, stdout as output } from 'process';
+import * as readline from 'readline';
+import { ALERT_MESSAGE, NAME_CONFIGURE } from './constants/index';
+import { validateInputToCarRacers } from './utils/index';
+
+export default class CarPrompter {
+  #readline = readline.createInterface({ input, output });
+
+  #inputCarRacers(listenerCallback) {
+    this.#readline.question(`${ALERT_MESSAGE.INPUT_CAR_RACERS}\n`, (inputMessage) => {
+      CarPrompter.validate(inputMessage);
+      if (listenerCallback) {
+        listenerCallback(inputMessage.split(NAME_CONFIGURE.SEPARATOR));
+      }
+      this.#readline.close();
+    });
+  }
+
+  static validate = validateInputToCarRacers;
+
+  execute = this.#inputCarRacers;
+}
