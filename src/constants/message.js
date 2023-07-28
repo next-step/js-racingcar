5000ba042544b8e3e26bfe0d6321032c83f9d408
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-28 23:58:53 +0900

    🔧 modify: 프롬프트 메시지, 자동차 경주 이름 입력 시 에러 메시지 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,10 @@
 export const ERROR_MESSAGE = Object.freeze({
   CAR_NAME_INCORRECT_LENGTH: '자동차 이름은 최대 5글자 까지 입력하실 수 있습니다.',
-  CAR_NAME_NOT_STRING: '자동차 이름은 문자열 타입만 허용됩니다.'
+  CAR_NAME_NOT_STRING: '자동차 이름은 문자열 타입만 허용됩니다.',
+  NOT_RECEIVED_INPUT: '참여 할 자동차들을 입력해주세요.',
+  DUPLICATE_CAR: '이미 해당 이름으로 참가중인 자동차가 존재합니다.'
 });
 
-export const ALERT_MESSAGE = Object.freeze({});
+export const ALERT_MESSAGE = Object.freeze({
+  INPUT_CAR_RACERS: '경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)'
+});
