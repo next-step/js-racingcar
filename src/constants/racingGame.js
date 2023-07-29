2d7354c06b39875aec5a4116be9ae2a6f06fa500
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-30 00:11:03 +0900

    refactor: racingGame default Value 상수 분리

--- src/constants/racingGame.js
+++ src/constants/racingGame.js
@@ -1,8 +1,13 @@
-export const RACING_GAME = Object.freeze({
+const DEFAULT_STATE = Object.freeze({
+  CARS: [],
   TOTAL_ROUNDS: 5,
+});
+
+export const RACING_GAME = Object.freeze({
   MOVEMENT_THRESHOLD: 4,
   DISTANCE_UNIT: 1,
   PROGRESS_TITLE: '\n실행 결과\n',
   GAME_RESULT: (gameProgress, winners) =>
     `${gameProgress}${winners.join(',')}가 최종 우승했습니다.`,
+  DEFAULT_STATE,
 });
