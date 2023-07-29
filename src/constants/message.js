dea5f3725037445c40f999d34adb0e430f820799
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-07-30 08:47:37 +0900

    ✨ feat: 자동차 경주 진행 메시지 정의, 로그 함수 추가

--- src/constants/message.js
+++ src/constants/message.js
@@ -6,5 +6,11 @@ export const ERROR_MESSAGE = Object.freeze({
 });
 
 export const ALERT_MESSAGE = Object.freeze({
-  INPUT_CAR_MESSAGE: '경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)'
+  INPUT_CAR_MESSAGE: '경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,)를 기준으로 구분)',
+  RACE_STATUS_MESSAGE(car, distance, track) {
+    return `${car} : ${track.repeat(distance)}`;
+  },
+  RACE_FINISH_MESSAGE(winners) {
+    return `${winners.join(', ')}가 최종 우승하였습니다.`;
+  }
 });
