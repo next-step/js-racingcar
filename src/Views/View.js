fc2151388b7eea75c2aaef8f1fad51975c9b220f
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-07-30 01:36:02 +0900

    feat: 입력을 전부 받고 게임이 종료될 때 readLine을 close한다

--- src/Views/View.js
+++ src/Views/View.js
@@ -9,17 +9,28 @@ export class View {
     this.#outputView = outputView;
   }
 
-  readCarName(handleUserInput) {
-    this.#inputView.readCarName(MESSAGE.READ.CAR_NAME, (userInput) => {
-      handleUserInput(userInput);
+  readCarName(inputHandler) {
+    this.#inputView.readUserInput(MESSAGE.READ.CAR_NAME, (userInput) => {
+      inputHandler(userInput);
+    });
+  }
+
+  readTotalRound(inputHandler) {
+    this.#inputView.readUserInput(MESSAGE.READ.TOTAL_ROUND, (userInput) => {
+      inputHandler(userInput);
     });
   }
 
   printGameResult(gameResult) {
     this.#outputView.print(gameResult);
+    this.#closeReadLine();
   }
 
   printError(error) {
     this.#outputView.print(error);
+  }
+
+  #closeReadLine() {
+    this.#inputView.close();
   }
 }
