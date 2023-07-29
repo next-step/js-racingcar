4f12b6b7c3808c2ba60f4f80e0141a4de6361a0e
froggy1014 <clcl6084@gmail.com>
2023-07-30 02:17:55 +0900

    feat: add constants w/ object_freeze

---
+++ src/constants/messages.js
@@ -0,0 +1,14 @@
+import { SETTINGS } from './settings.js';
+
+export const MESSAGES = Object.freeze({
+  ERROR: {
+    NO_NAME: '이름을 입력해주세요.',
+    MAX_NAME: `이름은 ${SETTINGS.MAX.NAME_LENGTH}자 이하만 가능합니다.`,
+    MIN_NAME: `이름은 ${SETTINGS.MIN.NAME_LENGTH}자 이상만 가능합니다.`,
+  },
+  GAME: {
+    START_PROMPT: `경주할 자동차 이름을 입력하세요(이름은 쉼표(${SETTINGS.SEPERATOR})를 기준으로 구분).`,
+    RESULT_HEADER: '실행 결과',
+    WINNER_ANNOUNCEMENT: '가 최종 우승했습니다.',
+  },
+});
---
+++ src/constants/settings.js
@@ -0,0 +1,14 @@
+export const SETTINGS = Object.freeze({
+  ROUND: 5,
+  MAX: {
+    NAME_LENGTH: 5,
+    RANDOM_NUMBER: 9,
+  },
+  MIN: {
+    NAME_LENGTH: 1,
+    RANDOM_NUMBER: 0,
+  },
+  MOVEMENT: 1,
+  SEPERATOR: ',',
+  MOVEMENT_CONDITION: 4,
+});
