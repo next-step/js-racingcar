1529a6d4da5797ee97b3205f7f18fa07aa15ec81
gusrb3164 <gusrb3164@gmail.com>
2023-07-29 17:01:16 +0900

    refactor: util함수 변경사항 적용

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import {splitByComma} from './utils/splitByComma';
-import {isValidCarNamesLength} from './utils/isValidCarNamesLength';
-import {isExistEmptyNameCars} from './utils/isExistEmptyNameCars';
+import {isExistOverLengthText} from './utils/isExistOverLengthText';
+import {isExistEmptyText} from './utils/isExistEmptyText';
 import {Controller} from './controller/Controller';
 import {printRacingInfo} from './view/printRacingInfo';
 import {printWinners} from './view/printWinners';
 import {getLineInput} from './view/getLineInput';
+import {trimTexts} from './utils/trimTexts';
+
+const MAXIMUM_RACING_NUMBER = 5;
+const MAXIMUM_CAR_NAME_LENGTH = 5;
 
 const startGame = carNames => {
-  const MAXIMUM_RACING_NUMBER = 5;
-
   const controller = new Controller();
   controller.createCars(carNames);
-
-  console.log('실행 결과');
 
   while (controller.currentRaceNumber < MAXIMUM_RACING_NUMBER) {
     controller.race();
@@ -29,13 +29,13 @@ function main() {
   console.log('경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).');
 
   getLineInput(input => {
-    const carNames = splitByComma(input);
-    if (!isValidCarNamesLength(carNames)) {
+    const carNames = trimTexts(splitByComma(input));
+    if (!isExistOverLengthText(carNames, MAXIMUM_CAR_NAME_LENGTH)) {
       console.log('자동차 이름은 5자 이하로 입력해주세요.');
       return;
     }
 
-    if (isExistEmptyNameCars(carNames)) {
+    if (isExistEmptyText(carNames)) {
       console.log('자동차 이름은 공백이 될 수 없습니다.');
       return;
     }
