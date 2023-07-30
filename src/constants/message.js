dc01b7a95a97db1a174c2986ca227165e05e20ce
kimhaneui <139193311+TraportStaffs@users.noreply.github.com>
2023-07-30 15:39:08 +0900

    객체지향 코드로 수정

---
+++ src/constants/message.js
@@ -0,0 +1,9 @@
+export const Message = {
+    DEFAULT_POSITION: 0,
+    CAR_RUN_UNIT: 1,
+    RUN_THRESHOLD: 4,
+    CAR_NAME_MIN_LENGTH: 1,
+    CAR_NAME_MAX_LENGTH: 5,
+    RACE_COUNT: 5
+} 
+export const Qusetion = '자동차 이름을 쉼표(,)로 구분하여 입력해주세요 (예: car1, car2, car3)';
--- src/index.js
+++ src/index.js
@@ -1,61 +1,11 @@
-function randomNumber() {
-  return Math.floor(Math.random() * 10);
-}
+import Car from './domain/car.js'
 
-function isInvalid(carNames) {
-  const names = carNames.split(',').map((name) => name.trim());
-  return names.some((name) => name === '' || name.length > 5);
-}
+import readline from 'readline';
 
-function carPositions(cars) {
-  cars.forEach((car) => {
-    console.log(`${car.name}: ${'-'.repeat(car.position)}`);
-  });
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const carGame = new Car(rl);
 
-function getRoundResult(cars) {
-  const roundResults = [];
-  cars.forEach((car) => {
-    const randomValue = randomNumber();
-    if (randomValue >= 4) {
-      car.position++;
-    }
-    roundResults.push(`${car.name}: ${'-'.repeat(car.position)}`);
-  });
-  return roundResults;
-}
-
-function getWinners(cars) {
-  const maxPosition = Math.max(...cars.map((car) => car.position));
-  return cars.filter((car) => car.position === maxPosition).map((car) => car.name);
-}
-
-function raceResultTxtSet(winners) {
-  console.log(`\n${winners.join(', ')}가 최종 우승했습니다.`);
-  const raceResultTxt = document.getElementById('raceResult');
-  raceResultTxt.innerHTML = `\n${winners.join(', ')}가 최종 우승했습니다.`;
-}
-
-function carRace(names) {
-  const cars = names.split(',').map((name) => ({ name: name.trim(), position: 0 }));
-  const rounds = 5;
-  for (let round = 1; round <= rounds; round++) {
-    console.log(`<Round ${round}>`);
-    const roundResults = getRoundResult(cars);
-    roundResults.forEach((result) => console.log(result));
-  }
-  carPositions(cars);
-  const winners = getWinners(cars);
-  raceResultTxtSet(winners);
-}
-
-function startRace() {
-  const carNames = document.getElementById('carNames').value;
-  if (isInvalid(carNames)) {
-    alert('자동차 이름은 쉼표(,)로 구분하며, 5자 이하만 가능합니다.');
-    return;
-  }
-  carRace(carNames);
-}
-
-export { isInvalid };
+carGame.startRace();
