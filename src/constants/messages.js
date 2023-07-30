195b916edeeeac7338169063d4addd5b99db8655
cobocho <rlaalsrb1111@naver.com>
2023-07-31 01:22:55 +0900

    Feat: 라운드 관련 상수 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -1,8 +1,17 @@
-const { MAX_USER_COUNT, MIN_USER_COUNT, SKID_MARK, MIN_NAME_LENGTH, MAX_NAME_LENGTH } = require('./racing-rule');
+const {
+  MAX_USER_COUNT,
+  MIN_USER_COUNT,
+  SKID_MARK,
+  MIN_NAME_LENGTH,
+  MAX_NAME_LENGTH,
+  MIN_ROUND_COUNT,
+  MAX_ROUND_COUNT,
+} = require('./racing-rule');
 
 const MESSAGES = Object.freeze({
   REQUEST: {
     ENTER_THE_CARS: '경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)를 기준으로 구분).',
+    ENTER_THE_ROUND: '시도할 회수는 몇회인가요?',
   },
 
   GAME: {
@@ -24,6 +33,9 @@ const ERROR_MESSAGES = Object.freeze({
 
   LESS_THAN_MIN_USER_COUNT: `${MIN_USER_COUNT}명 이상의 이름을 입력해주세요!`,
   MORE_THAN_MAX_USER_COUNT: `${MAX_USER_COUNT}명 이하의 이름을 입력해주세요!`,
+
+  LESS_THAN_MIN_ROUND_COUNT: `${MIN_ROUND_COUNT}를 넘는 라운드 수를 입력해주세요!`,
+  MORE_THAN_MAX_ROUND_COUNT: `${MAX_ROUND_COUNT} 미만의 라운드 수를 입력해주세요!`,
 
   HAS_DUPLICATED_NAME: '중복된 이름이 존재합니다!',
 });
