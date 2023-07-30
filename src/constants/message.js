febc6a93bf0a8dff892ce9b34f6158768e1b4b26
손진영 <sm099@naver.com>
2023-07-30 23:40:46 +0900

    refactor(message): RACING_CAR 내 하드 코딩된 부분 상수 값으로 변경

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,7 @@
+import { SEPERATOR_SYMBOLS } from './commons.js';
+
 export const INPUT_MESSAGE = Object.freeze({
-  RACING_CAR:
-    '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
+  RACING_CAR: `경주할 자동차 이름을 입력하세요(이름은 쉼표(${SEPERATOR_SYMBOLS.COMMA})를 기준으로 구분).\n`,
 });
 
 export const ERROR_MESSAGE = {
