b2c070ca51022ac240150018f0537059f3dbb945
callmejeje <choije401@naver.com>
2023-08-01 21:49:13 +0900

    feat: 숫자 상수화

---
+++ src/constants/Numbers.js
@@ -0,0 +1,15 @@
+export const CAR = Object.freeze({
+  START_POSITION: 0,
+  RUN_UNIT: 1,
+  RUN_THRESHOLD: 4,
+  NAME_MIN_LENGTH: 1,
+  NAME_MAX_LENGTH: 5,
+});
+
+export const RACE = Object.freeze({
+  MIN_CAR_COUNT: 2,
+  MAX_ROUND: 5,
+  MIN_WINNER_COUNT: 1,
+  MIN_ROLL_NUMBER: 0,
+  MAX_ROLL_NUMBER: 9,
+});
