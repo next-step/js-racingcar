45d905b1a0cc73f04ee5bb311218dda352a0c5d9
callmejeje <choije401@naver.com>
2023-08-01 21:50:51 +0900

    feat: 자동차 경주를 실행하는 Controller 구현

---
+++ src/controllers/CarRacingController.js
@@ -0,0 +1,60 @@
+import { CHARACTERS } from "../constants/Characters";
+import { MESSAGE } from "../constants/Messages";
+import { RACE } from "../constants/Numbers";
+import Car from "../models/Car";
+import Race from "../models/Race";
+import ConsoleInput from "../views/ConsoleInput";
+import ConsoleOutput from "../views/ConsoleOutput";
+
+class CarRacingController {
+  #readline;
+  #race;
+  #consoleInput;
+  #consoleOutput;
+
+  constructor(readline) {
+    this.#readline = readline;
+    this.#consoleInput = new ConsoleInput(readline);
+    this.#consoleOutput = new ConsoleOutput();
+  }
+
+  getRace() {
+    return this.#race;
+  }
+
+  splitToCars(carNames) {
+    const cars = carNames
+      .split(CHARACTERS.CAR_SEPERATOR)
+      .map((car) => new Car(car));
+
+    return cars;
+  }
+
+  async startRace() {
+    const carNames = await this.#consoleInput.readCarNames();
+    this.#readline.close();
+
+    const cars = this.splitToCars(carNames);
+    this.#race = new Race(cars);
+
+    this.playRace();
+  }
+
+  playRace() {
+    this.#consoleOutput.print(`\n${MESSAGE.RESULT_OF_RACE}`);
+    for (let i = 0; i < RACE.MAX_ROUND; i++) {
+      this.#race.playOneRound();
+      this.#consoleOutput.printAllPositions(this.#race.getCars());
+    }
+
+    this.#race.pickWinners();
+
+    this.endRace();
+  }
+
+  endRace() {
+    this.#consoleOutput.printWinners(this.#race.getWinners());
+  }
+}
+
+export default CarRacingController;
