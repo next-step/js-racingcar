422927023093dedae99a24824cca30ecd540d5f7
bxxmi <bmi6638@gmail.com>
2023-08-01 23:32:48 +0900

    [fix] 테스트 케이스 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -4,9 +4,15 @@ import RacingModel from "../src/model/RacingModel";
 import RacingView from "../src/view/RacingView";
 
 describe("자동차 경주 게임 테스트입니다.", () => {
-  const racingModel = new RacingModel();
-  const racingView = new RacingView(racingModel);
-  const racingController = new RacingController(racingModel, racingView);
+  let racingModel;
+  let racingView;
+  let racingController;
+
+  beforeEach(() => {
+    racingModel = new RacingModel();
+    racingView = new RacingView(racingModel);
+    racingController = new RacingController(racingModel, racingView);
+  });
 
   describe("사용자가 잘못된 값을 입력 시 경고와 함께 게임이 종료됩니다.", () => {
     test.each([
@@ -57,29 +63,78 @@ describe("자동차 경주 게임 테스트입니다.", () => {
   });
 
   test("랜덤 값이 4이상이 나와야 전진할 수 있습니다.", () => {
-    racingController.settingCarNames("car1,car2");
-    racingController.playSingleRound(1, 4);
+    racingController.settingCarNames("car1,car2,car3,car4");
+
+    racingController.playSingleRound(0, 2);
+    racingController.playSingleRound(1, 3);
+    racingController.playSingleRound(2, 6);
+    racingController.playSingleRound(3, 4);
 
     const carInfo = racingController.racingModel.getCarInfo();
 
-    expect(carInfo[1].position).toBe(1);
+    expect(carInfo[0].position).toBe(0);
+    expect(carInfo[1].position).toBe(0);
+    expect(carInfo[2].position).toBe(1);
+    expect(carInfo[3].position).toBe(1);
   });
 
-  test("우승자가 여러 명일 때 콤마로 구분해서 출력합니다.", () => {
-    const carList = [
-      { name: "car1", position: 4 },
-      { name: "car2", position: 4 },
-      { name: "car3", position: 4 },
-    ];
+  test.each([
+    [
+      [
+        { name: "car1", position: 4 },
+        { name: "car2", position: 4 },
+        { name: "car3", position: 4 },
+      ],
+      "우승자는 car1,car2,car3입니다",
+    ],
+    [
+      [
+        { name: "car1", position: 4 },
+        { name: "car2", position: 6 },
+        { name: "car3", position: 2 },
+        { name: "car4", position: 6 },
+      ],
+      "우승자는 car2,car4입니다",
+    ],
+    [
+      [
+        { name: "car1", position: 5 },
+        { name: "car2", position: 2 },
+        { name: "car3", position: 3 },
+        { name: "car4", position: 6 },
+      ],
+      "우승자는 car4입니다",
+    ],
+  ])(
+    "우승자가 여러 명일 때 콤마로 구분해서 출력합니다.",
+    (carList, expected) => {
+      const jestSpyOn = jest.spyOn(console, "log");
 
-    const jestSpyOn = jest.spyOn(console, "log");
+      racingModel.carList = carList;
+      racingView.racingModel = racingModel;
+      racingView.showRacingGameWinners();
 
-    racingModel.carList = carList;
-    racingView.racingModel = racingModel;
-    racingView.showRacingGameWinners();
+      expect(jestSpyOn).toHaveBeenCalledWith(expected);
 
-    expect(jestSpyOn).toHaveBeenCalledWith("우승자는 car1,car2,car3입니다");
+      jestSpyOn.mockRestore();
+    }
+  );
 
-    jestSpyOn.mockRestore();
-  });
+  // test("우승자가 여러 명일 때 콤마로 구분해서 출력합니다.", () => {
+  //   const carList = [
+  //     { name: "car1", position: 4 },
+  //     { name: "car2", position: 4 },
+  //     { name: "car3", position: 4 },
+  //   ];
+
+  //   const jestSpyOn = jest.spyOn(console, "log");
+
+  //   racingModel.carList = carList;
+  //   racingView.racingModel = racingModel;
+  //   racingView.showRacingGameWinners();
+
+  //   expect(jestSpyOn).toHaveBeenCalledWith("우승자는 car1,car2,car3입니다");
+
+  //   jestSpyOn.mockRestore();
+  // });
 });
