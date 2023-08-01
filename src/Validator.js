f13a9e2ee85b033266713834a0fddb8ff53ee0dd
손진영 <sm099@naver.com>
2023-08-01 16:29:25 +0900

    feat(Validator): 자동차 이름 중 공백이 있거나 영/문자가 아닌 것을 검증하는 로직 추가

--- src/Validator.js
+++ src/Validator.js
@@ -7,19 +7,20 @@ import {
 } from './constants/index.js';
 import { convertStringToArray } from './utils/commons.js';
 import {
+  isCharacter,
   isDuplicateRacingCars,
+  isIncludeSpaces,
   isInvalidLengthRacingCars,
 } from './utils/validate.js';
 
 const Validator = (function Validator() {
   const validateCarNames = (userInput) => {
     const racingCars = convertStringToArray(userInput, SEPERATOR_SYMBOLS.COMMA);
+    if (isIncludeSpaces(racingCars)) throw new SyntaxError(ERROR_MESSAGE.INCLUDE_EMPTY_WORDS);
+    if (!isCharacter(racingCars)) throw new TypeError(ERROR_MESSAGE.AVALIABLE_CHARACTER);
     if (isInvalidLengthRacingCars(racingCars))
-      throw new RangeError(
-        ERROR_MESSAGE.INVALID_RANGE(CAR_MIN_LENGTH, CAR_MAX_LENGTH),
-      );
-    if (isDuplicateRacingCars(racingCars))
-      throw new SyntaxError(ERROR_MESSAGE.DUPLICATE_CAR_NAMES);
+      throw new RangeError(ERROR_MESSAGE.INVALID_RANGE(CAR_MIN_LENGTH, CAR_MAX_LENGTH));
+    if (isDuplicateRacingCars(racingCars)) throw new SyntaxError(ERROR_MESSAGE.DUPLICATE_CAR_NAMES);
   };
 
   const validators = {
--- src/constants/validate.js
+++ src/constants/validate.js
@@ -1,2 +1,4 @@
 export const CAR_LENGTH_RANGE_REGEX = /^.{1,5}$/;
+export const INCLUDE_EMPTY_REGEX = /\s/;
+export const CHARACTER_REGEX = /^[가-힣a-zA-Z]*$/;
 export const [CAR_MIN_LENGTH, CAR_MAX_LENGTH] = [1, 5];
