9cbc2c9d002570332ae5d77f9da28574cda9401d
손진영 <sm099@naver.com>
2023-08-02 02:21:38 +0900

    style(message): 불필요한 메시지 삭제

--- src/constants/message.js
+++ src/constants/message.js
@@ -8,7 +8,6 @@ export const INPUT_MESSAGE = Object.freeze({
 export const ERROR_MESSAGE = {
   INVALID_RANGE: (min, max) => `[ERROR] : 자동차 이름은 ${min}~${max}자 사이로 입력해주세요.`,
   DUPLICATE_CAR_NAMES: '[ERROR] : 중복된 자동차 이름이 존재합니다.',
-  NOT_MESSAGE: '[ERROR] - 입력 받을 메시지가 없습니다.',
   INCLUDE_EMPTY_WORDS: '[ERROR] : 입력한 자동차 이름에 공백이 존재합니다. 공백 없이 입력해주세요.',
   AVALIABLE_CHARACTER: '[ERROR] : 자동차 이름은 영문자만 가능합니다.',
   AVALIABLE_NUMBER: '[ERROR] : 숫자만 입력 가능합니다.',
