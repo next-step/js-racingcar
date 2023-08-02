257501ae4f2bdacfc39fec6620f5560f3043fd62
손진영 <sm099@naver.com>
2023-08-02 12:13:23 +0900

    refactor(GameController): racingGame 필드를 private로 변경

--- src/controller/GameController.js
+++ src/controller/GameController.js
@@ -4,7 +4,7 @@ import { InputView, OutputView } from '../view/index.js';
 import { Validator } from '../Validator.js';
 
 export class GameController {
-  racingGame;
+  #racingGame;
 
   static async #getRacingCarNames() {
     try {
@@ -40,7 +40,7 @@ export class GameController {
   }
 
   #startRace() {
-    this.racingGame.race();
+    this.#racingGame.race();
   }
 
   static #printGameResult(result, racingWinners) {
@@ -52,7 +52,7 @@ export class GameController {
     try {
       const racingCarNames = await GameController.#getRacingCarNames();
       const racingCount = await GameController.#getRacingCount();
-      this.racingGame = new RacingGame(racingCarNames, racingCount);
+      this.#racingGame = new RacingGame(racingCarNames, racingCount);
     } catch (error) {
       OutputView.print(error);
       await this.#settingRacingGame();
@@ -60,7 +60,7 @@ export class GameController {
   }
 
   #confirmAfterRacing() {
-    return [this.racingGame.getRacingResult(), this.racingGame.getRacingWinners()];
+    return [this.#racingGame.getRacingResult(), this.#racingGame.getRacingWinners()];
   }
 
   async run() {
