af6dcb7ac215ed23e82f3e00a529f19679207a6c
손진영 <sm099@naver.com>
2023-08-02 11:43:31 +0900

    refactor(InputView): 불필요한 모듈 패턴 제거

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,24 +1,26 @@
-import { createReadMachine } from '../utils/index.js';
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
 
-const InputView = (function InputView() {
-  const createUserInputByQuestion = async (message) => {
-    const readMachine = createReadMachine();
-    const userInput = await readMachine.question(message);
-    readMachine.close();
+const createReadMachine = () => {
+  const readMachine = readline.createInterface({ input, output });
+  return readMachine;
+};
+
+const createUserInputByQuestion = async (message) => {
+  const readMachine = createReadMachine();
+  const userInput = await readMachine.question(message);
+  readMachine.close();
+  return userInput;
+};
+
+const getUserInput = async (message) => {
+  const userInput = await createUserInputByQuestion(message);
+  return userInput;
+};
+
+export const InputView = {
+  async input(message) {
+    const userInput = await getUserInput(message);
     return userInput;
-  };
-
-  const getUserInput = async (message) => {
-    const userInput = await createUserInputByQuestion(message);
-    return userInput;
-  };
-
-  return {
-    async input(message) {
-      const userInput = await getUserInput(message);
-      return userInput;
-    },
-  };
-})();
-
-export default InputView;
+  },
+};
