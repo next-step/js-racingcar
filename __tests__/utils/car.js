5e7cbbbde994531305bb38457472adcc05bc82a5
손진영 <sm099@naver.com>
2023-08-03 00:15:36 +0900

    style(test): 불필요한 테스트 관련 유틸함수 제거

--- __tests__/utils/car.js
+++ __tests__/utils/car.js
@@ -1,15 +1,5 @@
 import { AVALIABLE_RANDOM_NUMBER } from '../../src/constants/randomNumber.js';
 
-export const containsAllRacers = (str) => {
-  const patterns = ['jiny', 'pobi', 'conan'];
-  return patterns.every((pattern) => new RegExp(pattern).test(str));
-};
-
-export const containsAllStatus = (str) => {
-  const patterns = ['jiny', 'pobi', 'conan', ':'];
-  return patterns.every((pattern) => new RegExp(pattern).test(str));
-};
-
 export const MockNumberMaker = {
   createRandomNumber() {
     return AVALIABLE_RANDOM_NUMBER;
