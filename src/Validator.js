cba52fa121f29c1df8e4b65e3140c423586f25d3
손진영 <sm099@naver.com>
2023-08-02 11:47:52 +0900

    refactor(Validator): 불필요한 모듈 패턴 제거

--- src/Validator.js
+++ src/Validator.js
@@ -14,32 +14,28 @@ import {
   isNumber,
 } from './utils/validate.js';
 
-const Validator = (function Validator() {
-  const validateCarNames = (userInput) => {
-    const racingCars = convertStringToArray(userInput, SEPERATOR_SYMBOLS.COMMA);
-    if (isIncludeSpaces(racingCars)) throw new SyntaxError(ERROR_MESSAGE.INCLUDE_EMPTY_WORDS);
-    if (!isCharacter(racingCars)) throw new TypeError(ERROR_MESSAGE.AVALIABLE_CHARACTER);
-    if (isInvalidLengthRacingCars(racingCars))
-      throw new RangeError(ERROR_MESSAGE.INVALID_RANGE(CAR_MIN_LENGTH, CAR_MAX_LENGTH));
-    if (isDuplicateRacingCars(racingCars)) throw new SyntaxError(ERROR_MESSAGE.DUPLICATE_CAR_NAMES);
-  };
+const validateCarNames = (userInput) => {
+  const racingCars = convertStringToArray(userInput, SEPERATOR_SYMBOLS.COMMA);
+  if (isIncludeSpaces(racingCars)) throw new SyntaxError(ERROR_MESSAGE.INCLUDE_EMPTY_WORDS);
+  if (!isCharacter(racingCars)) throw new TypeError(ERROR_MESSAGE.AVALIABLE_CHARACTER);
+  if (isInvalidLengthRacingCars(racingCars))
+    throw new RangeError(ERROR_MESSAGE.INVALID_RANGE(CAR_MIN_LENGTH, CAR_MAX_LENGTH));
+  if (isDuplicateRacingCars(racingCars)) throw new SyntaxError(ERROR_MESSAGE.DUPLICATE_CAR_NAMES);
+};
 
-  const validateCount = (userInput) => {
-    const count = Number(userInput);
-    if (!isNumber(count)) throw new TypeError(ERROR_MESSAGE.AVALIABLE_NUMBER);
-  };
+const validateCount = (userInput) => {
+  const count = Number(userInput);
+  if (!isNumber(count)) throw new TypeError(ERROR_MESSAGE.AVALIABLE_NUMBER);
+};
 
-  const validators = {
-    [INPUT_MESSAGE.RACING_CAR]: validateCarNames,
-    [INPUT_MESSAGE.COUNT]: validateCount,
-  };
+const validators = {
+  [INPUT_MESSAGE.RACING_CAR]: validateCarNames,
+  [INPUT_MESSAGE.COUNT]: validateCount,
+};
 
-  return {
-    check(userInput, message) {
-      const validator = validators[message];
-      validator(userInput);
-    },
-  };
-})();
-
-export default Validator;
+export const Validator = {
+  check(userInput, message) {
+    const validator = validators[message];
+    validator(userInput);
+  },
+};
