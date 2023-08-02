22378c540798a3efd22d119449e3606679c65b14
sonsurim <surim014@naver.com>
2023-08-03 01:23:52 +0900

    refactor: car validate 메서드 분리

--- src/car.js
+++ src/car.js
@@ -19,26 +19,9 @@ export class Car {
   }
 
   #validate(name) {
-    if (!isString(name)) {
-      throw new CustomError({
-        cause: this,
-        message: CAR_ERROR_MESSAGE.INVALID_NAME_TYPE
-      })
-    }
-
-    if (name.length < MIN_NAME_LENGTH) {
-      throw new CustomError({
-        cause: this,
-        message: CAR_ERROR_MESSAGE.UNDER_NAME_MIN_LENGTH(MIN_NAME_LENGTH)
-      })
-    }
-
-    if (name.length > MAX_NAME_LENGTH) {
-      throw new CustomError({
-        cause: this,
-        message: CAR_ERROR_MESSAGE.OVER_NAME_MAX_LENGTH(MAX_NAME_LENGTH)
-      })
-    }
+    this.#validateIsString(name)
+    this.#validateMinLength(name)
+    this.#validateMaxLength(name)
   }
 
   run() {
@@ -62,5 +45,32 @@ export class Car {
 
   getPosition() {
     return this.#position
+  }
+
+  #validateIsString(name) {
+    if (!isString(name)) {
+      throw new CustomError({
+        cause: this,
+        message: CAR_ERROR_MESSAGE.INVALID_NAME_TYPE
+      })
+    }
+  }
+
+  #validateMinLength(name) {
+    if (name.length < MIN_NAME_LENGTH) {
+      throw new CustomError({
+        cause: this,
+        message: CAR_ERROR_MESSAGE.UNDER_NAME_MIN_LENGTH(MIN_NAME_LENGTH)
+      })
+    }
+  }
+
+  #validateMaxLength(name) {
+    if (name.length > MAX_NAME_LENGTH) {
+      throw new CustomError({
+        cause: this,
+        message: CAR_ERROR_MESSAGE.OVER_NAME_MAX_LENGTH(MAX_NAME_LENGTH)
+      })
+    }
   }
 }
