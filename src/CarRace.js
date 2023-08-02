33f18f62d83111b2fc99e3684208bc359b2b5ab0
Youngchai Kim <yckim1224@kigam.re.kr>
2023-08-02 18:15:36 +0900

    style: 많은 초기값을 DEFAULT_CONFIG 관리

--- src/CarRace.js
+++ src/CarRace.js
@@ -1,34 +1,32 @@
 import { Car } from "./Car";
 
-export const MAX_NUM_OF_CARS = 5;
-export const MIN_NUM_OF_CARS = 1;
-export const MIN_RANDOM_NUMBER = 0;
-export const MAX_RANDOM_NUMBER = 9;
-
-export const DEFAULT_LAPS = 5;
+export const DEFAULT_CONFIG = {
+  MAX_NUM_OF_CARS: 5,
+  MIN_NUM_OF_CARS: 1,
+  MIN_RANDOM_NUMBER: 0,
+  MAX_RANDOM_NUMBER: 9,
+  RACE_LAPS: 5,
+};
 
 const getRandomIntInclusive = (min, max) => {
   const minInt = Math.ceil(min);
   const maxInt = Math.floor(max);
-  // The maximum is exclusive and the minimum is inclusive
-  // return Math.floor(Math.random() * (maxInt - minInt) + minInt);
-  // The maximum is inclusive and the minimum is inclusive
   return Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
 };
 
 export class CarRace {
   #cars = [];
-  #laps = DEFAULT_LAPS;
+  #laps = DEFAULT_CONFIG.RACE_LAPS;
   #currentLap = 0;
   init(rawCarNames) {
     const carNames = this.parseCarNames(rawCarNames);
-    if (carNames.length < MIN_NUM_OF_CARS) {
+    if (carNames.length < DEFAULT_CONFIG.MIN_NUM_OF_CARS) {
       throw new Error(
         "A number of cars were given that are too small to race."
       );
     }
     this.#cars = carNames
-      .slice(0, MAX_NUM_OF_CARS)
+      .slice(0, DEFAULT_CONFIG.MAX_NUM_OF_CARS)
       .map((carName) => new Car(carName));
   }
 
@@ -46,7 +44,12 @@ export class CarRace {
 
   #race() {
     this.#cars.forEach((car) =>
-      car.move(getRandomIntInclusive(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER))
+      car.move(
+        getRandomIntInclusive(
+          DEFAULT_CONFIG.MIN_RANDOM_NUMBER,
+          DEFAULT_CONFIG.MAX_RANDOM_NUMBER
+        )
+      )
     );
     this.#currentLap += 1;
   }
