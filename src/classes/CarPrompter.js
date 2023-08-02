a7485028c747b6bfa07a1a15221cf175949bda9c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-03 04:00:10 +0900

    ✨ feat: 이름이 누락된 경우 유효성 검사 수행

--- src/classes/CarPrompter.js
+++ src/classes/CarPrompter.js
@@ -1,25 +1,28 @@
 import { stdin as input, stdout as output } from 'process';
 import * as readline from 'readline';
-import { ALERT_MESSAGE, ERROR_MESSAGE, NAME_CONFIGURE } from '../constants/index';
-import { validateInputToCarRacers, isFunction } from '../utils/index';
+import { ALERT_MESSAGE, NAME_CONFIGURE } from '../constants/index';
+import { validateDuplicateCars, validatePromptListenerType, validateInputMessage } from '../race/index';
 
 export default class CarPrompter {
   #readline = readline.createInterface({ input, output });
 
   #readInputCars(listenerCallback) {
     this.#readline.question(`${ALERT_MESSAGE.INPUT_CAR_MESSAGE}\n`, (inputMessage) => {
-      CarPrompter.validate(inputMessage);
-      listenerCallback(inputMessage.split(NAME_CONFIGURE.SEPARATOR));
+      this.#validateInputMessage(inputMessage);
+      const cars = CarPrompter.separateCars(inputMessage, NAME_CONFIGURE.SEPARATOR);
+      this.#validateDuplicateCars(cars);
+      listenerCallback(cars);
       this.#readline.close();
     });
   }
 
-  static validate = validateInputToCarRacers;
+  static separateCars = (inputCars, separator) => inputCars.split(separator);
+
+  #validateInputMessage = validateInputMessage;
+  #validateDuplicateCars = validateDuplicateCars;
 
   executeReadInput(listenerCallback) {
-    if (!isFunction(listenerCallback)) {
-      throw new Error(ERROR_MESSAGE.NOT_RECEIVED_FUNCTION);
-    }
+    validatePromptListenerType(listenerCallback);
     this.#readInputCars(listenerCallback);
   }
 }
