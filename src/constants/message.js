6c617cd33f8b238ba1a45461603167228bccdc1f
손진영 <sm099@naver.com>
2023-08-02 11:50:08 +0900

    refactor(message): ERROR_MESSAGE 객체 object.freeze 랩핑

--- src/constants/message.js
+++ src/constants/message.js
@@ -5,13 +5,13 @@ export const INPUT_MESSAGE = Object.freeze({
   COUNT: '시도할 횟수는 몇회인가요?\n',
 });
 
-export const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = Object.freeze({
   INVALID_RANGE: (min, max) => `[ERROR] : 자동차 이름은 ${min}~${max}자 사이로 입력해주세요.`,
   DUPLICATE_CAR_NAMES: '[ERROR] : 중복된 자동차 이름이 존재합니다.',
   INCLUDE_EMPTY_WORDS: '[ERROR] : 입력한 자동차 이름에 공백이 존재합니다. 공백 없이 입력해주세요.',
   AVALIABLE_CHARACTER: '[ERROR] : 자동차 이름은 영문자만 가능합니다.',
   AVALIABLE_NUMBER: '[ERROR] : 숫자만 입력 가능합니다.',
-};
+});
 
 export const OUTPUT_MESSAGE = Object.freeze({
   RESULT: '\n실행 결과',
