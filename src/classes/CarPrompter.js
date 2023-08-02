aa434561a2616381f897d795ec77c826c9c12c34
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-03 04:23:20 +0900

    🐛 fix: 변경 안된 상수명 수정

--- src/classes/CarPrompter.js
+++ src/classes/CarPrompter.js
@@ -9,7 +9,7 @@ export default class CarPrompter {
   #readInputCars(listenerCallback) {
     this.#readline.question(`${ALERT_MESSAGE.INPUT_CAR_MESSAGE}\n`, (inputMessage) => {
       this.#validateInputMessage(inputMessage);
-      const cars = CarPrompter.separateCars(inputMessage, CAR_CONFIGURE.SEPARATOR);
+      const cars = CarPrompter.separateCars(inputMessage, CAR_CONFIGURE.NAME_SEPARATOR);
       this.#validateDuplicateCars(cars);
       listenerCallback(cars);
       this.#readline.close();
--- src/constants/message.js
+++ src/constants/message.js
@@ -11,5 +11,5 @@ export const ERROR_MESSAGE = Object.freeze({
 });
 
 export const ALERT_MESSAGE = Object.freeze({
-  INPUT_CAR_MESSAGE: `경주 할 자동차 이름을 입력하세요.(이름은 (${CAR_CONFIGURE.SEPARATOR})를 기준으로 구분)`
+  INPUT_CAR_MESSAGE: `경주 할 자동차 이름을 입력하세요.(이름은 (${CAR_CONFIGURE.NAME_SEPARATOR})를 기준으로 구분)`
 });
