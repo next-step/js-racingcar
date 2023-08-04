1efb69c43028b1f70a15d2317e4819acb3685226
Hoseok Na <hsna7024@gmail.com>
2023-08-05 03:30:26 +0900

    refactor: util 함수 대신에 split 사용

--- src/GameSimulator/index.js
+++ src/GameSimulator/index.js
@@ -1,7 +1,6 @@
 import Car from '../Car/index.js';
 import RacingGame from '../RacingGame/index.js';
 import { getUserInputByQuestion } from '../utils/getUserInputByQuestion.js';
-import { splitStringByComma } from '../utils/splitStringByComma.js';
 import { MAX_ROUNDS } from './constants.js';
 import { validateCarName } from './utils.js';
 
@@ -17,7 +16,7 @@ class GameSimulator {
     const inputString = await getUserInputByQuestion(
       '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
     );
-    const carNames = splitStringByComma(inputString);
+    const carNames = inputString.split(',');
 
     carNames.forEach((name) => validateCarName(name));
 
