0e31a08ee0d1fbe963a9c3b2123f24b7b5520ded
kimhaneui <139193311+TraportStaffs@users.noreply.github.com>
2023-08-05 21:55:41 +0900

    test: test code delete

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -73,35 +73,6 @@ describe("자동차 게임의 횟수 테스트", () => {
     }
   );
 });
-describe("자동차 게임 전진 테스트", () => {
-  beforeAll(() => {
-    Game.register(carNames);
-    Game.declareCount(1);
-  });
-  test("4 이상인 경우 자동차가 전진한다", () => {
-    // randomNumber가 4를 반환하도록 설정
-    jest.spyOn(utils, "randomNumber").mockReturnValue(4);
-
-    Game.gameStart();
-
-    // 자동차가 전진했는지 확인
-    expect(Game.cars[0].position).toBe(1);
-    expect(Game.cars[1].position).toBe(1);
-    expect(Game.cars[2].position).toBe(1);
-  });
-
-  // test("4 미만인 경우 자동차가 멈춘다", () => {
-  //   // randomNumber가 3를 반환하도록 설정
-  //   jest.spyOn(utils.randomNumber()).mockReturnValue(3);
-
-  //   Game.gameStart();
-
-  //   // 자동차가 멈춘 것을 확인
-  //   expect(Game.cars[0].position).toBe(0);
-  //   expect(Game.cars[1].position).toBe(0);
-  //   expect(Game.cars[2].position).toBe(0);
-  // });
-});
 
 describe("우승자 출력 테스트", () => {
   test("우승자를 축하하는 메세지를 정확히 출력하는지 확인", () => {
