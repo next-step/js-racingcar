fcf0b800ffdf2b6aa0eb5b1b6583bb9fcb0733da
sonsurim <surim014@naver.com>
2023-08-07 01:08:49 +0900

    feat: Model 요구사항 구현 및 테스트 코드 작성

--- src/model/Model.js
+++ src/model/Model.js
@@ -1,6 +1,7 @@
 import { Observable } from '../utils/Observable'
 import { getRandomNumber } from '../utils/number'
 import { CAR } from '../constants/car'
+import { RACING_CAR_LIST } from '../constants/racingCarList'
 
 export class Model extends Observable {
   #state
@@ -9,15 +10,34 @@ export class Model extends Observable {
     super()
     this.#state = {
       carList: [],
-      winners: [],
+      winnerList: [],
+      maxMatchLength: RACING_CAR_LIST.DEFAULT_MAX_MATCH_LENGTH,
       runCondition: () => getRandomNumber() > CAR.RUN_THRESHOLDS
     }
   }
 
-  setState(newState) {
+  setCarList(carList) {
     this.#state = {
       ...this.#state,
-      ...newState
+      carList
+    }
+
+    this.notify(this.#state)
+  }
+
+  setWinnerList(winnerList) {
+    this.#state = {
+      ...this.#state,
+      winnerList
+    }
+
+    this.notify(this.#state)
+  }
+
+  setMaxMatchLength(maxMatchLength) {
+    this.#state = {
+      ...this.#state,
+      maxMatchLength
     }
 
     this.notify(this.#state)
