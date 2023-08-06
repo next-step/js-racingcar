3f1e2f6bda1e355dd2b5dbacacc39a397134b070
sonsurim <surim014@naver.com>
2023-08-07 01:04:29 +0900

    feat: Race component 요구사항 구현 및 테스트 코드 작성

---
+++ __tests__/components/Race.test.js
@@ -0,0 +1,74 @@
+import { Race } from '../../src/components/Race'
+
+describe('Race', () => {
+  let mockModel
+
+  beforeEach(() => {
+    mockModel = class {
+      constructor(name) {
+        this.name = name
+        this.position = 0
+      }
+
+      run() {
+        this.position = this.position + 1
+      }
+    }
+  })
+
+  test('Race는 현재 매치 수를 가지고 있다.', () => {
+    // Given, When
+    const race = new Race({})
+
+    // Then
+    expect(race.match).toBe(0)
+  })
+
+  test('Race는 최대 매치 수를 가지고 있다.', () => {
+    // Given, When
+    const race = new Race({
+      maxMatchLength: 5
+    })
+
+    // Then
+    expect(race.maxMatchLength).toBe(5)
+  })
+
+  test('Race는 전진 조건을 가지고 있다.', () => {
+    // Given, When
+    const runCondition = () => false
+    const race = new Race({
+      runCondition
+    })
+
+    // Then
+    expect(race.runCondition).toEqual(runCondition)
+  })
+
+  test('Race는 경주의 참여자 목록을 가지고 있다.', () => {
+    // Given, When
+    const participants = [new mockModel('sonny'), new mockModel('son')]
+    const race = new Race({
+      participants
+    })
+
+    // Then
+    expect(race.participants).toEqual(participants)
+  })
+
+  test('Race는 각 라운드마다 전진 조건에 해당하는 참여자를 이동시킬 수 있다.', () => {
+    // Given
+    const participants = [new mockModel('sonny'), new mockModel('son')]
+    const race = new Race({
+      participants
+    })
+
+    // When
+    race.startRound()
+
+    // Then
+    const [first, second] = participants
+    expect(first.position).toBe(1)
+    expect(second.position).toBe(1)
+  })
+})
