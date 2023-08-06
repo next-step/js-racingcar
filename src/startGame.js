e1f2df82b0ff07d0d2016bc117c3ce91200ed01c
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-06 16:46:58 +0900

    ♻️ refactor: 자동차 경주 실행 코드를 선언적인 코드로 재구성

--- src/startGame.js
+++ src/startGame.js
@@ -3,13 +3,7 @@ import { Car, CarRace } from './classes/index';
 const startGame = (inputCars) => {
   const cars = inputCars.map((car) => new Car(car));
   const carRace = new CarRace(cars);
-
-  while (!carRace.isRaceDone()) {
-    carRace.race();
-    carRace.printRace();
-    carRace.nextLap();
-  }
-  carRace.printWinners();
+  carRace.runFullRace();
 };
 
 export default startGame;
