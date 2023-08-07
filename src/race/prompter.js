3dabdcab8c8ad17ea332cf71b135b54e2ecf80fe
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-07 21:43:04 +0900

    ✨ feat: 프롬프트 실행 시 '자동차 경주 주행 횟수' 까지 입력가능하도록 변경

--- src/race/prompter.js
+++ src/race/prompter.js
@@ -1,36 +1,52 @@
 import { stdin as input, stdout as output } from 'process';
 import * as readline from 'readline';
-import { isString, splitString, isFunction } from '../utils/index';
+import { isOnlyNumber, isString, splitString, isFunction } from '../utils/index';
 import { ALERT_MESSAGE, ERROR_MESSAGE, CAR_CONFIGURE } from '../constants/index';
 import { printMessage } from './viewer';
 
-const { INPUT_CAR_MESSAGE, RETRY_MESSAGE } = ALERT_MESSAGE;
-const { NOT_RECEIVED_FUNCTION, NOT_RECEIVED_INPUT } = ERROR_MESSAGE;
-
 const readlineInterface = readline.createInterface({ input, output });
+
+const { INPUT_CAR_MESSAGE, RETRY_MESSAGE, QUESTION_RACE_LAP_MESSAGE } = ALERT_MESSAGE;
+const { NOT_RECEIVED_FUNCTION, NOT_RECEIVED_INPUT, INVALID_NUMBER } = ERROR_MESSAGE;
+
 const validatePromptListenerType = (listener) => {
   if (!isFunction(listener)) {
     throw new Error(NOT_RECEIVED_FUNCTION);
   }
 };
+
 const isValidInputMessage = (inputMessage) => isString(inputMessage) && inputMessage.trim();
 const convertInputMessageToArray = (inputMessage) => splitString(inputMessage, CAR_CONFIGURE.NAME_SEPARATOR);
 
-const readInputMessage = (message) =>
+const readInputMessage = ({ question, error }, validator) =>
   new Promise((resolve, reject) => {
-    readlineInterface.question(message, (inputMessage) => {
-      if (isValidInputMessage(inputMessage)) {
-        resolve(convertInputMessageToArray(inputMessage));
+    readlineInterface.question(`${question}\n`, (inputMessage) => {
+      if (validator && validator(inputMessage)) {
+        resolve(inputMessage);
       }
-      reject(new Error(NOT_RECEIVED_INPUT));
+      reject(new Error(error));
     });
   });
 
 const executeReadInput = async (listener) => {
   try {
-    const inputMessage = await readInputMessage(INPUT_CAR_MESSAGE);
+    const inputCars = await readInputMessage(
+      {
+        question: INPUT_CAR_MESSAGE,
+        error: NOT_RECEIVED_INPUT
+      },
+      isValidInputMessage
+    );
+    const inputRaceLap = await readInputMessage(
+      {
+        question: QUESTION_RACE_LAP_MESSAGE,
+        error: INVALID_NUMBER
+      },
+      isOnlyNumber
+    );
+
     validatePromptListenerType(listener);
-    listener(inputMessage);
+    listener(convertInputMessageToArray(inputCars), Number(inputRaceLap));
   } catch (error) {
     printMessage(error);
     printMessage(RETRY_MESSAGE);
