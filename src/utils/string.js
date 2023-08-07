ce527dc68a333ee764f02c0c8124651b929211ea
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-07 21:42:10 +0900

    ✨ feat: 숫자 형태의 문자열인지 체크하는 함수 구현

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,3 +1,4 @@
+export const isOnlyNumber = (string) => /^[0-9]*$/.test(string);
 export const isMinimumLength = (string, boundary) => string.length >= boundary;
 export const isMaximumLength = (string, boundary) => string.length <= boundary;
 export const splitString = (string, separator) => string.split(separator);
