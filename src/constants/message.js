702f7ccffb5ce9082243caf1d1ba1b1058948a7b
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-08 02:01:30 +0900

    🔧 modify: 에러메시지 수정 및 추가 (자동차 입력 누락, 주행 횟수 누락)

--- src/constants/message.js
+++ src/constants/message.js
@@ -5,7 +5,8 @@ export const ERROR_MESSAGE = Object.freeze({
   CAR_NAME_NOT_STRING: '자동차 이름은 문자열 타입만 허용됩니다.',
   DUPLICATE_CAR: '이미 해당 이름으로 참가중인 자동차가 존재합니다.',
   NOT_RECEIVED_CAR_NAME: '참여중인 자동차에 누락된 이름이 존재합니다.',
-  NOT_RECEIVED_INPUT: '참여 할 자동차들을 입력해주세요.',
+  NOT_RECEIVED_INPUT_CARS: '참여 할 자동차들을 입력해주세요.',
+  NOT_RECEIVED_INPUT_LAP: '주행 횟수를 입력해주세요.',
   NOT_RECEIVED_FUNCTION: '리스너 함수는 function 타입만 올 수 있습니다.',
   INVALID_INPUT: '올바르지 않은 입력입니다.',
   INVALID_NUMBER: '주행 횟수는 숫자만 입력해주세요.'
