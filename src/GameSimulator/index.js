1fe2d8448491b90573e5b0159095111990fd55fe
Hoseok Na <hsna7024@gmail.com>
2023-08-08 01:05:17 +0900

    feat: round 횟수 사용자로부터 입력 받기

--- src/GameSimulator/index.js
+++ src/GameSimulator/index.js
@@ -15,8 +15,9 @@ class GameSimulator {
       '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
     );
     const carNames = inputString.split(',');
+    const maxRounds = await getUserInputByQuestion('시도할 회수는 몇회인가요?');
 
-    this.#racingGame = new RacingGame(carNames.map(Car.of));
+    this.#racingGame = new RacingGame(carNames.map(Car.of), maxRounds);
   }
 
   printCarStatus(name, distanceDriven) {
