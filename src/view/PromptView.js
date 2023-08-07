d8d33b6f5d8611666131329f81288fa8b51e9eb0
sonsurim <surim014@naver.com>
2023-08-08 00:11:25 +0900

    feat: prompt 메시지 상수화

--- src/view/PromptView.js
+++ src/view/PromptView.js
@@ -1,4 +1,5 @@
 import { Observable } from '../utils/Observable'
+import { PROMPT_MESSAGE } from '../constants/view'
 import { ACTION_TYPE } from '../constants/viewModel'
 
 export class PromptView extends Observable {
@@ -7,37 +8,31 @@ export class PromptView extends Observable {
   }
 
   render(prompt) {
-    prompt.question(
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n',
-      carNames => {
-        prompt.question('시도할 회수는 몇회인가요?\n', maxMatchLength => {
-          this.notify({
-            type: ACTION_TYPE.READY,
-            state: { carNames, maxMatchLength }
-          })
-
-          console.log(
-            `시작하려면 ${ACTION_TYPE.START}를, 종료하려면 ${ACTION_TYPE.EXIT}를 입력하세요.`
-          )
-          prompt.prompt()
-
-          prompt.on('line', line => {
-            switch (line) {
-              case ACTION_TYPE.START:
-                this.notify({ type: ACTION_TYPE.START })
-                break
-              case ACTION_TYPE.EXIT:
-                prompt.close()
-                break
-              default:
-                console.log('지원하지 않는 명령어 입니다!')
-            }
-
-            prompt.close()
-          })
+    prompt.question(PROMPT_MESSAGE.QUESTION_CAR_NAMES, carNames => {
+      prompt.question(PROMPT_MESSAGE.QUESTION_MATCH_LENGTH, maxMatchLength => {
+        this.notify({
+          type: ACTION_TYPE.READY,
+          state: { carNames, maxMatchLength }
         })
-      }
-    )
+
+        console.log(PROMPT_MESSAGE.INFO)
+        prompt.prompt()
+
+        prompt.on('line', line => {
+          switch (line) {
+            case ACTION_TYPE.START:
+              this.notify({ type: ACTION_TYPE.START })
+              prompt.close()
+              break
+            case ACTION_TYPE.EXIT:
+              prompt.close()
+              break
+            default:
+              console.log(PROMPT_MESSAGE.INVALID_SCRIPT)
+          }
+        })
+      })
+    })
   }
 
   destroy() {
