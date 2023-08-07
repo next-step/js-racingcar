b8e42eb20553f6ac669acf2c90d790fc6bb48709
Hoseok Na <hsna7024@gmail.com>
2023-08-07 20:37:46 +0900

    refactor: max 값이 포함되서 나올 수 있도록 함수 수정
    기본값은 0 ~ 9가 나오도록 유지.

--- src/utils/getRandomInRange.js
+++ src/utils/getRandomInRange.js
@@ -1,3 +1,3 @@
-export const getRandomInRange = (min = 0, max = 10) => {
-  return min + Math.floor(Math.random() * (max - min));
+export const getRandomInRange = (min = 0, max = 9) => {
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
