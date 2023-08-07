1f32b98232cfa08b87d113242338bfbc6132c5e3
sonsurim <surim014@naver.com>
2023-08-07 23:39:24 +0900

    feat: 요구사항 구현 및 테스트 코드 작성 - ConsoleView

---
+++ src/view/ConsoleView.js
@@ -0,0 +1,22 @@
+export class ConsoleView {
+  renderCarList({ carList }) {
+    const currentTrack = car => `${car.name} : ${'-'.repeat(car.position)}`
+
+    const result = carList.map(currentTrack).join('\n')
+    console.log(`\n${result}`)
+  }
+
+  renderWinnerList({ winnerList }) {
+    if (winnerList.length === 0) {
+      console.log('\n우승자가 없습니다!')
+      return
+    }
+
+    console.log(`\n${winnerList.join(', ')}(이)가 최종 우승했습니다.`)
+  }
+
+  update(state) {
+    this.renderCarList(state)
+    this.renderWinnerList(state)
+  }
+}
