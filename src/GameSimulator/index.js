9ceae1aa0e6bf0d9698f1817e98d5ad1b64e500c
Hoseok Na <hsna7024@gmail.com>
2023-08-08 03:02:06 +0900

    refactor: 자동차 이름 입력 받았으면 먼저 Car 클래스들을 만들도록 수정
    잘못된 자동차 이름이면 바로 재시작할 수 있게 생성 순서 변경.

--- src/GameSimulator/index.js
+++ src/GameSimulator/index.js
@@ -15,9 +15,10 @@ class GameSimulator {
       '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).'
     );
     const carNames = inputString.split(',');
+    const cars = carNames.map(Car.of);
     const maxRounds = await getUserInputByQuestion('시도할 회수는 몇회인가요?');
 
-    this.#racingGame = new RacingGame(carNames.map(Car.of), maxRounds);
+    this.#racingGame = new RacingGame(cars, maxRounds);
   }
 
   printCarStatus(name, distanceDriven) {
