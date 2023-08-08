d6b35fb00ed467fff232e06400f01d981f655896
Jason <jasondal91@gmail.com>
2023-08-08 19:43:40 +0900

    test: 자동차 경주 횟수 추가

--- __tests__/CarRace.test.js
+++ __tests__/CarRace.test.js
@@ -1,6 +1,7 @@
 import { CAR_RACE_LAP_LIMIT } from '../src/constants';
 import Car from '../src/domain/car';
 import CarRace from '../src/domain/carRace';
+import { ERROR_MESSAGE } from '../src/validation/errorMessage';
 
 describe('CarRace', () => {
   it('자동차 이름은 쉼표(,)를 기준으로 구분한다.', () => {
@@ -13,14 +14,31 @@ describe('CarRace', () => {
     expect(carRace.participantNames).toBe('광민, 문광민, Jason');
   });
 
-  it('자동차 경주는 5회로 고정한다.', () => {
-    const car = new Car('광민');
-    const carRace = new CarRace([car]);
-    const runOneLap = jest.spyOn(car, 'runOneLap');
+  it('자동차 경주 횟수를 지정할 수 있다.', () => {
+    const carRace = new CarRace([
+      new Car('광민'),
+      new Car('문광민'),
+      new Car('Jason'),
+    ]);
 
-    carRace.start();
+    const lapCount = 7;
 
-    expect(runOneLap).toBeCalledTimes(CAR_RACE_LAP_LIMIT);
+    carRace.setLapCount(lapCount);
+
+    expect(carRace.lapCount).toBe(lapCount);
+  });
+
+  describe('자동차 경주 횟수는 1이상의 양수이어야 한다.', () => {
+    test.each([[0], [-1], [-1.2]])('.setLapCount(%n)', (lapCount) => {
+      const carRace = new CarRace([
+        new Car('광민'),
+        new Car('문광민'),
+        new Car('Jason'),
+      ]);
+      expect(() => carRace.setLapCount(lapCount)).toThrowError(
+        ERROR_MESSAGE.CAR_RACE_LAP_COUNT
+      );
+    });
   });
 
   describe('우승자', () => {
