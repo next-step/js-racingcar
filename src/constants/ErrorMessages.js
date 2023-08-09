6102b286338d9784bc88892afb65298da1ded842
callmejeje <choije401@naver.com>
2023-08-09 17:41:04 +0900

    refactor: Object에서 각 문자열로 변경

---
+++ src/constants/ErrorMessages.js
@@ -0,0 +1,11 @@
+import {
+  CAR_NAME_MAX_LENGTH,
+  CAR_NAME_MIN_LENGTH,
+  RACE_MIN_CAR_COUNT,
+} from "./Numbers";
+
+const INVALID_CAR_NAME_LENGTH = `자동차 이름은 ${CAR_NAME_MIN_LENGTH}글자 이상 ${CAR_NAME_MAX_LENGTH}글자 이하여야 합니다.`;
+const MIN_CAR_COUNT_NOT_REACHED = `자동차는 최소 ${RACE_MIN_CAR_COUNT}대 이상이어야 합니다.`;
+const DUPLICATE_NAME = `자동차 이름은 중복되지 않아야 합니다.`;
+
+export { DUPLICATE_NAME, INVALID_CAR_NAME_LENGTH, MIN_CAR_COUNT_NOT_REACHED };
