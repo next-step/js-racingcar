93ed812a269a907c2317fb5ca18dd1ac9c31043c
Hoseok Na <hsna7024@gmail.com>
2023-08-10 00:08:20 +0900

    refactor: readline 한번만 생성되도록 수정

--- src/utils/getUserInputByQuestion.js
+++ src/utils/getUserInputByQuestion.js
@@ -1,10 +1,10 @@
 import * as readline from 'node:readline/promises';
 
-export const getUserInputByQuestion = (question) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+export const getUserInputByQuestion = (question) => {
   return rl.question(question + '\n');
 };
