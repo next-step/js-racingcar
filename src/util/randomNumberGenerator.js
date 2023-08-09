5caf121211adef37e0586f89a33ad3f6f83a3d31
froggy1014 <clcl6084@gmail.com>
2023-08-10 02:25:09 +0900

    fix: util naming changes into noun convention

---
+++ src/util/randomNumberGenerator.js
@@ -0,0 +1,3 @@
+export const randomNumberGenerator = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
---
+++ src/util/readlineController.js
@@ -0,0 +1,39 @@
+import { stdin as input, stdout as output } from 'process';
+import readline from 'readline';
+import { MESSAGES } from '../domain/constants/messages.js';
+import { isMaxAttempt } from '../domain/validator.js';
+
+export const readlineController = {
+  rl: readline.createInterface({
+    input,
+    output,
+  }),
+
+  questionReadline(prompt, validator) {
+    let attempt = 0;
+    return new Promise((resolve) => {
+      const question = () => {
+        this.rl.question(`${prompt}\n`, (answer) => {
+          try {
+            validator(answer);
+            resolve(answer);
+          } catch (error) {
+            attempt += 1;
+            if (isMaxAttempt(attempt)) {
+              console.log(MESSAGES.ERROR.MAX_ATTEMPT_EXCEEDED);
+              this.closeReadline();
+            } else {
+              console.log(error.message);
+              question();
+            }
+          }
+        });
+      };
+      question();
+    });
+  },
+
+  closeReadline() {
+    this.rl.close();
+  },
+};
