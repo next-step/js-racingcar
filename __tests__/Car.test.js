1882c752888e82ea328a5ecf7761e29600f77f47
ericagong <48196721+ericagong@users.noreply.github.com>
2023-08-13 16:40:01 +0900

    test: Car 테스트코드 안내문 수정

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,22 +1,22 @@
 import Car from "../src/Models/Car.js";
 
-describe("자동차는 이름의 유효성을 확인하고, 유효할 경우만 자동차 객체를 생성한다.", () => {
+describe("Car는 이름의 유효성을 확인하고, 유효할 경우만 Car 객체를 생성한다.", () => {
   const CAR_ERROR_MESSAGE = Car.ERROR_MESSAGE;
 
-  it("자동차 이름이 빈 값이면 에러를 발생시킨다.", () => {
+  it("Car 이름이 빈 값이면 에러를 발생시킨다.", () => {
     expect(() => Car.of("")).toThrow(CAR_ERROR_MESSAGE.EMPTY_NAME);
   });
 
-  it("자동차 이름이 5자 초과라면 에러를 발생시킨다.", () => {
+  it("Car 이름이 5자 초과라면 에러를 발생시킨다.", () => {
     expect(() => Car.of("ericagong")).toThrow(CAR_ERROR_MESSAGE.LONG_NAME);
   });
 
-  it("5자 이하의 유효한 자동차 이름이라면, 오류를 발생시키지 않는다.", () => {
+  it("5자 이하의 유효한 Car 이름이라면, 오류를 발생시키지 않는다.", () => {
     expect(() => Car.of("erica")).not.toThrow();
   });
 
   it.each([{ position: 0 }, { position: 1 }])(
-    "유효한 자동차 이름과 위치가 주어지면, 이름과 위치를 상태로 갖는 자동차 객체를 생성한다.",
+    "유효한 Car 이름과 위치가 주어지면, 이름과 위치를 상태로 갖는 Car 객체를 생성한다.",
     ({ position }) => {
       const car = Car.of("erica", position);
 
@@ -26,9 +26,9 @@ describe("자동차는 이름의 유효성을 확인하고, 유효할 경우만 
   );
 });
 
-describe(`자동차가 전진하면, 현재 위치가 1만큼 증가한다.`, () => {
+describe(`Car가 전진하면, 현재 위치가 1만큼 증가한다.`, () => {
   it.each([{ position: 0 }, { position: 1 }])(
-    "자동차가 전진하면 현재 위치가 1만큼 증가한다.",
+    "Car가 전진하면 현재 위치가 1만큼 증가한다.",
     ({ position }) => {
       const car = Car.of("erica", position);
       const currentPosition = car.position;
@@ -67,18 +67,18 @@ describe(`자동차가 전진하면, 현재 위치가 1만큼 증가한다.`, ()
 //   );
 // });
 
-describe("자동차는 이름과 현재 위치 정보를 반환한다.", () => {
+describe("Car는 이름과 현재 위치 정보를 반환한다.", () => {
   const car = Car.of("erica", 0);
 
-  it("자동차 객체는 이름을 반환한다.", () => {
+  it("Car 객체는 이름을 반환한다.", () => {
     expect(car.name).toBe("erica");
   });
 
-  it("자동차 객체는 현재 위치를 반환한다.", () => {
+  it("Car 객체는 현재 위치를 반환한다.", () => {
     expect(car.position).toBe(0);
   });
 
-  it("자동차 객체는 이름과 현재 이름을 객체 형태로 반환한다.", () => {
+  it("Car 객체는 이름과 현재 이름을 객체 형태로 반환한다.", () => {
     expect(car.getRecord()).toBeInstanceOf(Object);
     expect(car.getRecord()).toHaveProperty("name");
     expect(car.getRecord()).toHaveProperty("position");
