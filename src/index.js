779c4f729f9dd6da8f76fd0adc65bbc6b48d867c
장정원 <longthegarden@innodep.com>
2023-08-14 21:19:56 +0900

    feat: 콘솔을 통해 값 입력

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import readline from "readline";
+
+import RacingGame from "./RacingGame";
+import { INPUT_MSG } from "./contants/messages";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const racingGame = new RacingGame();
+
+rl.question(INPUT_MSG, (answer) => {
+  racingGame.setPlayers(answer);
+  rl.close();
+});
+
+rl.on("close", () => {
+  racingGame.start();
+  racingGame.getWinners();
+  process.exit();
+});
