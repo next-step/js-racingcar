0bacbc285954090cda15f3f683032844f641e44b
y00n-lee <0613myday@gmail.com>
2023-08-15 12:46:13 +0900

    Feat: 자동차 이름 유효성 검사 작성
    - 이름의 공백, 특수문자는 제거.
    - 각 이름의 길이가 5자 이하인지 확인.
    - 동일한 이름이 있을 경우 구분을 위해 이름 뒤에 숫자를 붙임.
    - 유효하지 않은 이름이 입력되었을 경우 에러 출력 후 종료.

--- src/InputCarNames.js
+++ src/InputCarNames.js
@@ -1,10 +1,60 @@
 const ERROR_MESSAGE = Object.freeze({
     NO_INPUT_STRING: "사용자 입력이 없어 프로그램을 종료합니다.",
+    INVALID_NAME: "유효하지 않은 이름: ${}\n유효하지 않은 이름이 입력되어 프로그램을 종료합니다.",
+    NAME_LENGTH_EXCEED: "5자가 넘는 이름이 있어 프로그램을 종료합니다.",
 });
+
+const NAME_LENGTH_LIMIT = 5;
+
+const findInValidName = (nameList) => {
+    const re = /[ㄱ-ㅎㅏ-ㅣ]/gi;
+    const invalidNameString = nameList?.reduce((acc, cur) => {
+        if (cur.search(re) === -1) return acc;
+        return acc + cur + ",";
+    }, "");
+
+    if (invalidNameString) return invalidNameString.slice(0, -1);
+};
+
+const renameDuplicateNames = (nameList) => {
+    const nameObject = {};
+    const returnNameList = [];
+
+    nameList?.forEach(($name) => {
+        if (!nameObject[$name]) return (nameObject[$name] = 1);
+        return (nameObject[$name] += 1);
+    });
+
+    for (const [key, value] of Object.entries(nameObject)) {
+        if (value === 1) {
+            returnNameList.push(key);
+            delete nameObject[key];
+        }
+    }
+
+    for (const [key, value] of Object.entries(nameObject)) {
+        for (let i = 0; i < value; i++) {
+            returnNameList.push(`${key}${i + 1}`);
+        }
+    }
+
+    return returnNameList;
+};
 
 export const InputCarNames = (nameString) => {
     if (!nameString) return ERROR_MESSAGE.NO_INPUT_STRING;
 
-    const splitedNameArr = nameString.split(",");
-    return splitedNameArr;
+    const splitedNameList = nameString
+        .split(",")
+        .map(($name) => $name.replace(/[ \{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g, ""));
+
+    for (const name of splitedNameList) {
+        if (name.length > NAME_LENGTH_LIMIT) return ERROR_MESSAGE.NAME_LENGTH_EXCEED;
+    }
+
+    const invalidName = findInValidName(splitedNameList);
+    if (invalidName) return ERROR_MESSAGE.INVALID_NAME.replace("${}", invalidName);
+
+    const rename = renameDuplicateNames(splitedNameList);
+    return rename;
 };
