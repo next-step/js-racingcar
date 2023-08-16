d711e38da86641ff5972f846292640293e002984
Frank Kim <frankuserconnect@gmail.com>
2023-08-16 16:40:27 +0900

    refactor: domain -> GameTrack

--- src/domain/model/GameTrack.js
+++ src/domain/model/GameTrack.js
@@ -1,4 +1,17 @@
-import { ERROR_MESSAGE } from "../../constants/ErrorMessage";
+import { validateNameList } from "../../UI/Controller/validator";
+
+import { getRandomNumber } from "../../utils/helpers";
+import { validateDuplicationItemList } from "../../utils/validator";
+
+import {
+  MAX_RANDOM_NUMBER,
+  MIN_RANDOM_NUMBER,
+  RACE_FORWARD_RANDOM_NUMBER_LIMIT,
+} from "../../constants/rules";
+
+import { NO_EMPTY_NAME } from "../../constants/errorMessage";
+
+export const DEFAULT_SCORE = "-";
 
 export default class GameTrack {
   constructor() {
@@ -8,21 +21,36 @@ export default class GameTrack {
   /**
    * CAR_NAME_INPUT_PROMPT - 입력 받은 값
    * @param(string) carNames
-   * @throws {Error} maxCarNameLength, duplicateCarName
+   * @throws {Error} MAX_LENGTH_CAR_NAME, duplicateCarName
    * @return [{carName, forward: ""}]
    */
-  setGameStatus(carNames) {
-    carNames.split(",").forEach(carName => {
-      if (this.isValidCarDuplicateName(carName.trim())) {
-        throw new Error(ERROR_MESSAGE.duplicateName);
-      }
 
-      this.gameStatus.push({ carName: carName.trim(), forward: "" });
+  setGameCarList(carNames) {
+    return new Promise((resolve, reject) => {
+      try {
+        const carNameList = carNames.split(",").map(item => item.trim());
+        validateDuplicationItemList(carNameList);
+        validateNameList(carNameList, NO_EMPTY_NAME);
+
+        carNameList.forEach(carName =>
+          this.gameStatus.push({ carName: carName.trim(), forward: 0 })
+        );
+
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
     });
   }
 
-  /** 자동차 이름 중복처리 */
-  isValidCarDuplicateName(carName) {
-    return this.gameStatus.some(car => car.carName === carName);
+  setAdvanceCarsForward() {
+    this.gameStatus.forEach(currentStatus => {
+      if (
+        getRandomNumber(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) >=
+        RACE_FORWARD_RANDOM_NUMBER_LIMIT
+      ) {
+        currentStatus.forward++;
+      }
+    });
   }
 }
