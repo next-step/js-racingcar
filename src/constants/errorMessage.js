6db0c05c6b57a7d9eaa60ee13e794d04d90af62c
Frank Kim <frankuserconnect@gmail.com>
2023-08-16 16:46:46 +0900

    refactor: constatns/error message

--- src/constants/errorMessage.js
+++ src/constants/errorMessage.js
@@ -1,9 +1,17 @@
-import { MAX_CAR_NAME_LENGTH, RACE_FORWARD_RANDOM_NUMBER_LIMIT } from "./rules";
+import { MAX_CAR_NAME_LENGTH } from "./rules";
+
+export const NO_EMPTY_NAME = "NO_EMPTY_NAME";
+export const NO_EMPTY_STRING = "noEmptyString";
+export const NO_DUPLICATE_NAME = "NO_DUPLICATE_NAME";
+export const NUMBER_REQUIRED = "NUMBER_REQUIRED";
+export const MAX_LENGTH_CAR_NAME = "MAX_LENGTH_CAR_NAME";
+export const INVALID_INPUT = "INVALID_INPUT";
 
 export const ERROR_MESSAGE = {
-  maxCarNameLength: `이름은 ${MAX_CAR_NAME_LENGTH}자를 넘길 수 없습니다.`,
-  duplicateName: "이름은 중복될 수 없습니다.",
-  noEmptyName: "이름은 빈값일 수 없습니다.",
-  invalidInput: "잘못된 입력 값을 작성한 경우, 프로그램을 종료합니다.",
-  raceForwardNumberLimit: `무작위 숫자 값이 ${RACE_FORWARD_RANDOM_NUMBER_LIMIT}이상이여야 한다.`,
+  [MAX_LENGTH_CAR_NAME]: `이름은 ${MAX_CAR_NAME_LENGTH}자를 넘길 수 없습니다.`,
+  [NUMBER_REQUIRED]: `1 이상 숫자만 입력해주세요.`,
+  [NO_DUPLICATE_NAME]: "이름은 중복될 수 없습니다.",
+  [NO_EMPTY_STRING]: "빈 값일 수 없습니다.",
+  [NO_EMPTY_NAME]: "이름은 빈값일 수 없습니다.",
+  [INVALID_INPUT]: "잘못된 입력 값을 작성한 경우, 프로그램을 종료합니다.",
 };
