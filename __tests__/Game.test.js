86052fc42c7efaedfb6a3c876b4b87587ecc68ce
ericagong <48196721+ericagong@users.noreply.github.com>
2023-08-19 14:56:08 +0900

    refactor: [Game Model] 에러 객체 분리 및 테스트 코드 수정

--- __tests__/Game.test.js
+++ __tests__/Game.test.js
@@ -1,137 +1,160 @@
-import { Cars } from "../src/Models/Cars";
-import { Game } from "../src/Models/Game";
-import { MoveStrategies } from "../src/Models/MoveStrategy";
+import createGame from "../src/Models/Game";
+import {
+  CarNamesIsEmptyError,
+  TotalRoundsNotNumberError,
+  TotalRoundsNotIntegerError,
+  TotalRoundsNotPositiveError,
+  TotalRoundsIsEmptyError,
+} from "../src/Models/Game/errors";
+import MoveStrategies from "../src/Models/MoveStrategy/MoveStrategies";
 
 describe("게임 설정 테스트", () => {
+  const { setGame } = createGame();
   describe("유효하지 않은 값을 입력한 경우", () => {
-    it.each(["", null, undefined, " "])(
-      "자동차 이름이 빈 값인 경우, 에러를 발생시킨다.",
-      (carNamesInput) => {
-        expect(() => Game.setGame(carNamesInput, 5)).toThrow(
-          Game.ERROR_MESSAGE.EMPTY
-        );
-      }
-    );
+    describe("carNames가 빈 값이면, 에러를 발생시킨다.", () => {
+      it.each(["", null, undefined, " "])("%p", (carNamesInput) => {
+        expect(() => setGame(carNamesInput, 5)).toThrow(CarNamesIsEmptyError);
+      });
+    });
 
-    it.each(["", null, undefined, " "])(
-      "시도 횟수가 빈 값인 경우, 에러를 발생시킨다.",
-      (roundsInput) => {
-        expect(() => Game.setGame("erica", roundsInput)).toThrow(
-          Game.ERROR_MESSAGE.EMPTY
-        );
-      }
-    );
+    describe("totalRounds가 빈 값이면, 에러를 발생시킨다.", () => {
+      it.each(["", null, undefined, " "])(
+        "시도 횟수가 빈 값인 경우, 에러를 발생시킨다.",
+        (roundsInput) => {
+          expect(() => setGame("erica", roundsInput)).toThrow(
+            TotalRoundsIsEmptyError
+          );
+        }
+      );
+    });
 
-    it.each(["12@", "123456*", "12ab", "abcde", "-12a"])(
-      "시도 횟수가 숫자 형태가 아닌 경우, 에러를 발생시킨다.",
-      (roundsInput) => {
-        expect(() => Game.setGame("erica", roundsInput)).toThrow(
-          Game.ERROR_MESSAGE.NOT_NUMBER
-        );
-      }
-    );
+    describe("totalRounds가 숫자 형태가 아니면, 에러를 발생시킨다.", () => {
+      it.each(["12@", "123456*", "12ab", "abcde", "-12a"])(
+        "%p",
+        (roundsInput) => {
+          expect(() => setGame("erica", roundsInput)).toThrow(
+            TotalRoundsNotNumberError
+          );
+        }
+      );
+    });
 
-    it.each([1.5, 0.5, 1.03])(
-      "시도 횟수가 정수가 아닌 경우, 에러를 발생시킨다.",
-      (roundsInput) => {
-        expect(() => Game.setGame("erica", roundsInput)).toThrow(
-          Game.ERROR_MESSAGE.NOT_INTEGER
+    describe("totalRounds가 정수 형태가 아니면, 에러를 발생시킨다.", () => {
+      it.each([1.5, 0.5, 1.03])("%p", (roundsInput) => {
+        expect(() => setGame("erica", roundsInput)).toThrow(
+          TotalRoundsNotIntegerError
         );
-      }
-    );
+      });
+    });
 
-    it.each([-10, -1, 0])(
-      "시도 횟수가 양의 정수가 아닌 경우, 에러를 발생시킨다.",
-      (roundsInput) => {
-        expect(() => Game.setGame("erica", roundsInput)).toThrow(
-          Game.ERROR_MESSAGE.NOT_POSITIVE
+    describe("totalRounds가 양의 정수가 아니면, 에러를 발생시킨다.", () => {
+      it.each([-10, -1, 0])("%p", (roundsInput) => {
+        expect(() => setGame("erica", roundsInput)).toThrow(
+          TotalRoundsNotPositiveError
         );
-      }
-    );
+      });
+    });
   });
 
   describe("유효한 값을 입력한 경우", () => {
-    it.each(["e", "er", "eri", "eric", "erica", "  _", "!!! "])(
-      "자동차 이름을 하나만 입력한 경우",
-      (userInput) => {
-        expect(() => Game.setGame(userInput, 5)).not.toThrow();
-      }
-    );
+    describe("자동차 이름을 하나만 입력한 경우, 에러를 발생시키지 않는다.", () => {
+      it.each(["e", "er", "eri", "eric", "erica", "  _", "!!! "])(
+        "%p",
+        (userInput) => {
+          expect(() => setGame(userInput, 5)).not.toThrow();
+        }
+      );
+    });
 
+    describe("중복없이 자동차 이름을 여러개 입력한 경우, 에러를 발생시키지 않는다.", () => {
+      it.each([
+        "car1, car2, car3",
+        "apple, Peach, eGG",
+        "12345, aBcDe, !*_",
+        "test1, Test2, tEsT1, test2, TeSt1",
+        "name1, name2, name3, name4, name5",
+      ])("%p", (userInput) => {
+        expect(() => setGame(userInput, 5)).not.toThrow();
+      });
+    });
+  });
+});
+
+describe("playGame() 테스트", () => {
+  const { setGame, playGame, getGameResult } = createGame();
+  describe("게임을 총 5라운드 진행한다.", () => {
+    setGame("erica, Erica, ryang, yang, theon", 5);
+    playGame(new MoveStrategies("50011"));
+    const gameResult = getGameResult();
+
+    it("게임을 총 5라운드 진행한다.", () => {
+      expect(gameResult.roundHistory.length).toBe(5);
+    });
+
+    it("각 라운드 별 기록을 올바르게 roundHistory에 저장한다.", () => {
+      gameResult.roundHistory.forEach((roundRecord, idx) => {
+        expect(roundRecord).toEqual([
+          { name: "erica", position: idx + 1 },
+          { name: "Erica", position: 0 },
+          { name: "ryang", position: 0 },
+          { name: "yang", position: 0 },
+          { name: "theon", position: 0 },
+        ]);
+      });
+    });
+  });
+});
+
+describe("getGameResult() 테스트", () => {
+  describe("올바른 우승자를 반환한다.", () => {
+    const { setGame, playGame, getGameResult } = createGame();
     it.each([
-      "car1, car2, car3",
-      "apple, Peach, eGG",
-      "12345, aBcDe, !*_",
-      "test1, Test2, tEsT1, test2, TeSt1",
-      "name1, name2, name3, name4, name5",
-    ])("중복 없이 자동차 이름을 여러 개 입력한 경우", (userInput) => {
-      expect(() => Game.setGame(userInput, 5)).not.toThrow();
+      {
+        strategies: "50000",
+        winnerNames: ["erica"],
+        winnerCount: 1,
+      },
+      {
+        strategies: "55000",
+        winnerNames: ["erica", "Erica"],
+        winnerCount: 2,
+      },
+      {
+        strategies: "55500",
+        winnerNames: ["erica", "Erica", "ryang"],
+        winnerCount: 3,
+      },
+      {
+        strategies: "55550",
+        winnerNames: ["erica", "Erica", "ryang", "yang"],
+        winnerCount: 4,
+      },
+      {
+        strategies: "55555",
+        winnerNames: ["erica", "Erica", "ryang", "yang", "theon"],
+        winnerCount: 5,
+      },
+    ])("winnerNames: $winnerNames", ({ winnerNames, strategies }) => {
+      setGame("erica, Erica, ryang, yang, theon", 5);
+      playGame(new MoveStrategies(strategies));
+      expect(getGameResult().winnerNames).toEqual(winnerNames);
     });
   });
-});
 
-describe(`게임을 총 5라운드 진행한다.`, () => {
-  const spyPlayOneRound = jest.spyOn(Cars, "playOneRound");
-
-  beforeAll(() => {
-    Game.setGame("erica, Erica, ryang, yang, theon", 5);
-    Game.playGame(new MoveStrategies("50011"));
-  });
-
-  afterAll(() => {
-    spyPlayOneRound.mockClear();
-  });
-
-  it("각 라운드를 진행하는 함수를 5번 호출한다.", () => {
-    expect(spyPlayOneRound).toBeCalledTimes(5);
-  });
-
-  it("각 라운드 별 기록을 올바르게 roundHistory에 저장한다.", () => {
-    const carNames = "erica, Erica, ryang, yang, theon"
-      .split(",")
-      .map((carName) => carName.trim());
-    const expectedRoundHistory = carNames.map((carName) => ({
-      name: carName,
-      position: 0,
-    }));
-
-    const gameResult = Game.getGameResult();
-
-    gameResult.roundHistory.forEach((roundRecord) => {
-      expectedRoundHistory[0].position += 1;
-      expect(roundRecord).toEqual(expectedRoundHistory);
+  describe("게임 기록과 우승자 정보를 반환한다.", () => {
+    const { setGame, playGame, getGameResult } = createGame();
+    setGame("erica, Erica, ryang, yang, theon", 5);
+    playGame(new MoveStrategies("50011"));
+    const gameResult = getGameResult();
+    gameResult.roundHistory.forEach((roundRecord, idx) => {
+      expect(roundRecord).toEqual([
+        { name: "erica", position: idx + 1 },
+        { name: "Erica", position: 0 },
+        { name: "ryang", position: 0 },
+        { name: "yang", position: 0 },
+        { name: "theon", position: 0 },
+      ]);
     });
-
-    expect(gameResult.roundHistory.length).toBe(5);
-  });
-
-  it("우승자가 한 명인 경우, 올바른 우승자를 찾는다.", () => {
-    expect(Game.getGameResult().winnerNames).toEqual(["erica"]);
-  });
-});
-
-describe("우승자 판단 테스트", () => {
-  it("우승자가 한 명인 경우, 올바른 우승자를 반환한다.", () => {
-    Game.setGame("erica, Erica, ryang, yang, theon", 5);
-    Game.playGame(new MoveStrategies("50000"));
-    const gameResult = Game.getGameResult();
     expect(gameResult.winnerNames).toEqual(["erica"]);
   });
-
-  it.each(["55000", "55500", "55500", "55550", "55555"])(
-    "우승자가 두 명 이상인 경우, 올바른 우승자를 반환한다.",
-    (str) => {
-      Game.setGame("erica, Erica, ryang, yang, theon", 5);
-      Game.playGame(new MoveStrategies(str));
-      const gameResult = Game.getGameResult();
-
-      const carNames = "erica, Erica, ryang, yang, theon"
-        .split(",")
-        .map((carName) => carName.trim());
-      const expectedWinnerNames = carNames.filter(
-        (_, index) => str[index] === "5"
-      );
-      expect(gameResult.winnerNames).toEqual(expectedWinnerNames);
-    }
-  );
 });
---
+++ src/Models/Game/index.js
@@ -0,0 +1,87 @@
+import {
+  CarNamesIsEmptyError,
+  TotalRoundsIsEmptyError,
+  TotalRoundsNotNumberError,
+  TotalRoundsNotIntegerError,
+  TotalRoundsNotPositiveError,
+} from "./errors";
+import createCars from "../Cars";
+import MoveStrategies from "../MoveStrategy/MoveStrategies";
+
+export default createGame = (carNamesInput, roundsInput) => {
+  const roundHistory = [];
+  let cars = [];
+  let totalRounds = 0;
+  const { from, playOneRound, getRoundRecord } = createCars();
+
+  function isEmpty(value) {
+    return (
+      value === null ||
+      value === undefined ||
+      (typeof value === "string" && value.trim() === "")
+    );
+  }
+
+  function validateUserInput(input) {
+    if (isEmpty(input)) throw new CarNamesIsEmptyError();
+  }
+
+  function validateRoundsInput(input) {
+    if (isEmpty(input)) throw new TotalRoundsIsEmptyError();
+
+    const round = Number(input);
+    if (Number.isNaN(round)) throw new TotalRoundsNotNumberError();
+    if (!Number.isInteger(round)) throw new TotalRoundsNotIntegerError();
+    if (round < 1) throw new TotalRoundsNotPositiveError();
+  }
+
+  function parseCarNames(userInput) {
+    return userInput.split(",").map((carName) => carName.trim());
+  }
+
+  function setGame(carNamesInput, roundsInput) {
+    validateUserInput(carNamesInput);
+
+    validateRoundsInput(roundsInput);
+
+    const carNames = parseCarNames(carNamesInput);
+
+    cars = from(carNames);
+
+    totalRounds = Number(roundsInput);
+  }
+
+  function playGame(
+    moveStrategies = new MoveStrategies("R".repeat(cars.length))
+  ) {
+    while (totalRounds--) {
+      playOneRound(cars, moveStrategies);
+
+      roundHistory.push(getRoundRecord(cars));
+    }
+  }
+
+  function getWinners() {
+    const maxPosition = Math.max(
+      ...cars.map((car) => car.getRecord().position)
+    );
+
+    return cars.filter((car) => car.getRecord().position === maxPosition);
+  }
+
+  function getGameResult() {
+    const winners = getWinners();
+    const winnerNames = winners.map((car) => car.getRecord().name);
+
+    return {
+      roundHistory,
+      winnerNames,
+    };
+  }
+
+  return {
+    setGame,
+    playGame,
+    getGameResult,
+  };
+};
