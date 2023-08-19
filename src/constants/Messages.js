7907f931fff1f254b89bd45125374be0122c45d3
callmejeje <choije401@naver.com>
2023-08-19 20:37:42 +0900

    feat: 회수 입력 메시지 추가

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,5 +1,6 @@
 const INPUT_CAR_NAMES = `경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).`;
+const TRIAL_NUM_OF_ROUND = `시도할 회수는 몇회인가요?`;
 const RESULT_OF_RACE = `실행 결과`;
 const WINNER_IS = (carNames) => `${carNames}가 최종 우승했습니다.`;
 
-export { INPUT_CAR_NAMES, RESULT_OF_RACE, WINNER_IS };
+export { INPUT_CAR_NAMES, RESULT_OF_RACE, TRIAL_NUM_OF_ROUND, WINNER_IS };
