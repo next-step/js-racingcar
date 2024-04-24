6f74cb7b7aa076cfed317479181ff38c59a36973
oxlzlo <yeojin58@naver.com>
2024-04-24 21:25:55 +0900

    feat: 자동차는 0부터 9사이의 무작위 값이 4이상일 때 전진한다

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -12,4 +12,16 @@ describe('자동차 기능 테스트', () => {
     expect(name.length).toBe(4);
   })
 
+  test ('자동자는 0~9까지의 무작위 값을 구한 뒤 4이상일 경우 전진한다', () => {
+    // given
+    const car = new Car('huru');
+
+    // when
+    car.move();
+
+    // then
+    if (car.move() >= 4) {
+      expect(car.position).toBe(1);
+    }
+  })
 })
--- src/Car.js
+++ src/Car.js
@@ -1,15 +1,26 @@
 class Car {
 
   name;
+  position = 0;
 
   constructor(name) {
     this.name = name;
+  }
+
+  move() {
+    const randomValue = Math.floor(Math.random() * 10);
+    if (randomValue >= 4) {
+      this.position += 1
+    }
   }
 
   get name() {
     return this.name;
   }
 
+  get position() {
+    return this.position;
+  }
 }
 
 export default Car;
