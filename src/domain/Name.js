a36d3be0969b04bbf03676c288d6890e832d63df
Vespucci95 <donglonk@naver.com>
2024-04-26 02:11:28 +0900

    refactor: TypeError 를 발생하도록 변경

--- src/domain/Name.js
+++ src/domain/Name.js
@@ -2,7 +2,7 @@ class Name {
   #name;
   constructor(name, maxLen = 5) {
     if (name.length > maxLen) {
-      throw new Error('최대 길이를 초과하였습니다.');
+      throw new TypeError('최대 길이를 초과하였습니다.');
     }
     this.#name = name;
   }
