d106bf46ce54091f07695e806d05d7b54d258ca2
Vespucci95 <donglonk@naver.com>
2024-04-26 02:12:53 +0900

    refactor: 이름 최대길이 상수로 분리

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+const MAX_NAME_LENGTH = 5;
--- src/domain/Name.js
+++ src/domain/Name.js
@@ -1,6 +1,6 @@
 class Name {
   #name;
-  constructor(name, maxLen = 5) {
+  constructor(name, maxLen = MAX_NAME_LENGTH) {
     if (name.length > maxLen) {
       throw new TypeError('최대 길이를 초과하였습니다.');
     }
