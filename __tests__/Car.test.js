3232f82f577488816184a0ea7003f7ebf80c1447
Vespucci95 <donglonk@naver.com>
2024-04-26 02:37:36 +0900

    test: 이름 테스트코드 리팩터링

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -2,15 +2,27 @@ import Name from '../src/domain/Name';
 
 describe('이름 기능 테스트', () => {
   it('이름을 설정할 수 있다.', () => {
+    // given
     const name = new Name('pucci');
-    expect(name.getName()).toBe('pucci');
+
+    // when
+    const pucciName = name.getName();
+
+    // then
+    expect(pucciName).toBe('pucci');
   });
   it('이름 n자 이하로 길이를 제한할 수 있다. (default: 5)', () => {
+    // given
     const name1 = new Name('pucci');
     const name2 = new Name('hyunseo', 7);
 
-    expect(name1.getName()).toBe('pucci');
+    // when
+    const pucciName = name1.getName();
+    const hyunseoName = name2.getName();
+
+    // then
+    expect(pucciName.length).toBe(5);
     expect(() => new Name('hyunseo', 3)).toThrow();
-    expect(name2.getName()).toBe('hyunseo');
+    expect(hyunseoName.length).toBe(7);
   });
 });
