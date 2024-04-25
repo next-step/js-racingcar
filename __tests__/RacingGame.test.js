eaef60888012e246e6e83045b258fe6afa452496
민규 <xiahtic73@gmail.com>
2024-04-25 20:06:49 +0900

    test: 자동차 경주 게임을 완료한 후 우승자를 선별한다.
    - 기능 요구 사항 수정

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -1,9 +1,9 @@
 import RacingGame from '../src/RacingGame';
+import Car from '../src/Car';
 
 /* 
 경주 컨트롤러
 - 자동차 경주는 5회로 고정하여 진행한다.
-- 자동차는 쉼표를 기준으로 구분한다.
 - 자동차 경주 게임을 완료한 후 우승자를 선별한다. (우승자는 한 명 이상일 수 있다.)
 */
 
@@ -17,5 +17,23 @@ describe('경주 게임 테스트', () => {
 
     // then
     expect(racingGame.round).toBe(5);
+  });
+
+  test('자동차 경주 게임을 완료한 후 우승자를 선별한다.', () => {
+    // given
+    const cars = [new Car('pobi'), new Car('crong'), new Car('honux')];
+    const racingGame = new RacingGame(cars);
+
+    Car.getRandomNumber = jest
+      .fn()
+      .mockReturnValueOnce(1)
+      .mockReturnValueOnce(1)
+      .mockReturnValue(9);
+
+    // when
+    racingGame.start();
+
+    // then
+    expect(racingGame.winners).toEqual(['honux']);
   });
 });
