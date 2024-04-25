474314cad7304f68e25e421ac03e13956aafbd96
Vespucci95 <donglonk@naver.com>
2024-04-26 02:50:57 +0900

    style: 코드 정렬

--- src/domain/Name.js
+++ src/domain/Name.js
@@ -2,12 +2,14 @@ import { MAX_NAME_LENGTH } from '../constants';
 
 class Name {
   #name;
+
   constructor(name, maxLen = MAX_NAME_LENGTH) {
     if (name.length > maxLen) {
       throw new TypeError('최대 길이를 초과하였습니다.');
     }
     this.#name = name;
   }
+
   getName() {
     return this.#name;
   }
