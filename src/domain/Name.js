fc5db31ce281834e97dae0edf9cfbbcaba014531
Vespucci95 <donglonk@naver.com>
2024-04-26 02:07:36 +0900

    feat: 최대 길이가 넘어가면 에러를 낸다.

--- src/domain/Name.js
+++ src/domain/Name.js
@@ -1,6 +1,9 @@
 class Name {
   #name;
-  constructor(name) {
+  constructor(name, maxLen = 5) {
+    if (name.length > maxLen) {
+      throw new Error('최대 길이를 초과하였습니다.');
+    }
     this.#name = name;
   }
   getName() {
