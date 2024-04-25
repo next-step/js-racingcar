b0f3c64be4ee2ab10a2006d8acf3dac2e41c2109
민규 <xiahtic73@gmail.com>
2024-04-25 17:44:00 +0900

    feat: 자동차는 전진할 수 있다.

--- src/Car.js
+++ src/Car.js
@@ -1,12 +1,22 @@
 class Car {
   #name;
 
+  #position = 0;
+
   constructor(name) {
     this.#name = name;
   }
 
   get name() {
     return this.#name;
+  }
+
+  get position() {
+    return this.#position;
+  }
+
+  move() {
+    this.#position += 1;
   }
 }
 
