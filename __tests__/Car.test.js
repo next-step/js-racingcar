a24f7bb66c21050991c8562a4992c798836de5a8
Vespucci95 <donglonk@naver.com>
2024-04-26 03:13:00 +0900

    test: 자동차 이동 테스트 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -11,4 +11,22 @@ describe('자동차 기능 테스트', () => {
     // then
     expect(name).toBe('taxi');
   });
+  it('자동차는 이동(전진, 후진, 정지)할 수 있다.', () => {
+    // given
+    const [taxi, tesla, ford] = [
+      new Car('taxi'),
+      new Car('tesla'),
+      new Car('ford'),
+    ];
+
+    // when
+    const taxiPosition = taxi.forward().getPosition();
+    const teslaPosition = tesla.stop().getPosition();
+    const fordPosition = ford.backward().getPosition();
+
+    // then
+    expect(taxiPosition).toBe(1);
+    expect(teslaPosition).toBe(0);
+    expect(fordPosition).toBe(-1);
+  });
 });
