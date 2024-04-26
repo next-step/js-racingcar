ac88141d0dceb7f0078d9acc8c97890913d92ced
Vespucci95 <donglonk@naver.com>
2024-04-26 14:15:12 +0900

    refactor: 상수명 변경

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const MAX_NAME_LENGTH = 5;
+export const DEFAULT_MAX_NAME_LENGTH = 5;
 export const DEFAULT_LAP_COUNT = 5;
--- src/domain/Name.js
+++ src/domain/Name.js
@@ -1,9 +1,9 @@
-import { MAX_NAME_LENGTH } from '../constants';
+import { DEFAULT_MAX_NAME_LENGTH } from '../constants';
 
 class Name {
   #name;
 
-  constructor(name, maxLen = MAX_NAME_LENGTH) {
+  constructor(name, maxLen = DEFAULT_MAX_NAME_LENGTH) {
     if (name.length > maxLen) {
       throw new TypeError('최대 길이를 초과하였습니다.');
     }
