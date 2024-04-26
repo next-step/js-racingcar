96647d988d77fb670e34696cfdd3172871e726f8
oxlzlo <yeojin58@naver.com>
2024-04-27 01:53:48 +0900

    refactor: Race 클래스 내 필드 삭제 및 getter, setter 사용

--- src/Race.js
+++ src/Race.js
@@ -1,15 +1,27 @@
 class Race {
 
-  playNumber = 0;
+  constructor(competitors) {
+    this._competitors = competitors;
+    this._playNumber = 0;
+  }
 
   start() {
     for (let i=0; i<5; i++) {
-      this.playNumber += 1;
+      this._playNumber += 1;
     }
   }
 
   get playNumber() {
-    return this.playNumber;
+    return this._playNumber;
+  }
+
+  get winner() {
+    const highestPosition = this.getHighest();
+    return this._competitors.find(competitor => competitor.position === highestPosition);
+  }
+
+  getHighest() {
+    return Math.max(...this._competitors.map(competitor => competitor.position));
   }
 }
 
