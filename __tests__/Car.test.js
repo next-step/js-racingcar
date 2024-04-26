d6a0460bc98aa6e37732fbfa6ed0ba035f58a836
jambottle <jaewon.walks@gmail.com>
2024-04-27 02:20:25 +0900

    test: 자동차는 0~9 사이의 무작위 값이 4 이상인 경우에만 전진할 수 있다.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,7 +1,7 @@
 import Car from '../src/domain/Car';
 
-describe('Car', () => {
-  test('자동차 이름은 5자 이하로만 부여할 수 있다.', () => {
+describe('자동차 이름은', () => {
+  test('5자 이하로만 부여할 수 있다.', () => {
     // given: 5자 이하의 이름 준비
     const RANDOM_NAME = 'jam';
 
@@ -13,7 +13,7 @@ describe('Car', () => {
     expect(name.length).toBeLessThanOrEqual(5);
   });
 
-  test('자동차 이름은 5자를 초과하여 부여할 수 없다.', () => {
+  test('5자를 초과하여 부여할 수 없다.', () => {
     // given: 5자를 초과하는 이름 준비
     const RANDOM_NAME = 'bottle';
 
@@ -22,5 +22,41 @@ describe('Car', () => {
 
     // then: 자동차가 정상적으로 생성되었는지 확인
     expect(createCar).toThrow('자동차 이름은 5자 이하로만 부여할 수 있습니다.');
+  });
+});
+
+describe('자동차 위치는', () => {
+  let car;
+
+  beforeEach(() => {
+    const RANDOM_NAME = 'jam';
+    car = new Car(RANDOM_NAME);
+  });
+
+  test('자동차가 전진을 시도하기 전인 경우, 0에서 시작한다.', () => {
+    // then: 자동차의 최초 위치가 0인지 확인
+    expect(car.position).toBe(0);
+  });
+
+  test('0~9 사이의 무작위 값이 4 이상인 경우, 전진할 수 있다.', () => {
+    // given: 무작위 값이 4 이상인 경우를 가정
+    const RANDOM_VALUE = 4;
+
+    // when: 전진 시도
+    car.move(RANDOM_VALUE);
+
+    // then: 자동차가 전진에 성공했는지 확인
+    expect(car.position).toBe(1);
+  });
+
+  test('0~9 사이의 무작위 값이 4 미만인 경우, 전진할 수 없다.', () => {
+    // given: 무작위 값이 4 미만인 경우를 가정
+    const RANDOM_VALUE = 2;
+
+    // when: 전진 시도
+    car.move(RANDOM_VALUE);
+
+    // then: 자동차가 전진에 성공했는지 확인
+    expect(car.position).toBe(0);
   });
 });
