12e3581999dfd009b25a798d75c85333577077c3
이새미 <saemi.lee@day1company.co.kr>
2024-04-27 02:00:50 +0900

    feat: 자동차구현/자동차 이름은 5자 이하만 가능하다.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -5,4 +5,11 @@ describe("자동차 구현 테스트", () => {
     const car = new Car("pobi");
     expect(car.name).toBe("pobi");
   });
+
+  test("자동차 이름이 5자 초과인 경우 에러를 발생시킨다.", () => {
+    const carNameOver5 = "saemileee";
+    expect(() => new Car(carNameOver5)).toThrow(
+      "자동차 이름은 5자 이하만 가능합니다."
+    );
+  });
 });
--- src/domain/Car.js
+++ src/domain/Car.js
@@ -2,6 +2,9 @@ export default class Car {
   name;
 
   constructor(name) {
+    if (name.length > 5) {
+      throw new Error("자동차 이름은 5자 이하만 가능합니다.");
+    }
     this.name = name;
   }
 }
