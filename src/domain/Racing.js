575f01233f6964fc89d6a9b85bfb2de03eda871a
Vespucci95 <donglonk@naver.com>
2024-04-26 14:13:35 +0900

    feat: 경주 횟수 구현

--- src/domain/Racing.js
+++ src/domain/Racing.js
@@ -1,15 +1,21 @@
 class Racing {
   #players;
+  #lapCount;
 
-  constructor(players) {
+  constructor(players, lapCount = 5) {
     if (!Array.isArray(players)) {
       throw new TypeError('잘못된 형식입니다.');
     }
     this.#players = players;
+    this.#lapCount = lapCount;
   }
 
   getPlayerCount() {
     return this.#players.length;
+  }
+
+  getLapCount() {
+    return this.#lapCount;
   }
 }
 
