04e397abfb4aa45529eada7d1b65f04fa5f44447
oxlzlo <yeojin58@naver.com>
2024-04-27 01:54:35 +0900

    feat: 자동차 경주 게임을 완료한 후 누가 우승했는지 알려준다

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -11,5 +11,21 @@ describe('자동차 경주 테스트', () => {
 
     // then
     expect(race.playNumber).toBe(5);
-  })
+  });
+
+  test ('자동차 경주 게임을 완료한 후 누가 우승했는지를 알려준다.', () => {
+    // given
+    const competitors = [
+      { name: 'seol', position: 1 },
+      { name: 'tang', position: 2 },
+      { name: 'huru', position: 3 }
+    ];
+    const race = new Race(competitors);
+
+    // when
+    race.start();
+
+    // then
+    expect(race.winner.name).toEqual('huru');
+  });
 })
