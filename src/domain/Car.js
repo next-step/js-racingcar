88409a793131f2afad613d26bcfe083166388fdd
seungdeok <akk9132@naver.com>
2024-04-26 20:30:29 +0900

    refactor: private 적용

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -4,6 +4,9 @@ export class Car {
   static MOVE_FORWARD_CAR = 4;
   static CAR_NAME_MAX_LEN = 5;
 
+  #name = "";
+  #position = 0;
+
   constructor(name) {
     if (!name.length) {
       throw new Error(ERROR_EMPTY_CAR_NAME);
@@ -11,21 +14,21 @@ export class Car {
     if (name.length > Car.CAR_NAME_MAX_LEN) {
       throw new Error(ERROR_LONG_CAR_NAME);
     }
-    this.name = name;
-    this.position = 0;
+    this.#name = name;
+    this.#position = 0;
   }
 
   move(condition) {
     if (condition >= Car.MOVE_FORWARD_CAR) {
-      this.position += 1;
+      this.#position += 1;
     }
   }
 
   getPosition() {
-    return this.position;
+    return this.#position;
   }
 
   getName() {
-    return this.name;
+    return this.#name;
   }
 }
--- src/index.js
+++ src/index.js
@@ -11,25 +11,25 @@ export class App {
   static MOVE_MAX = 9;
   static RACE_ROUND = 5;
 
-  constructor() {
-    this.cars = [];
-    this.winners = [];
-  }
+  #cars = [];
+  #winners = [];
 
-  race() {
+  #race() {
     console.log("\n실행 결과");
     for (let round = 1; round <= App.RACE_ROUND; round++) {
-      this.cars.forEach((car) => {
+      this.#cars.forEach((car) => {
         car.move(getRandom(App.MOVE_MIN, App.MOVE_MAX));
         console.log(`${car.getName()} : ${"-".repeat(car.getPosition())}`);
       });
       console.log("");
     }
 
-    const maxPosition = Math.max(...this.cars.map((car) => car.getPosition()));
-    this.winners = this.cars.filter((car) => car.getPosition() === maxPosition);
+    const maxPosition = Math.max(...this.#cars.map((car) => car.getPosition()));
+    this.#winners = this.#cars.filter(
+      (car) => car.getPosition() === maxPosition
+    );
     console.log(
-      `${this.winners
+      `${this.#winners
         .map((car) => car.getName())
         .join(", ")}가 최종 우승했습니다.`
     );
@@ -54,8 +54,8 @@ export class App {
         throw new Error(ERROR_DUPLICATE_CAR_NAME);
       }
 
-      this.cars = carNames.map((name) => new Car(name));
-      this.race();
+      this.#cars = carNames.map((name) => new Car(name));
+      this.#race();
     } catch (error) {
       console.error(error.message);
       throw error;
@@ -63,11 +63,11 @@ export class App {
   }
 
   getCars() {
-    return this.cars;
+    return this.#cars;
   }
 
   getWinners() {
-    return this.winners;
+    return this.#winners;
   }
 }
 
