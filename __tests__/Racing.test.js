e8b69d928b0519b73a9b4c54e911bf65a2d7b86c
Vespucci95 <donglonk@naver.com>
2024-04-26 14:01:58 +0900

    test: 자동차 경주 참가자 수 테스트

---
+++ __tests__/Racing.test.js
@@ -0,0 +1,12 @@
+describe('자동차 경주 기능 테스트', () => {
+  it('n명의 참가자(자동차)를 받을 수 있다.', () => {
+    // given
+    const players = new Racing(['taxi', 'tesla', 'ford']);
+
+    // when
+    const playerCount = players.getPlayers();
+
+    // then
+    expect(playerCount).toBe(3);
+  });
+});
