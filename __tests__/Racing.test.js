9e622e74b24a26261244aeeafcb26e501ff22b1a
Vespucci95 <donglonk@naver.com>
2024-04-26 14:29:26 +0900

    test: 경주 참가자 테스트 추가

--- __tests__/Racing.test.js
+++ __tests__/Racing.test.js
@@ -1,4 +1,5 @@
 import Racing from '../src/domain/Racing';
+import Car from '../src/domain/Car';
 
 describe('자동차 경주 기능 테스트', () => {
   it('n명의 참가자(자동차)를 받을 수 있다.', () => {
@@ -7,9 +8,11 @@ describe('자동차 경주 기능 테스트', () => {
 
     // when
     const playerCount = players.getPlayerCount();
+    const [firstPlayer] = players.getPlayers();
 
     // then
     expect(playerCount).toBe(3);
+    expect(firstPlayer).toBeInstanceOf(Car);
   });
   it('자동차 경주는 n회로 고정하여 진행한다. (default: 5)', () => {
     // given
