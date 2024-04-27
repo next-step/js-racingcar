7dc93dc7f92dc5f63a856169cef50537fe794493
HoonDongKang <d159123@naver.com>
2024-04-27 16:46:54 +0900

    feat: 0에서 9 사이에서 무작위 값을 구한 후 값이 4 이상일 경우, 전진한다

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -19,4 +19,16 @@ describe("자동차 기능 테스트", () => {
     // then
     expect(name.length).toBeLessThanOrEqual(MAX_CAR_NAME_LENGTH);
   });
+
+  test("전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후, 무작위 값이 4 이상일 경우이다.", () => {
+    // given
+    const car = new Car("test");
+    
+    // when
+    const randomValue = car.getRandomValue();
+    car.moveForward(randomValue);
+
+    // then
+    randomValue >= 4 ? expect(car.position).toBe(1) : expect(car.position).toBe(0);
+  })
 });
--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,5 +1,6 @@
 class Car { 
   name;
+  position = 0;
   
   constructor(name){
     this.name = name;
@@ -7,8 +8,19 @@ class Car {
 
   get name(){
     return this.name;
-  }
+  };
 
+  get position(){
+    return this.position;
+  };
+
+  getRandomValue(){
+    return Math.floor(Math.random() * 10);
+  };
+
+  moveForward(randomValue){
+    if(randomValue >= 4) this.position++;
+  };
 };
 
 export default Car;
