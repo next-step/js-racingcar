fa82dfadcdfb02d712d624483d1983d9cb073d9f
ChanwooPark <chanw9503@gmail.com>
2024-04-27 17:24:32 +0900

    Feat. 자동차 경주 우승에 대한 결과값 기능 구현

--- __tests__/Rase.test.js
+++ __tests__/Rase.test.js
@@ -1,16 +1,24 @@
 import { Car } from '../src/domain/Car';
+import { Race } from '../src/domain/Race';
 
 describe('레이스 관련 내용 ', () => {
-  test('자동차 경우 게임이 완료한 후 누가 우승했는지 알려준다.', () => {
-    const audi = Car();
-    const bmw = Car();
-    const kia = Car();
+  test('자동차 경주 게임이 완료한 후 누가 우승했는지 알려준다.', () => {
+    //Given
+    const audi = new Car();
+    const bmw = new Car();
+    const kia = new Car();
+    audi.setName('audi');
+    bmw.setName('bmw');
+    kia.setName('kia');
 
     const racingCars = [audi, bmw, kia];
-    const rase = new Race(racingCars);
+    const race = new Race(racingCars);
 
-    rase.racingStart();
+    //When
+    race.racingStart();
 
-    expect(rase.winner()).toHaveReturned();
+    //then
+
+    expect(race.getWinner()).toBeDefined();
   });
 });
