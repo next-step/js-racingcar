b708686569dc9b3869c3fba874537a7d7a6e8777
Vespucci95 <donglonk@naver.com>
2024-04-27 22:39:11 +0900

    test: 자동차 이름 구분 테스트 추가

--- __tests__/RacingIO.test.js
+++ __tests__/RacingIO.test.js
@@ -1,7 +1,15 @@
+import RacingIO from '../src/domain/RacingIO';
+
 describe('레이싱 입출력 기능 테스트', () => {
-  it('hello', () => {
+  it('자동차 이름은 쉼표(,)를 기준으로 구분한다.', async () => {
     // given
+    const racingIO = new RacingIO();
+    const input = 'taxi, tesla,ford ';
+
     // when
+    const carName = racingIO.racingCarInput(input);
+
     // then
+    expect(carName).toEqual(['taxi', 'tesla', 'ford']);
   });
 });
