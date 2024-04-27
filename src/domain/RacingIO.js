8ddfffffc1085e0a31cf6aab4a09e58de7b910f3
Vespucci95 <donglonk@naver.com>
2024-04-27 23:02:04 +0900

    feat: 자동차 위치표시 구현

--- src/domain/RacingIO.js
+++ src/domain/RacingIO.js
@@ -2,6 +2,9 @@ class RacingIO {
   racingCarInput(input) {
     return input.replace(/\s/g, '').split(',');
   }
+  positionToString(position) {
+    return '-'.repeat(position);
+  }
 }
 
 export default RacingIO;
