67118eb3bd9bfe2fb90b7356a33cf0db479d0056
monat96 <42468323+monat96@users.noreply.github.com>
2024-04-27 21:46:52 +0900

    refactor: car 클래스 private 변수로 변경

--- src/car/car.model.js
+++ src/car/car.model.js
@@ -9,20 +9,31 @@ const BOUNDARY_RIGHT = 9;
 const CAR_DISPLAY = "-";
 
 export class Car {
+  #name;
+  #position;
+
   constructor({ name }) {
     validateCarName(name);
 
-    this.name = name;
-    this.position = 0;
+    this.#name = name;
+    this.#position = 0;
   }
   move() {
     const randomNumber = boundaryRandomNumber(BOUNDARY_LEFT, BOUNDARY_RIGHT);
     if (randomNumber >= MOVING_CONDITION) {
-      this.position += 1;
+      this.#position += 1;
     }
   }
 
   display() {
-    console.log(`${this.name}: ${CAR_DISPLAY.repeat(this.position)}`);
+    console.log(`${this.#name}: ${CAR_DISPLAY.repeat(this.#position)}`);
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get position() {
+    return this.#position;
   }
 }
