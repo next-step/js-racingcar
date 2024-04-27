33b1fbb71594e6d15024467a95a4f2dcbca01056
민규 <xiahtic73@gmail.com>
2024-04-27 20:01:25 +0900

    test: 전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.

--- __tests__/View.test.js
+++ __tests__/View.test.js
@@ -15,14 +15,38 @@ jest.mock('../src/utils/readLineAsync', () => {
 });
 
 describe('View 테스트', () => {
-  test('자동차는 쉼표를 기준으로 구분하여 입력받는다.', async () => {
-    // given
-    const view = new View();
+  describe('정상 케이스 테스트', () => {
+    test('자동차는 쉼표를 기준으로 구분하여 입력받는다.', async () => {
+      // given
+      const view = new View();
 
-    // when
-    const cars = await view.readCars();
+      // when
+      const cars = await view.readCars();
 
-    // then
-    expect(cars).toEqual(['pobi', 'crong', 'honux']);
+      // then
+      expect(cars).toEqual(['pobi', 'crong', 'honux']);
+    });
+
+    test('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
+      // given
+      const view = new View();
+      const logSpy = jest.spyOn(console, 'log');
+      const input = [
+        { name: 'pobi', position: 2 },
+        { name: 'crong', position: 4 },
+        { name: 'honux', position: 0 },
+      ];
+      const expected = 'pobi : --\ncrong : ----\nhonux : \n';
+
+      // when
+      view.printGameState(input);
+
+      // then
+      expect(logSpy).toHaveBeenCalledWith(expected);
+    });
+  });
+
+  describe('예외 케이스 테스트', () => {
+    // ...
   });
 });
