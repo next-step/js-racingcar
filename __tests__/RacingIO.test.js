4e4c452a9a9b79def23559f9c7f766407c43e0e7
Vespucci95 <donglonk@naver.com>
2024-04-27 23:35:16 +0900

    refactor: 클래스 제거

--- __tests__/RacingIO.test.js
+++ __tests__/RacingIO.test.js
@@ -1,37 +1,38 @@
-import RacingIO from '../src/domain/RacingIO';
+import {
+  readWinners,
+  readCarPosition,
+  writeRacingCar,
+} from '../src/domain/RacingIO';
 
 describe('레이싱 입출력 기능 테스트', () => {
   it('자동차 이름은 쉼표(,)를 기준으로 구분한다.', async () => {
     // given
-    const racingIO = new RacingIO();
     const input = 'taxi, tesla,ford ';
 
     // when
-    const carName = racingIO.writeRacingCar(input);
+    const carName = writeRacingCar(input);
 
     // then
     expect(carName).toEqual(['taxi', 'tesla', 'ford']);
   });
   it('자동차의 위치는 - 로 표시한다.', () => {
     // given
-    const racingIO = new RacingIO();
     const input = 3;
 
     // when
-    const output = racingIO.readCarPosition(input);
+    const output = readCarPosition(input);
 
     // then
     expect(output).toBe('---');
   });
   it('우승자가 여러 명일 경우 쉼표(,)를 이용하여 구분한다.', () => {
     // given
-    const racingIO = new RacingIO();
     const winners = ['taxi', 'ford'];
     const [winner] = winners;
 
     // when
-    const winnersOutput = racingIO.readWinners(winners);
-    const winnerOutput = racingIO.readWinners(winner);
+    const winnersOutput = readWinners(winners);
+    const winnerOutput = readWinners(winner);
 
     // then
     expect(winnersOutput).toBe('taxi, ford');
--- src/domain/RacingIO.js
+++ src/domain/RacingIO.js
@@ -1,13 +1,11 @@
-class RacingIO {
-  writeRacingCar(input) {
-    return input.replace(/\s/g, '').split(',');
-  }
-  readCarPosition(position) {
-    return '-'.repeat(position);
-  }
-  readWinners(winners) {
-    return winners.toString().replace(/,/g, ', ');
-  }
+export function writeRacingCar(input) {
+  return input.replace(/\s/g, '').split(',');
 }
 
-export default RacingIO;
+export function readCarPosition(position) {
+  return '-'.repeat(position);
+}
+
+export function readWinners(winners) {
+  return winners.toString().replace(/,/g, ', ');
+}
