b405a74026812a7a364d2cdec9cd16a659634cf3
HoonDongKang <d159123@naver.com>
2024-04-27 16:18:16 +0900

    feat: 자동차는 5자 이하의 이름을 갖는다.

---
+++ __tests__/Car.test.js
@@ -0,0 +1,22 @@
+import Car from "../src/domain/Car";
+
+const MAX_CAR_NAME_LENGTH = 5;
+/**
+ * 
+## 자동차
+- [x] 자동차는 5자 이하의 이름을 갖는다.
+- [ ] 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우, 전진한다
+ */
+
+describe("자동차 기능 테스트", () => {
+  test("자동차는 5자 이하의 이름을 갖는다.", () => {
+    // given
+    const car = new Car("test");
+
+    // when
+    const name = car.name;
+
+    // then
+    expect(name.length).toBeLessThanOrEqual(MAX_CAR_NAME_LENGTH);
+  });
+});
---
+++ src/domain/Car.js
@@ -0,0 +1,14 @@
+class Car { 
+  name;
+  
+  constructor(name){
+    this.name = name;
+  };
+
+  get name(){
+    return this.name;
+  }
+
+};
+
+export default Car;
