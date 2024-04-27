48e2b2a08b6442352c8c45664e105d736978d229
민규 <xiahtic73@gmail.com>
2024-04-27 20:26:12 +0900

    test: 우승자는 쉼표를 기준으로 구분하여 출력된다.

--- __tests__/View.test.js
+++ __tests__/View.test.js
@@ -15,11 +15,15 @@ jest.mock('../src/utils/readLineAsync', () => {
 });
 
 describe('View 테스트', () => {
+  // given
+  let view;
+
+  beforeEach(() => {
+    view = new View();
+  });
+
   describe('정상 케이스 테스트', () => {
     test('자동차는 쉼표를 기준으로 구분하여 입력받는다.', async () => {
-      // given
-      const view = new View();
-
       // when
       const cars = await view.readCars();
 
@@ -29,7 +33,6 @@ describe('View 테스트', () => {
 
     test('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
       // given
-      const view = new View();
       const logSpy = jest.spyOn(console, 'log');
       const input = [
         { name: 'pobi', position: 2 },
@@ -40,6 +43,19 @@ describe('View 테스트', () => {
 
       // when
       view.printRoundState(input);
+
+      // then
+      expect(logSpy).toHaveBeenCalledWith(expected);
+    });
+
+    test('우승자는 쉼표를 기준으로 구분하여 출력된다.', () => {
+      // given
+      const logSpy = jest.spyOn(console, 'log');
+      const input = ['pobi', 'honux'];
+      const expected = 'pobi, honux가 최종 우승했습니다.';
+
+      // when
+      view.printWinner(input);
 
       // then
       expect(logSpy).toHaveBeenCalledWith(expected);
