ab4627e1b3c788c679260c2c1486af00e2c17b1f
ChanwooPark <chanw9503@gmail.com>
2024-04-27 15:30:52 +0900

    Test. 자동차에 대한 모델을 정의하고 테스트 케이스 통과하기

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,15 +1,14 @@
+import { Car } from '../src/domain/Car';
 
+describe('자동차 관련 내용', () => {
+  test('자동차에 이름을 부여할 수 있다.', () => {
+    //Given(준비)
+    const car = new Car();
 
-describe('자동차 관련 내용', ()=>{
+    //When(실행)
+    car.setName('audi');
 
-    test('자동차에 이름을 부여할 수 있다.' ()=> {
-        //Given(준비)
-        const Car = new Car(); 
-
-        //When(실행)
-        Car.setName('audi');
-
-        //Then(검증)
-        expect(Car.getName()).toBe('audi');
-    })
-})
+    //Then(검증)
+    expect(car.getName()).toBe('audi');
+  });
+});
---
+++ src/domain/Car.js
@@ -0,0 +1,14 @@
+export class Car {
+  name;
+  constructor() {
+    this.name = 0;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
