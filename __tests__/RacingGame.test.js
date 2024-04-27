c11b963fa510e727e639e594b4dde86a91086e72
민규 <xiahtic73@gmail.com>
2024-04-28 00:31:09 +0900

    test: 입력받은 문자열에 대한 예외를 처리한다.

--- __tests__/RacingGame.test.js
+++ __tests__/RacingGame.test.js
@@ -1,5 +1,7 @@
-import RacingGame from '../src/Domain/RacingGame';
+import CONSTANTS from '../src/constants/Constants';
+import ERROR from '../src/constants/Error';
 import Car from '../src/Domain/Car';
+import RacingGame from '../src/Domain/RacingGame';
 
 /* 
 경주 컨트롤러
@@ -8,33 +10,71 @@ import Car from '../src/Domain/Car';
 */
 
 describe('경주 게임 테스트', () => {
-  test('자동차 경주는 5회 진행한다.', () => {
-    // given
-    const cars = [new Car('pobi'), new Car('crong'), new Car('honux')];
-    const racingGame = new RacingGame(cars);
+  let cars;
+  let racingGame;
 
-    // when
-    racingGame.start();
+  describe('정상 케이스 테스트', () => {
+    beforeEach(() => {
+      cars = [new Car('pobi'), new Car('crong'), new Car('honux')];
+      racingGame = new RacingGame(cars);
+    });
 
-    // then
-    expect(racingGame.round).toBe(5);
+    test('자동차 경주는 5회 진행한다.', () => {
+      // when
+      racingGame.start();
+
+      // then
+      expect(racingGame.round).toBe(CONSTANTS.racingGame.round);
+    });
+
+    test('자동차 경주 게임을 완료한 후 우승자를 선별한다.', () => {
+      // given
+      Car.getRandomNumber = jest
+        .fn()
+        .mockReturnValueOnce(1)
+        .mockReturnValueOnce(1)
+        .mockReturnValue(9);
+
+      // when
+      racingGame.start();
+
+      // then
+      expect(racingGame.winners).toEqual([racingGame.cars[2]]);
+    });
   });
 
-  test('자동차 경주 게임을 완료한 후 우승자를 선별한다.', () => {
-    // given
-    const cars = [new Car('pobi'), new Car('crong'), new Car('honux')];
-    const racingGame = new RacingGame(cars);
+  describe('예외 케이스 테스트', () => {
+    test.each([
+      [new Car('pobi'), new Car('cr ng')],
+      [new Car(' pobi'), new Car('crong')],
+      [new Car(' '), new Car('crong')],
+    ])('자동차 이름에 공백이 포함된 경우 예외 처리한다.', input => {
+      // when + then
+      expect(() => new RacingGame(input)).toThrow(ERROR.invalidFormat);
+    });
 
-    Car.getRandomNumber = jest
-      .fn()
-      .mockReturnValueOnce(1)
-      .mockReturnValueOnce(1)
-      .mockReturnValue(9);
+    test('빈 자동차 이름이 있는 경우 예외 처리한다.', () => {
+      // given
+      cars = [new Car(''), new Car('crong')];
 
-    // when
-    racingGame.start();
+      // when + then
+      expect(() => new RacingGame(cars)).toThrow(ERROR.invalidFormat);
+    });
 
-    // then
-    expect(racingGame.winners).toEqual([racingGame.cars[2]]);
+    test('중복된 자동차가 존재하는 경우 예외 처리한다.', () => {
+      // given
+      cars = [new Car('pobi'), new Car('pobi')];
+
+      // when + then
+      expect(() => new RacingGame(cars)).toThrow(ERROR.duplicateValue);
+    });
+
+    test('자동차가 한 개인 경우 예외 처리한다.', () => {
+      // given
+      cars = [new Car('pobi')];
+
+      // when + then
+      expect(() => new RacingGame(cars)).toThrow(ERROR.invalidSingleValue);
+    });
   });
 });
