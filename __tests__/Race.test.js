d5828e6dd4f8f43e779390d6f0baf2d4308f0234
jambottle <jaewon.walks@gmail.com>
2024-04-30 00:20:14 +0900

    test: 자동차 경주의 우승자는 한 명 이상일 수 있다.

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -2,14 +2,14 @@ import Car from '../src/domain/Car';
 import Race from '../src/domain/Race';
 
 describe('자동차 경주는', () => {
-  test('5라운드로 진행한다.', () => {
-    // given: 임의의 자동차들이 경주에 참가한 경우를 가정
-    const race = new Race([
-      new Car('pobi'),
-      new Car('crong'),
-      new Car('honux'),
-    ]);
+  let race;
 
+  beforeEach(() => {
+    // given: 임의의 자동차들이 경주에 참가한 경우를 가정
+    race = new Race([new Car('pobi'), new Car('crong'), new Car('honux')]);
+  });
+
+  test('5라운드로 진행한다.', () => {
     // when: 경주 시작
     jest.spyOn(race, 'initRound');
     race.initRace();
@@ -17,5 +17,13 @@ describe('자동차 경주는', () => {
     // then: 자동차 경주가 5라운드로 진행되는지 확인
     expect(Race.rounds).toBe(5);
     expect(race.initRound).toHaveBeenCalledTimes(Race.rounds);
+  });
+
+  test('한 명 이상의 우승자가 발생할 수 있다.', () => {
+    // when: 경주 시작
+    race.initRace();
+
+    // then: 우승자가 한 명 이상인지 확인
+    expect(race.winners.length).toBeGreaterThanOrEqual(1);
   });
 });
