aa41bc189bcdcdd4f18ae931e86e1082ba211fab
min1378 <qwes123@naver.com>
2024-04-29 20:46:59 +0900

    feat: Car 객체의 메서드를 구현한다.

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -5,7 +5,31 @@ export class Car {
     _position;
 
     constructor(name) {
+        this.validateName(name);
         this._name = name;
         this._position = Car.DEFAULT_CAR_POISITION;
+    }
+
+    validateName(name) {
+        if(!name.trim().length) {
+            throw new Error('Car name should not be empty');
+        }
+
+
+        if (name.length > Car.CAR_NAME_MAX_LENGTH) {
+            throw new Error(`Car name length should be less than ${Car.CAR_NAME_MAX_LENGTH}`);
+        }
+    }
+
+    move() {
+        this._position += 1;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    get name() {
+        return this._name;
     }
 }
