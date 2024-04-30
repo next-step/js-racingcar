6f038790ba4c8d0f65bf8e4449017f84bb7c16b9
supreme-d <dmhuh.data@gmail.com>
2024-05-01 00:49:59 +0900

    feat: add parseOutput function

--- src/utils.js
+++ src/utils.js
@@ -16,4 +16,12 @@ const parseInput = (input) => {
   return input.split(",");
 };
 
-module.exports = { getRandomNumber, parseInput };
+const parseOutput = (input) => {
+  if (!Array.isArray(input)) {
+    throw new Error(MESSAGE.INPUT_NOT_ARRAY_ERROR);
+  }
+
+  return input.join(",");
+};
+
+module.exports = { getRandomNumber, parseInput, parseOutput };
