8ab2857beb59be33c58d4322cffd7e34dce96ae9
DongHyeonWon <dhe7700@naver.com>
2024-05-01 01:06:04 +0900

    test: 조건 상수화 처리

--- __tests__/CarRace.test.js
+++ __tests__/CarRace.test.js
@@ -12,7 +12,7 @@ describe("자동차 경주 테스트", () => {
     expect(winner.length).toEqual(3);
   });
 
-  test("랜덤 값은 0~9 사이의 값이다.", () => {
+  test(`랜덤 값은 ${CAR_RACE.MIN_RANDOM_NUMBER}~${CAR_RACE.MAX_RANDOM_NUMBER} 사이의 값이다.`, () => {
     const randomNumber = generateRandomNumber(
       CAR_RACE.MIN_RANDOM_NUMBER,
       CAR_RACE.MAX_RANDOM_NUMBER,
@@ -21,21 +21,21 @@ describe("자동차 경주 테스트", () => {
     expect(randomNumber).toBeGreaterThanOrEqual(CAR_RACE.MIN_RANDOM_NUMBER);
   });
 
-  test("랜덤 값이 4 이상일 때 전진한다.", () => {
-    const randomNumber = 4;
+  test(`랜덤 값이 ${CAR_RACE.MOVE_THRESHOLD} 이상일 때 전진한다.`, () => {
+    const randomNumber = CAR_RACE.MOVE_THRESHOLD;
     const carRace = new CarRace(["a"]);
 
-    carRace.moveCar(carRace.cars[0], randomNumber);
+    carRace.moveCar(carRace.cars.at(0), randomNumber);
 
-    expect(carRace.cars[0].position).toEqual(1);
+    expect(carRace.cars.at(0).position).toEqual(1);
   });
 
   test("랜덤 값이 4 미만일 때는 전진하지 않는다.", () => {
-    const randomNumber = 3;
+    const randomNumber = CAR_RACE.MOVE_THRESHOLD - 1;
     const carRace = new CarRace(["a"]);
 
-    carRace.moveCar(carRace.cars[0], randomNumber);
+    carRace.moveCar(carRace.cars.at(0), randomNumber);
 
-    expect(carRace.cars[0].position).toEqual(0);
+    expect(carRace.cars.at(0).position).toEqual(0);
   });
 });
