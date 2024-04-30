16435b04c72f2b1fffe0de4b4103ff0b98c520fd
supreme-d <dmhuh.data@gmail.com>
2024-05-01 00:02:39 +0900

    test: add tests for Car class move method

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,17 +1,45 @@
 const { Car } = require("../src/Car");
 
 describe("Car class", () => {
-  it("should have a name property", () => {
-    const name = "pobi";
+  describe("Name property", () => {
+    it("should have a name property", () => {
+      const name = "pobi";
 
-    const car = new Car(name);
+      const car = new Car(name);
 
-    expect(car).toHaveProperty("name");
+      expect(car).toHaveProperty("name");
+    });
+
+    it("should throw an error if the name is longer than 5 characters", () => {
+      const name = "abcdefrr";
+
+      expect(() => new Car(name)).toThrow("Name must be 5 characters or fewer");
+    });
   });
 
-  it("should throw an error if the name is longer than 5 characters", () => {
-    const name = "abcdefrr";
+  describe("move()", () => {
+    it("should increase position by 1 if the given value is greater than or equal to 4", () => {
+      const name = "pobi";
+      const value = 4;
+      const car = new Car(name);
 
-    expect(() => new Car(name)).toThrow("Name must be 5 characters or fewer");
+      expect(car.position).toBe(0);
+
+      car.move(value);
+
+      expect(car.position).toBe(1);
+    });
+
+    it("should remain in the same position when the given value is less than 4", () => {
+      const name = "pobi";
+      const value = 1;
+      const car = new Car(name);
+
+      expect(car.position).toBe(0);
+
+      car.move(value);
+
+      expect(car.position).toBe(0);
+    });
   });
 });
