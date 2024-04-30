d6ea8aefd404c751426a0a0183e6dd78f76a0885
supreme-d <dmhuh.data@gmail.com>
2024-05-01 00:03:18 +0900

    feat: add Car class move method

--- src/Car.js
+++ src/Car.js
@@ -1,12 +1,21 @@
 const { MESSAGE } = require("../constants/message");
 
+const MOVE_THRESHOLD = 4;
+
 class Car {
-  constructor(name) {
+  constructor(name, position = 0) {
     if (name.length > 5) {
       throw new Error(MESSAGE.NAME_LENGTH_ERROR);
     }
 
     this.name = name;
+    this.position = position;
+  }
+
+  move(value) {
+    if (value >= MOVE_THRESHOLD) {
+      this.position++;
+    }
   }
 }
 
