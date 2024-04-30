34d0b937dfb324afd1ebc010d5f70f6ee951d141
supreme-d <dmhuh.data@gmail.com>
2024-05-01 01:31:30 +0900

    fix: change argument for getWinners function

--- src/racing.js
+++ src/racing.js
@@ -1,15 +1,15 @@
-const getWinners = (...args) => {
+const getWinners = (cars) => {
   const result = [];
   let maxPosition = 0;
 
-  for (let i = 0; i < args.length; i++) {
-    if (args[i].position > maxPosition) {
-      maxPosition = args[i].position;
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].position > maxPosition) {
+      maxPosition = cars[i].position;
     }
   }
-  for (let i = 0; i < args.length; i++) {
-    if (args[i].position === maxPosition) {
-      result.push(args[i].name);
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].position === maxPosition) {
+      result.push(cars[i].name);
     }
   }
 
