823a492e586b944393fb6a261b85a356df9c06dd
kimjaehyune <wogus4041@naver.com>
2024-04-30 20:48:57 +0900

    테스트 코드 수정

--- __tests__/InputOutput.test.js
+++ __tests__/InputOutput.test.js
@@ -10,16 +10,16 @@ describe('입출력에 ', () => {
   
   it('자동차 이름은 쉼표(,)를 기준으로 구분', () => {
     // given
-    const input = new InputOutput("제발,그만,해주세요");
+    const input = new InputOutput("a,b,c");
     // when
     const car_names = input.car_names;
     // then 
-    expect(car_names).toBe("제발,그만,해주세요");
+    expect(car_names).toBe("a,b,c");
   });
 
   it('사용자가 잘못된 입력 값을 작성한 경우 프로그램을 종료한다', () => {
     // given
-    const car_names = "잘못,입력,";
+    const car_names = "a,b,";
     // when
     
     // then 
@@ -28,7 +28,7 @@ describe('입출력에 ', () => {
 
   it('전진하는 자동차는 이름을 같이 출력', () => {
     // given
-    const input = new InputOutput("제발,그만,해주세요");
+    const input = new InputOutput("a,b,c");
     const cars = input.cars;
     cars[0].conditionsMove(5);
     cars[0].conditionsMove(5);
@@ -36,24 +36,24 @@ describe('입출력에 ', () => {
     const race_output = input.race_output;
 
     // then 
-    expect(race_output).toBe("제발 : --\n그만 : \n해주세요 : \n");
+    expect(race_output).toBe("a : --\nb : \nc : \n");
   });
 
   it('자동차 경주 완료한후 누가 우승했는지 알려준다 우승자가 한명일경우', () => {
     // given
-    const input = new InputOutput("제발,그만,해주세요");
+    const input = new InputOutput("a,b,c");
     const cars = input.cars;
     cars[0].conditionsMove(5);
     cars[0].conditionsMove(5);
     // when
     const race_winner = input.winner;
     // then 
-    expect(race_winner).toBe("제발가 최종 우승했습니다.");
+    expect(race_winner).toBe("a가 최종 우승했습니다.");
   });
 
   it('자동차 경주 완료한후 누가 우승했는지 알려준다 우승자가 두명일경우', () => {
     // given
-    const input = new InputOutput("제발,그만,해주세요");
+    const input = new InputOutput("a,b,c");
     const cars = input.cars;
     cars[0].conditionsMove(5);
     cars[0].conditionsMove(5);
@@ -62,6 +62,6 @@ describe('입출력에 ', () => {
     // when
     const race_winner = input.winner;
     // then 
-    expect(race_winner).toBe("제발,그만가 최종 우승했습니다.");
+    expect(race_winner).toBe("a,b가 최종 우승했습니다.");
   });
 });
