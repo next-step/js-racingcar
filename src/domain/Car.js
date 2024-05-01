49d8d0e16ecbced903e22da58e90b66430528659
ChanwooPark <chanw9503@gmail.com>
2024-05-01 18:19:06 +0900

    Refacotor. 함수 기능 최소화

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -7,11 +7,19 @@ export class Car {
 
   /** 유효성 검사는 함수로 빼서 관리는게 좋을까? */
   constructor(name) {
-    if (name.length > MAX_CAR_NAME_LENGTH)
-      throw new Error('이름은 5자 이하만 가능합니다.');
-
+    this.validateName(name);
     this.#name = name;
     this.#position = 0;
+  }
+
+  isValidLength(name) {
+    return name.length <= 5 && name.length >= 1;
+  }
+
+  validateName(name) {
+    if (!this.isValidLength(name)) {
+      throw new Error('이름은 5자 이하만 가능합니다.');
+    }
   }
 
   get name() {
