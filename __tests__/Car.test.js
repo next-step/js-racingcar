ad2b6ee55a3d19f26236e5f60f69598e234b2d21
ChanwooPark <chanw9503@gmail.com>
2024-05-01 16:49:25 +0900

    Fix. 자동차 이름에 대한 테스트 변경

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -38,25 +38,10 @@ describe('자동차 관련 내용', () => {
     expect(car.position).toBe(0);
   });
 
-  test('이름은 5자 이하만 가능하다', () => {
-    //Given(준비)
-
-    const car = new Car('audi');
-
-    //When(실행)
-
-    //Then(검증)
-    expect(car.getName()).toBe('audi');
-  });
-
-  test('이름은 5자 이상이면 생성이 안된다.', () => {
-    //Given(준비)
-    const car = new Car('audiii');
-
-    //When(실행)
-
-    //Then(검증)
-    expect(car.isValidateName()).toBe(false);
+  test('자동차이름을 5자 초과시 에러를 발생시킨다.', () => {
+    expect(() => {
+      const car = new Car('audiiii');
+    }).toThrow();
   });
 
   test('전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 통과', () => {
