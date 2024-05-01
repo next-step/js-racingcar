14f9415eab019f11bae976d13aafa60d39d18367
ChanwooPark <chanw9503@gmail.com>
2024-05-01 18:02:07 +0900

    Fix. 하드 코딩된 값 변수화

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,10 +1,14 @@
+const MAX_CAR_NAME_LENGTH = 5;
+const MOVE_CAR_COUNT = 4;
+
 export class Car {
   #name;
   #position;
 
   /** 유효성 검사는 함수로 빼서 관리는게 좋을까? */
   constructor(name) {
-    if (name.length > 5) throw new Error('이름은 5자 이하만 가능합니다.');
+    if (name.length > MAX_CAR_NAME_LENGTH)
+      throw new Error('이름은 5자 이하만 가능합니다.');
 
     this.#name = name;
     this.#position = 0;
@@ -19,7 +23,7 @@ export class Car {
   }
 
   move(randomValue) {
-    if (randomValue >= 4) {
+    if (randomValue >= MOVE_CAR_COUNT) {
       this.#position += 1;
     }
   }
