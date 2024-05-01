775ebb7ebbe5fe2baacb31ce59226dc88eb3b0bd
ChanwooPark <chanw9503@gmail.com>
2024-05-01 16:52:15 +0900

    Fix. 자동차 움직일 경우 대한 실패 테스트 케이스 작성

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -14,6 +14,24 @@ describe('자동차 관련 내용', () => {
     }).toThrow();
   });
 
+  test('무작위 값이 4 미만일 경우, 위치가 변경되지 않는다.', () => {
+    //Given(준비)
+    const car = new Car('audi');
+
+    //When(실행)
+    const RANDOM_COUNT = 2;
+    car.move(RANDOM_COUNT);
+
+    //Then(검증)
+    expect(car.position).toBe(0);
+  });
+
+  test('자동차이름을 5자 초과시 에러를 발생시킨다.', () => {
+    expect(() => {
+      const car = new Car('audiiii');
+    }).toThrow();
+  });
+
   test('무작위 값이 4 이상일 경우, 위치가 변경된다.', () => {
     //Given(준비)
     const car = new Car('audi');
@@ -50,16 +68,27 @@ describe('자동차 관련 내용', () => {
     }).toThrow();
   });
 
-  test('전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 통과', () => {
+  test('무작위 값이 4 이상일 경우, 위치가 변경된다.', () => {
     //Given(준비)
-    const car = new Car();
+    const car = new Car('audi');
 
     //When(실행)
-    car.getRandomValue = jest.fn().mockReturnValue(5); // 항상 이동 조건 충족
-
-    car.move();
+    const RANDOM_COUNT = 5;
+    car.move(RANDOM_COUNT);
 
     //Then(검증)
-    expect(car.getPosition()).toBe(1);
+    expect(car.position).toBe(1);
+  });
+
+  test('무작위 값이 4 미만일 경우, 위치가 변경되지 않는다.', () => {
+    //Given(준비)
+    const car = new Car('audi');
+
+    //When(실행)
+    const RANDOM_COUNT = 2;
+    car.move(RANDOM_COUNT);
+
+    //Then(검증)
+    expect(car.position).toBe(0);
   });
 });
