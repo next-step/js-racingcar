4fb8daf48e3b04080439c20d3b3081dde90560bb
ChanwooPark <chanw9503@gmail.com>
2024-05-01 18:31:11 +0900

    Test. 실패 테스트 케이스 재정의 (예외 케이스로 변경)

--- __tests__/IO.test.js
+++ __tests__/IO.test.js
@@ -3,17 +3,16 @@ import { Race } from '../src/domain/Race';
 import { CarIO } from '../src/view/CarIO';
 
 describe('입출력 관련된 것들', () => {
-  test('자동차 이름은 쉼표(,)를 기준으로 구분하여 입력받는다.', async () => {
-    // 비동기 테스트를 위해 async 추가
+  test('자동차 이름은 쉼표(,)로 구분하지 않을경우, Error가 발생한다.', async () => {
     //Given
     const cario = new CarIO();
-
     // when
-    cario.readLineAsync = jest.fn().mockResolvedValue('bmw,audi,kia');
-    await cario.inputCars(); // 비동기 호출을 기다리기 위해 await 추가
+    cario.readLineAsync = jest.fn().mockResolvedValue('bmw audi kia');
 
     // then
-    expect(cario.getInputs()).toBe('bmw,audi,kia');
+    expect(async () => {
+      await cario.inputCars();
+    }).toThrow();
   });
 
   test('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', () => {
