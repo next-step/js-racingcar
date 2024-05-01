653d75fe599fe176db6f0fe2876b19c1a57589c6
ChanwooPark <chanw9503@gmail.com>
2024-05-01 16:52:15 +0900

    Fix. 자동차 움직일 경우 대한 실패 테스트 케이스 작성

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -43,17 +43,4 @@ describe('자동차 관련 내용', () => {
       const car = new Car('audiiii');
     }).toThrow();
   });
-
-  test('전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 통과', () => {
-    //Given(준비)
-    const car = new Car();
-
-    //When(실행)
-    car.getRandomValue = jest.fn().mockReturnValue(5); // 항상 이동 조건 충족
-
-    car.move();
-
-    //Then(검증)
-    expect(car.getPosition()).toBe(1);
-  });
 });
