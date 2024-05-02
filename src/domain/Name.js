f172bd9d01b05f9e3b515874ef6cd301a9edb3ee
Vespucci95 <donglonk@naver.com>
2024-05-02 23:52:41 +0900

    feat: 이름 유효성 검사 로직 구현

--- src/domain/Name.js
+++ src/domain/Name.js
@@ -3,9 +3,28 @@ import { ERROR_MESSAGE } from '../constnats';
 class Name {
   #name;
 
-  static DEFAULT_MAX_NAME_LENGTH = 5;
+  static DEFAULT_OPTIONS = {
+    MIN_NAME_LENGTH: 2,
+    MAX_NAME_LENGTH: 5,
+  };
 
-  constructor(name, maxLen = Name.DEFAULT_MAX_NAME_LENGTH) {
+  constructor(
+    name,
+    {
+      maxLen = Name.DEFAULT_OPTIONS.MAX_NAME_LENGTH,
+      minLen = Name.DEFAULT_OPTIONS.MIN_NAME_LENGTH,
+      regex = /^[a-zA-Z가-힣]+$/g,
+    } = Name.DEFAULT_OPTIONS
+  ) {
+    if (!(typeof name === 'string')) {
+      throw new TypeError(ERROR_MESSAGE.INVALID_PARAMETER);
+    }
+    if (!regex.test(name)) {
+      throw new TypeError(ERROR_MESSAGE.INVALID_PARAMETER);
+    }
+    if (name.length < minLen) {
+      throw new TypeError(ERROR_MESSAGE.MIN_LENGTH);
+    }
     if (name.length > maxLen) {
       throw new TypeError(ERROR_MESSAGE.MAX_LENGTH);
     }
