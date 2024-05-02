23ceb420d5da50745eb9fab71b9d8879207968f1
이새미 <saemi.lee@day1company.co.kr>
2024-05-03 00:13:13 +0900

    fix: 게임 실행, 종료 분리하여 실행
    (cherry picked from commit 945e715085eddbffdccf0ca1c0420c2a29ceb452)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { askCarNames, askMaxRound } from "./view.js";
-import Controller, { playGame } from "./controller.js";
+import Controller from "./controller.js";
 
 async function app() {
   const controller = new Controller();
   await controller.initCarNames(askCarNames, async () => {
     await controller.initMaxRound(askMaxRound);
   });
-  // const maxRound = await askMaxRound();
 
-  // playGame(carNames, maxRound);
+  controller.playRaceGame();
+  controller.finish();
 }
 
 app();
