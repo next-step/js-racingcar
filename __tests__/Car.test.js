aa8a8e5e1f3c2ea50b3992519cf49e9d4747798b
Donghoon Kang <d159123@naver.com>
2024-05-04 00:12:31 +0900

    refactor: 랜덤값에 따른 테스트 추가 및 테스트 beforeEach 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -7,10 +7,15 @@ import { MAXIMUM_CAR_NAME_LENGTH } from "../src/constants/index.js";
 - [x] 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우, 전진한다
  */
 
+let car;
+
+beforeEach(() => {
+  car = new Car("car1");
+});
+
 describe("자동차 기능 테스트", () => {
   test("자동차는 1자 이상, 5자 이하의 이름을 갖는다.", () => {
     // given
-    const car = new Car("car1");
 
     // when
     const name = car.name;
@@ -20,15 +25,25 @@ describe("자동차 기능 테스트", () => {
     expect(name.length).toBeGreaterThanOrEqual(MINIMUM_CAR_NAME_LENGTH);
   });
 
-  test("전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후, 무작위 값이 4 이상일 경우이다.", () => {
+  test("무작위 값이 4 이상일 경우, 1 전진한다.", () => {
     // given
-    const car = new Car("test");
+    const getRandomValue = jest.fn().mockReturnValue(5);
     
     // when
-    const randomValue = car.getRandomValue();
-    car.moveForward(randomValue);
+    car.moveForward(getRandomValue());
 
     // then
-    expect(car.position).toBe(randomValue >= MOVE_THRESHOLD ? 1 : 0);
+    expect(car.position).toBe(1);
+  })
+
+  test("무작위 값이 4 미만일 경우, 정지한다.", () => {
+    // given
+    const getRandomValue = jest.fn().mockReturnValue(3);
+    
+    // when
+    car.moveForward(getRandomValue());
+
+    // then
+    expect(car.position).toBe(0);
   })
 });
