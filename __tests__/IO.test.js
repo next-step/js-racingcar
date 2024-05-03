6e707a59db02e7aeb7cee72a138bdc52163d64d4
ChanwooPark <chanw9503@gmail.com>
2024-05-04 02:08:43 +0900

    Test. 새로운기능에 대한 테스트케이스 작성

--- __tests__/IO.test.js
+++ __tests__/IO.test.js
@@ -1,6 +1,10 @@
-import { ERROR_MESSAGE_NAME_LENGTH } from '../src/domain/Car';
+import { Car, ERROR_MESSAGE_NAME_LENGTH } from '../src/domain/Car';
 import { Race } from '../src/domain/Race';
-import { CarIO, ERROR_MESSAGE_COMMA_SEPARTED } from '../src/view/CarIO';
+import {
+  CarIO,
+  ERROR_MESSAGE_COMMA_SEPARTED,
+  ERROR_MESSAGE_NUMERIC,
+} from '../src/view/CarIO';
 
 describe('입출력 관련된 것들', () => {
   test('전진하는 자동차를 출력할 때 자동차 이름을 같이 출력한다.', async () => {
@@ -43,5 +47,43 @@ describe('입출력 관련된 것들', () => {
     cario.readLineAsync = jest.fn().mockResolvedValue('bmasdasw,audi,kia');
 
     await expect(cario.inputCars()).rejects.toThrow(ERROR_MESSAGE_NAME_LENGTH);
+  });
+
+  test('사용자는 몇 번의 이동을 할 것인지를 입력할 수 있어야 한다.', async () => {
+    //Given
+    const cario = new CarIO();
+
+    //when
+    cario.readLineAsync = jest.fn().mockResolvedValue('5');
+
+    //then
+    await expect(cario.inputNumberOfRaces()).resolves.toBe('5');
+  });
+
+  test('사용자가 숫자 이외 다른값을 입력할 경우 콘솔에 에러가 입력된다.', async () => {
+    //Given
+    const cario = new CarIO();
+    const logSpy = jest.spyOn(global.console, 'log');
+
+    //when
+    cario.readLineAsync = jest.fn().mockResolvedValueOnce('asdasdas');
+    const result = await cario.inputNumberOfRaces(0);
+
+    //then
+    expect(logSpy).toHaveBeenCalled();
+    expect(logSpy).toHaveBeenCalledWith(ERROR_MESSAGE_NUMERIC);
+  });
+
+  test('잘못 입력한 경우, 다시입력이 가능하나 가능 횟수에 제한을 둔다.', async () => {
+    //Given
+    const cario = new CarIO();
+    const logSpy = jest.spyOn(global.console, 'log');
+
+    //when
+    cario.readLineAsync = jest.fn().mockResolvedValueOnce('asdasdas');
+    await cario.inputNumberOfRaces(10);
+
+    //then
+    expect(logSpy).toHaveBeenCalledWith(ERROR_MESSAGE_NUMERIC);
   });
 });
