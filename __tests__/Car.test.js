f211ff67bacb3813ee3637513dbe5b304a2fd8ce
seungdeok <akk9132@naver.com>
2024-05-04 08:18:03 +0900

    refactor: Strategy Test 케이스 분리

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,18 +1,7 @@
 import { ERROR_CODES } from "../src/constants";
 import { Car } from "../src/domain";
-import { RandomMoveStrategy } from "../src/domain/strategies";
-import { getRandom } from "../src/utils";
-
-jest.mock("../src/utils", () => ({
-  readLineAsync: jest.fn(),
-  getRandom: jest.fn(),
-}));
 
 describe("자동차 테스트", () => {
-  afterEach(() => {
-    getRandom.mockReset();
-  });
-
   test("자동차 이름은 5자 이하인 경우 정상", () => {
     // given
     const carName = "Car";
@@ -44,19 +33,5 @@ describe("자동차 테스트", () => {
 
     // then
     expect(app).toThrow(ERROR_CODES.ERROR_EMPTY_CAR_NAME);
-  });
-
-  test("0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우 전진", () => {
-    // given
-    const car = new Car("Car");
-    const position = 1;
-    getRandom.mockReturnValue(RandomMoveStrategy.MOVE_FORWARD_CAR);
-
-    // when
-    car.move(new RandomMoveStrategy().shouldMove);
-
-    // then
-    expect(car.position).toBe(position);
-    expect(getRandom).toHaveBeenCalled();
   });
 });
