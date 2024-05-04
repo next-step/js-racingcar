90aa318989fcbece2c89939247c620b0af63321d
jambottle <jaewon.walks@gmail.com>
2024-05-04 16:50:12 +0900

    feat: 자동차 객체 > 생성 시, 예외 처리
    - 빈 문자열을 이름으로 갖는 자동차를 생성할 경우, 예외 발생 처리

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -6,6 +6,9 @@ export default class Car {
   #position = 0;
 
   constructor(name) {
+    if (name.length === 0) {
+      throw new Error('자동차 이름은 빈 문자열일 수 없습니다.');
+    }
     if (name.length > Car.MAX_NAME_LENGTH) {
       throw new Error('자동차 이름은 5자 이하로만 부여할 수 있습니다.');
     }
