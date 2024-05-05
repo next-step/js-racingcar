43eba8b41b5b11182b5a401fb07b6d056c999f22
supreme-d <dmhuh.data@gmail.com>
2024-05-05 14:51:33 +0900

    refactor: separate executing a lap from main function to enhance code readability

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import IO from "./IO";
 import Car from "../src/Car";
 
-import { parseInput, getRandomNumber, parseOutput } from "./utils";
-import { createCars, getWinners } from "./racing";
+import { parseInput, parseOutput } from "./utils";
+import { createCars, executeLap, getWinners } from "./racing";
 
 const LAPS = 5;
-const RANDOM_MIN = 0;
-const RANDOM_MAX = 9;
 
 const play = async () => {
   const input = await IO.readLineAsync("자동차 이름을 입력해주세요");
@@ -16,11 +14,7 @@ const play = async () => {
     const cars = createCars(carNames, Car);
 
     for (let i = 0; i < LAPS; i++) {
-      cars.forEach((car) => {
-        const randomNumber = getRandomNumber(RANDOM_MIN, RANDOM_MAX);
-        car.move(randomNumber);
-        console.log(`${car.name} / ${car.position}`);
-      });
+      executeLap(cars);
     }
 
     const winners = getWinners(cars);
--- src/racing.js
+++ src/racing.js
@@ -1,3 +1,8 @@
+import { getRandomNumber } from "./utils";
+
+const RANDOM_MIN = 0;
+const RANDOM_MAX = 9;
+
 export const getWinners = (cars) => {
   const maxPosition = Math.max(...cars.map((car) => car.position));
 
@@ -6,4 +11,12 @@ export const getWinners = (cars) => {
 
 export const createCars = (carData, CarClass) => {
   return carData.map((data) => new CarClass(data));
+};
+
+export const executeLap = (cars) => {
+  cars.forEach((car) => {
+    const randomNumber = getRandomNumber(RANDOM_MIN, RANDOM_MAX);
+    car.move(randomNumber);
+    console.log(`${car.name} / ${car.position}`);
+  });
 };
