a6fccb9292c54c667fa272e956c7dd7a4668d6ad
supreme-d <dmhuh.data@gmail.com>
2024-05-05 12:16:44 +0900

    test: change test case for boundary value

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -11,7 +11,7 @@ describe("Car class", () => {
     });
 
     it("should throw an error if the name is longer than 5 characters", () => {
-      const name = "abcdefrr";
+      const name = "abcdef";
 
       expect(() => new Car(name)).toThrow("Name must be 5 characters or fewer");
     });
