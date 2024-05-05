b3255a680f39126c482956cee2d209326adbfd7e
supreme-d <dmhuh.data@gmail.com>
2024-05-05 12:09:51 +0900

    refactor: clean up code using functional approach to enhance readability

--- src/racing.js
+++ src/racing.js
@@ -1,17 +1,5 @@
 export const getWinners = (cars) => {
-  const result = [];
-  let maxPosition = 0;
+  const maxPosition = Math.max(...cars.map((car) => car.position));
 
-  for (let i = 0; i < cars.length; i++) {
-    if (cars[i].position > maxPosition) {
-      maxPosition = cars[i].position;
-    }
-  }
-  for (let i = 0; i < cars.length; i++) {
-    if (cars[i].position === maxPosition) {
-      result.push(cars[i].name);
-    }
-  }
-
-  return result;
+  return cars.filter((car) => car.position === maxPosition).map((car) => car.name);
 };
