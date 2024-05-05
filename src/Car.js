c4fec712b4c560726f8575bb92156a0563c34844
supreme-d <dmhuh.data@gmail.com>
2024-05-05 15:37:59 +0900

    refactor: add constant for maximum name length

--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,11 @@
 import { MESSAGE } from "../constants/message";
 
 const MOVE_THRESHOLD = 4;
+const NAME_LENGTH_MAX = 5;
 
 export default class Car {
   constructor(name, position = 0) {
-    if (name.length > 5) {
+    if (name.length > NAME_LENGTH_MAX) {
       throw new Error(MESSAGE.NAME_LENGTH_ERROR);
     }
 
