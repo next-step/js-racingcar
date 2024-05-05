47064a1f7cf3c30d645bfe5aeb85c0cc1d008f0f
supreme-d <dmhuh.data@gmail.com>
2024-05-05 20:18:06 +0900

    refactor: change function names for clarity

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import IO from "./IO";
 import Car from "../src/Car";
 
-import { parseInput, parseOutput } from "./utils";
+import { parseCarNames, parseLaps, parseOutput } from "./utils";
 import { createCars, executeLap, getWinners } from "./racing";
 
-const LAPS = 5;
-
 const play = async () => {
-  const input = await IO.readLineAsync("자동차 이름을 입력해주세요");
+  const carNamesInput = await IO.readLineAsync("자동차 이름을 입력해주세요");
+  const lapsInput = await IO.readLineAsync("시도할 회수는 몇회인가요?");
 
   try {
-    const carNames = parseInput(input);
+    const carNames = parseCarNames(carNamesInput);
+    const laps = parseLaps(lapsInput);
     const cars = createCars(carNames, Car);
 
-    for (let i = 0; i < LAPS; i++) {
+    for (let i = 0; i < laps; i++) {
       console.log(`Lap #${i + 1} ${"=".repeat(50)}`);
       executeLap(cars);
     }
