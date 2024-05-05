f07eaf8cc48f44c5e37538a7e67473aea7055d16
supreme-d <dmhuh.data@gmail.com>
2024-05-05 15:59:27 +0900

    feat: add input validation for Car class move method

--- src/Car.js
+++ src/Car.js
@@ -3,6 +3,11 @@ import { MESSAGE } from "../constants/message";
 const MOVE_THRESHOLD = 4;
 const NAME_LENGTH_MAX = 5;
 
+export const MOVABLE_RANGE = {
+  MIN: 0,
+  MAX: 9,
+};
+
 export default class Car {
   constructor(name, position = 0) {
     if (name.length > NAME_LENGTH_MAX) {
@@ -14,6 +19,13 @@ export default class Car {
   }
 
   move(value) {
+    if (typeof value !== "number") {
+      throw new TypeError(MESSAGE.INPUT_TYPE_ERROR);
+    }
+    if (value < MOVABLE_RANGE.MIN || value > MOVABLE_RANGE.MAX) {
+      throw new RangeError(MESSAGE.INPUT_RANGE_ERROR);
+    }
+
     if (value >= MOVE_THRESHOLD) {
       this.position++;
     }
