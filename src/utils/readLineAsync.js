e0493cf373fcd7329f07cde8d7521451cc45baab
seungdeok <akk9132@naver.com>
2024-05-06 16:01:29 +0900

    refactor: Sperate domain from util

--- src/utils/readLineAsync.js
+++ src/utils/readLineAsync.js
@@ -1,15 +1,14 @@
 import readline from "readline";
-import { InputError } from "../domain/Error";
 import { ERROR_CODES } from "../constants";
 
 export function readLineAsync(query) {
   return new Promise((resolve, reject) => {
     if (arguments.length !== 1) {
-      reject(new InputError(ERROR_CODES.ERROR_EMPTY_INPUT_LEN));
+      reject(new Error(ERROR_CODES.ERROR_EMPTY_INPUT_LEN));
     }
 
     if (typeof query !== "string") {
-      reject(new InputError(ERROR_CODES.ERROR_INPUT_QUERY_TYPE));
+      reject(new Error(ERROR_CODES.ERROR_INPUT_QUERY_TYPE));
     }
 
     const rl = readline.createInterface({
