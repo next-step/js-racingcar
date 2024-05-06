a79e8c1747dc56b2e396c2f3385e16042c8f34eb
seungdeok <akk9132@naver.com>
2024-05-06 20:05:32 +0900

    refactor: Update App 불필요한 로직 제거

--- src/index.js
+++ src/index.js
@@ -1,36 +1,24 @@
 import { Race } from "./domain";
+import { RandomMoveStrategy } from "./domain/strategies";
 import { View } from "./views";
 
 export class App {
-  #race;
-
   async play() {
     try {
-      let carNames = await View.getCarNamesPrompt();
-      if (carNames.length === 0) return;
-      if (!Array.isArray(carNames))
-        carNames = carNames.split(",").map((name) => name.trim());
-
+      const carNames = await View.getCarNamesPrompt();
       const raceRound = await View.getRaceRoundPrompt();
 
-      this.#race = new Race(carNames, raceRound);
-      const result = this.#race.race();
-      this.#render(result);
+      const race = new Race(carNames, raceRound);
+      const raceResult = race.race(new RandomMoveStrategy());
+      View.printRoundStart();
+      raceResult.forEach((result) => {
+        View.printRaceResult(result.cars);
+      });
+      View.printWinners(race.winners);
     } catch (error) {
+      console.log(error);
       View.printError(error);
     }
-  }
-
-  #render(raceResult) {
-    View.printRoundStart();
-    raceResult.forEach((result) => {
-      View.printRaceResult(result.cars);
-    });
-    View.printWinners(this.#race.winners);
-  }
-
-  getRace() {
-    return this.#race;
   }
 }
 
