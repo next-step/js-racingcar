642050df8fab1e4b430eedb76ebda61c5a3b6a8f
Vespucci95 <donglonk@naver.com>
2024-05-06 21:21:02 +0900

    refactor: 테스트 문구 수정

--- __tests__/RacingIO.test.js
+++ __tests__/RacingIO.test.js
@@ -8,16 +8,6 @@ import {
 import Car from '../src/domain/Car';
 
 describe('레이싱 입출력 기능 테스트', () => {
-  it('자동차 이름은 쉼표(,)를 기준으로 구분한다.', async () => {
-    // given
-    const input = 'taxi, tesla,ford ';
-
-    // when
-    const carName = writeRacingCar(input);
-
-    // then
-    expect(carName).toEqual(['taxi', 'tesla', 'ford']);
-  });
   it('자동차의 위치는 - 로 표시한다.', () => {
     // given
     const input = 3;
@@ -28,6 +18,7 @@ describe('레이싱 입출력 기능 테스트', () => {
     // then
     expect(output).toBe('---');
   });
+
   it('레이싱 진행 상황을 표시한다.', () => {
     // given
     const car = new Car('tesla');
@@ -52,17 +43,31 @@ describe('레이싱 입출력 기능 테스트', () => {
     expect(winnersOutput).toBe('taxi, ford');
     expect(winnerOutput).toBe('taxi');
   });
-  it('사용자가 잘못된 입력 값을 작성한 경우 프로그램을 종료한다.', () => {
-    // given
-    const input = '';
 
-    // when
-    const userAction = writeRacingCar;
+  describe('자동차 이름 입력', () => {
+    it('쉼표(,)를 기준으로 구분한다.', () => {
+      // given
+      const input = 'taxi, tesla,ford ';
 
-    // then
-    expect(() => userAction(input)).toThrow();
+      // when
+      const carName = writeRacingCar(input);
+
+      // then
+      expect(carName).toEqual(['taxi', 'tesla', 'ford']);
+    });
+    it('잘못 입력된 경우 에러를 발생시킨다.', () => {
+      // given
+      const input = '';
+
+      // when
+      const userAction = writeRacingCar;
+
+      // then
+      expect(() => userAction(input)).toThrow();
+    });
   });
-  describe('사용자는 경기 진행 횟수를 입력할 수 있다.', () => {
+
+  describe('경기 진행 횟수 입력', () => {
     it('정수만 입력할 수 있다.', () => {
       // given
       const input = 10;
