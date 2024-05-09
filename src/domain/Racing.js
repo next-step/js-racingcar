aac028e16671d7139f35212def30770b6fa7a83d
jiyeon <ejy1105@gmail.com>
2024-05-09 23:15:32 +0900

    fix: round 최대값 setter 함수로 받기

--- src/domain/Racing.js
+++ src/domain/Racing.js
@@ -1,4 +1,4 @@
-import { EXECUTE_RACING_MAX_COUNT } from "../constants/number.js";
+import { EXECUTE_RACING_MAX_COUNT, NINE, ZERO } from "../constants/number.js";
 import { randomNumber } from "../util/random.js";
 import Car from "./Car.js";
 
@@ -10,12 +10,15 @@ export class Racing {
   constructor(enter) {
     this.cars = enter.map((name) => new Car(name));
     this.currRound = 0;
-    this.maxRound = EXECUTE_RACING_MAX_COUNT;
   }
 
   gameRound() {
-    this.cars.forEach((car) => car.drive(randomNumber(0, 9)));
+    this.cars.forEach((car) => car.drive(randomNumber(ZERO, NINE)));
     this.currRound++;
+  }
+
+  set maxRound(round) {
+    this.maxRound = round;
   }
 
   get winner() {
