c4bacaf0862784ac03655ede9dc0a37c83a2160c
HoonDongKang <d159123@naver.com>
2024-05-10 21:05:34 +0900

    feat: 각 자동차는 1자 이상, 5자 이하의 이름을 갖는다.

--- __tests__/constants/index.js
+++ __tests__/constants/index.js
@@ -1,7 +1,7 @@
-export const TEST_CARS = ["car1", "car2", "car3"];
+export const TEST_CARS = ['car1', 'car2', 'car3']
 
-export const TEST_DUPLICATED_CARS = ["car1", "car1", "car2"];
+export const TEST_DUPLICATED_CARS = ['car1', 'car1', 'car2']
 
-export const TEST_NONEXISTENT_CARS = [];
+export const TEST_NONEXISTENT_CARS = []
 
-export const TEST_WHITESPACE_CARS = [" ", "   ", "    "];
+export const TEST_WRONG_LENGTH_CARS = [' ', 'over6chars', 'normal']
