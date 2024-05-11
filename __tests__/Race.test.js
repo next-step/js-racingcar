b90d86f94b20837c4d9093ff5227f912be9d3042
seungdeok <akk9132@naver.com>
2024-05-11 20:47:38 +0900

    refactor: Update var name

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -16,18 +16,18 @@ describe("자동차 경주 게임 테스트", () => {
     const car1 = new Car("Car1");
     const car2 = new Car("Car2");
     const car3 = new Car("Car3");
-    const app = new Race([car1, car2, car3], 5);
+    const race = new Race([car1, car2, car3], 5);
     getRandom
       .mockReturnValueOnce(RandomMoveStrategy.MOVE_FORWARD_CAR)
       .mockReturnValueOnce(0)
       .mockReturnValueOnce(0);
 
     // when
-    app.race(new RandomMoveStrategy());
+    race.race(new RandomMoveStrategy());
 
     // then
-    expect(app.winners.length).toBe(1);
-    expect(app.winners[0].name).toBe(car1.name);
+    expect(race.winners.length).toBe(1);
+    expect(race.winners[0].name).toBe(car1.name);
   });
 
   test("라운드마다 전진 조건이 바뀔 수 있다.", async () => {
@@ -35,20 +35,20 @@ describe("자동차 경주 게임 테스트", () => {
     const car1 = new Car("Car1");
     const car2 = new Car("Car2");
     const car3 = new Car("Car3");
-    const app = new Race([car1, car2, car3], 5);
+    const race = new Race([car1, car2, car3], 5);
     getRandom.mockReturnValueOnce(RandomMoveStrategy.MOVE_FORWARD_CAR);
-    app.setStrategyPerRound(2, new DefaultMoveStrategy());
-    app.setStrategyPerRound(3, new DefaultMoveStrategy());
-    app.setStrategyPerRound(4, new DefaultMoveStrategy());
-    app.setStrategyPerRound(5, new DefaultMoveStrategy());
+    race.setStrategyPerRound(2, new DefaultMoveStrategy());
+    race.setStrategyPerRound(3, new DefaultMoveStrategy());
+    race.setStrategyPerRound(4, new DefaultMoveStrategy());
+    race.setStrategyPerRound(5, new DefaultMoveStrategy());
 
     // when
-    app.race(new RandomMoveStrategy());
+    race.race(new RandomMoveStrategy());
 
     // then
-    expect(app.cars[0].position).toBe(5);
-    expect(app.cars[1].position).toBeGreaterThan(3);
-    expect(app.cars[2].position).toBeGreaterThan(3);
+    expect(race.cars[0].position).toBe(5);
+    expect(race.cars[1].position).toBeGreaterThan(3);
+    expect(race.cars[2].position).toBeGreaterThan(3);
   });
 
   test("중복된 자동차 이름이 존재할 때 종료한다.", async () => {
@@ -56,10 +56,10 @@ describe("자동차 경주 게임 테스트", () => {
     const carNames = "crong,crong,honux".split(",");
 
     // when
-    const app = () => new Race(carNames);
+    const race = () => new Race(carNames);
 
     // then
-    expect(app).toThrow(ERROR_CODES.ERROR_DUPLICATE_CAR_NAME);
+    expect(race).toThrow(ERROR_CODES.ERROR_DUPLICATE_CAR_NAME);
   });
 
   test("자동차 경주는 N회로 진행한다.", () => {
