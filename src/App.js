09c444d7eb803f00dc406cf6558d87f8282773b6
Vespucci95 <donglonk@naver.com>
2024-05-14 17:05:40 +0900

    feat: 사용자 입력에러에 대해서만 다시 입력을 받는다.

--- src/App.js
+++ src/App.js
@@ -8,33 +8,48 @@ import {
 import { readLineAsync } from './utils';
 
 async function App() {
-  try {
-    const racingCarNames = await readLineAsync(
-      '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n'
-    );
-    const racingLapCount = await readLineAsync('시도할 회수는 몇회인가요?\n');
+  let [isInValidNames, isInValidLapCount] = [true, true];
+  let racingCarNames = '';
+  let racing = new Racing();
 
-    const racing = new Racing(writeNumber(racingLapCount));
-    racing.players = writeRacingCar(racingCarNames);
+  while (isInValidNames) {
+    try {
+      racingCarNames = await readLineAsync(
+        '경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n'
+      );
+      racing.players = writeRacingCar(racingCarNames);
 
-    console.log('\n실행 결과');
-    while (!racing.isEndedRace()) {
-      racing.race1Lap();
-
-      for (const car of racing.players) {
-        console.log(readCarProgress(car));
-      }
-      console.log('\n');
+      isInValidNames = false;
+    } catch (e) {
+      console.log(e.message + '\n');
     }
-
-    racing.end();
-    const winners = racing.getWinnersName();
-
-    console.log(`${readWinners(winners)}가 최종 우승했습니다.`);
-  } catch (e) {
-    console.error(e + '\n');
-    await App();
   }
+
+  while (isInValidLapCount) {
+    try {
+      const racingLapCount = await readLineAsync('시도할 회수는 몇회인가요?\n');
+      racing.maxLap = writeNumber(racingLapCount);
+
+      isInValidLapCount = false;
+    } catch (e) {
+      console.log(e.message + '\n');
+    }
+  }
+
+  console.log('\n실행 결과');
+  while (!racing.isEndedRace()) {
+    racing.race1Lap();
+
+    for (const car of racing.players) {
+      console.log(readCarProgress(car));
+    }
+    console.log('\n');
+  }
+
+  racing.end();
+  const winners = racing.getWinnersName();
+
+  console.log(`${readWinners(winners)}가 최종 우승했습니다.`);
 }
 
 export default App;
