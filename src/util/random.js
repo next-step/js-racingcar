10715ce2d8cfa8fbe3242eba51cf0ccf7aaa99f1
jiyeon <ejy1105@gmail.com>
2024-06-09 18:16:13 +0900

    fix: 랜덤넘버함수 유효성 검사 추가

--- src/util/random.js
+++ src/util/random.js
@@ -1,5 +1,7 @@
+import { SYSTEM } from "../constants/error.js";
 import { NINE, ZERO } from "../constants/number.js";
 
-export const generateRandomNumber = () => {
-  return Math.random() * (NINE - ZERO) + ZERO;
+export const generateRandomNumber = (min = ZERO, max = NINE) => {
+  if (min > max) throw new Error(SYSTEM.MIN_IS_GREATER_THAN_MAX);
+  return Math.random() * (max - min) + min;
 };
