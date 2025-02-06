6643fd55279493397f49530d50047b726211a33b
jest-won <jest@archisketch.com>
2025-02-06 22:05:35 +0900

    feat: car class 작성

--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log("Car");
+class Car {
+  name = "";
+  location = 0;
+  constructor(name) {
+    this.name = name;
+  }
+
+  moveForward() {
+    this.location += 1;
+  }
+
+  moveBackward() {
+    this.location -= 1;
+  }
+
+  getLocation() {
+    return this.location;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+export default Car;
