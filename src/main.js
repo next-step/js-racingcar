12be516b6a5a3f2892fbda6fae7bc784b019dd5c
김보경 <rlaqhrud6263@naver.com>
2025-02-06 20:56:09 +0900

    feat: class function car 작성

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log("Car");
+class Car {
+  constructor(name) {
+    this.name = name;
+    this.position = 1;
+  }
+
+  move() {
+    this.position += 1;
+  }
+
+  getStatus() {
+    const progress = "-".repeat(this.position);
+    return `${this.name} : ${progress || "-"}`;
+  }
+}
+const car = new Car("bokeeey");
+
+console.log(car.getStatus());
+car.move();
+console.log(car.getStatus());
