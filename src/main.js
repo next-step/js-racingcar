f4a641614340c963a341e6f610e5c5f8d5b4be18
inaemin <inaemin@gmail.com>
2025-02-06 21:18:15 +0900

    feat: 자동차 클래스 구현

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log("Car");
+class Car {
+  // 자동차는 이름을 상태로 가질 수 있다.
+  // 자동차는 위치 값을 가지며, 초기 상태는 0이다.
+  // 자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.
+
+  #name;
+  #position;
+
+  constructor() {
+    this.#name = "";
+    this.#position = 0;
+  }
+
+  changeName(name) {
+    this.#name = name;
+  }
+
+  move() {
+    this.#position++;
+  }
+}
