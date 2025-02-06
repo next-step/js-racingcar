cdf3cb2066d361e459ea05114d824519de30d4e9
김보경 <rlaqhrud6263@naver.com>
2025-02-06 22:15:48 +0900

    test: add descride code block

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,21 +1,25 @@
 import Car from "../src/main";
 
-it("자동차는 이름을 가진다.", () => {
-  const car = new Car("bokeeeey");
+const name = "bokeeeey";
 
-  expect(car.name).toEqual("bokeeeey");
-});
+describe("자동차 클래스 기능 테스트", () => {
+  it("자동차는 이름을 가진다.", () => {
+    const car = new Car(name);
 
-it("자동차는 위치 값을 가지며, 초기 상태는 0이다.", () => {
-  const car = new Car("bokeeeey");
+    expect(car.name).toEqual(name);
+  });
 
-  expect(car.position).toEqual(0);
-});
+  it("자동차는 위치 값을 가지며, 초기 상태는 0이다.", () => {
+    const car = new Car(name);
 
-it("자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.", () => {
-  const car = new Car("bokeeeey");
-  car.move();
-  const forwardedLocation = car.position;
+    expect(car.position).toEqual(0);
+  });
 
-  expect(forwardedLocation).toEqual(1);
+  it("자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.", () => {
+    const car = new Car(name);
+
+    car.move();
+    const forwardedLocation = car.position;
+    expect(forwardedLocation).toEqual(1);
+  });
 });
