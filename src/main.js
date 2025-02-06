f351cbbd0542bc27fc482a1dcbd9097591923b24
Benzy <97730865+benzyminzy@users.noreply.github.com>
2025-02-06 21:34:41 +0900

    Feat: 자동차경주 1단계 - 자동차 클래스 설계 (#268)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("Car");
+class Car {
+  INITIAL_LOCATION = 0;
+  FORWARD_STEP = 1;
+
+  constructor(name) {
+    this.name = name;
+    this.location = INITIAL_LOCATION;
+  }
+
+  forward() {
+    this.location += FORWARD_STEP;
+    return this.location;
+  }
+}
