825c4b3f23bec913a76a2db72b6795c4f35e6e76
etff <wish2rich@gmail.com>
2025-02-07 14:14:43 +0900

    리뷰 반영
    - describe depth를 추가하여, 표현 쉽게 수정

--- __tests__/car.test.js
+++ __tests__/car.test.js
@@ -1,24 +1,26 @@
 import Car from "../src/car.js";
 
-describe("Car 클래스는", () => {
+describe("자동차는", () => {
     let car;
-
     beforeEach(() => {
-        car = new Car("test");
+        car = new Car("포르쉐");
     });
-
-    it("생성자를 통해 전달받은 이름을 리턴한다", () => {
-        const expectedName = "test";
-        expect(car.name).toBe(expectedName);
+    describe("=== 자동차의 이름에 대한 테스트 ===", () => {
+        it("생성될 때 전달받은 이름을 가지고 있어야 한다", () => {
+            const expectedName = "포르쉐";
+            expect(car.name).toBe(expectedName);
+        });
     });
-
-    it("초기 position 값을 리턴한다", () => {
-        expect(car.position).toBe(0);
-    });
-
-    it("전진하면 position의 값이 증가한다", () => {
-        car.moveForward();
-        expect(car.position).toBe(1);
+    describe("=== 자동차의 위치에 대한 테스트 ===", () => {
+        it("처음 생성되었을 때는 시작 위치(0)에 있어야 한다", () => {
+            expect(car.position).toBe(0);
+        });
+        describe("> 전진할 때", () => {
+            it("위치가 1만큼 증가해야 한다", () => {
+                const initialPosition = car.position;
+                car.moveForward();
+                expect(car.position).toBe(initialPosition + 1);
+            });
+        });
     });
 });
-
