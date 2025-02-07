ed444ce5a5aad48ebac68c65391bfd2ba39d6fe3
mandarin-sep <vheh0901@gmail.com>
2025-02-08 00:26:58 +0900

    feat: 기능 요구 사항 아닌거 제거거

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
-import Car, { makeCar, drawSkidMark } from "./car.js";
+import { makeCar, drawSkidMark } from "./car.js";
 
 export default async function start() {
   const cars = await makeCar();
 
   console.log("경주 시작!");
+
   for (let i = 0; i < 5; i++) {
     cars.forEach((car) => {
       car.go();
       console.log(`${car.name} : ${drawSkidMark(car.state)}`);
     });
   }
-
-  console.log(cars);
 }
-
-// await start();
