14b4a5bb1f72ebe01c60bf13312063da3fddf19d
inaemin <inaemin@gmail.com>
2025-02-09 00:42:30 +0900

    🎨 feat: 경주 시작 메소드 추가

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ async function play() {
   const carNames = await readLineAsync("자동차 이름을 입력하세요\n");
   try {
     const racing = new Racing(carNames);
-
-    console.log(`경주를 완료했습니다.\n`);
+    console.log(`\n실행 결과`);
+    racing.start();
+    console.log(`경주를 완료했습니다.`);
   } catch (error) {
     throw error;
   }
