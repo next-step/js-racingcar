bd81dd088aa280b3c40beef91ce6b035af2cf01c
mandarin-sep <vheh0901@gmail.com>
2025-02-08 16:10:17 +0900

    feat:  전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.

--- __tests__/race.test.js
+++ __tests__/race.test.js
@@ -1,7 +1,7 @@
-import { findFarthestCar } from "../src/race";
+import { findFarthestCar, checkHeCanGo } from "../src/race";
 import Car from "../src/car";
 
-describe("가장 멀리간 차를 찾는다", () => {
+describe("전달 받은 차들이 레이싱을 해서 우승자를 찾는다.", () => {
   let firstCar;
   let secondCar;
   let thirdCar;
@@ -37,5 +37,23 @@ describe("가장 멀리간 차를 찾는다", () => {
     expect(findFarthestCar([firstCar, secondCar, thirdCar])).toBe(
       "일번차,이번차"
     );
+  });
+
+  test("차들이 전달 받은 값이 4 이상일때만 전진한다.", () => {
+    const originalRandom = Math.random;
+
+    Math.random = jest.fn().mockReturnValue(0.5);
+
+    checkHeCanGo(firstCar);
+
+    expect(firstCar.state).toBe(1);
+
+    Math.random = jest.fn().mockReturnValue(0.3);
+
+    checkHeCanGo(secondCar);
+
+    expect(secondCar.state).toBe(0);
+
+    Math.random = originalRandom;
   });
 });
--- src/race.js
+++ src/race.js
@@ -10,7 +10,21 @@ export function findFarthestCar(cars) {
   return winners.join(",");
 }
 
+export function checkHeCanGo(car) {
+  const randomNum = Math.random() * 10;
+  if (randomNum >= 4) {
+    car.go();
+  }
+}
+
 export function race(count, cars) {
   if (Number(count) === 0) {
+    console.log(findFarthestCar(cars));
   }
+
+  //   for(let i = 0; i < Number(count); i++){
+  //     cars.forEach(car => {
+
+  //     })
+  //   }
 }
