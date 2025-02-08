60a35e190536838dd81c8bacb8db02fce439c868
etff <wish2rich@gmail.com>
2025-02-09 03:17:00 +0900

    레이스 테스트 설명 추가

--- __tests__/domain/Race.test.js
+++ __tests__/domain/Race.test.js
@@ -20,35 +20,45 @@ describe("레이스는", () => {
     });
 
     describe("=== 레이스 초기화 테스트 ===", () => {
-        it("전달받은 자동차 리스트를 가지고 있어야 한다", () => {
-            expect(race.cars).toEqual(cars);
-        });
+        describe("> 생성할 때", () => {
+            it("전달받은 자동차 리스트를 가지고 있어야 한다", () => {
+                expect(race.cars).toEqual(cars);
+            });
 
-        it("라운드 수를 전달받지 않으면 기본값이 설정되어야 한다", () => {
-            const expectedRounds = 5;
-            expect(race.rounds).toBe(expectedRounds);
+            it("라운드 수를 전달받지 않으면 기본값이 설정되어야 한다", () => {
+                const expectedRounds = 5;
+                expect(race.rounds).toBe(expectedRounds);
+            });
+
+            it("라운드 수가 1 미만이면 예외를 발생시켜야 한다", () => {
+                expect(() => new Race(cars, 0)).toThrow("라운드는 1 이상이어야 합니다.");
+            });
         });
     });
 
     describe("=== 레이스 기록 테스트 ===", () => {
-        it("레이스 결과가 라운드 수만큼 기록되어야 한다", () => {
-            const raceResult = race.start();
-            expect(raceResult.raceHistory.length).toBe(race.rounds);
-        });
+        describe("> 시작되면 ", () => {
+            it("레이스 결과가 라운드 수만큼 기록되어야 한다", () => {
+                const raceResult = race.start();
+                expect(raceResult.raceHistory.length).toBe(race.rounds);
+            });
 
-        it("각 라운드마다 자동차의 상태가 기록되어야 한다", () => {
-            const raceResult = race.start();
-            expect(
-                raceResult.raceHistory.every(roundData => Array.isArray(roundData.cars))
-            ).toBe(true);
+            it("각 라운드마다 자동차의 상태가 기록되어야 한다", () => {
+                const raceResult = race.start();
+                expect(
+                    raceResult.raceHistory.every(roundData => Array.isArray(roundData.cars))
+                ).toBe(true);
+            });
         });
     });
 
     describe("=== 레이스 결과 테스트 ===", () => {
-        it("우승자 목록을 반환해야한다.", () => {
-            const raceResult = race.start();
+        describe("> 결과 조회 시", () => {
+            it("우승자 목록을 반환해야한다.", () => {
+                const raceResult = race.start();
 
-            expect(raceResult.getWinners().length).toBeGreaterThan(0);
+                expect(raceResult.getWinners().length).toBeGreaterThan(0);
+            });
         });
     })
 });
