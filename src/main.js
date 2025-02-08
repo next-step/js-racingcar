abbbeed919a70cc531f9ea24a7793d8a20cbcaea
inaemin <inaemin@gmail.com>
2025-02-09 00:00:03 +0900

    ✨ feat: try-catch로 에러 리턴 구조 설계

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import Racing from "./Racing.js";
 
 async function play() {
   const carNames = await readLineAsync("자동차 이름을 입력하세요\n");
+  try {
+    const racing = new Racing(carNames);
+
+    console.log(`경주를 완료했습니다.\n`);
+  } catch (error) {
+    throw error;
+  }
 }
 
 play();
