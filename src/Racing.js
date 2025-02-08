673fb939294fa77203291fd8852b34981b671a56
inaemin <inaemin@gmail.com>
2025-02-09 00:40:27 +0900

    ✨ feat: 경주 클래스 구현

---
+++ src/Racing.js
@@ -0,0 +1,25 @@
+import Car from "./Car.js";
+import Validator from "./Validator.js";
+import { TOTAL_RACING_COUNT } from "./constants.js";
+
+class Racing {
+  #cars;
+
+  constructor(carNamesStr) {
+    const carNames = carNamesStr.split(",").map((name) => name.trim());
+    Validator.validateCarName(carNames);
+    this.#cars = carNames.map((name) => new Car(name));
+  }
+
+  start() {
+    for (let i = 0; i < TOTAL_RACING_COUNT; i++) {
+      for (let car of this.#cars) {
+        car.move();
+        car.printTrack();
+      }
+      console.log();
+    }
+  }
+}
+
+export default Racing;
