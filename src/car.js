8291e07fcf529d69f179627730286b5927cb9cec
rjsdnql123 <rjsdnql123@gmail.com>
2025-02-09 18:27:57 +0900

    feat: error message 를 class의 static으로 갖고 있도록 변경

---
+++ src/car.js
@@ -0,0 +1,43 @@
+import ValidationError from './shared/validationError.js';
+
+class Car {
+  static nameLengthErrorMessage = '자동차의 이름은 1글자 이상 5글자 미만으로 입력하셔야 합니다.';
+
+  static nameTypeErrorMessage = '자동차의 이름은 문자열만 입력 가능합니다.';
+
+  name = '';
+
+  #location = 0;
+
+  constructor(name) {
+    Car.validationName(name);
+    this.name = name;
+  }
+
+  static validationName(name) {
+    if (typeof name !== 'string') {
+      throw new ValidationError(Car.nameTypeErrorMessage);
+    }
+    if (name.length === 0) {
+      throw new ValidationError(Car.nameLengthErrorMessage);
+    }
+  }
+
+  moveForward() {
+    this.#location += 1;
+  }
+
+  moveBackward() {
+    this.#location -= 1;
+  }
+
+  getLocation() {
+    return this.#location;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+export default Car;
--- src/main.test.js
+++ src/main.test.js
@@ -1,29 +1,29 @@
 import ValidationError from './shared/validationError';
-import Car from './main';
+import Car from './car';
 
 describe('자동차 경주 테스트', () => {
   const CAR_NAME = 'toyota';
 
   describe('자동차 경주 테스트 - 에러 처리', () => {
-    it('Car에 String이 아닌 다른 타입의 데이터가 들어오면 에러를 뱉어야 한다.', () => {
+    it('자동차의 이름의 타입은 String만 입력할 수 있다.', () => {
       try {
         // eslint-disable-next-line no-new
         new Car(123);
         throw new Error('테스트 작동하지 않음');
       } catch (err) {
         expect(err).toBeInstanceOf(ValidationError);
-        expect(err.message).toBe('자동차의 이름은 문자열만 입력 가능합니다.');
+        expect(err.message).toBe(Car.nameTypeErrorMessage);
       }
     });
 
-    it('Car에 빈 문자열이 들어오면 에러를 뱉어야 한다.', () => {
+    it('자동차의 이름은 1글자 이상 5글자 이하로 입력 하여야 한다.', () => {
       try {
         // eslint-disable-next-line no-new
         new Car('');
         throw new Error('테스트 작동하지 않음');
       } catch (err) {
         expect(err).toBeInstanceOf(ValidationError);
-        expect(err.message).toBe('자동차의 이름은 1글자 이상 입력하셔야 합니다.');
+        expect(err.message).toBe(Car.nameLengthErrorMessage);
       }
     });
   });
