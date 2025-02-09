60304da28d5255ca9ce3e471161ed837f0c6e015
eunnbi <jenabill@naver.com>
2025-02-09 16:26:25 +0900

    feat: 자동차 경주 실행 결과 출력 기능 추가

--- src/io.js
+++ src/io.js
@@ -12,3 +12,20 @@ export const getCarNamesFromInput = async (readline) => {
 
   return names;
 };
+
+export const printRacingGameResult = (racingGame) => {
+  console.log("\n실행 결과");
+
+  for (const result of racingGame) {
+    printCarPositions(result);
+  }
+
+  console.log("경주를 완료했습니다.");
+};
+
+const printCarPositions = (cars) => {
+  for (const { name, position } of cars) {
+    console.log(`${name} : ${"-".repeat(position)}`);
+  }
+  console.log("");
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Car from "./Car.js";
-import { createReadlineInterface, getCarNamesFromInput } from "./io.js";
+import {
+  createReadlineInterface,
+  getCarNamesFromInput,
+  printRacingGameResult,
+} from "./io.js";
+import { racingGameGenerator } from "./racing.js";
 
 const main = async () => {
   const readline = createReadlineInterface();
@@ -7,6 +12,9 @@ const main = async () => {
   try {
     const names = await getCarNamesFromInput(readline);
     const cars = names.map((name) => new Car(name));
+    const racingGame = racingGameGenerator(cars);
+
+    printRacingGameResult(racingGame);
   } catch (e) {
     if (!(e instanceof Error)) return;
     console.log(e.message);
