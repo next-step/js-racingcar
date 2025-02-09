3d58bbdfd8e829ee052da3c93b11af3dc2ba8683
etff <wish2rich@gmail.com>
2025-02-10 00:31:51 +0900

    레이스 리팩터링
    - controller 추가

--- src/main.js
+++ src/main.js
@@ -1,20 +1,8 @@
-import InputView from "./view/InputView.js";
-import OutputView from "./view/OutputView.js";
-import {RandomAcceleration} from "./domain/Acceleration.js";
-import {Car} from "./domain/Car.js";
-import {Race} from "./domain/Race.js";
+import RacingCarController from "./ui/RacingCarController.js";
 
 async function main() {
-    const inputView = new InputView();
-    const outputView = new OutputView();
-
-    const carsNames = await inputView.askCarNames();
-    const rounds = await inputView.askRounds();
-    const cars = Car.createCars(carsNames, new RandomAcceleration());
-    const race = new Race(cars, rounds);
-    const raceResult = race.start();
-
-    outputView.printRaceResult(raceResult);
+    const racingCarController = new RacingCarController();
+    await racingCarController.run();
 }
 
 main();
