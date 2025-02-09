5c860f595cf82ef0c71de374c30cd5eff9efce40
eunnbi <jenabill@naver.com>
2025-02-09 15:27:12 +0900

    feat: 자동차 이름 유효성 검사 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -7,6 +7,14 @@ describe("자동차 클래스 테스트", () => {
     expect(car.name).toBe("강은비");
   });
 
+  it("자동차의 이름은 1자 이상이어야 한다.", () => {
+    expect(() => new Car("")).toThrow(Car.VALIDATION_ERROR_MESSAGE);
+  });
+
+  it("자동차의 이름은 5자 이하여야 한다.", () => {
+    expect(() => new Car("강은비강은비")).toThrow(Car.VALIDATION_ERROR_MESSAGE);
+  });
+
   it("자동차는 위치 값을 가지며, 초기 상태는 0이다.", () => {
     const car = new Car("강은비");
 
--- src/Car.js
+++ src/Car.js
@@ -2,12 +2,26 @@ class Car {
   name;
   position = 0;
 
+  static NAME_MIN_LENGTH = 1;
+  static NAME_MAX_LENGTH = 5;
+  static VALIDATION_ERROR_MESSAGE = `자동차 이름은 ${Car.NAME_MIN_LENGTH}자 이상, ${Car.NAME_MAX_LENGTH}자 이하여야 합니다.`;
+
   constructor(name) {
+    if (!this.#isValidName(name)) {
+      throw new Error(Car.VALIDATION_ERROR_MESSAGE);
+    }
+
     this.name = name;
   }
 
   moveForward() {
     this.position += 1;
+  }
+
+  #isValidName(name) {
+    if (name.length < Car.NAME_MIN_LENGTH) return false;
+    if (name.length > Car.NAME_MAX_LENGTH) return false;
+    return true;
   }
 }
 
