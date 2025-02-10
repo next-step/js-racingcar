d0e96811aab489f35dfe32f951172f68c63618cd
2sjeong918 <2sjeong918@gmail.com>
2025-02-11 08:51:47 +0900

    test: 자동차 기능 테스트에 여러번 전진 케이스 추가

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -7,8 +7,8 @@ describe("자동차 기능 테스트", () => {
     car = new Car("myCar");
   });
 
-  test("자동차는 myCar라는 이름을 가진다", () => {
-    expect(car.name).toEqual("myCar");
+  test("자동차는 name과 position을 가진다", () => {
+    expect(car).toEqual({ name: "myCar", position: 0 });
   });
 
   test("자동차는 초기 상태로 위치 값을 0을 가진다", () => {
@@ -19,4 +19,15 @@ describe("자동차 기능 테스트", () => {
     car.move();
     expect(car.position).toEqual(1);
   });
+
+  test.each([
+    { count: 3, position: 3 },
+    { count: 5, position: 5 },
+  ])(
+    "자동차가 $count번 전진하면 위치 값은 $position이다.",
+    ({ count, position }) => {
+      Array.from({ length: count }).forEach(() => car.move());
+      expect(car.position).toEqual(position);
+    }
+  );
 });
