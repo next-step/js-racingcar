74de32e02f71f77151e7c4548418546607e4bf96
rjsdnql123 <rjsdnql123@gmail.com>
2025-02-10 19:52:05 +0900

    feat: 자동차가 지나간 궤적을 Array 형태로 저장하도록

--- src/main.test.js
+++ src/main.test.js
@@ -1,5 +1,5 @@
-import ValidationError from './shared/validationError';
-import Car from './car';
+import ValidationError from './shared/validationError.js';
+import Car from './car.js';
 
 describe('자동차 경주 테스트', () => {
   const CAR_NAME = 'kia';
--- src/race.js
+++ src/race.js
@@ -1,24 +1,34 @@
 class Race {
-  static RaceConst = 5;
+  static RaceMaxCount = 5;
+
+  static setTrajectory(car, array) {
+    const location = car.getLocation();
+    array.push(`${car.getName()} : ${'-'.repeat(location)}`);
+  }
 
   constructor(cars) {
     this.cars = cars;
+    this.result = [];
   }
 
   startRace() {
     let i = 0;
-    while (Race.RaceConst > i) {
+
+    while (Race.RaceMaxCount > i) {
+      const array = [];
       i += 1;
       this.cars.forEach((car) => {
         car.moveForward();
-        Race.getTrajectory(car);
+        Race.setTrajectory(car, array);
       });
+
+      this.result.push(array);
     }
+    return this.getTrajectory();
   }
 
-  static getTrajectory(car) {
-    const location = car.getLocation();
-    console.log(`${car.getName()} : ${'-'.repeat(location)}`);
+  getTrajectory() {
+    return this.result;
   }
 }
 
