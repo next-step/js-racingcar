6eb0a93ccaf5385dc2157b7f7413a4298993bb6a
minji Kong <minzyaaaaaa@gmail.com>
2025-02-11 01:47:53 +0900

    Refactor: 자동차가 전진하는 조건을 forward의 인자로 받도록 수정

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -4,8 +4,8 @@ class Car {
   static NAME_MAX_LENGTH = 5;
 
   static #FORWARD_MIN_VALUE = 4;
-  static #FORWARD_INPUT_MIN_VALUE = 0;
-  static #FORWARD_INPUT_MAX_VALUE = 9;
+  static #FORWARD_MIN_RANGE = 0;
+  static #FORWARD_MAX_RANGE = 9;
 
   #name;
   #location = Car.INITIAL_LOCATION;
@@ -28,33 +28,21 @@ class Car {
     return true;
   }
 
-  static validateForwardInput(forwardInput) {
-    if (typeof forwardInput !== 'number' && !Number.isInteger(forwardInput))
-      return false;
-
-    if (
-      forwardInput > Car.#FORWARD_INPUT_MAX_VALUE ||
-      forwardInput < Car.#FORWARD_INPUT_MIN_VALUE
-    )
-      return false;
-
-    return true;
-  }
-
-  static generateForwardCondition() {
+  /** #FORWARD_MAX_RANGE와 #FORWARD_MIN_RANGE 중 숫자를 랜덤으로 골라, 해당 숫자가 FORWARD_MIN_VALUE 보다 큰지 검사한다. */
+  static forwardCondition() {
     // Math.random()은 0에서 1사이의 숫자를 반환한다. 원하는 범위를 얻기 위해 최소값을 더해 범위를 이동시킨다.
-    return (
-      Math.random() *
-        (Car.#FORWARD_INPUT_MAX_VALUE - Car.#FORWARD_INPUT_MIN_VALUE) +
-      Car.#FORWARD_INPUT_MIN_VALUE
-    );
+
+    const randomNumber =
+      Math.random() * (Car.#FORWARD_MAX_RANGE - Car.#FORWARD_MIN_RANGE) +
+      Car.#FORWARD_MIN_RANGE;
+
+    return randomNumber >= Car.#FORWARD_MIN_VALUE;
   }
 
-  forward(forwardInput) {
-    const isValidInput = Car.validateForwardInput(forwardInput);
-    const input = isValidInput ? forwardInput : Car.generateForwardCondition();
+  forward(condition) {
+    const isCanForward = condition ? condition() : Car.forwardCondition();
 
-    if (input >= Car.#FORWARD_MIN_VALUE) {
+    if (isCanForward) {
       this.#location += Car.FORWARD_STEP;
     }
 
