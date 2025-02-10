835e7c9e4a2306cc13027cf02a7b20ca4e8eb5f7
김보경 <rlaqhrud6263@naver.com>
2025-02-11 00:03:30 +0900

    chore: create car 함수 분리

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,23 +1,22 @@
 import Car from "../src/main";
 
-const name = "bokeeeey";
+const NAME = "boky";
+
+const createCar = () => new Car(NAME);
 
 describe("자동차 클래스 기능 테스트", () => {
   it("자동차는 이름을 가진다.", () => {
-    const car = new Car(name);
-
-    expect(car.name).toEqual(name);
+    const car = createCar();
+    expect(car.name).toEqual(NAME);
   });
 
   it("자동차는 위치 값을 가지며, 초기 상태는 0이다.", () => {
-    const car = new Car(name);
-
+    const car = createCar();
     expect(car.position).toEqual(0);
   });
 
   it("자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.", () => {
-    const car = new Car(name);
-
+    const car = createCar();
     car.move();
     const forwardedLocation = car.position;
     expect(forwardedLocation).toEqual(1);
