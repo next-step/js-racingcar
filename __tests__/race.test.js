bbebba31605ecddd7a1f4d6b1c085e17c6a9df63
rjsdnql123 <rjsdnql123@gmail.com>
2025-02-10 22:20:06 +0900

    fix: race beforEach 를 사용하여 코드 간결하게 개선

--- __tests__/race.test.js
+++ __tests__/race.test.js
@@ -2,17 +2,21 @@ import Race from '../src/race.js';
 import Car from '../src/car.js';
 
 describe('자동차 경주', () => {
+  let name = null;
+  let cars = null;
+  let race = null;
+
+  beforeEach(() => {
+    name = ['to', 'kia', 'jest'];
+    cars = name.map((car) => new Car(car));
+    race = new Race(cars);
+  });
+
   it('Car List 인스턴스를 받아 자동차 경주를 준비 할 수 있다.', () => {
-    const name = ['to', 'kia', 'jest'];
-    const cars = name.map((car) => new Car(car));
-    const race = new Race(cars);
     expect(race.cars).toEqual(cars);
   });
 
   it('라운드의 궤적 데이터를 저장 하여야 한다.', () => {
-    const name = ['to', 'kia', 'jest'];
-    const cars = name.map((car) => new Car(car));
-    const race = new Race(cars);
     race.startRace();
     expect(race.getTrajectory()).toHaveLength(Race.RaceMaxCount);
   });
