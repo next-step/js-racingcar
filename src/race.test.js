025000f1d99214b74ce3fdbad55c757804403826
rjsdnql123 <rjsdnql123@gmail.com>
2025-02-10 19:52:31 +0900

    feat: race 테스트 코드 작성

---
+++ src/race.test.js
@@ -0,0 +1,19 @@
+import Race from './race.js';
+import Car from './car.js';
+
+describe('자동차 경주', () => {
+  it('Car List 인스턴스를 받아 자동차 경주를 준비 할 수 있다.', () => {
+    const name = ['to', 'kia', 'jest'];
+    const cars = name.map((car) => new Car(car));
+    const race = new Race(cars);
+    expect(race.cars).toEqual(cars);
+  });
+
+  it('라운드의 궤적 데이터를 저장 하여야 한다.', () => {
+    const name = ['to', 'kia', 'jest'];
+    const cars = name.map((car) => new Car(car));
+    const race = new Race(cars);
+    race.startRace();
+    expect(race.getTrajectory()).toHaveLength(Race.RaceMaxCount);
+  });
+});
