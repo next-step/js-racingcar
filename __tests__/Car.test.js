ae072b314af70c799eadfc8bafe417e11c182ce7
Terry <kdjtiger@gmail.com>
2025-02-11 23:02:05 +0900

    Refactor: 테스트 코드 중복 변수들 전역 변수화 & 에러 메세지 상수화

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,14 +1,24 @@
+import { ERROR_MESSAGES } from "../src/utils/constants.js";
 import Car from "../src/domain/Car.js";
+
+const carNames = {
+  N: "N",
+  NEXT: "NEXT",
+  NEXTSTEP: "NEXTSTEP",
+  MYCAR: "MYCAR",
+  G70: "G70",
+  GV80: "GV80",
+};
 
 describe("자동차 세팅 테스트 ", () => {
   describe("자동차는", () => {
     let car;
-    beforeAll(() => {
-      car = new Car("NEXT");
+    beforeEach(() => {
+      car = new Car(carNames.NEXT);
     });
 
     it("이름을 가진다", () => {
-      expect(car.getName()).toEqual("NEXT");
+      expect(car.getName()).toEqual(carNames.NEXT);
     });
 
     it("위치 값을 가지며, 초기 값은 0이다.", () => {
@@ -19,7 +29,7 @@ describe("자동차 세팅 테스트 ", () => {
 
 describe("자동차 동작 테스트", () => {
   it(" 자동차는 전진할 수 있으며 한 번에 1만큼 전진한다.", () => {
-    const car = new Car("NEXT");
+    const car = new Car(carNames.NEXT);
 
     car.moveForward();
 
@@ -29,46 +39,41 @@ describe("자동차 동작 테스트", () => {
 
 describe("자동차에 이름을 부여할 수 있다.", () => {
   it("자동차는 생성 시 부여한 이름을 가진다.", () => {
-    const name = "GV80";
-    const car = new Car(name);
-    expect(car.getName()).toBe(name);
+    const car = new Car(carNames.GV80);
+    expect(car.getName()).toBe(carNames.GV80);
   });
 
   it("자동차는 여러 대가 생성될 수 있다.", () => {
-    const carNames = ["GV80", "G70"];
-    const cars = carNames.map((car) => new Car(car));
-    expect(cars.map((car) => car.getName())).toEqual(carNames);
+    const carArray = [carNames.GV80, carNames.G70];
+    const cars = carArray.map((car) => new Car(car));
+    expect(cars.map((car) => car.getName())).toEqual(carArray);
   });
 });
 
 describe("자동차 이름 유효성 테스트", () => {
-  const INVALID_CAR_NAME = "잘못된 자동차 이름입니다.";
-
   it("이름이 1자 미만이면 에러를 던진다.", () => {
     const car = () => new Car("");
 
     expect(car).toThrow(Error);
-    expect(car).toThrow(INVALID_CAR_NAME);
+    expect(car).toThrow(ERROR_MESSAGES.INVALID_CAR_NAME);
   });
 
   it("이름이 1자면 유효성 검사에 통과한다.", () => {
-    const name = "N";
-    const car = () => new Car(name);
+    const car = () => new Car(carNames.N);
 
     expect(car).not.toThrow();
   });
 
   it("이름이 5자이면 유효성 검사에 통과한다.", () => {
-    const name = "MYCAR";
-    const car = () => new Car(name);
+    const car = () => new Car(carNames.MYCAR);
 
     expect(car).not.toThrow();
   });
 
   it("이름이 6자 이상이면 에러를 던진다.", () => {
-    const car = () => new Car("NEXTSTEP");
+    const car = () => new Car(carNames.NEXTSTEP);
 
     expect(car).toThrow(Error);
-    expect(car).toThrow(INVALID_CAR_NAME);
+    expect(car).toThrow(ERROR_MESSAGES.INVALID_CAR_NAME);
   });
 });
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+    INVALID_CAR_NAME: "자동차 이름은 1자 이상 5자 이하이어야 합니다.",
+    NOT_SPACE_IN_NAME: "이름 중간에 공백이 들어갈 수 없습니다.",
+  };
+  
