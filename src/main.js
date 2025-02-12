e0b8a78212015dbb754909383463ff97decd3159
inaemin <inaemin@gmail.com>
2025-02-13 00:19:16 +0900

    update: 우승자 출력 추가

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { readLineAsync } from "./utils.js";
 import Racing from "./Racing.js";
 
 async function play() {
-  const carNames = await readLineAsync("자동차 이름을 입력하세요\n");
+  const carNames = await readLineAsync(
+    "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).\n"
+  );
   try {
     const racing = new Racing(carNames);
     console.log(`\n실행 결과`);
     racing.start();
-    console.log(`경주를 완료했습니다.`);
+    console.log(`${racing.getWinners().join(", ")}가 최종 우승했습니다.`);
   } catch (error) {
     throw error;
   }
