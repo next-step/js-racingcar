7d9187f02c491074af4a474a89ce03df27ce8184
inaemin <inaemin@gmail.com>
2025-02-13 00:15:17 +0900

    update: 경계값을 기준으로 에러 메시지를 달리하여 리턴

--- src/Validator.js
+++ src/Validator.js
@@ -2,16 +2,14 @@ import { MIN_CAR_NAME_LENGTH, MAX_CAR_NAME_LENGTH } from "./constants.js";
 
 class Validator {
   static validateValidCarName(carName) {
-    if (
-      carName.length >= MIN_CAR_NAME_LENGTH &&
-      carName.length <= MAX_CAR_NAME_LENGTH
-    ) {
-      return true;
+    if (carName.length < MIN_CAR_NAME_LENGTH) {
+      throw new Error(`자동차의 이름은 최소 ${MIN_CAR_NAME_LENGTH}자이다.`);
     }
-    throw new Error(
-      `자동차의 이름은 최소 ${MIN_CAR_NAME_LENGTH}자, 최대 ${MAX_CAR_NAME_LENGTH}자이다.`
-    );
+    if (carName.length > MAX_CAR_NAME_LENGTH) {
+      throw new Error(`자동차의 이름은 최대 ${MAX_CAR_NAME_LENGTH}자이다.`);
+    }
   }
+
   static validateDuplicateCarName(carNames) {
     if (new Set([...carNames]).size !== carNames.length) {
       throw new Error(`자동차의 이름은 중복되지 않아야 한다.`);
