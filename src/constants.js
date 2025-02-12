95b4dd8008c9ce8e58892474d4896bcc488a93be
inaemin <inaemin@gmail.com>
2025-02-13 00:14:45 +0900

    update: add constant

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const MAX_CAR_NAME_LENGTH = 5;
 export const TOTAL_RACING_COUNT = 5;
 export const CAR_MOVE_PER_RACING = 1;
 export const TRACK_MARK = "-";
+export const FORWARD_CONDITION = 4;
