952748f6be69eed6a9c7682bbeef6dce600a8d92
rjsdnql123 <rjsdnql123@gmail.com>
2025-02-12 23:23:03 +0900

    feat: main 에 view class 적용

---
+++ src/view/main.js
@@ -0,0 +1,22 @@
+import Car from '../domain/car.js';
+import Race from '../domain/race.js';
+import ReadLineInput from './readLineInput.js';
+import Output from './output.js';
+
+async function main() {
+  const readLineInput = new ReadLineInput();
+  const output = new Output();
+
+  const name = await readLineInput.askCarNames('자동차 이름을 입력하세요. \n');
+  const cars = name.split(',').map((car) => new Car(car));
+
+  const count = await readLineInput.askRound('시도할 회수는 몇회인가요? \n');
+  const race = new Race(cars, count);
+  race.startRace();
+
+  output.resultText();
+  output.trajectory(race.getTrajectory());
+  console.log(`${output.printWinner(race.getWinner())}가 최종 우승했습니다.`);
+}
+
+main();
