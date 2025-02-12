8f920e463d069bcdf7a41b592c24134fc7b2a23c
김보경 <rlaqhrud6263@naver.com>
2025-02-12 22:01:42 +0900

    test: 자동차는 1회당 1칸씩 전진한다.

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -17,4 +17,14 @@ describe("Race 클래스 테스트", () => {
     race.start();
     expect(consoleSpy).toHaveBeenCalledTimes(ROUNDS);
   });
+
+  it("자동차는 1회당 1칸씩 전진한다", () => {
+    const race = new Race("boky");
+    race.start();
+    expect(consoleSpy.mock.calls[0][0]).toBe("boky : -");
+    expect(consoleSpy.mock.calls[1][0]).toBe("boky : --");
+    expect(consoleSpy.mock.calls[2][0]).toBe("boky : ---");
+    expect(consoleSpy.mock.calls[3][0]).toBe("boky : ----");
+    expect(consoleSpy.mock.calls[4][0]).toBe("boky : -----");
+  });
 });
