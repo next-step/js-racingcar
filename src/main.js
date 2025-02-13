b79dd775f962246b51075d07fa68657a9b3860d8
minji Kong <minzyaaaaaa@gmail.com>
2025-02-14 01:45:14 +0900

    Refactor: input.js에서 도메인 관련 로직 분리

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
-import { getCarName, getRaceRoundCount } from './input.js';
-import { consoleRaceResult } from './output.js';
+import { inputCarNames, inputRaceRoundCount } from './view/input.js';
+import { consoleRaceResult } from './view/output.js';
 
 import Race from './domain/Race.js';
 import Car from './domain/Car.js';
 
+async function getCarNames() {
+  const names = await inputCarNames();
+
+  names.forEach((name) => {
+    if (!Car.validateName(name)) process.exit();
+  });
+
+  return names;
+}
+
+async function getRaceRoundCount() {
+  const count = await inputRaceRoundCount();
+
+  if (!Race.validateRoundCount(count)) process.exit();
+
+  return count;
+}
+
 async function play() {
-  const names = await getCarName();
+  const names = await getCarNames();
   const count = await getRaceRoundCount();
 
   const cars = names.map((name) => new Car(name));
--- src/view/input.js
+++ src/view/input.js
@@ -1,28 +1,19 @@
-import { readLineAsync } from './readline-utils.js';
-import Car from './domain/Car.js';
-import Race from './domain/Race.js';
+import { readLineAsync } from '../readline-utils.js';
 
 export function formatCarNameInput(names) {
   return names.split(',');
 }
 
-export async function getCarName() {
+export async function inputCarNames() {
   const input = await readLineAsync('경주할 자동차 이름을 입력하세요\n');
-
   const names = formatCarNameInput(input);
-
-  names.forEach((name) => {
-    if (!Car.validateName(name)) process.exit();
-  });
 
   return names;
 }
 
-export async function getRaceRoundCount() {
+export async function inputRaceRoundCount() {
   const input = await readLineAsync('시도할 회수는 몇회인가요?\n');
   const roundCount = Number(input);
-
-  if (!Race.validateRoundCount(roundCount)) process.exit();
 
   return roundCount;
 }
