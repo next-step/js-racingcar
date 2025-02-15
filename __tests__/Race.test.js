ce8c1a0d7996ca3dbc4c6b6eb363980c408717a7
minji Kong <minzyaaaaaa@gmail.com>
2025-02-15 23:05:02 +0900

    Test: Race.js 자동차 전진 조건 테스트 추가

--- __tests__/Race.test.js
+++ __tests__/Race.test.js
@@ -2,8 +2,14 @@ import Race from "../src/domain/Race.js";
 import Car from "../src/domain/Car.js";
 
 describe("자동차 경주 테스트", () => {
+  let cars;
+
+  beforeEach(() => {
+    cars = [new Car("벤틀리"), new Car("캐딜락")];
+  });
+
   it("자동차 경주 횟수를 정할 수 있다.", () => {
-    const race = new Race([new Car("벤틀리"), new Car("캐딜락")], 5);
+    const race = new Race(cars, 5);
 
     const result = race.start(3);
 
@@ -12,11 +18,23 @@ describe("자동차 경주 테스트", () => {
   });
 
   it("자동차 경주 횟수는 0 이상의 숫자를 입력해야 한다.", () => {
-    expect(() => new Race([new Car("벤틀리"), new Car("캐딜락")], 0)).toThrow(
+    expect(() => new Race(cars, 0)).toThrow(Race.ROUND_COUNT_ERROR_MESSAGE);
+    expect(() => new Race(cars, "five")).toThrow(
       Race.ROUND_COUNT_ERROR_MESSAGE
     );
-    expect(
-      () => new Race([new Car("벤틀리"), new Car("캐딜락")], "five")
-    ).toThrow(Race.ROUND_COUNT_ERROR_MESSAGE);
+  });
+
+  it("경주 마다 자동차 전진 조건을 정할 수 있다", () => {
+    // 모든 자동차가 매 라운드마다 전진하도록 조건을 설정
+    const ROUND_COUNT = 3;
+    const race = new Race(cars, ROUND_COUNT, () => true);
+
+    const results = race.start();
+
+    const lastRound = results[results.length - 1];
+    expect(lastRound).toEqual([
+      { name: "벤틀리", location: ROUND_COUNT },
+      { name: "캐딜락", location: ROUND_COUNT },
+    ]);
   });
 });
