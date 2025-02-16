20cf8079c655022ef47d08128db953b39805e753
minji Kong <minzyaaaaaa@gmail.com>
2025-02-16 14:29:52 +0900

    Refactor: 자동차 이름 검증 실패 시 CustomError 반환

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,16 +1,29 @@
+class InvalidCarNameException extends Error {
+  constructor(minLength, maxLength) {
+    const message = `자동차의 이름은 ${minLength}자 이상 ${maxLength}자 이하로 입력해 주세요.`;
+    super(message);
+  }
+}
+
 class Car {
   static INITIAL_LOCATION = 0;
   static FORWARD_STEP = 1;
+
+  static NAME_MIN_LENGTH = 1;
   static NAME_MAX_LENGTH = 5;
-  static NAME_LENGTH_ERROR_MESSAGE = `자동차의 이름은 1자 이상 ${Car.NAME_MAX_LENGTH}자 이하로 입력해 주세요.`;
 
   #name;
   #location = Car.INITIAL_LOCATION;
 
   static validateName(name) {
-    const validName = name.length > 0 && name.length <= Car.NAME_MAX_LENGTH;
+    const validName =
+      name.length >= Car.NAME_MIN_LENGTH && name.length <= Car.NAME_MAX_LENGTH;
 
-    if (!validName) throw new Error(Car.NAME_LENGTH_ERROR_MESSAGE);
+    if (!validName)
+      throw new InvalidCarNameException(
+        Car.NAME_MIN_LENGTH,
+        Car.NAME_MAX_LENGTH
+      );
   }
 
   constructor(name) {
