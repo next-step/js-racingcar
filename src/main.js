b8b4963f49b1c76259a53affe3c3ad0eea133d5c
강현석 <kekemusa37@gmail.com>
2025-08-26 21:27:54 +0900

    feat: foward 함수의 파라미터로 값을 전달받을 수 있도록 합니다

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ class Car {
   static FORWARD_STEP = 1;
 
   constructor(name, position = 0) {
-	  this.name = name;
-	  this.position = position;
+    this.name = name;
+    this.position = position;
   }
 
-  forward() {
-	  this.position += FORWARD_STEP;
+  forward(position = FORWARD_STEP) {
+    this.position += position;
   }
 }
