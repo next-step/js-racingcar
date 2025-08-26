9b2d87f49b8c40c3bd3aae911d9747a1cd091818
Seung-wan <tmddhks0104@gmail.com>
2025-08-26 21:02:57 +0900

    feat: Car 클래스 간단하게 설계하기

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-console.log("Car");
+class Car {
+  #name;
+
+  #xPosition = 0;
+
+  constructor(name) {
+    this.#name = name;
+  }
+
+  goForward() {
+    this.#go(1);
+  }
+
+  #go(amount) {
+    this.#xPosition += amount;
+  }
+}
+
+const car = new Car("BMW");
