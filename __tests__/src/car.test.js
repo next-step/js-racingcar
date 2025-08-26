f33d88b941a9a3ea383519e199cfbb5b8ab712bb
강현석 <kekemusa37@gmail.com>
2025-08-26 22:31:25 +0900

    test: 자동차는 위치 값을 가지며 초기 위치는 0입니다

--- __tests__/src/car.test.js
+++ __tests__/src/car.test.js
@@ -5,4 +5,8 @@ describe("Car", () => {
     const car = new Car("커트");
     expect(car.name).toEqual("커트");
   });
+  it("자동차는 위치 값을 가지며 초기 위치는 0입니다.", () => {
+    const car = new Car("커트");
+    expect(car.position).toEqual(0);
+  });
 });
