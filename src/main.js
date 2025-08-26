e9518f50ce22f23d9692aebeb1afd2c961b1781a
강현석 <kekemusa37@gmail.com>
2025-08-26 21:00:36 +0900

    feat: 자동차는 전진할 수 있으며 한 번에 1만큼 전진한다

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 class Car {
+  static FORWARD_STEP = 1
+
   constructor(name, position = 0) {
 	this.name = name
 	this.position = position
+  }
+
+  forward() {
+	this.position += FORWARD_STEP
   }
 }
