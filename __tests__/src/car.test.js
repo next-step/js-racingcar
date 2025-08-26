5177ce7192b3cd8c7385c6a5e8d1a72d160fb483
강현석 <kekemusa37@gmail.com>
2025-08-26 22:33:56 +0900

    test: 자동차는 한 번에 1만큼 전진합니다

--- __tests__/src/car.test.js
+++ __tests__/src/car.test.js
@@ -9,4 +9,9 @@ describe("Car", () => {
     const car = new Car("커트");
     expect(car.position).toEqual(0);
   });
+  it("자동차는 한 번에 1만큼 전진합니다.", () => {
+    const car = new Car("커트");
+    car.forward();
+    expect(car.position).toEqual(1);
+  });
 });
