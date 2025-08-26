cc2a67777803d8dcd0acc409c5dc995894886ecf
강현석 <kekemusa37@gmail.com>
2025-08-26 21:59:58 +0900

    feat: Car 클래스를 별도의 파일로 분리합니다

---
+++ src/car.js
@@ -0,0 +1,12 @@
+class Car {
+  static FORWARD_STEP = 1;
+
+  constructor(name, position = 0) {
+    this.name = name;
+    this.position = position;
+  }
+
+  forward(position = Car.FORWARD_STEP) {
+    this.position += position;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,1 @@
-class Car {
-  static FORWARD_STEP = 1;
-
-  constructor(name, position = 0) {
-    this.name = name;
-    this.position = position;
-  }
-
-  forward(position = Car.FORWARD_STEP) {
-    this.position += position;
-  }
-}
+console.log("Car");
