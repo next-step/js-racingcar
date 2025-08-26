fd9a55d02cbfa194595dfeec1ba052c35cedd565
강현석 <kekemusa37@gmail.com>
2025-08-26 21:08:39 +0900

    chore: 세미콜론을 찍습니다

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 class Car {
-  static FORWARD_STEP = 1
+  static FORWARD_STEP = 1;
 
   constructor(name, position = 0) {
-	  this.name = name
-	  this.position = position
+	  this.name = name;
+	  this.position = position;
   }
 
   forward() {
-	  this.position += FORWARD_STEP
+	  this.position += FORWARD_STEP;
   }
 }
