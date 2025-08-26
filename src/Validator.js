2ac5f7937153142dce3f237becbd1f8115f646db
Seung-wan <tmddhks0104@gmail.com>
2025-08-27 01:30:26 +0900

    refactor: 상수명 수정

--- src/Validator.js
+++ src/Validator.js
@@ -1,15 +1,15 @@
 export class Validator {
-  static MAX_CAR_NAME = 5;
+  static CAR_NAME_LENGTH_MAX = 5;
 
   static validateCarNames(names) {
     const isValid = names.every((name) => Validator.isValidCarName(name));
 
     if (!isValid) {
-      throw new Error(`자동차 이름을 ${Validator.MAX_CAR_NAME}자 이하로 입력해주세요.`);
+      throw new Error(`자동차 이름을 ${Validator.CAR_NAME_LENGTH_MAX}자 이하로 입력해주세요.`);
     }
   }
 
   static isValidCarName(name) {
-    return name.length <= Validator.MAX_CAR_NAME;
+    return name.length <= Validator.CAR_NAME_LENGTH_MAX;
   }
 }
