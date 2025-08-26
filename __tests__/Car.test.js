f791aa6ae3458d6c15614da55161711227f5fe1f
Seung-wan <tmddhks0104@gmail.com>
2025-08-27 00:03:29 +0900

    fix: 테스트 코드 오류를 수정합니다.

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -2,10 +2,11 @@ import { Car } from "../src/Car";
 
 describe(Car.name, () => {
   it("자동차는 이름을 상태로 가질 수 있다", () => {
-    const car = new Car("씽씽붕붕이");
+    const carName = "현대차";
+    const car = new Car(carName);
     const carInfo = car.getCarInfo();
 
-    expect(carInfo.name).toBe("씽씽붕붕이");
+    expect(carInfo.name).toBe(carName);
   });
 
   it("자동차는 위치 값을 가지며, 초기 상태는 0이다", () => {
@@ -17,12 +18,13 @@ describe(Car.name, () => {
 
   it("자동차는 전진할 수 있으며 한 번에 1만큼 전진하다.", () => {
     const car = new Car("소나타");
-    const carInfo = car.getCarInfo();
 
-    expect(carInfo.xPosition).toBe(0);
+    const carInfo1 = car.getCarInfo();
+    expect(carInfo1.xPosition).toBe(0);
 
     car.goForward();
 
-    expect(carInfo.xPosition).toBe(1);
+    const carInfo2 = car.getCarInfo();
+    expect(carInfo2.xPosition).toBe(1);
   });
 });
