a73186c4b851a05e4df9efb2f209f9a85f344358
eun0leee <eun0leee@gmail.com>
2025-08-27 22:40:08 +0900

    test : Car.js 단위 테스트 진행

---
+++ __tests__/car.test.js
@@ -0,0 +1,28 @@
+import Car from '../src/Car.js';
+
+describe('자동차 테스트', () => {
+  const CAR_NAME = '싼타페';
+  let car;
+
+  beforeEach(() => {
+    car = new Car({ name: CAR_NAME });
+  });
+
+  test('자동차는 이름을 가진다', () => {
+    expect(car.name).toBe(CAR_NAME);
+  });
+
+  test('초기 위치는 0이다', () => {
+    expect(car.position).toBe(0);
+  });
+
+  test('위치는 1만큼 전진한다', () => {
+    expect(car.position).toBe(Car.INITIAL_POSITION);
+
+    car.moveForward();
+    expect(car.position).toBe(1);
+
+    car.moveForward();
+    expect(car.position).toBe(2);
+  });
+});
