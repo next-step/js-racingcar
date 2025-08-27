88a39af55d6c861e86614d389d644979b731349a
강현석 <kekemusa37@gmail.com>
2025-08-27 21:27:19 +0900

    fix: Car의 name에는 빈 문자열을 가지지 않도록 합니다

--- __tests__/src/car.test.js
+++ __tests__/src/car.test.js
@@ -5,6 +5,9 @@ describe("Car", () => {
     const car = new Car("커트");
     expect(car.name).toEqual("커트");
   });
+  it("자동차 이름은 빈 문자열일 수 없습니다", () => {
+    expect(() => new Car("")).toThrow();
+  });
   it("자동차는 위치 값을 가지며 초기 위치는 0입니다.", () => {
     const car = new Car("커트");
     expect(car.position).toEqual(0);
--- src/car.js
+++ src/car.js
@@ -2,6 +2,9 @@ class Car {
   static FORWARD_STEP = 1;
 
   constructor(name, position = 0) {
+    if (typeof name !== "string" || name.length === 0) {
+      throw new Error("자동차 이름은 빈 문자열일 수 없습니다.");
+    }
     this.name = name;
     this.position = position;
   }
