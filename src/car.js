53e4e0cc82e3dff9bf92b37529a3274bb7b4c647
강현석 <kekemusa37@gmail.com>
2025-08-27 21:48:27 +0900

    fix: 외부에서 값을 설정하지 못하도록 하였습니다.

--- src/car.js
+++ src/car.js
@@ -1,16 +1,26 @@
 class Car {
   static FORWARD_STEP = 1;
 
+  #name;
+  get name() {
+    return this.#name;
+  }
+
+  #position = 0;
+  get position() {
+    return this.#position;
+  }
+
   constructor(name, position = 0) {
     if (typeof name !== "string" || name.length === 0) {
       throw new Error("자동차 이름은 빈 문자열일 수 없습니다.");
     }
-    this.name = name;
-    this.position = position;
+    this.#name = name;
+    this.#position = position;
   }
 
   forward(position = Car.FORWARD_STEP) {
-    this.position += position;
+    this.#position += position;
   }
 }
 
