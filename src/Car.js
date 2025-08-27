dc22604e22d1b9fee3612e6c0a05cc110b7ad14d
eun0leee <eun0leee@gmail.com>
2025-08-27 22:36:58 +0900

    refactor : 기존 Car class 에 리뷰사항 반영 및 static 추가

--- src/Car.js
+++ src/Car.js
@@ -1,10 +1,16 @@
 class Car {
+  static INITIAL_POSITION = 0;
+  static STEP_SIZE = 1;
+
+  position = Car.INITIAL_POSITION;
+
   constructor({ name }) {
     this.name = name;
-    this.position = 0;
   }
 
-  move() {
-    this.position = this.position + 1;
+  moveForward() {
+    this.position += Car.STEP_SIZE;
   }
 }
+
+export default Car;
