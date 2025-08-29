512f8aa1f4ccb586c7dd3fef1f15db357cc7465a
unknown <syjin@in4ucloud.com>
2025-08-29 12:06:38 +0900

    feat: 자동차 이름을 쉼표로 구분하여 배열로 만들어 자동차 이름의 길이를 체크 하는 함수

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,26 @@
 import readline from "readline";
 
+// 자동차 이름 길이 체크
+const checkCarNameLength = (carName) => {
+  return carName.length <= 5;
+};
+
+// 자동차 이름 체크
+const checkCarName = (carName) => {
+  const carNames = carName.split(",");
+
+  if (Array.isArray(carNames) && carNames.every(checkCarNameLength)) {
+    return carNames;
+  }
+
+  if (typeof carNames === "string" && checkCarNameLength(carNames)) {
+    return carNames;
+  }
+
+  return false;
+};
+
+// 사용자 입력 비동기 처리 함수
 function readLineAsync(query) {
   return new Promise((resolve, reject) => {
     if (arguments.length !== 1) {
@@ -17,15 +38,20 @@ function readLineAsync(query) {
 
     rl.question(query, (input) => {
       rl.close();
-      resolve(input);
+      const removeSpacesCarNames = input.replace(/ /g, "");
+      const correctCarNames = checkCarName(removeSpacesCarNames);
+
+      if (correctCarNames) {
+        resolve(correctCarNames);
+      }
     });
   });
 }
 
-// 입출력 예시
-async function play() {
-  const name = await readLineAsync("경주할 자동차 이름을 입력하세요. > ");
-  console.log(name);
-}
+// 게임 시작
+const play = async () => {
+  const carNames = await readLineAsync("경주할 자동차 이름을 입력하세요. > ");
+  console.log(carNames);
+};
 
 play();
