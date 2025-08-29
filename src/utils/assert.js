77a564187cfd88e40e66aa88233b455f12a1c18e
Seung-wan <tmddhks0104@gmail.com>
2025-08-30 00:19:11 +0900

    refactor: assert 함수를 early return 스타일로 리팩토링합니다.

--- src/utils/assert.js
+++ src/utils/assert.js
@@ -1,9 +1,11 @@
 export function assert(condition, error) {
-  if (!condition) {
-    if (typeof error === "string") {
-      throw new Error(error);
-    }
-
-    throw error;
+  if (condition) {
+    return;
   }
+
+  if (typeof error === "string") {
+    throw new Error(error);
+  }
+
+  throw error;
 }
