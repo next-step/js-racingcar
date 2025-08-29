cde87216cd4499e5c9726c4248e9eedf1ad9b21c
unknown <syjin@in4ucloud.com>
2025-08-29 10:36:40 +0900

    feat: 사용자가 요청한 자동차를 출력 및 경주를 실행시키는 함수

--- src/Car.js
+++ src/Car.js
@@ -1,11 +1,31 @@
-class Car {
-  constructor() {
-    // this.name = "BMW";
-    this.location = 0;
-  }
+import readline from "readline";
 
-  moveFoward() {
-    this.location++;
-  }
+function readLineAsync(query) {
+  return new Promise((resolve, reject) => {
+    if (arguments.length !== 1) {
+      reject(new Error("arguments must be 1"));
+    }
+
+    if (typeof query !== "string") {
+      reject(new Error("query must be string"));
+    }
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, (input) => {
+      rl.close();
+      resolve(input);
+    });
+  });
 }
-export default Car;
+
+// 입출력 예시
+async function play() {
+  const name = await readLineAsync("경주할 자동차 이름을 입력하세요. > ");
+  console.log(name);
+}
+
+play();
