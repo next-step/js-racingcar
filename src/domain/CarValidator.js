1d098305dc688371b1b4c7e37dafbafe12f15afb
유승완 <tmddhks0104@gmail.com>
2025-08-30 19:45:39 +0900

    [클린코드 8기 유승완] 자동차 경주 미션 STEP 4 (#346)
    * feat:. 유틸 함수를 추가합니다.
    
    * feat: 0~9 숫자중에 4이상일때 전진하는 코드를 추가하고, 우승자를 출력할 수 있게 합니다.
    
    * refactor: assert 함수를 early return 스타일로 리팩토링합니다.
    
    * refactor: racingTimes도 별도 Validator에서 검증합니다. 기존 에러를 던지는 코드를 assert로 표현합니다.
    
    ---------
    
    Co-authored-by: seungwan-yu <seungwan.yu@toss.im>

--- src/domain/CarValidator.js
+++ src/domain/CarValidator.js
@@ -1,12 +1,12 @@
+import { assert } from "../utils/assert.js";
+
 export class CarValidator {
   static CAR_NAME_LENGTH_MAX = 5;
 
   static validateCarNames(names) {
     const isValid = names.every((name) => CarValidator.isValidCarName(name));
 
-    if (!isValid) {
-      throw new Error(`자동차 이름을 ${CarValidator.CAR_NAME_LENGTH_MAX}자 이하로 입력해주세요.`);
-    }
+    assert(isValid, `자동차 이름을 ${CarValidator.CAR_NAME_LENGTH_MAX}자 이하로 입력해주세요.`);
   }
 
   static isValidCarName(name) {
