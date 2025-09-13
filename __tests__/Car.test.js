c34133c86d6f5860711e71bb61799d2ea0c60b7c
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-09-13 23:13:58 +0900

    feat: 모킹한 함수 제거 후 엑셀러레이터 로직 반영

--- __tests__/Car.test.js
+++ __tests__/Car.test.js
@@ -1,34 +1,30 @@
 import { Car } from '../src/domain/models/Car/index.js';
-// import { CAR_MOVE_THRESHOLD } from '../src/domain/models/Car/constant.js';
+import { Accelerator } from '../src/domain/models/Accelerator/index.js';
 
-// jest.mock('../src/models/Car/index.js', () => {
-//   const actual = jest.requireActual('../src/models/Car/index.js');
-//   return {
-//     ...actual,
-//     getRandomInt: jest.fn(),
-//   };
-// });
-
-const createCar = (name = '이름') => {
-  return new Car(name);
+const createCar = ({ name = '이름', accelerator = new Accelerator() } = {}) => {
+  return new Car({ name, accelerator });
 };
 
 describe('자동차는', () => {
   it('1자 이상 5자 이하의 이름을 상태로 가질 수 있다', () => {
     const CAR_NAME = '이름';
 
-    const car = createCar(CAR_NAME);
+    const car = createCar({ name: CAR_NAME });
 
     expect(car.information.name).toBe(CAR_NAME);
   });
 
   it('이름이 빈 문자열이거나 공백일 경우 에러가 발생한다.', () => {
-    expect(() => createCar('')).toThrow(Car.ErrorMessages.NAME_REQUIRED);
-    expect(() => createCar('   ')).toThrow(Car.ErrorMessages.NAME_REQUIRED);
+    expect(() => createCar({ name: '' })).toThrow(
+      Car.ErrorMessages.NAME_REQUIRED,
+    );
+    expect(() => createCar({ name: '   ' })).toThrow(
+      Car.ErrorMessages.NAME_REQUIRED,
+    );
   });
 
   it('이름이 5자를 넘어가면 에러가 발생한다.', () => {
-    expect(() => createCar('이름이6글자')).toThrow(
+    expect(() => createCar({ name: '이름이6글자' })).toThrow(
       Car.ErrorMessages.NAME_MAX_LENGTH,
     );
   });
@@ -39,21 +35,31 @@ describe('자동차는', () => {
     expect(car.information.location).toBe(0);
   });
 
-  // it('전진하는 조건을 만족하는 경우 한 번에 1만큼 전진한다.', () => {
-  //   jest.mocked(getRandomInt).mockReturnValue(CAR_MOVE_THRESHOLD);
-  //   const car = createCar();
+  it('전진하는 조건을 만족하는 경우 한 번에 1만큼 전진한다.', () => {
+    const car = createCar({
+      accelerator: new Accelerator({
+        lowerBound: 0,
+        upperBound: 10,
+        threshold: 0,
+      }),
+    });
 
-  //   car.move();
+    car.move();
 
-  //   expect(car.information.location).toBe(1);
-  // });
+    expect(car.information.location).toBe(1);
+  });
 
-  // it('전진하는 조건을 만족하지 않는 경우 전진하지 않는다.', () => {
-  //   jest.mocked(getRandomInt).mockReturnValue(CAR_MOVE_THRESHOLD - 1);
-  //   const car = createCar();
+  it('전진하는 조건을 만족하지 않는 경우 전진하지 않는다.', () => {
+    const car = createCar({
+      accelerator: new Accelerator({
+        lowerBound: 0,
+        upperBound: 10,
+        threshold: 11,
+      }),
+    });
 
-  //   car.move();
+    car.move();
 
-  //   expect(car.information.location).toBe(0);
-  // });
+    expect(car.information.location).toBe(0);
+  });
 });
